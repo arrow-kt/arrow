fd7905fc1e035a6f56472ba39def3bb39e7ae9a4
MarioAriasC <mario@marioarias.org>
2013-04-12 17:08:00 -0500

    0.1.5

--- src/main/kotlin/org/funktionale/composition/namespace.kt
+++ src/main/kotlin/org/funktionale/composition/namespace.kt
@@ -1,8 +1,6 @@
 package org.funktionale.composition
 
-public fun<P1, IP, R> Function1<P1, IP>.andThen(f: (IP) -> R): (P1) -> R {
-    return this.forwardCompose(f)
-}
+public fun<P1, IP, R> Function1<P1, IP>.andThen(f: (IP) -> R): (P1) -> R = forwardCompose(f)
 
 public fun<P1, IP, R> Function1<P1, IP>.forwardCompose(f: (IP) -> R): (P1) -> R {
     return {(p1: P1) -> f(this(p1)) }
