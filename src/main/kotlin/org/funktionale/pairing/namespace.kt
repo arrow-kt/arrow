664ba7c8d58049bc9c66798e555b0667a1e83265
Jaakko Pallari <jkpl@lepovirta.org>
2016-11-10 20:19:24 +0000

    Expression syntax

--- src/main/kotlin/org/funktionale/pairing/namespace.kt
+++ src/main/kotlin/org/funktionale/pairing/namespace.kt
@@ -1,9 +1,9 @@
 package org.funktionale.pairing
 
-fun<P1, P2, R> Function2<P1, P2, R>.paired(): (Pair<P1, P2>) -> R {
-    return { pair: Pair<P1, P2> -> this(pair.component1(), pair.component2()) }
+fun<P1, P2, R> Function2<P1, P2, R>.paired(): (Pair<P1, P2>) -> R = { pair: Pair<P1, P2> ->
+    this(pair.component1(), pair.component2())
 }
 
-fun<P1, P2, R> Function1<Pair<P1, P2>, R>.unpaired(): (P1, P2) -> R {
-    return { p1: P1, p2: P2 -> this(p1 to p2) }
+fun<P1, P2, R> Function1<Pair<P1, P2>, R>.unpaired(): (P1, P2) -> R = { p1: P1, p2: P2 ->
+    this(p1 to p2)
 }
