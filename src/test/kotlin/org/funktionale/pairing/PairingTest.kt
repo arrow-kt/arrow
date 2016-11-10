a4767e370d7461f881618cc497c87d66f807c5d1
Jaakko Pallari <jkpl@lepovirta.org>
2016-11-10 20:24:04 +0000

    Add tripled/untripled

--- src/test/kotlin/org/funktionale/pairing/PairingTest.kt
+++ src/test/kotlin/org/funktionale/pairing/PairingTest.kt
@@ -13,4 +13,14 @@ class PairingTest {
         Assert.assertEquals(sum2ints(5, 9), paired(5 to 9))
         Assert.assertEquals(paired(5 to 9), unpaired(5, 9))
     }
+
+    @Test fun testTripled() {
+        val sum3ints = { x: Int, y: Int, z: Int -> x + y + z }
+
+        val tripled = sum3ints.tripled()
+        val untripled = tripled.untripled()
+
+        Assert.assertEquals(sum3ints(1, 2, 3), tripled(Triple(1, 2, 3)))
+        Assert.assertEquals(tripled(Triple(9, 8, 7)), untripled(9, 8, 7))
+    }
 }
