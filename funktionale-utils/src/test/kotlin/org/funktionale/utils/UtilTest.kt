a56fc30d466067724711cd3ab6d288458be3df6e
MarioAriasC <mario@marioarias.org>
2016-12-28 22:09:08 +0000

    update copyright

--- funktionale-utils/src/test/kotlin/org/funktionale/utils/UtilTest.kt
+++ funktionale-utils/src/test/kotlin/org/funktionale/utils/UtilTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Mario Arias
+ * Copyright 2013 - 2016 Mario Arias
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
