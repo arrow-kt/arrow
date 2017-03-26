26280742277b0473a92f05cbeee7c6416e79f578
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-03-26 12:57:50 +0200

    add local combinator

--- kats/src/main/kotlin/kats/Reader.kt
+++ kats/src/main/kotlin/kats/Reader.kt
@@ -6,10 +6,21 @@ package kats
  */
 class Reader<C : Any, out A : Any>(val rd: (C) -> A) {
 
-  inline fun <B: Any> map(crossinline f: (A) -> B): Reader<C, B> = Reader { c -> f(rd(c)) }
+  inline fun <B : Any> map(crossinline fa: (A) -> B): Reader<C, B> = Reader { c -> fa(rd(c)) }
 
-  inline fun <B: Any> flatMap(crossinline f: (A) -> Reader<C, B>): Reader<C, B> = Reader { c ->
-    f(rd(c)).rd(c)
+  inline fun <B : Any> flatMap(crossinline fa: (A) -> Reader<C, B>): Reader<C, B> = Reader { c ->
+    fa(rd(c)).rd(c)
+  }
+
+  /**
+   * local combinator allows switching the environment to unify two different dependency types, so
+   * you can compose readers with different type dependencies.
+   *
+   * D: type represents a bigger context than C.
+   * @param fd: function to convert from the bigger context D to a context of type C.
+   */
+  inline fun <D : Any> local(crossinline fd: (D) -> C): Reader<D, A> = Reader { bc ->
+    rd(fd(bc))
   }
 
   fun run(c: C) = rd(c)
