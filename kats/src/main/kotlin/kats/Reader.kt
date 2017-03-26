27aa0efb1ff4b90ab19794cab85eb3590590e1bb
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-03-26 12:36:38 +0200

    adding declaration-site variance and top type bound

--- kats/src/main/kotlin/kats/Reader.kt
+++ kats/src/main/kotlin/kats/Reader.kt
@@ -4,11 +4,11 @@ package kats
  * Basic implementation of the Reader monad. Provides an "implicit" context (configuration) for
  * function execution. Intended to provide Dependency Injection.
  */
-class Reader<C, A>(val rd: (C) -> A) {
+class Reader<C : Any, out A : Any>(val rd: (C) -> A) {
 
-  inline fun <B> map(crossinline f: (A) -> B): Reader<C, B> = Reader { c -> f(rd(c)) }
+  inline fun <B: Any> map(crossinline f: (A) -> B): Reader<C, B> = Reader { c -> f(rd(c)) }
 
-  inline fun <B> flatMap(crossinline f: (A) -> Reader<C, B>): Reader<C, B> = Reader { c ->
+  inline fun <B: Any> flatMap(crossinline f: (A) -> Reader<C, B>): Reader<C, B> = Reader { c ->
     f(rd(c)).rd(c)
   }
 
