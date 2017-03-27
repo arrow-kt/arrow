8336737fb67d8f4f8e6fb7c3fb6a5b5b09effdc2
Raul Raja <raulraja@gmail.com>
2017-03-27 23:08:00 +0200

    WIP

--- katz/src/main/kotlin/katz/typeclasses/Applicative.kt
+++ katz/src/main/kotlin/katz/typeclasses/Applicative.kt
@@ -16,17 +16,34 @@
 
 package katz
 
+
 interface Applicative<F> : Functor<F> {
 
     fun <A> pure(a: A): HK<F, A>
 
     fun <A, B> ap(fa: HK<F, A>, ff: HK<F, (A) -> B>): HK<F, B>
 
-    fun <A, B> product(fa: HK<F, A>, fb: HK<F, B>): HK<F, Pair<A, B>> =
-            ap(fb, map(fa) { a: A -> { b: B -> Pair(a, b) } })
+    fun <A, B> product(fa: HK<F, A>, fb: HK<F, B>): HK<F, Tuple2<A, B>> =
+            ap(fb, map(fa) { a: A -> { b: B -> Tuple2(a, b) } })
 
     override fun <A, B> map(fa: HK<F, A>, f: (A) -> B): HK<F, B> = ap(fa, pure(f))
 
-    fun <A, B, Z> map2(fa: HK<F, A>, fb: HK<F, B>, f: (Pair<A, B>) -> Z): HK<F, Z> =
+    fun <A, B, Z> map2(fa: HK<F, A>, fb: HK<F, B>, f: (Tuple2<A, B>) -> Z): HK<F, Z> =
             map(product(fa, fb), f)
+}
+
+data class Tuple2<out A, out B>(val a: A, val b: B)
+data class Tuple3<out A, out B, out C>(val a: A, val b: B, val c: C)
+data class Tuple4<out A, out B, out C, out D>(val a: A, val b: B, val c: C, val d: D)
+data class Tuple5<out A, out B, out C, out D, out E>(val a: A, val b: B, val c: C, val d: D, val e: E)
+data class Tuple6<out A, out B, out C, out D, out E, out F>(val a: A, val b: B, val c: C, val d: D, val e: E, val f: F)
+data class Tuple7<out A, out B, out C, out D, out E, out F, out G>(val a: A, val b: B, val c: C, val d: D, val e: E, val f: F, val g: G)
+data class Tuple8<out A, out B, out C, out D, out E, out F, out G, out H>(val a: A, val b: B, val c: C, val d: D, val e: E, val f: F, val g: G, val h: H)
+
+fun <A> Option<A>.product()
+
+object Test {
+    fun test() {
+        Option(1).
+    }
 }
