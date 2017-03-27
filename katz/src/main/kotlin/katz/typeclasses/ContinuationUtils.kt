a553b00ad327de8da47078f7ede7a935b541b2ff
Juan Ramón González <jrgonzalez@gmail.com>
2017-03-27 23:34:19 +0200

    Added monad for NonEmptyList plus some tests (#40)
    * Added monad for NonEmptyList plus some tests
    
    * Fixes monad comprehensions by keeping track of stack labels

---
+++ katz/src/main/kotlin/katz/typeclasses/ContinuationUtils.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright Sergey Igushkin https://github.com/h0tk3y
+ *
+ * This file adapted and modified to fit this project originally comes from
+ * https://github.com/h0tk3y/kotlin-monads/blob/master/src/main/kotlin/com/github/h0tk3y/kotlinMonads/ContinuationUtils.kt
+ */
+
+package katz
+
+
+import kotlin.coroutines.experimental.Continuation
+
+private val coroutineImplClass by lazy { Class.forName("kotlin.coroutines.experimental.jvm.internal.CoroutineImpl") }
+
+private val labelField by lazy { coroutineImplClass.getDeclaredField("label").apply { isAccessible = true } }
+private val completionField by lazy { coroutineImplClass.getDeclaredField("completion").apply { isAccessible = true } }
+
+private var <T> Continuation<T>.label
+    get() = labelField.get(this)
+    set(value) = labelField.set(this@label, value)
+
+private var <T> Continuation<T>.completion: Continuation<*>?
+    get() = completionField.get(this) as Continuation<*>
+    set(value) = completionField.set(this@completion, value)
+
+internal var <T> Continuation<T>.stackLabels: List<Any>
+    get() = if (coroutineImplClass.isInstance(this)) listOf(label) + completion?.stackLabels.orEmpty() else emptyList()
+    set(value) {
+        if (coroutineImplClass.isInstance(this)) {
+            label = value.first()
+            completion?.stackLabels = value.subList(1, value.size)
+        }
+    }
