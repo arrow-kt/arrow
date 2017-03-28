d9f8b9c8164718201d5b87e765bf33e9aad4e778
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-03-28 20:47:57 +0200

    fix tests for new version of KotlinTest

--- katz/src/test/kotlin/katz/ReaderTest.kt
+++ katz/src/test/kotlin/katz/ReaderTest.kt
@@ -17,33 +17,34 @@
 package katz
 
 import io.kotlintest.KTestJUnitRunner
+import io.kotlintest.matchers.shouldBe
 import org.junit.runner.RunWith
 
 @RunWith(KTestJUnitRunner::class)
 class ReaderTest : UnitSpec() {
     init {
-        "map" should "return mapped value" {
+        "map should return mapped value" {
             Reader<Int, Int> { it -> it * 2 }.map { it -> it * 3 }.run(2) shouldBe 12
         }
 
-        "flatMap" should "map over the inner value" {
+        "flatMap should map over the inner value" {
             Reader<Int, Int> { it -> it * 2 }.flatMap { a -> Reader.pure<Int, Int>(a * 3) }
                     .run(2) shouldBe 12
         }
 
-        "zip" should "return a new Reader zipping two given ones" {
+        "zip should return a new Reader zipping two given ones" {
             val r1 = Reader<Int, Int> { it -> it * 2 }
             val r2 = Reader<Int, Int> { it -> it * 3 }
             r1.zip(r2).run(2) shouldBe Pair(4, 6)
         }
 
-        "local" should "switch context to be able to combine Readers with different contexts" {
+        "local should switch context to be able to combine Readers with different contexts" {
             val r = Reader<Int, Int> { it -> it * 2 }
             r.local<Boolean> { it -> if (it) 1 else 3 }.run(false) shouldBe 6
             r.local<Boolean> { it -> if (it) 1 else 3 }.run(true) shouldBe 2
         }
 
-        "reader" should "lift a reader from any (A) -> B function" {
+        "reader should lift a reader from any (A) -> B function" {
             val r = { x: Int -> x * 2 }.reader()
             r::class.java shouldBe Reader::class.java
             r.run(2) shouldBe 4
