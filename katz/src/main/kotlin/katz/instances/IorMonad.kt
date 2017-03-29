2c84a5dc1d04c17d70998e286dab7cd0cb259b34
Fernando Franco Giráldez <ffrancogiraldez@gmail.com>
2017-03-29 10:28:29 +0200

    Implement OptionT as HK2
    use tipealias for HKX

--- katz/src/main/kotlin/katz/instances/IorMonad.kt
+++ katz/src/main/kotlin/katz/instances/IorMonad.kt
@@ -16,12 +16,13 @@
 
 package katz
 
-class IorMonad<A>(val semigroup: Semigroup<A>) : Monad<HK<Ior.F, A>> {
-    override fun <B> pure(b: B): HK2<Ior.F, A, B> =
-            Ior.Right(b)
+class IorMonad<L>(val AA: Semigroup<L>) : Monad<HK<Ior.F, L>> {
+    override fun <A, B> flatMap(fa: HK<HK<Ior.F, L>, A>, f: (A) -> HK<HK<Ior.F, L>, B>): HK<HK<Ior.F, L>, B> =
+            fa.ev().flatMap(AA) { f(it).ev() }
 
-    override fun <B, C> flatMap(fa: HK<HK<Ior.F, A>, B>, f: (B) -> HK<HK<Ior.F, A>, C>): HK<HK<Ior.F, A>, C> =
-            fa.ev().flatMap(semigroup) { f(it).ev() }
+    override fun <A> pure(a: A): HK2<Ior.F, L, A> = Ior.Right(a)
+
+
 }
 
 fun <A, B> HK2<Ior.F, A, B>.ev(): Ior<A, B> = this as Ior<A, B>
