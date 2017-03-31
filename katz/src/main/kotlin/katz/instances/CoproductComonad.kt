e0cfa5127a6e03ceec0d59d82ab0ca2c8a0f4243
Raul Raja <raulraja@gmail.com>
2017-03-31 14:14:39 +0200

    CoproductComonad

---
+++ katz/src/main/kotlin/katz/instances/CoproductComonad.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2017 The Katz Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package katz
+
+class CoproductComonad<F, G>(val FC : Comonad<F>, val GC: Comonad<G>) : Comonad<CoproductFG<F, G>> {
+
+    override fun <A, B> coflatMap(fa: CoproductKind<F, G, A>, f: (CoproductKind<F, G, A>) -> B): CoproductKind<F, G, B> =
+        fa.ev().coflatMap(FC, GC, f)
+
+
+    override fun <A> extract(fa: CoproductKind<F, G, A>): A =
+        fa.ev().extract(FC, GC)
+
+}
