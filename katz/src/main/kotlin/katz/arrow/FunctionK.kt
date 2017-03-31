707117d13a9d5999c9601a7b2c61224ab4df51c4
Raul Raja <raulraja@gmail.com>
2017-03-31 23:28:09 +0200

    `or` as extension

--- katz/src/main/kotlin/katz/arrow/FunctionK.kt
+++ katz/src/main/kotlin/katz/arrow/FunctionK.kt
@@ -23,11 +23,11 @@ interface FunctionK<F, G> {
      */
     operator fun <A> invoke(fa: HK<F, A>): HK<G, A>
 
-    fun <H> or(h: FunctionK<H, G>): FunctionK<CoproductFG<F, H>, G> =
-            object : FunctionK<CoproductFG<F, H>, G> {
-                override fun <A> invoke(fa: CoproductKind<F, H, A>): HK<G, A> {
-                    return fa.ev().fold(this@FunctionK, h)
-                }
-            }
-
 }
+
+fun <F, G, H> FunctionK<F, G>.or(h: FunctionK<H, G>): FunctionK<CoproductFG<F, H>, G> =
+        object : FunctionK<CoproductFG<F, H>, G> {
+            override fun <A> invoke(fa: CoproductKind<F, H, A>): HK<G, A> {
+                return fa.ev().fold(this@or, h)
+            }
+        }
