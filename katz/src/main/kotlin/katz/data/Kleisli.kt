d733c9e3de8e08dc8735cebdb77ccecb9a83e75b
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-04-01 02:57:10 +0200

    remove it usage to clarify implementation

--- katz/src/main/kotlin/katz/data/Kleisli.kt
+++ katz/src/main/kotlin/katz/data/Kleisli.kt
@@ -6,13 +6,13 @@ import katz.Monad
 
 class Kleisli<F, D, out A>(val run: (D) -> HK<F, A>) {
 
-    fun <B> map(ft: Functor<F>, f: (A) -> B): Kleisli<F, D, B> = Kleisli { ft.map(run(it), f) }
+    fun <B> map(ft: Functor<F>, f: (A) -> B): Kleisli<F, D, B> = Kleisli { a -> ft.map(run(a), f) }
 
     fun <B> flatMap(m: Monad<F>, f: (A) -> Kleisli<F, D, B>): Kleisli<F, D, B> = Kleisli {
-        m.flatMap(run(it)) { a -> f(a).run(it) }
+        d -> m.flatMap(run(d)) { a -> f(a).run(d) }
     }
 
-    fun <DD> local(f: (DD) -> D): Kleisli<F, DD, A> = Kleisli { run(f(it)) }
+    fun <DD> local(f: (DD) -> D): Kleisli<F, DD, A> = Kleisli { dd -> run(f(dd)) }
 }
 
 typealias ReaderT<F, D, A> = Kleisli<F, D, A>
