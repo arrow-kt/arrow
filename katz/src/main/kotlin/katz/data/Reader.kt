d448f12a19512e97694cbca184e72662f2ea1f48
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-04-02 13:35:15 +0200

    fix compilation issues

--- katz/src/main/kotlin/katz/data/Reader.kt
+++ katz/src/main/kotlin/katz/data/Reader.kt
@@ -9,11 +9,11 @@ class Reader<D, A>(val rd: (D) -> A) : Kleisli<Id.F, D, A>({ d -> Id(rd(d)) }) {
         fun <D> ask(): Reader<D, D> = Reader { it }
     }
 
-    fun <B> map(fa: (A) -> B): Reader<D, B> = map(IdMonad, fa)
+    fun <B> map(fa: (A) -> B) = map(IdMonad, fa)
 
-    fun <B> flatMap(fa: (A) -> Reader<D, B>): Reader<D, B> = flatMap(IdMonad, fa)
+    fun <B> flatMap(fa: (A) -> Reader<D, B>) = flatMap(IdMonad, fa)
 
-    fun <B> zip(o: Reader<D, B>): Reader<D, Pair<A, B>> = zip(IdMonad, o)
+    fun <B> zip(o: Reader<D, B>) = zip(IdMonad, o)
 }
 
 fun <D, A> ((D) -> A).reader(): Reader<D, A> = Reader(this)
