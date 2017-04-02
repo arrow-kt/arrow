67c97dee599bdf3789dc5b1de7f26bab0d39ed31
Raul Raja <raulraja@gmail.com>
2017-04-02 16:48:34 +0200

    Move `local` to instance

--- katz/src/main/kotlin/katz/data/Reader.kt
+++ katz/src/main/kotlin/katz/data/Reader.kt
@@ -9,7 +9,10 @@ class Reader<D, A>(val k: Kleisli<Id.F, D, A>) {
         fun <A> pure(x: A): Reader<Nothing, A> = Reader { _ -> x }
 
         fun <D> ask(): Reader<D, D> = Reader { it }
+
     }
+
+    fun <DD> local(f: (DD) -> D): Reader<DD, A> = Reader(k.local(f))
 
 }
 
@@ -20,8 +23,6 @@ infix fun <A, B, C> ((A) -> B).andThen(g: (B) -> C): (A) -> C = { a: A -> g(this
 fun <D, A, B> Reader<D, A>.map(fa: (A) -> B): Reader<D, B> = Reader(k.map(IdMonad, fa.andThen { Id(it).value }))
 
 fun <D, A, B> Reader<D, A>.flatMap(fa: (A) -> Reader<D, B>): Reader<D, B> = Reader(k.flatMap(IdMonad, fa.andThen { it.k }))
-
-fun <DD, D, A> Reader<D, A>.local(f: (DD) -> D): Reader<DD, A> = Reader(k.local(f))
 
 fun <D, A, B> Reader<D, A>.zip(o: Reader<D, B>) = Reader(k.zip(IdMonad, o.k))
 
