22d49300433433c7e124683d199ffce1c36784a1
Raul Raja <raulraja@gmail.com>
2017-04-04 20:30:59 +0200

    WIP registering typeclasses

--- katz/src/main/kotlin/katz/typeclasses/Typeclass.kt
+++ katz/src/main/kotlin/katz/typeclasses/Typeclass.kt
@@ -20,18 +20,63 @@ import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 import java.util.concurrent.ConcurrentHashMap
 
+/**
+ * Marker trait that all Functional typeclasses such as Monad, Functor, etc... must implement to be considered
+ * candidates to pair with global instances
+ */
 interface Typeclass
 
-open class GlobalInstance<T : Typeclass> {
-    val type: Type
-        get() = (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0]
+/**
+ * A parametrized type calculated from walking up the interface chain in a Typeclass and finding other relevant
+ * typeclasses that should be registered
+ */
+data class InstanceParametrizedType(val raw: Type, val typeArgs: List<Type>) : ParameterizedType {
+    override fun getRawType(): Type = raw
 
-    init {
-        GlobalInstances.put(type, this)
-        println("Registered $this -> $type")
-    }
+    override fun getOwnerType(): Type = null as Type
+
+    override fun getActualTypeArguments(): Array<Type> = typeArgs.toTypedArray()
 }
 
+/**
+ * Auto registers subtypes as a global instance for all the functional typeclass interfaces they implement
+ */
+open class GlobalInstance<T : Typeclass>() {
+
+    init {
+        recurseInterfaces(javaClass)
+    }
+
+    /**
+     * The original typeclass parametrization that trigger this interface lookup
+     */
+    val type: ParameterizedType
+        get() = (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0] as ParameterizedType
+
+    /**
+     * REcursively scan all implemented interfaces and add as global instances all the ones that match a Typeclass
+     */
+    fun recurseInterfaces(c : Class<*>) : Unit {
+        return when {
+            c.interfaces.isEmpty() -> println("$c has no interfaces")
+            else -> {
+                c.interfaces.filter {
+                    it != Typeclass::class.java && Typeclass::class.java.isAssignableFrom(it)
+                }.forEach { i ->
+                    val instanceType = InstanceParametrizedType(i, listOf(type.actualTypeArguments[0]))
+                    GlobalInstances.put(type, this)
+                    println("registered global instance for type class : $instanceType")
+                    recurseInterfaces(i)
+                }
+            }
+        }
+    }
+
+}
+
+/**
+ * Instrospects the generic type arguments to locate generic interfaces and their type args
+ */
 open class TypeLiteral<T> {
     val type: Type
         get() = (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0]
@@ -39,6 +84,21 @@ open class TypeLiteral<T> {
 
 inline fun <reified T> typeLiteral(): Type = object : TypeLiteral<T>() {}.type
 
+/**
+ * A concurrent hash map of local global instances that may be invoked at runtime as if they were implicitly summoned
+ */
 val GlobalInstances: MutableMap<Type, GlobalInstance<*>> = ConcurrentHashMap()
+
+/**
+ * Obtains a global registered typeclass instance when fast unsafe runtime lookups are desired over passing instances
+ * as args to functions. Use with care. This lookup will throw an exception if a typeclass is summoned and can't be found
+ * in the GlobalInstances map
+ */
+
+data class TypeClassInstanceNotFound(val type : Type)
+    : RuntimeException("$type not found in Global Typeclass Instances registry. " +
+        "Please ensure your instances implement `GlobalInstance<$type>` for automatic registration." +
+        "Alternatively invoke `GlobalInstances.put(typeLiteral<$type>(), instance)` if you wish to register " +
+        "or override a typeclass manually")
 
 inline fun <reified T : Typeclass> instance(): T = GlobalInstances.getValue(typeLiteral<T>()) as T
