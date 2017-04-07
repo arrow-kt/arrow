7237b6101c2de72712cb8913c61f3ca36bc89ced
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-04-07 19:22:48 +0200

    more methods to Foldable

--- katz/src/main/kotlin/katz/typeclasses/Foldable.kt
+++ katz/src/main/kotlin/katz/typeclasses/Foldable.kt
@@ -16,9 +16,41 @@
 
 package katz
 
+/**
+ * Data structures that can be folded to a summary value.
+ *
+ * In the case of a collection (such as `List` or `Set`), these methods will fold together (combine) the values
+ * contained in the collection to produce a single result. Most collection types have `foldLeft` methods, which will
+ * usually be used by the associated `Foldable[_]` instance.
+ *
+ * Foldable[F] is implemented in terms of two basic methods:
+ *
+ *  - `foldLeft(fa, b)(f)` eagerly folds `fa` from left-to-right.
+ *  - `foldRight(fa, b)(f)` lazily folds `fa` from right-to-left.
+ *
+ * Beyond these it provides many other useful methods related to folding over F[A] values.
+ */
 interface Foldable<F> {
 
     fun <A, B> foldL(fa: HK<F, A>, b: B): (B, A) -> B
 
     fun <A, B> foldR(fa: HK<F, A>, lb: Eval<B>): (A, Eval<B>) -> Eval<B>
+
+    /**
+     * The size of this Foldable.
+     *
+     * This is overriden in structures that have more efficient size implementations
+     * (e.g. Vector, Set, Map).
+     *
+     * Note: will not terminate for infinite-sized collections.
+     */
+    fun <A> size(fa: HK<F,A>): Long = foldMap(fa)(_ => 1)
+
+    /**
+     * Fold implemented using the given Monoid[A] instance.
+     */
+    fun <A> fold(fa: HK<F,A>)(implicit A: Monoid<A>): A =
+    foldL(fa, A.empty) { (acc, a) =>
+        A.combine(acc, a)
+    }
 }
