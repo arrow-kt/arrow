184c6b6fbea6fbd52e2756cffd164d3340969275
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-04-09 11:10:28 +0200

    fix PR requested requirements related to docs and licensing

--- katz/src/main/kotlin/katz/data/Eval.kt
+++ katz/src/main/kotlin/katz/data/Eval.kt
@@ -1,17 +1,77 @@
 /*
- * Copyright (C) 2017 The Katz Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+Copyright (C) 2017 The Katz Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+------------------------------------------------------------------------
+
+Code in Katz is derived in part from typelevel/Cats and Scalaz.
+
+Cats license follows:
+
+------------------------------------------------------------------------
+
+Cats Copyright (c) 2015 Erik Osheim.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Scalaz license follows:
+
+------------------------------------------------------------------------
+
+Copyright (c) 2009-2014 Tony Morris, Runar Bjarnason, Tom Adams,
+Kristian Domagala, Brad Clow, Ricky Clarkson, Paul Chiusano, Trygve
+Laugstøl, Nick Partridge, Jason Zaugg. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+------------------------------------------------------------------------
  */
 
 package katz
@@ -72,12 +132,10 @@ sealed class Eval<A> {
     }
 
     /**
-     * Construct an eager Eval[A] instance.
+     * Construct an eager Eval<A> instance. In some sense it is equivalent to using a val.
      *
-     * In some sense it is equivalent to using a val.
-     *
-     * This type should be used when an A value is already in hand, or
-     * when the computation to produce an A value is pure and very fast.
+     * This type should be used when an A value is already in hand, or when the computation to produce an A value is
+     * pure and very fast.
      */
     class Now<A>(val value: A) : Eval<A>() {
         override fun value(): A = value
@@ -85,18 +143,15 @@ sealed class Eval<A> {
     }
 
     /**
-     * Construct a lazy Eval[A] instance.
+     * Construct a lazy Eval<A> instance.
      *
-     * This type should be used for most "lazy" values. In some sense it
-     * is equivalent to using a lazy val.
+     * This type should be used for most "lazy" values. In some sense it is equivalent to using a lazy val.
      *
-     * When caching is not required or desired (e.g. if the value produced
-     * may be large) prefer Always. When there is no computation
-     * necessary, prefer Now.
+     * When caching is not required or desired (e.g. if the value produced may be large) prefer Always. When there
+     * is no computation necessary, prefer Now.
      *
-     * Once Later has been evaluated, the closure (and any values captured
-     * by the closure) will not be retained, and will be available for
-     * garbage collection.
+     * Once Later has been evaluated, the closure (and any values captured by the closure) will not be retained, and
+     * will be available for garbage collection.
      */
     class Later<A>(val f: () -> A) : Eval<A>() {
         override fun value(): A = value
@@ -113,14 +168,12 @@ sealed class Eval<A> {
     }
 
     /**
-     * Construct a lazy Eval[A] instance.
+     * Construct a lazy Eval<A> instance.
      *
-     * This type can be used for "lazy" values. In some sense it is
-     * equivalent to using a Function0 value.
+     * This type can be used for "lazy" values. In some sense it is equivalent to using a Function0 value.
      *
-     * This type will evaluate the computation every time the value is
-     * required. It should be avoided except when laziness is required and
-     * caching must be avoided. Generally, prefer Later.
+     * This type will evaluate the computation every time the value is required. It should be avoided except when
+     * laziness is required and caching must be avoided. Generally, prefer Later.
      */
     class Always<A>(val f: () -> A) : Eval<A>() {
         override fun value(): A = f()
@@ -146,11 +199,9 @@ sealed class Eval<A> {
     }
 
     /**
-     * Call is a type of Eval[A] that is used to defer computations
-     * which produce Eval[A].
+     * Call is a type of Eval<A> that is used to defer computations which produce Eval<A>.
      *
-     * Users should not instantiate Call instances themselves. Instead,
-     * they will be automatically created when needed.
+     * Users should not instantiate Call instances themselves. Instead, they will be automatically created when needed.
      */
     class Call<A>(val thunk: () -> Eval<A>) : Eval<A>() {
         override fun memoize(): Eval<A> = Later { value() }
@@ -175,24 +226,20 @@ sealed class Eval<A> {
         }
 
         /**
-         * Alias for loop that can be called in a non-tail position
-         * from an otherwise tailrec-optimized loop.
+         * Alias for loop that can be called in a non-tail position from an otherwise tailrec-optimized loop.
          */
         fun <A> loop1(fa: Eval<A>): Eval<A> = loop(fa)
     }
 
     /**
-     * Compute is a type of Eval[A] that is used to chain computations
-     * involving .map and .flatMap. Along with Eval#flatMap it
-     * implements the trampoline that guarantees stack-safety.
+     * Compute is a type of Eval<A> that is used to chain computations involving .map and .flatMap. Along with
+     * Eval#flatMap. It implements the trampoline that guarantees stack-safety.
      *
-     * Users should not instantiate Compute instances
-     * themselves. Instead, they will be automatically created when
+     * Users should not instantiate Compute instances themselves. Instead, they will be automatically created when
      * needed.
      *
-     * Unlike a traditional trampoline, the internal workings of the
-     * trampoline are not exposed. This allows a slightly more efficient
-     * implementation of the .value method.
+     * Unlike a traditional trampoline, the internal workings of the trampoline are not exposed. This allows a slightly
+     * more efficient implementation of the .value method.
      */
     abstract class Compute<A, StartT> : Eval<A>() {
         open class Start
--- katz/src/main/kotlin/katz/typeclasses/Foldable.kt
+++ katz/src/main/kotlin/katz/typeclasses/Foldable.kt
@@ -1,17 +1,77 @@
 /*
- * Copyright (C) 2017 The Katz Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+Copyright (C) 2017 The Katz Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+------------------------------------------------------------------------
+
+Code in Katz is derived in part from typelevel/Cats and Scalaz.
+
+Cats license follows:
+
+------------------------------------------------------------------------
+
+Cats Copyright (c) 2015 Erik Osheim.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Scalaz license follows:
+
+------------------------------------------------------------------------
+
+Copyright (c) 2009-2014 Tony Morris, Runar Bjarnason, Tom Adams,
+Kristian Domagala, Brad Clow, Ricky Clarkson, Paul Chiusano, Trygve
+Laugstøl, Nick Partridge, Jason Zaugg. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+------------------------------------------------------------------------
  */
 
 package katz
@@ -21,70 +81,63 @@ import katz.Eval.EvalFactory.always
 /**
  * Data structures that can be folded to a summary value.
  *
- * In the case of a collection (such as `List` or `Set`), these methods will fold together (combine) the values
- * contained in the collection to produce a single result. Most collection types have `foldLeft` methods, which will
- * usually be used by the associated `Foldable[_]` instance.
- *
- * Foldable[F] is implemented in terms of two basic methods:
+ * Foldable<F> is implemented in terms of two basic methods:
  *
  *  - `foldLeft(fa, b)(f)` eagerly folds `fa` from left-to-right.
  *  - `foldRight(fa, b)(f)` lazily folds `fa` from right-to-left.
  *
- * Beyond these it provides many other useful methods related to folding over F[A] values.
+ * Beyond these it provides many other useful methods related to folding over F<A> values.
  */
 interface Foldable<F> {
 
     /**
-     * Left associative fold on 'F' using the function 'f'.
+     * Left associative fold on F using the provided function.
      */
     fun <A, B> foldL(fa: HK<F, A>, b: B): ((B, A) -> B) -> B
 
     /**
-     * Right associative lazy fold on `F` using the folding function 'f'.
+     * Right associative lazy fold on F using the provided function.
      *
-     * This method evaluates `lb` lazily (in some cases it will not be needed), and returns a lazy value. We are using
-     * `(A, Eval[B]) => Eval[B]` to support laziness in a stack-safe way. Chained computation should be performed via
+     * This method evaluates lb lazily (in some cases it will not be needed), and returns a lazy value. We are using
+     * (A, Eval<B>) => Eval<B> to support laziness in a stack-safe way. Chained computation should be performed via
      * .map and .flatMap.
      *
-     * For more detailed information about how this method works see the documentation for `Eval[_]`.
+     * For more detailed information about how this method works see the documentation for Eval<A>.
      */
     fun <A, B> foldR(fa: HK<F, A>, lb: Eval<B>): ((A, Eval<B>) -> Eval<B>) -> Eval<B>
 
     /**
      * The size of this Foldable.
      *
-     * This is overriden in structures that have more efficient size implementations (e.g. Vector, Set, Map).
+     * This can be overriden in structures that have more efficient size implementations.
      *
      * Note: will not terminate for infinite-sized collections.
      */
     fun <A> size(ml: Monoid<Long>, fa: HK<F, A>): Long = foldMap(ml, fa)({ _ -> 1L })
 
     /**
-     * Fold implemented using the given Monoid[A] instance.
+     * Fold implemented using the given Monoid<A> instance.
      */
     fun <A> fold(ma: Monoid<A>, fa: HK<F, A>): A =
             foldL(fa, ma.empty())({ acc, a -> ma.combine(acc, a) })
 
     /**
-     * Alias for [[fold]].
+     * Alias for fold.
      */
     fun <A : Monoid<*>> combineAll(m: Monoid<A>, fa: HK<F, A>): A = fold(m, fa)
 
     /**
-     * Fold implemented by mapping `A` values into `B` and then
-     * combining them using the given `Monoid[B]` instance.
+     * Fold implemented by mapping A values into B and then combining them using the given Monoid<B> instance.
      */
     fun <A, B> foldMap(mb: Monoid<B>, fa: HK<F, A>): (f: (A) -> B) -> B =
             { f: (A) -> B -> foldL(fa, mb.empty())({ b, a -> mb.combine(b, f(a)) }) }
 
     /**
-     * Left associative monadic folding on `F`.
+     * Left associative monadic folding on F.
      *
-     * The default implementation of this is based on `foldLeft`, and thus will
-     * always fold across the entire structure. Certain structures are able to
-     * implement this in such a way that folds can be short-circuited (not
-     * traverse the entirety of the structure), depending on the `G` result
-     * produced at a given step.
+     * The default implementation of this is based on foldL, and thus will always fold across the entire structure.
+     * Certain structures are able to implement this in such a way that folds can be short-circuited (not traverse the
+     * entirety of the structure), depending on the G result produced at a given step.
      */
     fun <G, A, B> foldM(g: Monad<G>, fa: HK<F, A>, z: B): ((B, A) -> HK<G, B>) -> HK<G, B> {
         val foldL = foldL(fa, g.pure(z))
@@ -94,22 +147,9 @@ interface Foldable<F> {
     }
 
     /**
-     * Monadic folding on `F` by mapping `A` values to `G[B]`, combining the `B`
-     * values using the given `Monoid[B]` instance.
+     * Monadic folding on F by mapping A values to G<B>, combining the B values using the given Monoid<B> instance.
      *
-     * Similar to [[foldM]], but using a `Monoid[B]`.
-     *
-     * {{{
-     * scala> import cats.Foldable
-     * scala> import cats.implicits._
-     * scala> val evenNumbers = List(2,4,6,8,10)
-     * scala> val evenOpt: Int => Option[Int] =
-     *      |   i => if (i % 2 == 0) Some(i) else None
-     * scala> Foldable[List].foldMapM(evenNumbers)(evenOpt)
-     * res0: Option[Int] = Some(30)
-     * scala> Foldable[List].foldMapM(evenNumbers :+ 11)(evenOpt)
-     * res1: Option[Int] = None
-     * }}}
+     * Similar to foldM, but using a Monoid<B>.
      */
     fun <G, A, B> foldMapM(g: Monad<G>, bb: Monoid<B>, fa: HK<F, A>): ((A) -> HK<G, B>) -> HK<G, B> {
         val foldM = foldM(g, fa, bb.empty())
@@ -119,26 +159,12 @@ interface Foldable<F> {
     }
 
     /**
-     * Traverse `F[A]` using `Applicative[G]`.
+     * Traverse F<A> using Applicative<G>.
      *
-     * `A` values will be mapped into `G[B]` and combined using
-     * `Applicative#map2`.
+     * A values will be mapped into G<B> and combined using Applicative#map2.
      *
-     * For example:
-     *
-     * {{{
-     * scala> import cats.implicits._
-     * scala> def parseInt(s: String): Option[Int] = Either.catchOnly[NumberFormatException](s.toInt).toOption
-     * scala> val F = Foldable[List]
-     * scala> F.traverse_(List("333", "444"))(parseInt)
-     * res0: Option[Unit] = Some(())
-     * scala> F.traverse_(List("333", "zzz"))(parseInt)
-     * res1: Option[Unit] = None
-     * }}}
-     *
-     * This method is primarily useful when `G[_]` represents an action
-     * or effect, and the specific `A` aspect of `G[A]` is not otherwise
-     * needed.
+     * This method is primarily useful when G<_> represents an action or effect, and the specific A aspect of G<A> is
+     * not otherwise needed.
      */
     fun <G, A, B> traverse_(ag: Applicative<G>, fa: HK<F, A>): (f: (A) -> HK<G, B>) -> HK<G, Unit> {
         val foldR = foldR(fa, always { ag.pure(Unit) })
@@ -148,7 +174,7 @@ interface Foldable<F> {
     /**
      * Check whether at least one element satisfies the predicate.
      *
-     * If there are no elements, the result is `false`.
+     * If there are no elements, the result is false.
      */
     fun <A> exists(fa: HK<F, A>): (p: (A) -> Boolean) -> Boolean =
             { p: (A) -> Boolean -> foldR(fa, Eval.False)({ a, lb -> if (p(a)) Eval.True else lb }).value() }
@@ -156,7 +182,7 @@ interface Foldable<F> {
     /**
      * Check whether all elements satisfy the predicate.
      *
-     * If there are no elements, the result is `true`.
+     * If there are no elements, the result is true.
      */
     fun <A> forall(fa: HK<F, A>): (p: (A) -> Boolean) -> Boolean =
             { p: (A) -> Boolean -> foldR(fa, Eval.True)({ a, lb -> if (p(a)) lb else Eval.False }).value() }
