3afb7cf0c7bf5aa0d74bb8d8cbf0506fdfa2b277
pakoito <jueces.jus@gmail.com>
2017-04-19 23:14:39 +0100

    Add tailRecM to EitherTMonad

--- katz/src/main/kotlin/katz/instances/EitherTMonad.kt
+++ katz/src/main/kotlin/katz/instances/EitherTMonad.kt
@@ -1,11 +1,26 @@
 package katz
 
 class EitherTMonad<F, L>(val MF : Monad<F>) : Monad<EitherTF<F, L>> {
-  
-    override fun <A> pure(a: A): EitherT<F, L, A> = EitherT(MF, MF.pure(Either.Right(a)))
+    override fun <A> pure(a: A): EitherT<F, L, A> =
+            EitherT(MF, MF.pure(Either.Right(a)))
 
     override fun <A, B> flatMap(fa: EitherTKind<F, L, A>, f: (A) -> EitherTKind<F, L, B>): EitherT<F, L, B> =
             fa.ev().flatMap { f(it).ev() }
+
+    override fun <A, B> tailRecM(a: A, f: (A) -> HK<EitherTF<F, L>, Either<A, B>>): EitherT<F, L, B> =
+            EitherT(MF, MF.tailRecM(a, {
+                MF.map(f(it).ev().value) {
+                    it.fold({
+                        Either.Right(Either.Left(it))
+                    }, {
+                        it.fold({
+                                Either.Left(it)
+                        }, {
+                                Either.Right(Either.Right(it))
+                        })
+                    })
+                }
+            }))
 }
 
 fun <F, A, B> EitherTKind<F, A, B>.ev(): EitherT<F, A, B> = this as EitherT<F, A, B>
