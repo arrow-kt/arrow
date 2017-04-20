e662e169518ebb9b5e14dd0d18fba39090d2a8f1
pakoito <jueces.jus@gmail.com>
2017-04-21 00:19:13 +0100

    Fix StackOverflow on cartesian by using EitherT map instance function

--- katz/src/main/kotlin/katz/instances/EitherTMonad.kt
+++ katz/src/main/kotlin/katz/instances/EitherTMonad.kt
@@ -4,6 +4,9 @@ class EitherTMonad<F, L>(val MF : Monad<F>) : Monad<EitherTF<F, L>> {
     override fun <A> pure(a: A): EitherT<F, L, A> =
             EitherT(MF, MF.pure(Either.Right(a)))
 
+    override fun <A, B> map(fa: EitherTKind<F, L, A>, f: (A) -> B): EitherT<F, L, B> =
+            fa.ev().map { f(it) }
+
     override fun <A, B> flatMap(fa: EitherTKind<F, L, A>, f: (A) -> EitherTKind<F, L, B>): EitherT<F, L, B> =
             fa.ev().flatMap { f(it).ev() }
 
