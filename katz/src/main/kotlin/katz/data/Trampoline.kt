c23ce46f90c0d7766155011d6459f3f644f39e74
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-04-21 18:04:30 +0200

    add some javadoc for Trampoline

--- katz/src/main/kotlin/katz/data/Trampoline.kt
+++ katz/src/main/kotlin/katz/data/Trampoline.kt
@@ -1,5 +1,11 @@
 package katz.data
 
+/**
+ * Trampoline is often used to emulate tail recursion. The idea is to have some step code that can be trampolined itself
+ * to emulate recursion. The difference with standard recursion would be that there is no need to rewind the whole stack
+ * when we reach the end of the stack, since the first value returned that is not a trampoline would be directly
+ * returned as the overall result value for the whole function chain.
+ */
 sealed class Trampoline<out A> {
 
     class More<out A>(val f: () -> Trampoline<A>) : Trampoline<A>()
