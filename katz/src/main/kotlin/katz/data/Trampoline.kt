c8e9388f1077283d50df1b285ce9c4464f924946
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-04-21 18:06:36 +0200

    update javadoc

--- katz/src/main/kotlin/katz/data/Trampoline.kt
+++ katz/src/main/kotlin/katz/data/Trampoline.kt
@@ -4,7 +4,8 @@ package katz.data
  * Trampoline is often used to emulate tail recursion. The idea is to have some step code that can be trampolined itself
  * to emulate recursion. The difference with standard recursion would be that there is no need to rewind the whole stack
  * when we reach the end of the stack, since the first value returned that is not a trampoline would be directly
- * returned as the overall result value for the whole function chain.
+ * returned as the overall result value for the whole function chain. That means Trampoline emulates what tail recursion
+ * does.
  */
 sealed class Trampoline<out A> {
 
