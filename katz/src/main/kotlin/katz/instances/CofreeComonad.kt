77881705bb174e07e58694d549909d18b1f8fcb8
pakoito <jueces.jus@gmail.com>
2017-05-13 16:20:19 +0100

    Add CofreeComonad

---
+++ katz/src/main/kotlin/katz/instances/CofreeComonad.kt
@@ -0,0 +1,12 @@
+package katz
+
+interface CofreeComonad<S> : Comonad<CofreeF<S>>, Typeclass {
+    override fun <A, B> coflatMap(fa: HK<CofreeF<S>, A>, f: (HK<CofreeF<S>, A>) -> B): Cofree<S, B> =
+            fa.ev().coflatmap(f)
+
+    override fun <A> extract(fa: HK<CofreeF<S>, A>): A =
+            fa.ev().head()
+
+    override fun <A, B> map(fa: HK<CofreeF<S>, A>, f: (A) -> B): Cofree<S, B> =
+            fa.ev().map(f)
+}
