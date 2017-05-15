9871566b377c4f1d31e21033b11d0358c851ce72
pakoito <jueces.jus@gmail.com>
2017-05-15 22:13:27 +0100

    Add FunctionBimonad

---
+++ katz/src/main/kotlin/katz/instances/FunctionBimonad.kt
@@ -0,0 +1,35 @@
+package katz
+
+// We don't we want an inherited class to avoid equivalence issues, so a simple HK wrapper will do
+data class Function0<out A>(val f: () -> A) : HK<Function0.F, A> {
+
+    class F private constructor()
+
+    companion object : Bimonad<Function0.F>, GlobalInstance<Bimonad<Function0.F>>() {
+
+        override fun <A, B> flatMap(fa: HK<Function0.F, A>, f: (A) -> HK<Function0.F, B>): HK<Function0.F, B> =
+                f(fa.ev().invoke())
+
+        override fun <A, B> coflatMap(fa: HK<Function0.F, A>, f: (HK<Function0.F, A>) -> B): HK<Function0.F, B> =
+                Function0 { f(fa) }
+
+        override fun <A> pure(a: A): HK<Function0.F, A> =
+                Function0 { a }
+
+        override fun <A> extract(fa: HK<Function0.F, A>): A =
+                fa.ev().invoke()
+
+        override fun <A, B> tailRecM(a: A, f: (A) -> HK<F, Either<A, B>>): HK<F, B> = Function0 {
+            tailrec fun loop(thisA: A): B =
+                    f(thisA).ev().invoke().let { either ->
+                        when (either) {
+                            is Either.Left -> loop(either.a)
+                            is Either.Right -> either.b
+                        }
+                    }
+            loop(a)
+        }
+    }
+}
+
+fun <A> HK<Function0.F, A>.ev() = (this as Function0<A>).f
