1f4346860e8644ee3983e7325872898c65cc3b87
pakoito <jueces.jus@gmail.com>
2017-05-19 23:53:30 +0100

    Add a cleanup pass to Eval

--- katz/src/main/kotlin/katz/data/Eval.kt
+++ katz/src/main/kotlin/katz/data/Eval.kt
@@ -27,33 +27,34 @@ package katz
  * Eval instance -- this can defeat the trampolining and lead to stack
  * overflows.
  */
-sealed class Eval<A> {
+sealed class Eval<out A> {
 
     abstract fun value(): A
 
     abstract fun memoize(): Eval<A>
 
-    fun <B> map(f: (A) -> B): Eval<B> = flatMap { a -> Now(f(a)) }
+    fun <B> map(f: (A) -> B): Eval<B> =
+            flatMap { a -> Now(f(a)) }
 
-    fun <B> flatMap(f: (A) -> Eval<B>): Eval<B> = when (this) {
-        is Compute<A, *> -> object : Compute<B, Compute.Start>() {
-            override val start: () -> Eval<Start> = (this@Eval as Compute<A, Start>).start
-            override val run: (Start) -> Eval<B> = { s: Compute.Start ->
-                object : Compute<B, A>() {
-                    override val start: () -> Eval<A> = { (this@Eval as Compute<A, Start>).run(s) }
-                    override val run = f
+    fun <B> flatMap(f: (A) -> Eval<B>): Eval<B> =
+            when (this) {
+                is Compute<A> -> object : Compute<B>() {
+                    override fun <S> start(): Eval<S> = (this@Eval).start()
+                    override fun <S> run(s: S): Eval<B> =
+                            object : Compute<B>() {
+                                override fun <S1> start(): Eval<S1> = (this@Eval).run(s) as Eval<S1>
+                                override fun <S1> run(s: S1): Eval<B> = f(s as A)
+                            }
+                }
+                is Eval.Call<A> -> object : Eval.Compute<B>() {
+                    override fun <S> start(): Eval<S> = this@Eval.thunk() as Eval<S>
+                    override fun <S> run(s: S): Eval<B> = f(s as A)
+                }
+                else -> object : Eval.Compute<B>() {
+                    override fun <S> start(): Eval<S> = this@Eval as Eval<S>
+                    override fun <S> run(s: S): Eval<B> = f(s as A)
                 }
             }
-        }
-        is Eval.Call<A> -> object : Eval.Compute<B, A>() {
-            override val start: () -> Eval<A> = this@Eval.thunk
-            override val run: (A) -> Eval<B> = f
-        }
-        else -> object : Eval.Compute<B, A>() {
-            override val start: () -> Eval<A> = { this@Eval }
-            override val run: (A) -> Eval<B> = f
-        }
-    }
 
     /**
      * Construct an eager Eval<A> instance. In some sense it is equivalent to using a val.
@@ -61,7 +62,7 @@ sealed class Eval<A> {
      * This type should be used when an A value is already in hand, or when the computation to produce an A value is
      * pure and very fast.
      */
-    class Now<A>(val value: A) : Eval<A>() {
+    data class Now<out A>(val value: A) : Eval<A>() {
         override fun value(): A = value
         override fun memoize(): Eval<A> = this
     }
@@ -77,19 +78,13 @@ sealed class Eval<A> {
      * Once Later has been evaluated, the closure (and any values captured by the closure) will not be retained, and
      * will be available for garbage collection.
      */
-    class Later<A>(val f: () -> A) : Eval<A>() {
-        override fun value(): A = value
-        val value: A by lazy {
-            val result = f()
-            result
-        }
+    data class Later<out A>(private val f: () -> A) : Eval<A>() {
+        val value: A by lazy(f)
 
+        override fun value(): A = value
         override fun memoize(): Eval<A> = this
     }
 
-    object LaterFactory {
-        fun <A> apply(a: () -> A): Later<A> = Later(a)
-    }
 
     /**
      * Construct a lazy Eval<A> instance.
@@ -99,13 +94,86 @@ sealed class Eval<A> {
      * This type will evaluate the computation every time the value is required. It should be avoided except when
      * laziness is required and caching must be avoided. Generally, prefer Later.
      */
-    class Always<A>(val f: () -> A) : Eval<A>() {
+    data class Always<out A>(private val f: () -> A) : Eval<A>() {
         override fun value(): A = f()
         override fun memoize(): Eval<A> = Later(f)
     }
 
-    object AlwaysFactory {
-        fun <A> apply(a: () -> A): Always<A> = Always(a)
+    /**
+     * Call is a type of Eval<A> that is used to defer computations which produce Eval<A>.
+     *
+     * Users should not instantiate Call instances themselves. Instead, they will be automatically created when needed.
+     */
+    data class Call<out A>(val thunk: () -> Eval<A>) : Eval<A>() {
+        override fun memoize(): Eval<A> = Later { value() }
+        override fun value(): A = CallFactory.loop(this).value()
+    }
+
+    object CallFactory {
+
+        /**
+         * Collapse the call stack for eager evaluations.
+         */
+        tailrec fun <A> loop(fa: Eval<A>): Eval<A> =
+                when (fa) {
+                    is Call -> loop(fa.thunk())
+                    is Compute -> {
+                        object : Compute<A>() {
+                            override fun <S> start(): Eval<S> = fa.start()
+                            override fun <S> run(s: S): Eval<A> = loop1(fa.run(s))
+                        }
+                    }
+                    else -> fa
+                }
+
+        /**
+         * Alias for loop that can be called in a non-tail position from an otherwise tailrec-optimized loop.
+         */
+        fun <A> loop1(fa: Eval<A>): Eval<A> =
+                loop(fa)
+    }
+
+    /**
+     * Compute is a type of Eval<A> that is used to chain computations involving .map and .flatMap. Along with
+     * Eval#flatMap. It implements the trampoline that guarantees stack-safety.
+     *
+     * Users should not instantiate Compute instances themselves. Instead, they will be automatically created when
+     * needed.
+     *
+     * Unlike a traditional trampoline, the internal workings of the trampoline are not exposed. This allows a slightly
+     * more efficient implementation of the .value method.
+     */
+    private abstract class Compute<out A> : Eval<A>() {
+
+        abstract fun <S> start(): Eval<S>
+
+        abstract fun <S> run(s: S): Eval<A>
+
+        override fun memoize(): Eval<A> =
+                Later { value() }
+
+        override fun value(): A =
+                loop(this, listOf())
+
+        tailrec fun <A> loop(curr: Eval<A>, fs: List<(Any?) -> Eval<A>>): A =
+                when (curr) {
+                    is Compute<A> ->
+                        curr.start<A>().let { cc ->
+                            when (cc) {
+                                is Compute -> {
+                                    val startFun: (Any?) -> Eval<A> = { cc.run(it) }
+                                    loop(cc.start<A>(), listOf(startFun, startFun) + fs)
+                                }
+                                else -> loop(curr.run(cc.value()), fs)
+                            }
+                        }
+                    else ->
+                        if (fs.size >= 2) {
+                            loop(fs[0](curr.value()), fs)
+                        } else {
+                            curr.value()
+                        }
+                }
     }
 
     companion object EvalFactory {
@@ -121,80 +189,4 @@ sealed class Eval<A> {
         @JvmStatic val Zero: Eval<Int> = Now(0)
         @JvmStatic val One: Eval<Int> = Now(1)
     }
-
-    /**
-     * Call is a type of Eval<A> that is used to defer computations which produce Eval<A>.
-     *
-     * Users should not instantiate Call instances themselves. Instead, they will be automatically created when needed.
-     */
-    class Call<A>(val thunk: () -> Eval<A>) : Eval<A>() {
-        override fun memoize(): Eval<A> = Later { value() }
-        override fun value(): A = CallFactory.loop(this).value()
-    }
-
-    object CallFactory {
-
-        /**
-         * Collapse the call stack for eager evaluations.
-         */
-        tailrec fun <A> loop(fa: Eval<A>): Eval<A> = when (fa) {
-            is Call<A> -> loop(fa.thunk())
-            is Compute<A, *> -> {
-                fa as Compute<A, Compute.Start>
-                object : Compute<A, Compute.Start>() {
-                    override val start: () -> Eval<Compute.Start> = { fa.start() }
-                    override val run: (Compute.Start) -> Eval<A> = { s -> loop1(fa.run(s)) }
-                }
-            }
-            else -> fa
-        }
-
-        /**
-         * Alias for loop that can be called in a non-tail position from an otherwise tailrec-optimized loop.
-         */
-        fun <A> loop1(fa: Eval<A>): Eval<A> = loop(fa)
-    }
-
-    /**
-     * Compute is a type of Eval<A> that is used to chain computations involving .map and .flatMap. Along with
-     * Eval#flatMap. It implements the trampoline that guarantees stack-safety.
-     *
-     * Users should not instantiate Compute instances themselves. Instead, they will be automatically created when
-     * needed.
-     *
-     * Unlike a traditional trampoline, the internal workings of the trampoline are not exposed. This allows a slightly
-     * more efficient implementation of the .value method.
-     */
-    abstract class Compute<A, StartT> : Eval<A>() {
-        open class Start
-
-        abstract val start: () -> Eval<StartT>
-        abstract val run: (StartT) -> Eval<A>
-
-        override fun memoize(): Eval<A> = Later { value() }
-
-        override fun value(): A {
-            tailrec fun loop(curr: L, fs: List<C>): Any =
-                    when (curr) {
-                        is Compute<*, *> -> {
-                            curr as Compute<A, StartT>
-                            val cc = curr.start()
-                            when (cc) {
-                                is Compute<*, *> -> loop(
-                                        cc.start() as L,
-                                        listOf(cc.run as C) + listOf(curr.run as C) + fs)
-                                else -> loop(curr.run(cc.value()) as L, fs)
-                            }
-                        }
-                        else -> when {
-                            fs.size >= 2 -> loop(fs[0](curr.value()), fs)
-                            else -> curr.value()!!
-                        }
-                    }
-            return loop(this as L, listOf()) as A
-        }
-    }
 }
-
-typealias L = Eval<Any>
-typealias C = (Any) -> Eval<Any>
