3bf359b41a0aef83e5b901ae908278cfa51fa6b0
pakoito <jueces.jus@gmail.com>
2017-06-11 22:16:06 +0100

    Add IO implementation. Add Duration for blocking operations.

---
+++ katz/src/main/kotlin/katz/effects/Duration.kt
@@ -0,0 +1,70 @@
+/**
+ * Copyright [2016] [sksamuel]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package katz
+
+import java.util.concurrent.TimeUnit
+
+data class Duration(val amount: Long, val timeUnit: TimeUnit) {
+    val nanoseconds: Long by lazy { timeUnit.toNanos(amount) }
+
+    companion object {
+        // Actually limited to 9223372036854775807 days, so unless you are very patient, it is unlimited ;-)
+        val Infinite = Duration(amount = Long.MAX_VALUE, timeUnit = TimeUnit.DAYS)
+    }
+}
+
+val Long.days: Duration
+    get() = Duration(this, TimeUnit.DAYS)
+
+val Int.days: Duration
+    get() = Duration(this.toLong(), TimeUnit.DAYS)
+
+val Long.hours: Duration
+    get() = Duration(this, TimeUnit.HOURS)
+
+val Int.hours: Duration
+    get() = Duration(this.toLong(), TimeUnit.HOURS)
+
+val Long.microseconds: Duration
+    get() = Duration(this, TimeUnit.MICROSECONDS)
+
+val Int.microseconds: Duration
+    get() = Duration(this.toLong(), TimeUnit.MICROSECONDS)
+
+val Long.minutes: Duration
+    get() = Duration(this, TimeUnit.MINUTES)
+
+val Int.minutes: Duration
+    get() = Duration(this.toLong(), TimeUnit.MINUTES)
+
+val Long.milliseconds: Duration
+    get() = Duration(this, TimeUnit.MILLISECONDS)
+
+val Int.milliseconds: Duration
+    get() = Duration(this.toLong(), TimeUnit.MILLISECONDS)
+
+val Long.nanoseconds: Duration
+    get() = Duration(this, TimeUnit.NANOSECONDS)
+
+val Int.nanoseconds: Duration
+    get() = Duration(this.toLong(), TimeUnit.NANOSECONDS)
+
+val Long.seconds: Duration
+    get() = Duration(this, TimeUnit.SECONDS)
+
+val Int.seconds: Duration
+    get() = Duration(this.toLong(), TimeUnit.SECONDS)
---
+++ katz/src/main/kotlin/katz/effects/IO.kt
@@ -0,0 +1,220 @@
+package katz
+
+import katz.effects.internal.AndThen
+import katz.effects.internal.Platform
+import katz.effects.internal.Platform.unsafeResync
+
+sealed class IO<out A> {
+    abstract fun <B> map(f: (A) -> B): IO<B>
+
+    fun <B> flatMap(f: (A) -> IO<B>): IO<B> =
+            flatMapTotal(
+                    AndThen { a: A ->
+                        try {
+                            f(a)
+                        } catch (error: Throwable) {
+                            RaiseError<B>(error)
+                        }
+                    })
+
+    internal abstract fun <B> flatMapTotal(f: AndThen<A, IO<B>>): IO<B>
+
+    abstract fun attempt(): IO<Either<Throwable, A>>
+
+    fun runAsync(cb: (Either<Throwable, A>) -> IO<Unit>): IO<Unit> =
+            IO { unsafeRunAsync(cb.andThen { it.unsafeRunAsync { } }) }
+
+    fun unsafeRunAsync(cb: (Either<Throwable, A>) -> Unit): Unit =
+            unsafeStep().unsafeRunAsyncTotal(cb)
+
+    abstract fun unsafeRunAsyncTotal(cb: (Either<Throwable, A>) -> Unit)
+
+    abstract fun unsafeStep(): IO<A>
+
+    fun unsafeRunSync(): Option<A> =
+            unsafeRunTimed(Duration.Infinite)
+
+    fun unsafeRunTimed(limit: Duration): Option<A> =
+            unsafeStep().unsafeRunTimedTotal(limit)
+
+    abstract fun unsafeRunTimedTotal(limit: Duration): Option<A>
+
+    companion object {
+        internal fun <A, B> mapDefault(t: IO<A>, f: (A) -> B): IO<B> =
+                t.flatMap(f.andThen { Pure(it) })
+
+        internal fun <A> attemptValue(): AndThen<A, IO<Either<Throwable, A>>> =
+                AndThen({ a: A -> Pure(Either.Right(a)) }, { e -> Pure(Either.Left(e)) })
+
+        operator fun <A> invoke(f: (Unit) -> A): IO<A> =
+                suspend { Pure(f(Unit)) }
+
+        fun <A> pure(f: (Unit) -> A): IO<A> =
+                Pure(f(Unit))
+
+        fun <A> suspend(f: (Unit) -> IO<A>): IO<A> =
+                Suspend(AndThen { _ ->
+                    try {
+                        f(Unit)
+                    } catch (throwable: Throwable) {
+                        raiseError(throwable)
+                    }
+                })
+
+        fun <A> raiseError(throwable: Throwable): IO<A> =
+                RaiseError(throwable)
+
+        fun <A> async(k: ((Either<Throwable, A>) -> Unit) -> Unit): IO<A> =
+                Async { callBack ->
+                    Platform.onceOnly(callBack).let {
+                        try {
+                            k(it)
+                        } catch (throwable: Throwable) {
+                            it(Either.Left(throwable))
+                        }
+                    }
+                }
+
+        fun unit(): IO<Unit> =
+                pure({})
+
+        fun <A> eval(eval: Eval<A>) =
+                when (eval) {
+                    is Eval.Now -> pure { eval.value }
+                    else -> invoke { eval.value() }
+                }
+    }
+}
+
+internal data class Pure<out A>(val a: A) : IO<A>() {
+    override fun <B> flatMapTotal(f: AndThen<A, IO<B>>): IO<B> =
+            Suspend(AndThen({ _: Unit -> a }.andThen(f)))
+
+    override fun <B> map(f: (A) -> B): IO<B> =
+            try {
+                Pure(f(a))
+            } catch (exception: Throwable) {
+                RaiseError(exception)
+            }
+
+    override fun attempt(): IO<Either<Throwable, A>> =
+            Pure(Either.Right(a))
+
+    override fun unsafeStep(): IO<A> =
+            this
+
+    override fun unsafeRunAsyncTotal(cb: (Either<Throwable, A>) -> Unit) =
+            cb(Either.Right(a))
+
+    override fun unsafeRunTimedTotal(limit: Duration): Option<A> =
+            Option.Some(a)
+}
+
+internal data class RaiseError<out A>(val exception: Throwable) : IO<A>() {
+    override fun <B> map(f: (A) -> B): IO<B> =
+            RaiseError(exception)
+
+    override fun <B> flatMapTotal(f: AndThen<A, IO<B>>): IO<B> =
+            Suspend(AndThen { f.error(exception, { RaiseError<B>(it) }) })
+
+    override fun attempt(): IO<Either<Throwable, A>> =
+            Pure(Either.Left(exception))
+
+    override fun unsafeStep(): IO<A> =
+            this
+
+    override fun unsafeRunAsyncTotal(cb: (Either<Throwable, A>) -> Unit) =
+            cb(Either.Left(exception))
+
+    override fun unsafeRunTimedTotal(limit: Duration): Option<A> =
+            throw exception
+}
+
+internal data class Suspend<out A>(val cont: AndThen<Unit, IO<A>>) : IO<A>() {
+    override fun <B> map(f: (A) -> B): IO<B> =
+            mapDefault(this, f)
+
+    override fun <B> flatMapTotal(f: AndThen<A, IO<B>>): IO<B> =
+            BindSuspend(cont, f)
+
+    override fun attempt(): IO<Either<Throwable, A>> =
+            BindSuspend(cont, attemptValue())
+
+    override fun unsafeStep(): IO<A> =
+            cont(Unit).unsafeStep()
+
+    override fun unsafeRunAsyncTotal(cb: (Either<Throwable, A>) -> Unit) =
+            throw AssertionError("Unreachable")
+
+    override fun unsafeRunTimedTotal(limit: Duration): Option<A> =
+            throw AssertionError("Unreachable")
+}
+
+internal data class BindSuspend<E, out A>(val cont: AndThen<Unit, IO<E>>, val f: AndThen<E, IO<A>>) : IO<A>() {
+    override fun <B> map(f: (A) -> B): IO<B> =
+            mapDefault(this, f)
+
+    override fun <B> flatMapTotal(ff: AndThen<A, IO<B>>): IO<B> =
+            BindSuspend(cont, f.andThen(AndThen({ it.flatMapTotal(ff) }, { ff.error(it, { RaiseError<B>(it) }) })))
+
+    override fun attempt(): IO<Either<Throwable, A>> =
+            BindSuspend(AndThen { _ -> this }, attemptValue())
+
+    override fun unsafeStep(): IO<A> =
+            cont(Unit).flatMapTotal(this.f).unsafeStep()
+
+    override fun unsafeRunAsyncTotal(cb: (Either<Throwable, A>) -> Unit) =
+            throw AssertionError("Unreachable")
+
+    override fun unsafeRunTimedTotal(limit: Duration): Option<A> =
+            throw AssertionError("Unreachable")
+}
+
+internal data class Async<out A>(val cont: ((Either<Throwable, A>) -> Unit) -> Unit) : IO<A>() {
+    override fun <B> map(f: (A) -> B): IO<B> =
+            mapDefault(this, f)
+
+    override fun <B> flatMapTotal(f: AndThen<A, IO<B>>): IO<B> =
+            BindAsync(cont, f)
+
+    override fun attempt(): IO<Either<Throwable, A>> =
+            BindAsync(cont, attemptValue())
+
+    override fun unsafeStep(): IO<A> =
+            this
+
+    override fun unsafeRunAsyncTotal(cb: (Either<Throwable, A>) -> Unit) =
+            cont(cb)
+
+    override fun unsafeRunTimedTotal(limit: Duration): Option<A> =
+            unsafeResync(this, limit)
+}
+
+internal data class BindAsync<E, out A>(val cont: ((Either<Throwable, E>) -> Unit) -> Unit, val f: AndThen<E, IO<A>>) : IO<A>() {
+    override fun <B> map(f: (A) -> B): IO<B> =
+            mapDefault(this, f)
+
+    override fun <B> flatMapTotal(ff: AndThen<A, IO<B>>): IO<B> =
+            BindAsync(cont, f.andThen(AndThen({ it.flatMapTotal(ff) }, { ff.error(it, { RaiseError<B>(it) }) })))
+
+    override fun attempt(): IO<Either<Throwable, A>> =
+            BindSuspend(AndThen { _ -> this }, attemptValue())
+
+    override fun unsafeStep(): IO<A> =
+            this
+
+    override fun unsafeRunAsyncTotal(cb: (Either<Throwable, A>) -> Unit) =
+            cont { result ->
+                try {
+                    when (result) {
+                        is Either.Right -> f(result.b).unsafeRunAsync(cb)
+                        is Either.Left -> f.error(result.a, { RaiseError<A>(it) }).unsafeRunAsync(cb)
+                    }
+                } catch (throwable: Throwable) {
+                    cb(Either.Left(throwable))
+                }
+            }
+
+    override fun unsafeRunTimedTotal(limit: Duration): Option<A> =
+            unsafeResync(this, limit)
+}
