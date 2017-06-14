dc18c455709584910892a9fa190f715147aa5d44
Raul Raja <raulraja@gmail.com>
2017-06-14 23:40:19 +0200

    Fixed functor laws working arounf reified and kotlin test bug on Gen.default()

--- katz/src/test/kotlin/laws/FunctorLaws.kt
+++ katz/src/test/kotlin/laws/FunctorLaws.kt
@@ -3,23 +3,23 @@ package katz
 import io.kotlintest.properties.Gen
 import io.kotlintest.properties.forAll
 
-inline fun <reified F, reified A, reified B, reified C> functorLaws(): List<Law> =
+inline fun <reified F> functorLaws(): List<Law> =
         listOf(
-                Law("Functor: Covariant Identity", { covariantIdentity<F, A>() }) ,
-                Law("Functor: Covariant Composition", {  covariantComposition<F, A, B, C>()})
+               Law("Functor Laws: Covariant Identity", { covariantIdentity<F>() }),
+               Law("Functor: Covariant Composition", {  covariantComposition<F>() })
         )
 
-inline fun <reified F, reified A> covariantIdentity(functor: Functor<F> = functor<F>()): Unit =
-        forAll(genMonad(), { fa: HK<F, A> ->
+inline fun <reified F> covariantIdentity(functor: Functor<F> = functor<F>()): Unit =
+        forAll(genApplicative<F, Int>(Gen.int()), { fa: HK<F, Int> ->
             functor.map(fa, ::identity) == fa
         })
 
-inline fun <reified F, reified A, reified B, reified C> covariantComposition(functor: Functor<F> = functor<F>()): Unit =
+inline fun <reified F> covariantComposition(functor: Functor<F> = functor<F>()): Unit =
         forAll(
-                genMonad(),
-                genFunctionAToB<A, B>(Gen.default()),
-                genFunctionAToB<B, C>(Gen.default()),
-                { fa: HK<F, A>, f, g ->
+                genApplicative<F, Int>(Gen.int()),
+                genFunctionAToB<Int, Int>(Gen.int()),
+                genFunctionAToB<Int, Int>(Gen.int()),
+                { fa: HK<F, Int>, f, g ->
                     functor.map(functor.map(fa, f), g) == functor.map(fa, f andThen g)
                 }
         )
