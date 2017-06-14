f26280ba36e0fd2d307492b1cbc64af2fbfec238
Raul Raja <raulraja@gmail.com>
2017-06-15 00:26:20 +0200

    Functor first please! (to allow type constructors of different arity)

--- katz/src/test/kotlin/laws/FunctorLaws.kt
+++ katz/src/test/kotlin/laws/FunctorLaws.kt
@@ -3,10 +3,10 @@ package katz
 import io.kotlintest.properties.Gen
 import io.kotlintest.properties.forAll
 
-inline fun <reified F> functorLaws(): List<Law> =
+inline fun <reified F> functorLaws(functor: Functor<F> = functor<F>()): List<Law> =
         listOf(
-               Law("Functor Laws: Covariant Identity", { covariantIdentity<F>() }),
-               Law("Functor: Covariant Composition", {  covariantComposition<F>() })
+               Law("Functor Laws: Covariant Identity", { covariantIdentity(functor) }),
+               Law("Functor: Covariant Composition", {  covariantComposition(functor) })
         )
 
 inline fun <reified F> covariantIdentity(functor: Functor<F> = functor<F>()): Unit =
