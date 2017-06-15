682dc3c819cca80ba895c1a730aae03041cf6edf
Raul Raja <raulraja@gmail.com>
2017-06-15 23:42:54 +0200

    Added Applicative Laws

--- kategory/src/test/kotlin/kategory/data/OptionTest.kt
+++ kategory/src/test/kotlin/kategory/data/OptionTest.kt
@@ -13,7 +13,7 @@ class OptionTest: UnitSpec() {
 
     init {
 
-        testLaws(functorLaws(Option))
+        testLaws(ApplicativeLaws.laws(Option))
 
         "map should modify value" {
             Some(12).map { "flower" } shouldBe Some("flower")
