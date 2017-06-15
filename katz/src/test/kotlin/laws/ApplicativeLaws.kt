6d9fe8f697010d5079efb61b267b526826aacd3d
Raul Raja <raulraja@gmail.com>
2017-06-15 23:42:54 +0200

    Added Applicative Laws

--- katz/src/test/kotlin/laws/ApplicativeLaws.kt
+++ katz/src/test/kotlin/laws/ApplicativeLaws.kt
@@ -3,18 +3,34 @@ package katz
 import io.kotlintest.properties.Gen
 import io.kotlintest.properties.forAll
 
-//abstract class ApplicativeLaws<F, A, B, C>(val applicative : Applicative<F>,
-//                                           val applicativeGen: Gen<HK<F, A>>,
-//                                           val applicativeGenB: Gen<B>,
-//                                           val applicativeGenC: Gen<C>) :
-//        FunctorLaws<F, A, B, C>(applicative, applicativeGen, applicativeGenB, applicativeGenC) {
-//
-//    init {
-//        "Applicative: ap composition" {
-//            forAll(functorGen, { fa: HK<F, A> ->
-//                functor.map(fa, ::identity) == fa
-//            })
-//        }
-//
-//    }
-//}
+object ApplicativeLaws {
+
+    inline fun <reified F> laws(A: Applicative<F> = applicative<F>()): List<Law> =
+            FunctorLaws.laws(A) + listOf(
+                    Law("Applicative Laws: ap identity", { apIdentity(A) }),
+                    Law("Applicative Laws: homomorphism", { homomorphism(A) }),
+                    Law("Applicative Laws: interchange", { interchange(A) }),
+                    Law("Applicative Laws: map derived", { mapDerived(A) })
+            )
+
+    inline fun <reified F> apIdentity(A: Applicative<F> = applicative<F>()): Unit =
+            forAll(genApplicative<F, Int>(Gen.int()), { fa: HK<F, Int> ->
+                A.ap(fa, A.pure({ n: Int -> n })) == fa
+            })
+
+    inline fun <reified F> homomorphism(A: Applicative<F> = applicative<F>()): Unit =
+            forAll(genFunctionAToB<Int, Int>(Gen.int()), Gen.int(), { ab: (Int) -> Int, a: Int ->
+                A.ap(A.pure(a), A.pure(ab)) == A.pure(ab(a))
+            })
+
+    inline fun <reified F> interchange(A: Applicative<F> = applicative<F>()): Unit =
+            forAll(genApplicative<F, (Int) -> Int>(genFunctionAToB<Int, Int>(Gen.int())), Gen.int(), { fa: HK<F, (Int) -> Int>, a: Int ->
+                A.ap(A.pure(a), fa) == A.ap(fa, A.pure({ x: (Int) -> Int -> x(a) }))
+            })
+
+    inline fun <reified F> mapDerived(A: Applicative<F> = applicative<F>()): Unit =
+            forAll(genApplicative<F, Int>(Gen.int()), genFunctionAToB<Int, Int>(Gen.int()), { fa: HK<F, Int>, f: (Int) -> Int ->
+                A.map(fa, f) == A.ap(fa, A.pure(f))
+            })
+
+}
--- katz/src/test/kotlin/laws/FunctorLaws.kt
+++ katz/src/test/kotlin/laws/FunctorLaws.kt
@@ -3,27 +3,30 @@ package katz
 import io.kotlintest.properties.Gen
 import io.kotlintest.properties.forAll
 
-inline fun <reified F> functorLaws(functor: Functor<F> = functor<F>()): List<Law> =
-        listOf(
-               Law("Functor Laws: Covariant Identity", { covariantIdentity(functor) }),
-               Law("Functor: Covariant Composition", {  covariantComposition(functor) })
-        )
+object FunctorLaws {
 
-inline fun <reified F> covariantIdentity(functor: Functor<F> = functor<F>()): Unit =
-        forAll(genApplicative<F, Int>(Gen.int()), { fa: HK<F, Int> ->
-            functor.map(fa, ::identity) == fa
-        })
+    inline fun <reified F> laws(functor: Functor<F> = functor<F>()): List<Law> =
+            listOf(
+                    Law("Functor Laws: Covariant Identity", { covariantIdentity(functor) }),
+                    Law("Functor: Covariant Composition", { covariantComposition(functor) })
+            )
 
-inline fun <reified F> covariantComposition(functor: Functor<F> = functor<F>()): Unit =
-        forAll(
-                genApplicative<F, Int>(Gen.int()),
-                genFunctionAToB<Int, Int>(Gen.int()),
-                genFunctionAToB<Int, Int>(Gen.int()),
-                { fa: HK<F, Int>, f, g ->
-                    functor.map(functor.map(fa, f), g) == functor.map(fa, f andThen g)
-                }
-        )
+    inline fun <reified F> covariantIdentity(functor: Functor<F> = functor<F>()): Unit =
+            forAll(genApplicative<F, Int>(Gen.int()), { fa: HK<F, Int> ->
+                functor.map(fa, ::identity) == fa
+            })
 
+    inline fun <reified F> covariantComposition(functor: Functor<F> = functor<F>()): Unit =
+            forAll(
+                    genApplicative<F, Int>(Gen.int()),
+                    genFunctionAToB<Int, Int>(Gen.int()),
+                    genFunctionAToB<Int, Int>(Gen.int()),
+                    { fa: HK<F, Int>, f, g ->
+                        functor.map(functor.map(fa, f), g) == functor.map(fa, f andThen g)
+                    }
+            )
+
+}
 
 
 
