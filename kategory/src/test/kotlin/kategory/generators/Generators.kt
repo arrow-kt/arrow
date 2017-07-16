0c019aaed05d7b4ff469bb863306fe4db8a41e33
pakoito <jueces.jus@gmail.com>
2017-07-16 22:15:08 +0100

    Add new generators

--- kategory/src/test/kotlin/kategory/generators/Generators.kt
+++ kategory/src/test/kotlin/kategory/generators/Generators.kt
@@ -2,18 +2,40 @@ package kategory
 
 import io.kotlintest.properties.Gen
 
-inline fun <reified F, A> genApplicative(valueGen: Gen<A>, AP: Applicative<F> = applicative<F>()): Gen<HK<F, A>> = object : Gen<HK<F, A>> {
-    override fun generate(): HK<F, A> = AP.pure(valueGen.generate())
-}
+inline fun <reified F, A> genApplicative(valueGen: Gen<A>, AP: Applicative<F> = applicative<F>()): Gen<HK<F, A>> =
+        object : Gen<HK<F, A>> {
+            override fun generate(): HK<F, A> = AP.pure(valueGen.generate())
+        }
 
-fun <A, B> genFunctionAToB(genB: Gen<B>): Gen<(A) -> B> = object : Gen<(A) -> B> {
-    override fun generate(): (A) -> B {
-        val v = genB.generate()
-        return { a -> v }
-    }
-}
+fun <A, B> genFunctionAToB(genB: Gen<B>): Gen<(A) -> B> =
+        object : Gen<(A) -> B> {
+            override fun generate(): (A) -> B {
+                val v = genB.generate()
+                return { _ -> v }
+            }
+        }
 
 fun genThrowable(): Gen<Throwable> = object : Gen<Throwable> {
     override fun generate(): Throwable =
             Gen.oneOf(listOf(RuntimeException(), NoSuchElementException(), IllegalArgumentException())).generate()
 }
+
+inline fun <F, A> genConstructor(valueGen: Gen<A>, crossinline cf: (A) -> HK<F, A>): Gen<HK<F, A>> =
+        object : Gen<HK<F, A>> {
+            override fun generate(): HK<F, A> = cf(valueGen.generate())
+        }
+
+fun genIntSmall(): Gen<Int> =
+        Gen.oneOf(Gen.negativeIntegers(), Gen.choose(0, Int.MAX_VALUE / 10000))
+
+fun genIntPredicate(): Gen<(Int) -> Boolean> =
+        Gen.int().let { gen ->
+            /* If you ever see two 0s in a row please contact the maintainers for a pat in the back */
+            val num = gen.generate().let { if (it == 0) gen.generate() else it }
+            Gen.oneOf(listOf<(Int) -> Boolean>(
+                    { it > num },
+                    { it <= num },
+                    { it % num == 0 },
+                    { it % num == num - 1 })
+            )
+        }
