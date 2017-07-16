d2b3fdc5b6d6e6acea597a26d1a6cb0ecc649199
pakoito <jueces.jus@gmail.com>
2017-07-16 13:53:40 +0100

    Add tests for runAsync, unsafeRunAsync, monad laws, monad and binding

--- kategory/src/test/kotlin/kategory/effects/IOTest.kt
+++ kategory/src/test/kotlin/kategory/effects/IOTest.kt
@@ -10,6 +10,12 @@ import org.junit.runner.RunWith
 class IOTest : UnitSpec() {
 
     init {
+
+        testLaws(MonadLaws.laws(IO, object : Eq<HK<IO.F, Int>> {
+            override fun eqv(a: HK<IO.F, Int>, b: HK<IO.F, Int>): Boolean =
+                a.ev().unsafeRunSync() == b.ev().unsafeRunSync()
+        }))
+
         "should defer evaluation until run" {
             var run = false
             val ioa = IO { run = true }
@@ -86,6 +92,126 @@ class IOTest : UnitSpec() {
             val value = IO.pure<Int?>(null).unsafeRunSync()
 
             value shouldBe null
+        }
+
+        "should complete when running a pure value with unsafeRunAsync" {
+            val expected = 0
+            IO.pure(expected).unsafeRunAsync { either ->
+                either.fold({ fail("") }, { it shouldBe expected })
+            }
+        }
+
+
+        "should complete when running a return value with unsafeRunAsync" {
+            val expected = 0
+            IO { expected }.unsafeRunAsync { either ->
+                either.fold({ fail("") }, { it shouldBe expected })
+            }
+        }
+
+        "should return an error when running an exception with unsafeRunAsync" {
+            IO.raiseError<Int>(MyException()).unsafeRunAsync { either ->
+                either.fold({
+                    when (it) {
+                        is MyException -> {}
+                        else -> fail("Should only throw MyException")
+                    }
+                }, { fail("") })
+            }
+        }
+
+        "should return exceptions within main block with unsafeRunAsync" {
+            val exception = MyException()
+            val ioa = IO<Int> { throw exception }
+            ioa.unsafeRunAsync { either ->
+                either.fold({ it shouldBe exception }, { fail("") })
+            }
+        }
+
+        "should not catch exceptions within run block with unsafeRunAsync" {
+            try {
+                val exception = MyException()
+                val ioa = IO<Int> { throw exception }
+                ioa.unsafeRunAsync { either ->
+                    either.fold({ throw exception }, { fail("") })
+                }
+            } catch (myException: MyException) {
+                // Success
+            } catch (throwable: Throwable) {
+                fail("Should only throw MyException")
+            }
+        }
+
+        "should complete when running a pure value with runAsync" {
+            val expected = 0
+            IO.pure(expected).runAsync { either ->
+                either.fold({ fail("") }, { IO { it shouldBe expected } })
+            }
+        }
+
+
+        "should complete when running a return value with runAsync" {
+            val expected = 0
+            IO { expected }.runAsync { either ->
+                either.fold({ fail("") }, { IO { it shouldBe expected } })
+            }
+        }
+
+        "should return an error when running an exception with runAsync" {
+            IO.raiseError<Int>(MyException()).runAsync { either ->
+                either.fold({
+                    when (it) {
+                        is MyException -> { IO { } }
+                        else -> fail("Should only throw MyException")
+                    }
+                }, { fail("") })
+            }
+        }
+
+        "should return exceptions within main block with runAsync" {
+            val exception = MyException()
+            val ioa = IO<Int> { throw exception }
+            ioa.runAsync { either ->
+                either.fold({ IO { it shouldBe exception } }, { fail("") })
+            }
+        }
+
+        "should catch exceptions within run block with runAsync" {
+            try {
+                val exception = MyException()
+                val ioa = IO<Int> { throw exception }
+                ioa.runAsync { either ->
+                    either.fold({ throw exception }, { fail("") })
+                }
+            } catch (throwable: Throwable) {
+                fail("Should catch any exception")
+            }
+        }
+
+        "should map values correctly on success" {
+            val run = IO.map(IO.pure(1)) { it + 1 }.unsafeRunSync()
+
+            val expected = 2
+
+            run shouldBe expected
+        }
+
+        "should flatMap values correctly on success" {
+            val run = IO.flatMap(IO.pure(1)) { num -> IO { num + 1 } }.unsafeRunSync()
+
+            val expected = 2
+
+            run shouldBe expected
+        }
+
+        "IO.binding should for comprehend over IO" {
+            val result = IO.binding {
+                val x = IO.pure(1).bind()
+                val y = !IO { x + 1 }
+                val z = bind { IO { y + 1 } }
+                yields(z)
+            }.ev()
+            result.unsafeRunSync() shouldBe 3
         }
     }
 }
