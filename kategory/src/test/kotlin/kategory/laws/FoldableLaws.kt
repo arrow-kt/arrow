9192c081d877c45746ad117ec0bf1307fc019394
pakoito <jueces.jus@gmail.com>
2017-07-20 23:46:18 +0100

    Fix required instances for new laws

--- kategory/src/test/kotlin/kategory/laws/FoldableLaws.kt
+++ kategory/src/test/kotlin/kategory/laws/FoldableLaws.kt
@@ -4,11 +4,11 @@ import io.kotlintest.properties.Gen
 import io.kotlintest.properties.forAll
 
 object FoldableLaws {
-    inline fun <reified F> laws(FF: Foldable<F> = foldable<F>(), crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Any?>): List<Law> =
+    inline fun <reified F> laws(FF: Foldable<F> = foldable<F>(), crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Int>): List<Law> =
             listOf(
                     Law("Foldable Laws: Left fold consistent with foldMap", { leftFoldConsistentWithFoldMap(FF, cf, EQ) }),
                     Law("Foldable Laws: Right fold consistent with foldMap", { rightFoldConsistentWithFoldMap(FF, cf, EQ) }),
-                    Law("Foldable Laws: Exists is consistent with find", { existsConsistentWithFind(FF, cf, EQ) }),
+                    Law("Foldable Laws: Exists is consistent with find", { existsConsistentWithFind(FF, cf) }),
                     Law("Foldable Laws: Exists is lazy", { existsIsLazy(FF, cf, EQ) }),
                     Law("Foldable Laws: ForAll is lazy", { forAllIsLazy(FF, cf, EQ) }),
                     Law("Foldable Laws: ForAll consistent with exists", { forallConsistentWithExists(FF, cf) }),
@@ -16,22 +16,22 @@ object FoldableLaws {
                     Law("Foldable Laws: FoldM for Id is equivalent to fold left", { foldMIdIsFoldL(FF, cf, EQ) })
             )
 
-    inline fun <reified F> leftFoldConsistentWithFoldMap(FF: Foldable<F>, crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Any?>) =
+    inline fun <reified F> leftFoldConsistentWithFoldMap(FF: Foldable<F>, crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Int>) =
             forAll(genFunctionAToB<Int, Int>(genIntSmall()), genConstructor(genIntSmall(), cf), { f: (Int) -> Int, fa: HK<F, Int> ->
                 FF.foldMap(IntMonoid, fa, f).equalUnderTheLaw(FF.foldL(fa, IntMonoid.empty(), { acc, a -> IntMonoid.combine(acc, f(a)) }), EQ)
             })
 
-    inline fun <reified F> rightFoldConsistentWithFoldMap(FF: Foldable<F>, crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Any?>) =
+    inline fun <reified F> rightFoldConsistentWithFoldMap(FF: Foldable<F>, crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Int>) =
             forAll(genFunctionAToB<Int, Int>(genIntSmall()), genConstructor(genIntSmall(), cf), { f: (Int) -> Int, fa: HK<F, Int> ->
                 FF.foldMap(IntMonoid, fa, f).equalUnderTheLaw(FF.foldR(fa, Eval.later { IntMonoid.empty() }, { a, lb: Eval<Int> -> lb.map { IntMonoid.combine(f(a), it) } }).value(), EQ)
             })
 
-    inline fun <reified F> existsConsistentWithFind(FF: Foldable<F>, crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Any?>) =
+    inline fun <reified F> existsConsistentWithFind(FF: Foldable<F>, crossinline cf: (Int) -> HK<F, Int>) =
             forAll(genIntPredicate(), genConstructor(Gen.int(), cf), { f: (Int) -> Boolean, fa: HK<F, Int> ->
-                FF.exists(fa, f).equalUnderTheLaw(FF.find(fa, f).fold({ false }, { true }), EQ)
+                FF.exists(fa, f).equalUnderTheLaw(FF.find(fa, f).fold({ false }, { true }), Eq.any())
             })
 
-    inline fun <reified F> existsIsLazy(FF: Foldable<F>, crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Any?>) =
+    inline fun <reified F> existsIsLazy(FF: Foldable<F>, crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Int>) =
             forAll(genConstructor(Gen.int(), cf), { fa: HK<F, Int> ->
                 val sideEffect = SideEffect()
                 FF.exists(fa, { _ ->
@@ -42,7 +42,7 @@ object FoldableLaws {
                 sideEffect.counter.equalUnderTheLaw(expected, EQ)
             })
 
-    inline fun <reified F> forAllIsLazy(FF: Foldable<F>, crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Any?>) =
+    inline fun <reified F> forAllIsLazy(FF: Foldable<F>, crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Int>) =
             forAll(genConstructor(Gen.int(), cf), { fa: HK<F, Int> ->
                 val sideEffect = SideEffect()
                 FF.forall(fa, { _ ->
@@ -71,7 +71,7 @@ object FoldableLaws {
                 !FF.isEmpty(fa) || FF.forall(fa, f)
             })
 
-    inline fun <reified F> foldMIdIsFoldL(FF: Foldable<F>, crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Any?>) =
+    inline fun <reified F> foldMIdIsFoldL(FF: Foldable<F>, crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Int>) =
             forAll(genFunctionAToB<Int, Int>(genIntSmall()), genConstructor(genIntSmall(), cf), { f: (Int) -> Int, fa: HK<F, Int> ->
                 val foldL: Int = FF.foldL(fa, IntMonoid.empty(), { acc, a -> IntMonoid.combine(acc, f(a)) })
                 val foldM: Int = FF.foldM(fa, IntMonoid.empty(), { acc, a -> Id(IntMonoid.combine(acc, f(a))) }, Id).value()
--- kategory/src/test/kotlin/kategory/laws/TraverseLaws.kt
+++ kategory/src/test/kotlin/kategory/laws/TraverseLaws.kt
@@ -3,13 +3,13 @@ package kategory
 import io.kotlintest.properties.forAll
 
 object TraverseLaws {
-    inline fun <reified F> laws(FF: Traverse<F> = traverse<F>(), crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Any?>): List<Law> =
-            FoldableLaws.laws(FF, cf, EQ) + listOf(
-                    Law("Traverse Laws: Identity", { identityTraverse(FF, cf, EQ) })
+    inline fun <reified F> laws(FF: Traverse<F> = traverse<F>(), AP: Applicative<F> = applicative<F>(), crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<HK<F, Int>>): List<Law> =
+            FoldableLaws.laws(FF, cf, Eq.any()) + FunctorLaws.laws(AP, EQ) + listOf(
+                    Law("Traverse Laws: Identity", { identityTraverse(FF, AP, cf, EQ) })
             )
 
-    inline fun <reified F> identityTraverse(FF: Traverse<F>, crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<Any?>) =
+    inline fun <reified F> identityTraverse(FF: Traverse<F>, AP: Applicative<F> = applicative<F>(), crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<HK<F, Int>>) =
             forAll(genFunctionAToB<Int, HK<Id.F, Int>>(genConstructor(genIntSmall(), ::Id)), genConstructor(genIntSmall(), cf), { f: (Int) -> HK<Id.F, Int>, fa: HK<F, Int> ->
-                FF.traverse(fa, f, Id).equalUnderTheLaw(FF.map(fa, f), EQ)
+                FF.traverse(fa, f, Id).value().equalUnderTheLaw(FF.map(fa, f).map(AP) { it.value() }, EQ)
             })
 }
