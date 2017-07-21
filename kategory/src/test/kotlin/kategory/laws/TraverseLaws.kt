1602798cab34124fc2b0fd218479029abb532a9e
pakoito <jueces.jus@gmail.com>
2017-07-21 01:02:54 +0100

    Fix TODOs

--- kategory/src/test/kotlin/kategory/laws/TraverseLaws.kt
+++ kategory/src/test/kotlin/kategory/laws/TraverseLaws.kt
@@ -20,10 +20,10 @@ object TraverseLaws {
     inline fun <reified F> laws(FF: Traverse<F> = traverse<F>(), AP: Applicative<F> = applicative<F>(), crossinline cf: (Int) -> HK<F, Int>, EQ: Eq<HK<F, Int>>): List<Law> =
             FoldableLaws.laws(FF, cf, Eq.any()) + FunctorLaws.laws(AP, EQ) + listOf(
                     Law("Traverse Laws: Identity", { identityTraverse(FF, AP, cf, EQ) }),
-                    // TODO (#69)
+                    // TODO (#136)
                     // Law("Traverse Laws: Sequential composition", { sequentialComposition(FF, AP, cf, EQ) })
                     Law("Traverse Laws: Parallel composition", { parallelComposition(FF, cf, EQ) })
-                    // TODO (#69)
+                    // TODO (#136)
                     // Law("Traverse Laws: FoldMap derived", { foldMapDerived(FF, AP, cf, EQ) })
             )
 
