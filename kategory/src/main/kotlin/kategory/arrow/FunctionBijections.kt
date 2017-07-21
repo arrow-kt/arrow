66d56bcf9ccf78bb12bb5fd77eced262a55ea261
pakoito <jueces.jus@gmail.com>
2017-07-21 03:08:33 +0100

    Rename to sound more mathy

--- kategory/src/main/kotlin/kategory/arrow/FunctionBijections.kt
+++ kategory/src/main/kotlin/kategory/arrow/FunctionBijections.kt
@@ -4,6 +4,6 @@ interface FunctionInject<out R> {
     operator fun <P> invoke(p: P): R
 }
 
-interface FunctionProject<in P> {
+interface FunctionSurject<in P> {
     operator fun <R> invoke(p: P): R
 }
