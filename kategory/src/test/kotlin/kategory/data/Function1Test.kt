ba17ce9dcac7e38ddb82d9a050e94ef403a5c0c2
pakoito <jueces.jus@gmail.com>
2017-07-21 02:56:24 +0100

    Add tests for Function1

---
+++ kategory/src/test/kotlin/kategory/data/Function1Test.kt
@@ -0,0 +1,25 @@
+package kategory
+
+import io.kotlintest.KTestJUnitRunner
+import io.kotlintest.matchers.shouldBe
+import org.junit.runner.RunWith
+
+@RunWith(KTestJUnitRunner::class)
+class Function1Test : UnitSpec() {
+    init {
+        testLaws(MonadLaws.laws(Function1.monad<Int>(), object : Eq<HK<Function1.F, Int>> {
+            override fun eqv(a: HK<Function1.F, Int>, b: HK<Function1.F, Int>): Boolean =
+                    a.ev()(1) == b.ev()(1)
+        }))
+
+        "Function1Monad.binding should for comprehend over all values of multiple Function0" {
+            val M = Function1.monad<Int>()
+            M.binding {
+                val x = Function1 { _: Int -> 1 }.bind()
+                val y = !Function1 { _: Int -> 2 }
+                val z = bind { Function1 { _: Int -> 3 } }
+                yields(x + y + z)
+            }.ev().invoke(0) shouldBe 6
+        }
+    }
+}
