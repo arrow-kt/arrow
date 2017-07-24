17a84d0f6d26e40ff313d9ec0883642cc555bc73
pakoito <jueces.jus@gmail.com>
2017-07-24 21:29:11 +0100

    Add evil test

--- kategory/src/test/kotlin/kategory/data/PartialFunctionTests.kt
+++ kategory/src/test/kotlin/kategory/data/PartialFunctionTests.kt
@@ -10,9 +10,9 @@ class PartialFunctionTests : UnitSpec() {
     init {
 
         "case syntax can be used to create partial functions" {
-            val predicate = {n : Int -> n > 0 }
-            val fa = case({n : Int -> n > 0 } then { it * 2 })
-            val fb = object: PartialFunction<Int, Int>() {
+            val predicate = { n: Int -> n > 0 }
+            val fa = case({ n: Int -> n > 0 } then { it * 2 })
+            val fb = object : PartialFunction<Int, Int>() {
                 override fun isDefinedAt(a: Int): Boolean = predicate(a)
                 override fun invoke(p1: Int): Int = p1 * 2
             }
@@ -23,10 +23,10 @@ class PartialFunctionTests : UnitSpec() {
         "match statements " {
             val value: Any = "1"
             val result: Int = match(value)(
-                case(typeOf<String>() then { (it).toInt() }),
-                case(typeOf<Option.Some<Int>>() then { it.value }),
-                case(typeOf<Int>() then { it }),
-                default = { 0 }
+                    case(typeOf<String>() then { (it).toInt() }),
+                    case(typeOf<Option.Some<Int>>() then { it.value }),
+                    case(typeOf<Int>() then { it }),
+                    default = { 0 }
             )
             result shouldBe 1
         }
@@ -41,6 +41,21 @@ class PartialFunctionTests : UnitSpec() {
                     default = { 0 }
             )
             result shouldBe 10
+        }
+
+        "match nested statements" {
+            val a: Option<Either<Option<String>, Int>> = "1".some().left().some()
+            val result: Int = match(a)(
+                    case(typeOf<String>() then { (it).toInt() }),
+                    case(typeOf<Option.None>() then { 3 }),
+                    case(typeOf<Option.Some<Int>>() then { it.value + 1 }),
+                    case(typeOf<Option<Either<Option<Int>, Int>>>() then { it.fold({ 111.right() }, { it }).fold({ it.fold({ 222 }, { 333 }) }, { it }) }),
+                    case(typeOf<Option<Either<Option<String>, Int>>>() then { it.fold({ 444.right() }, { it }).fold({ it.fold({ 555 }, { 666 }) }, { it }) }),
+                    case(typeOf<Option.Some<Double>>() then { it.value.toInt() }),
+                    case(typeOf<Int>() then { it }),
+                    default = { 0 }
+            )
+            result shouldBe 666
         }
 
     }
