787daa35d86bc3c9135c23e700dcfc9680bff7c2
Raul Raja <raulraja@gmail.com>
2017-07-25 23:32:11 +0200

    Removes unsound typeOf and match cases until we have implicit chains + unapply to implement Typeable/TypeCase ala Shapeless

--- kategory/src/main/kotlin/kategory/instances/IterableInstances.kt
+++ kategory/src/main/kotlin/kategory/instances/IterableInstances.kt
@@ -1,23 +1,8 @@
 package kategory
 
-tailrec fun <A, B> collectLoop(a: A, l : List<PartialFunction<A, B>>, acc: Option<B>): Option<B> =
-    when {
-        l.isEmpty() || acc.isDefined -> acc
-        else -> {
-            val head = l[0]
-            val tail = l.drop(1)
-            val result =
-                    if (Try({ head.isDefinedAt(a) }).fold({ false }, { true })) {
-                        Try({ head(a) }).fold({ e -> Option.None }, { b -> Option.Some(b) })
-                    } else { Option.None }
-            collectLoop(a, tail, result)
-        }
+fun <A: Any, B> Iterable<A>.collect(vararg cases: PartialFunction<A, B>): List<B> =
+    flatMap { a ->
+        val f: PartialFunction<A, B> = cases.reduce({ a, b -> a.orElse(b)})
+        if (f.isDefinedAt(a)) listOf(f(a))
+        else emptyList()
     }
-
-@Suppress("UNCHECKED_CAST")
-fun <A: Any, B> Iterable<A>.collect(vararg cases: PartialFunction<*, B>): List<B> {
-    return flatMap { a ->
-        val c = cases.toList() as List<PartialFunction<A, B>>
-        collectLoop(a, c, Option.None).fold({ emptyList<B>() }, { listOf(it) })
-    }
-}
