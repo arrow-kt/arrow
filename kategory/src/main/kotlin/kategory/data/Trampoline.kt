fba3bb2a1c391d8bdf41752b6ced67f6f99f4015
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-07-25 12:34:57 +0200

    rebase master and update packages

--- kategory/src/main/kotlin/kategory/data/Trampoline.kt
+++ kategory/src/main/kotlin/kategory/data/Trampoline.kt
@@ -1,5 +1,10 @@
 package kategory
 
+typealias TrampolineKind<A> = HK<Trampoline.F, A>
+
+fun <A> TrampolineKind<A>.ev(): Trampoline<A> =
+        this as Trampoline<A>
+
 /**
  * Trampoline is often used to emulate tail recursion. The idea is to have some step code that can be trampolined itself
  * to emulate recursion. The difference with standard recursion would be that there is no need to rewind the whole stack
@@ -7,7 +12,9 @@ package kategory
  * returned as the overall result value for the whole function chain. That means Trampoline emulates what tail recursion
  * does.
  */
-sealed class Trampoline<out A> {
+sealed class Trampoline<out A> : TrampolineKind<A> {
+
+    class F private constructor()
 
     class More<out A>(val f: () -> Trampoline<A>) : Trampoline<A>()
     class Done<out A>(val result: A) : Trampoline<A>()
--- kategory/src/test/kotlin/kategory/data/TrampolineTest.kt
+++ kategory/src/test/kotlin/kategory/data/TrampolineTest.kt
@@ -2,17 +2,17 @@ package kategory
 
 import io.kotlintest.KTestJUnitRunner
 import io.kotlintest.matchers.shouldBe
-import io.kotlintest.properties.forAll
-import katz.Either.Left
-import katz.Either.Right
-import katz.data.Trampoline
 import org.junit.runner.RunWith
 
 @RunWith(KTestJUnitRunner::class)
 class TrampolineTest : UnitSpec() {
     init {
-        "trampoline over 10 should return false" {
-            Trampoline.More { odd(10) }.runT() shouldBe false
+        "trampoline over 10000 should return false and not break the stack" {
+            Trampoline.More { odd(10000) }.runT() shouldBe false
+        }
+
+        "trampoline over 10001 should return true and not break the stack" {
+            Trampoline.More { odd(10001) }.runT() shouldBe true
         }
     }
 
