4c0718592fb62d8fed2525847d1b98cf10da06d6
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-07-26 20:39:08 +0200

    add semigroupK with basic law tests

---
+++ kategory/src/main/kotlin/kategory/typeclasses/SemigroupK.kt
@@ -0,0 +1,28 @@
+package kategory.typeclasses
+
+import kategory.*
+
+interface SemigroupK<F> : Typeclass {
+
+    /**
+     * Combine two F<A> values.
+     */
+    fun <A> combineK(x: HK<F, A>, y: HK<F, A>): HK<F, A>
+
+    /**
+     * Given a type A, create a concrete Semigroup<F<A>>.
+     */
+    fun <A> algebra(): Semigroup<HK<F, A>> = object : Semigroup<HK<F, A>> {
+        override fun combine(x: HK<F, A>, y: HK<F, A>): HK<F, A> =
+                combineK(x, y)
+    }
+}
+
+/**
+ * Dummy SemigroupK instance to be able to test laws.
+ */
+class OptionSemigroupK : SemigroupK<Option.F> {
+    override fun <A> combineK(x: HK<Option.F, A>, y: HK<Option.F, A>): HK<Option.F, A> {
+        return x.flatMap { y }
+    }
+}
