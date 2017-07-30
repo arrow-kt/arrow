1e9f7c4174bba8046ef900eb1f2c66dd13c4045c
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-07-30 18:40:41 +0200

    test SemigroupK laws for OptionT.semigroupK

--- kategory/src/test/kotlin/kategory/data/OptionTTest.kt
+++ kategory/src/test/kotlin/kategory/data/OptionTTest.kt
@@ -9,8 +9,18 @@ import org.junit.runner.RunWith
 class OptionTTest : UnitSpec() {
     init {
 
+        val OptionTFIdEq = object : Eq<HK<OptionTF<Id.F>, Int>> {
+            override fun eqv(a: HK<OptionTF<Id.F>, Int>, b: HK<OptionTF<Id.F>, Int>): Boolean {
+                return a.ev().value == b.ev().value
+            }
+        }
+
         testLaws(MonadLaws.laws(OptionT.monad(NonEmptyList), Eq.any()))
         testLaws(TraverseLaws.laws(OptionT.traverse(), OptionT.applicative(Id), { OptionT(Id(it.some())) }, Eq.any()))
+        testLaws(SemigroupKLaws.laws(
+                OptionT.semigroupK(Id),
+                OptionT.applicative(Id),
+                OptionTFIdEq))
         testLaws(MonoidKLaws.laws(
                 OptionT.monoidK(Id),
                 OptionT.applicative(Id),
@@ -19,11 +29,7 @@ class OptionTTest : UnitSpec() {
                         return a.ev().value == b.ev().value
                     }
                 },
-                object : Eq<HK<OptionTF<Id.F>, Int>> {
-                    override fun eqv(a: HK<OptionTF<Id.F>, Int>, b: HK<OptionTF<Id.F>, Int>): Boolean {
-                        return a.ev().value == b.ev().value
-                    }
-                }))
+                OptionTFIdEq))
 
         "map should modify value" {
             forAll { a: String ->
