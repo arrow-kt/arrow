a3f384365aed03322521e1a59103c677f6f435db
Raul Raja <raulraja@gmail.com>
2017-07-30 22:57:33 +0200

    Added automatic tests for safe and unsafe comprehensions to all monads

--- kategory/src/test/kotlin/kategory/laws/MonadLaws.kt
+++ kategory/src/test/kotlin/kategory/laws/MonadLaws.kt
@@ -12,6 +12,8 @@ object MonadLaws {
                     Law("Monad Laws: kleisli left identity", { kleisliLeftIdentity(M, EQ) }),
                     Law("Monad Laws: kleisli right identity", { kleisliRightIdentity(M, EQ) }),
                     Law("Monad Laws: map / flatMap coherence", { mapFlatMapCoherence(M, EQ) }),
+                    Law("Monad Laws: monad comprehensions", { monadComprehensions(M, EQ) }),
+                    Law("Monad Laws: stack-safe//unsafe monad comprehensions equivalence", { equivalentComprehensions(M, EQ) }),
                     Law("Monad / JVM: stack safe", { stackSafety(5000, M, EQ) }),
                     Law("Monad / JVM: stack safe comprehensions", { stackSafetyComprehensions(5000, M, EQ) })
             )
@@ -52,10 +54,39 @@ object MonadLaws {
         res.run(M).equalUnderTheLaw(M.pure(iterations), EQ)
     }
 
+    inline fun <reified F> equivalentComprehensions(M: Monad<F> = monad<F>(), EQ: Eq<HK<F, Int>>): Unit =
+            forAll(Gen.int(), { x: Int ->
+                val a = M.binding {
+                    val a = M.pure(x).bind()
+                    val b = M.pure(a + 1).bind()
+                    val c = M.pure(b + 1).bind()
+                    yields(c)
+                }
+                val b = M.bindingT {
+                    val a = M.pure(x).bind()
+                    val b = M.pure(a + 1).bind()
+                    val c = M.pure(b + 1).bind()
+                    yields(c)
+                }.run(M)
+                a.equalUnderTheLaw(b, EQ)
+                a.equalUnderTheLaw(M.pure(x + 2), EQ)
+            })
+
+    inline fun <reified F> monadComprehensions(M: Monad<F> = monad<F>(), EQ: Eq<HK<F, Int>>): Unit =
+            forAll(Gen.int(), { a: Int ->
+                M.binding {
+                    val a = M.pure(a).bind()
+                    val b = M.pure(a + 1).bind()
+                    val c = M.pure(b + 1).bind()
+                    yields(c)
+                }.equalUnderTheLaw(M.pure(a + 2), EQ)
+            })
+
     fun <F> stackSafeTestProgram(M: Monad<F>, n: Int, stopAt: Int): Free<F, Int> = M.bindingT {
         val v = M.pure(n + 1).bind()
         val r = if (v < stopAt) stackSafeTestProgram(M, v, stopAt).bind() else M.pure(v).bind()
         yields(r)
     }
+
 
 }
