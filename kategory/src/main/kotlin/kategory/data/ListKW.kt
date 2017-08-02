ef841d68c0468f1877db18985670cefdc474fef3
Marc Moreno <ignis.fatue@gmail.com>
2017-08-02 23:29:10 +0200

    using delegation to implement Collection interface

--- kategory/src/main/kotlin/kategory/data/ListKW.kt
+++ kategory/src/main/kotlin/kategory/data/ListKW.kt
@@ -5,31 +5,9 @@ typealias ListKindW<A> = HK<ListKW.F, A>
 fun <A> ListKindW<A>.ev(): ListKW<A> =
         this as ListKW<A>
 
-class ListKW<A> private constructor(private val list: List<A>) : ListKindW<A> {
-
-    companion object : ListKWInstances, GlobalInstance<Monad<ListKW.F>>() {
-        @JvmStatic fun <A> listOfK(vararg a: A): ListKW<A> = ListKW(if (a.isEmpty()) emptyList() else a.asList())
-
-        fun functor(): Functor<ListKW.F> = this
-
-        fun applicative(): Applicative<ListKW.F> = this
-
-        fun monad(): Monad<ListKW.F> = this
-
-        fun <A> semigroup(): Semigroup<ListKW<A>> = object : ListKWSemigroup<A> {}
-
-        fun semigroupK(): SemigroupK<ListKW.F> = object : ListKWSemigroupK {}
-    }
+class ListKW<A> private constructor(private val list: List<A>) : ListKindW<A>, Collection<A> by list {
 
     class F private constructor()
-
-    val size: Int = list.size
-
-    fun contains(element: A): Boolean = list.contains(element)
-
-    fun containsAll(elements: Collection<A>): Boolean = list.containsAll(elements)
-
-    fun isEmpty(): Boolean = list.isEmpty()
 
     fun <B> map(f: (A) -> B): ListKW<B> = ListKW(list.map(f))
 
@@ -54,5 +32,18 @@ class ListKW<A> private constructor(private val list: List<A>) : ListKindW<A> {
         return super.hashCode()
     }
 
-    fun iterator(): Iterator<A> = list.iterator()
+    companion object : ListKWInstances, GlobalInstance<Monad<ListKW.F>>() {
+        @JvmStatic fun <A> listOfK(vararg a: A): ListKW<A> = ListKW(if (a.isEmpty()) emptyList() else a.asList())
+
+        fun functor(): Functor<ListKW.F> = this
+
+        fun applicative(): Applicative<ListKW.F> = this
+
+        fun monad(): Monad<ListKW.F> = this
+
+        fun <A> semigroup(): Semigroup<ListKW<A>> = object : ListKWSemigroup<A> {}
+
+        fun semigroupK(): SemigroupK<ListKW.F> = object : ListKWSemigroupK {}
+    }
+
 }
