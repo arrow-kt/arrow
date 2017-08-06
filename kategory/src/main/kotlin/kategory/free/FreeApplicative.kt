f78a0bca0863874c1cd8d50398e3a6583a755690
pakoito <jueces.jus@gmail.com>
2017-08-06 20:59:22 +0100

    Add ap, map, fold, compile, flatCompile and monad

--- kategory/src/main/kotlin/kategory/free/FreeApplicative.kt
+++ kategory/src/main/kotlin/kategory/free/FreeApplicative.kt
@@ -6,37 +6,88 @@ typealias FreeApplicativeF<S> = HK<FreeApplicative.F, S>
 fun <S, A> FreeApplicativeKind<S, A>.ev(): FreeApplicative<S, A> =
         this as FreeApplicative<S, A>
 
-sealed class FreeApplicative<S, out A> : FreeKind<S, A> {
+/**
+ * See [https://github.com/edmundnoble/cats/blob/6454b4f8b7c5cefd15d8198fa7d52e46e2f45fea/docs/src/main/tut/datatypes/freeapplicative.md]
+ */
+sealed class FreeApplicative<F, out A> : FreeApplicativeKind<F, A> {
 
     class F private constructor()
 
     companion object {
-        fun <S, A> pure(a: A): FreeApplicative<S, A> =
+        fun <F, A> pure(a: A): FreeApplicative<F, A> =
                 Pure(a)
+
+        fun <F, P, A> ap(fp: FreeApplicative<F, P>, fn: FreeApplicative<F, (P) -> A>): FreeApplicative<F, A> =
+                Ap(fn, fp)
+
+        fun <F, A> lift(fa: HK<F, A>): FreeApplicative<F, A> =
+                Lift(fa)
     }
 
-    inline fun <C> fold(crossinline ifPure: (A) -> C, crossinline ifAp: (B) -> C): C =
-            when (this) {
-                is Pure -> ifPure(value)
-                is Ap<S, *, *> -> trans.map()
-            }
-
-    inline fun <B> ap(ap: FreeApplicative<S, (A)  -> B>): FreeApplicative<S, B> =
+    fun <B> ap(ap: FreeApplicative<F, (A) -> B>): FreeApplicative<F, B> =
             when (ap) {
                 is Pure -> map(ap.value)
-                is Ap<S, *, *> -> Ap(ap.value, ap(ap.trans.map { }))
+                else -> Ap(ap, this)
             }
 
-    abstract fun <C> map(f: (A) -> C): FreeApplicative<S, C>
+    fun <C> map(f: (A) -> C): FreeApplicative<F, C> =
+            when (this) {
+                is Pure -> Pure(f(value))
+                else -> Ap(Pure(f), this)
+            }
 
-    data class Pure<S, out A>(val value: A) : FreeApplicative<S, A>() {
-        override fun <C> map(f: (A) -> C): FreeApplicative<S, C> =
-                Pure(f(value))
-    }
+    fun fold(FA: Applicative<F>): HK<F, A> =
+            foldMap(FunctionK.id(), FA)
 
-    data class Ap<S, out A, B>(val value: HK<S, B>, val trans: FreeApplicative<S, (B) -> A>) : FreeApplicative<S, A>() {
-        override fun <C> map(f: (A) -> C): FreeApplicative<S, C> =
-                Ap(value, trans.map { it andThen f })
+    fun <G> compile(f: FunctionK<F, G>, GA: Applicative<G>): FreeApplicative<G, A> =
+            foldMap(object : FunctionK<F, FreeApplicativeF<G>> {
+                override fun <A> invoke(fa: HK<F, A>): FreeApplicative<G, A> =
+                        FreeApplicative.lift(f(fa)).ev()
+
+            }, object : Applicative<FreeApplicativeF<G>> {
+                override fun <A> pure(a: A): FreeApplicative<G, A> =
+                        FreeApplicative.pure(a)
+
+                override fun <A, B> ap(fa: HK<FreeApplicativeF<G>, A>, ff: HK<FreeApplicativeF<G>, (A) -> B>): FreeApplicative<G, B> =
+                        FreeApplicative.ap(fa.ev(), ff.ev())
+            }).ev()
+
+    fun <G> flatCompile(f: FunctionK<F, FreeApplicativeF<G>>, GFA: Applicative<FreeApplicativeF<G>>): FreeApplicative<G, A> =
+            foldMap(f, GFA).ev()
+
+    // TODO(paco): requires Const
+    // final def analyze[M: Monoid](f: FunctionK[F, λ[α => M]]): M
+
+    fun monad(): Free<F, A> =
+            foldMap(object : FunctionK<F, FreeF<F>> {
+                override fun <A> invoke(fa: HK<F, A>): Free<F, A> =
+                        Free.liftF(fa)
+
+            }, object : Applicative<FreeF<F>> {
+                override fun <A> pure(a: A): Free<F, A> =
+                        Free.pure(a)
+
+                override fun <A, B> ap(fa: HK<FreeF<F>, A>, ff: HK<FreeF<F>, (A) -> B>): Free<F, B> =
+                        Free.applicative<F>().ap(fa, ff).ev()
+            }).ev()
+
+    fun <G> foldMap(f: FunctionK<F, G>, GA: Applicative<G>): HK<G, A> =
+            TODO()
+
+    /** Represents a curried function `F[A => B => C => ...]`
+     * that has been constructed with chained `ap` calls.
+     * Fn#argc denotes the amount of curried params remaining.
+     */
+    private data class CurriedFunction<out G, in A, out B>(val gab: HK<G, (A) -> B>, val argc: Int)
+
+    private data class Pure<S, out A>(val value: A) : FreeApplicative<S, A>()
+
+    private data class Lift<S, out A>(val fa: HK<S, A>) : FreeApplicative<S, A>()
+
+    private data class Ap<S, P, out A>(val fn: FreeApplicative<S, (P) -> A>, val fp: FreeApplicative<S, P>) : FreeApplicative<S, A>()
+
+    override fun toString(): String {
+        return "FreeApplicative(...)"
     }
 }
 
