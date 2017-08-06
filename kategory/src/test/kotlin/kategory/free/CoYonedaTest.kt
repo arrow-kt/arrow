2fe47400957eb9db5c62d3e00dd2d64135b1eae4
pakoito <jueces.jus@gmail.com>
2017-08-05 17:09:29 -0700

    Add tests. Add function law tests.

--- kategory/src/test/kotlin/kategory/free/CoYonedaTest.kt
+++ kategory/src/test/kotlin/kategory/free/CoYonedaTest.kt
@@ -8,10 +8,26 @@ import org.junit.runner.RunWith
 class CoYonedaTest : UnitSpec() {
 
     init {
+        val EQ = object : Eq<CoYonedaKind<Id.F, Int, Int>> {
+            override fun eqv(a: CoYonedaKind<Id.F, Int, Int>, b: CoYonedaKind<Id.F, Int, Int>): Boolean =
+                    a.ev().lower(Id) == a.ev().lower(Id)
+
+        }
+        testLaws(FunctorLaws.laws(object : Applicative<CoYonedaF<Id.F, Int>> {
+            override fun <A> pure(a: A): CoYonedaKind<Id.F, Int, A> =
+                    CoYoneda.apply(Id(0), { a })
+
+            override fun <A, B> map(fa: CoYonedaKind<Id.F, Int, A>, f: (A) -> B): CoYonedaKind<Id.F, Int, B> =
+                    fa.ev().map(f)
+
+            override fun <A, B> ap(fa: CoYonedaKind<Id.F, Int, A>, ff: CoYonedaKind<Id.F, Int, (A) -> B>): CoYonedaKind<Id.F, Int, B> =
+                    throw IllegalStateException("Operation not allowed")
+        }, EQ))
+
         "map should modify the content of any HK1" {
             forAll { x: Int ->
                 val op = CoYoneda.apply(Id(x), { _ -> "" })
-                val mapped = op.map { _ -> true }.lower(functor<Id.F>())
+                val mapped = op.map { _ -> true }.lower(Id)
                 val expected = Id(true)
 
                 expected == mapped
@@ -21,17 +37,35 @@ class CoYonedaTest : UnitSpec() {
         "instance map should be consistent with CoYonedaFunctor#map" {
             forAll { x: Int ->
                 val op = CoYoneda.apply(Id(x), { _ -> "" })
-                val mapped = op.map { _ -> true }.lower(functor<Id.F>())
-                val expected = CoYoneda.functor<Id.F, Int>().map(op, { _ -> true}).ev().lower(functor<Id.F>())
+                val mapped = op.map { _ -> true }.lower(Id)
+                val expected = CoYoneda.functor<Id.F, Int>().map(op, { _ -> true }).ev().lower(Id)
 
                 expected == mapped
             }
         }
 
+        "map should retain function application ordering" {
+            forAll { x: Int ->
+                val op = CoYoneda.apply(Id(x), { it })
+                val mapped = op.map { it + 1 }.map { it * 3 }.lower(Id).ev()
+                val expected = Id((x + 1) * 3)
+
+                expected == mapped
+            }
+        }
+
+        "map should be stack-safe" {
+            fun loop(n: Int, acc: CoYoneda<Option.F, Int, Int>): CoYoneda<Option.F, Int, Int> =
+                    if (n <= 0) acc
+                    else loop(n - 1, acc.map { it + 1 })
+
+            loop(10000, CoYoneda.apply(Option.Some(1), { it })).lower(Option)
+        }
+
         "toYoneda should convert to an equivalent Yoneda" {
             forAll { x: Int ->
                 val op = CoYoneda.apply(Id(x), Int::toString)
-                val toYoneda = op.toYoneda(functor<Id.F>()).lower().ev()
+                val toYoneda = op.toYoneda(Id).lower().ev()
                 val expected = Yoneda.apply(Id(x.toString())).lower().ev()
 
                 expected == toYoneda
