9ac74986bf43cdd1a84cece222bfbb55432c4490
Raul Raja <raulraja@gmail.com>
2017-08-07 19:14:33 +0200

    @higherkind over `Function0`

--- kategory/src/main/kotlin/kategory/arrow/Function0.kt
+++ kategory/src/main/kotlin/kategory/arrow/Function0.kt
@@ -3,34 +3,30 @@ package kategory
 fun <A> (() -> A).k(): Function0<A> =
         Function0(this)
 
-fun <A> HK<Function0.F, A>.ev(): () -> A =
-        (this as Function0<A>).f
-
-operator fun <A> HK<Function0.F, A>.invoke(): A =
+operator fun <A> HK<Function0HK, A>.invoke(): A =
         (this as Function0<A>).f()
 
 // We don't want an inherited class to avoid equivalence issues, so a simple HK wrapper will do
-data class Function0<out A>(internal val f: () -> A) : HK<Function0.F, A> {
-    class F private constructor()
+@higherkind data class Function0<out A>(internal val f: () -> A) : Function0Kind<A> {
 
-    companion object : Bimonad<Function0.F>, GlobalInstance<Bimonad<Function0.F>>() {
+    companion object : Bimonad<Function0HK>, GlobalInstance<Bimonad<Function0HK>>() {
 
-        override fun <A, B> flatMap(fa: HK<Function0.F, A>, f: (A) -> HK<Function0.F, B>): Function0<B> =
-                Function0(f(fa()).ev())
+        override fun <A, B> flatMap(fa: HK<Function0HK, A>, f: (A) -> HK<Function0HK, B>): Function0<B> =
+                Function0(f(fa()).ev().f)
 
-        override fun <A, B> coflatMap(fa: HK<Function0.F, A>, f: (HK<Function0.F, A>) -> B): Function0<B> =
+        override fun <A, B> coflatMap(fa: HK<Function0HK, A>, f: (HK<Function0HK, A>) -> B): Function0<B> =
                 { f(fa) }.k()
 
         override fun <A> pure(a: A): Function0<A> =
                 { a }.k()
 
-        override fun <A> extract(fa: HK<Function0.F, A>): A =
+        override fun <A> extract(fa: HK<Function0HK, A>): A =
                 fa()
 
-        override fun <A, B> map(fa: HK<F, A>, f: (A) -> B): Function0<B> =
+        override fun <A, B> map(fa: HK<Function0HK, A>, f: (A) -> B): Function0<B> =
                 pure(f(fa()))
 
-        tailrec fun <A, B> loop(a: A, f: (A) -> HK<F, Either<A, B>>): B {
+        tailrec fun <A, B> loop(a: A, f: (A) -> HK<Function0HK, Either<A, B>>): B {
             val fa = f(a).ev()()
             return when (fa) {
                 is Either.Right<A, B> -> fa.b
@@ -38,13 +34,13 @@ data class Function0<out A>(internal val f: () -> A) : HK<Function0.F, A> {
             }
         }
 
-        override fun <A, B> tailRecM(a: A, f: (A) -> HK<F, Either<A, B>>): Function0<B> =
+        override fun <A, B> tailRecM(a: A, f: (A) -> HK<Function0HK, Either<A, B>>): Function0<B> =
                 { loop(a, f) }.k()
 
-        fun bimonad(): Bimonad<Function0.F> = this
+        fun bimonad(): Bimonad<Function0HK> = this
 
-        fun monad(): Monad<Function0.F> = this
+        fun monad(): Monad<Function0HK> = this
 
-        fun comonad(): Comonad<Function0.F> = this
+        fun comonad(): Comonad<Function0HK> = this
     }
 }
