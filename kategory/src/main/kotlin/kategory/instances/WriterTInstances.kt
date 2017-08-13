53299fdfeadbfa95491292ca7665a77db9dd462a
Nicolás Patarino <npatarino@gmail.com>
2017-08-14 00:43:49 +0200

    Replace WriterF for WriterKindPartial

--- kategory/src/main/kotlin/kategory/instances/WriterTInstances.kt
+++ kategory/src/main/kotlin/kategory/instances/WriterTInstances.kt
@@ -1,21 +1,21 @@
 package kategory
 
 interface WriterTInstances<F, W> :
-        Functor<WriterF<F, W>>,
-        Applicative<WriterF<F, W>>,
-        Monad<WriterF<F, W>> {
+        Functor<WriterTKindPartial<F, W>>,
+        Applicative<WriterTKindPartial<F, W>>,
+        Monad<WriterTKindPartial<F, W>> {
 
     fun MM(): Monad<F>
 
     fun SG(): Monoid<W>
 
-    override fun <A> pure(a: A): HK<WriterF<F, W>, A> = WriterT(MM(), MM().pure(SG().empty() toT a))
+    override fun <A> pure(a: A): HK<WriterTKindPartial<F, W>, A> = WriterT(MM(), MM().pure(SG().empty() toT a))
 
-    override fun <A, B> map(fa: HK<WriterF<F, W>, A>, f: (A) -> B): HK<WriterF<F, W>, B> = fa.ev().map { f(it) }
+    override fun <A, B> map(fa: HK<WriterTKindPartial<F, W>, A>, f: (A) -> B): HK<WriterTKindPartial<F, W>, B> = fa.ev().map { f(it) }
 
-    override fun <A, B> flatMap(fa: WriterTKind<F, W, A>, f: (A) -> HK<WriterF<F, W>, B>): WriterT<F, W, B> = fa.ev().flatMap({ f(it).ev() }, SG())
+    override fun <A, B> flatMap(fa: WriterTKind<F, W, A>, f: (A) -> HK<WriterTKindPartial<F, W>, B>): WriterT<F, W, B> = fa.ev().flatMap({ f(it).ev() }, SG())
 
-    override fun <A, B> tailRecM(a: A, f: (A) -> HK<WriterF<F, W>, Either<A, B>>): WriterT<F, W, B> =
+    override fun <A, B> tailRecM(a: A, f: (A) -> HK<WriterTKindPartial<F, W>, Either<A, B>>): WriterT<F, W, B> =
             WriterT(MM(), MM().tailRecM(a, {
                 MM().map(f(it).ev().value) {
                     when (it.b) {
@@ -27,18 +27,18 @@ interface WriterTInstances<F, W> :
 
 }
 
-interface WriterTSemigroupK<F, W> : SemigroupK<WriterF<F, W>> {
+interface WriterTSemigroupK<F, W> : SemigroupK<WriterTKindPartial<F, W>> {
 
     fun MF(): Monad<F>
 
     fun F0(): SemigroupK<F>
 
-    override fun <A> combineK(x: HK<WriterF<F, W>, A>, y: HK<WriterF<F, W>, A>): WriterT<F, W, A> = WriterT(MF(), F0().combineK(x.ev().value, y.ev().value))
+    override fun <A> combineK(x: HK<WriterTKindPartial<F, W>, A>, y: HK<WriterTKindPartial<F, W>, A>): WriterT<F, W, A> = WriterT(MF(), F0().combineK(x.ev().value, y.ev().value))
 }
 
-interface WriterTMonoidK<F, W> : MonoidK<WriterF<F, W>>, WriterTSemigroupK<F, W> {
+interface WriterTMonoidK<F, W> : MonoidK<WriterTKindPartial<F, W>>, WriterTSemigroupK<F, W> {
 
     override fun F0(): MonoidK<F>
 
-    override fun <A> empty(): HK<WriterF<F, W>, A> = WriterT(MF(), F0().empty())
+    override fun <A> empty(): HK<WriterTKindPartial<F, W>, A> = WriterT(MF(), F0().empty())
 }
