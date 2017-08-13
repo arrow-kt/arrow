6e6e795c18f46e1273001f8c765541e2cc8f64a0
pakoito <jueces.jus@gmail.com>
2017-08-13 20:13:54 +0100

    Fix detekt

--- kategory-effects/src/main/kotlin/kategory/effects/instances/IOInstances.kt
+++ kategory-effects/src/main/kotlin/kategory/effects/instances/IOInstances.kt
@@ -13,14 +13,11 @@ interface IOInstances :
 
     override fun <A, B> flatMap(fa: IOKind<A>, f: (A) -> IOKind<B>): IO<B> = fa.ev().flatMap { f(it).ev() }
 
-    override fun <A> raiseError(e: Throwable): IO<A> =
-            RaiseError(e)
+    override fun <A> raiseError(e: Throwable): IO<A> = RaiseError(e)
 
-    override fun <A> handleErrorWith(fa: HK<IOHK, A>, f: (Throwable) -> HK<IOHK, A>): HK<IOHK, A> =
-            fa.ev().attempt().flatMap { it.fold(f, { IO.pure(it) }).ev() }
+    override fun <A> handleErrorWith(fa: HK<IOHK, A>, f: (Throwable) -> HK<IOHK, A>): HK<IOHK, A> = fa.ev().attempt().flatMap { it.fold(f, { IO.pure(it) }).ev() }
 
-    override fun <A> runAsync(fa: Proc<A>): IO<A> =
-            IO.async(fa)
+    override fun <A> runAsync(fa: Proc<A>): IO<A> = IO.async(fa)
 
     tailrec override fun <A, B> tailRecM(a: A, f: (A) -> IOKind<Either<A, B>>): IO<B> =
             f(a).ev().flatMap {
