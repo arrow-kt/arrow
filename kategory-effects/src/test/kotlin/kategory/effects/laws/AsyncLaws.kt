dca912a34548045c9e39e35e5a5a055786176c23
pakoito <jueces.jus@gmail.com>
2017-08-13 21:00:38 +0100

    Add laws for new coroutine binding

--- kategory-effects/src/test/kotlin/kategory/effects/laws/AsyncLaws.kt
+++ kategory-effects/src/test/kotlin/kategory/effects/laws/AsyncLaws.kt
@@ -9,6 +9,9 @@ object AsyncLaws {
                     Law("Async Laws: success equivalence", { asyncSuccess(AC, M, EQ) }),
                     Law("Async Laws: error equivalence", { asyncError(AC, M, EQER) }),
                     Law("Async bind: binding blocks", { asyncBind(AC, M, EQ) }),
+                    Law("Async bind: binding failure", { asyncBindError(AC, M, EQER) }),
+                    Law("Async bind: unsafe binding", { asyncBindUnsafe(AC, M, EQ) }),
+                    Law("Async bind: unsafe binding failure", { asyncBindUnsafeError(AC, M, EQ) }),
                     Law("Async bind: binding in parallel", { asyncParallelBind(AC, M, EQ) })
             )
 
@@ -24,13 +27,43 @@ object AsyncLaws {
 
     inline fun <reified F> asyncBind(AC: AsyncContext<F> = asyncContext(), M: MonadError<F, Throwable> = monadError<F, Throwable>(), EQ: Eq<HK<F, Int>>): Unit =
             forAll(genIntSmall(), genIntSmall(), genIntSmall(), { x: Int, y: Int, z: Int ->
-                val bound = M.binding {
-                    val a = bindAsync(AC) { x }
-                    val b = bindAsync(AC) { a + y }
-                    val c = bindAsync(AC) { b + z }
+                val bound = AC.bindingAsync(M) {
+                    val a = bindAsync { x }
+                    val b = bindAsync { a + y }
+                    val c = bindAsync { b + z }
                     yields(c)
                 }
                 bound.equalUnderTheLaw(M.pure<Int>(x + y + z), EQ)
+            })
+
+    inline fun <reified F> asyncBindError(AC: AsyncContext<F> = asyncContext(), M: MonadError<F, Throwable> = monadError<F, Throwable>(), EQ: Eq<HK<F, Int>>): Unit =
+            forAll(genIntSmall(), genIntSmall(), genIntSmall(), { x: Int, y: Int, z: Int ->
+                val bound = AC.bindingAsync(M) {
+                    val a = bindAsync { x }
+                    val b = bindAsync { a + y }
+                    val c = bindAsync { b + z }
+                    yields(c)
+                }
+                bound.equalUnderTheLaw(M.pure<Int>(x + y + z), EQ)
+            })
+
+    inline fun <reified F> asyncBindUnsafe(AC: AsyncContext<F> = asyncContext(), M: MonadError<F, Throwable> = monadError<F, Throwable>(), EQ: Eq<HK<F, Int>>): Unit =
+            forAll(genIntSmall(), genIntSmall(), genIntSmall(), { x: Int, y: Int, z: Int ->
+                val bound = AC.bindingAsync(M) {
+                    val a = bindAsyncUnsafe { x.right() }
+                    val b = bindAsyncUnsafe { (a + y).right() }
+                    val c = bindAsyncUnsafe { (b + z).right() }
+                    yields(c)
+                }
+                bound.equalUnderTheLaw(M.pure<Int>(x + y + z), EQ)
+            })
+
+    inline fun <reified F> asyncBindUnsafeError(AC: AsyncContext<F> = asyncContext(), M: MonadError<F, Throwable> = monadError<F, Throwable>(), EQ: Eq<HK<F, Int>>): Unit =
+            forAll(genThrowable(), { e: Throwable ->
+                val bound: HK<F, Int> = AC.bindingAsync(M) {
+                    bindAsyncUnsafe { e.left() }
+                }
+                bound.equalUnderTheLaw(M.raiseError<Int>(e), EQ)
             })
 
     inline fun <reified F> asyncParallelBind(AC: AsyncContext<F> = asyncContext(), M: MonadError<F, Throwable> = monadError<F, Throwable>(), EQ: Eq<HK<F, Int>>): Unit =
