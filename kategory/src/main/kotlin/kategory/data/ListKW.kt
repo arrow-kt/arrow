53798ceb5836599b350e1686b11184b3c68157b3
Raul Raja <raulraja@gmail.com>
2017-08-14 23:31:44 +0200

    fixes Detekt's bullshit

--- kategory/src/main/kotlin/kategory/data/ListKW.kt
+++ kategory/src/main/kotlin/kategory/data/ListKW.kt
@@ -1,50 +1,39 @@
 package kategory
 
-typealias ListKindW<A> = HK<ListKW.F, A>
-
-fun <A> ListKindW<A>.ev(): ListKW<A> = this as ListKW<A>
-
 @higherkind data class ListKW<A> constructor(val list: List<A>) : ListKWKind<A> {
-
-    class F private constructor()
 
     fun <B> map(f: (A) -> B): ListKW<B> = ListKW(list.map(f))
 
-    fun <B> flatMap(f: (A) -> ListKW<B>): ListKW<B> =
-            ListKW(list.flatMap { f(it).list })
+    fun <B> flatMap(f: (A) -> ListKW<B>): ListKW<B> = ListKW(list.flatMap { f(it).list })
 
-    operator fun plus(list: List<A>): ListKW<A> =
-            ListKW(this.list + list)
+    operator fun plus(list: List<A>): ListKW<A> = ListKW(this.list + list)
 
-    operator fun plus(listKW: ListKW<A>): ListKW<A> =
-            ListKW(this.list + listKW.list)
+    operator fun plus(listKW: ListKW<A>): ListKW<A> = ListKW(this.list + listKW.list)
 
-    operator fun get(position: Int): Option<A> =
-            if (list.isEmpty() || position < 0 || position > list.size) Option.None else Option.Some(list[position])
+    operator fun get(position: Int): Option<A> = if (list.isEmpty() || position < 0 || position > list.size) Option.None else Option.Some(list[position])
 
-    fun <B> fold(b: B, f: (B, A) -> B): B =
-            list.fold(b, f)
+    fun <B> fold(b: B, f: (B, A) -> B): B = list.fold(b, f)
 
-    companion object : ListKWInstances, GlobalInstance<Monad<ListKW.F>>() {
+    companion object : ListKWInstances, GlobalInstance<Monad<ListKWHK>>() {
 
         @JvmStatic fun <A> listOfK(vararg a: A): ListKW<A> = ListKW(a.asList())
         @JvmStatic fun <A> listOfK(list: List<A>): ListKW<A> = ListKW(list)
 
-        fun functor(): Functor<ListKW.F> = this
+        fun functor(): Functor<ListKWHK> = this
 
-        fun applicative(): Applicative<ListKW.F> = this
+        fun applicative(): Applicative<ListKWHK> = this
 
-        fun monad(): Monad<ListKW.F> = this
+        fun monad(): Monad<ListKWHK> = this
 
         fun <A> semigroup(): Semigroup<ListKW<A>> = object : ListKWMonoid<A> {}
 
-        fun semigroupK(): SemigroupK<ListKW.F> = object : ListKWMonoidK {}
+        fun semigroupK(): SemigroupK<ListKWHK> = object : ListKWMonoidK {}
 
         fun <A> monoid(): ListKWMonoid<A> = object : ListKWMonoid<A> {}
 
-        fun monoidK(): MonoidK<ListKW.F> = object : ListKWMonoidK {}
+        fun monoidK(): MonoidK<ListKWHK> = object : ListKWMonoidK {}
 
-        fun traverse(): Traverse<ListKW.F> = this
+        fun traverse(): Traverse<ListKWHK> = this
 
     }
 
--- kategory/src/main/kotlin/kategory/instances/ListKWInstances.kt
+++ kategory/src/main/kotlin/kategory/instances/ListKWInstances.kt
@@ -1,25 +1,27 @@
 package kategory
 
 interface ListKWInstances :
-        Functor<ListKW.F>,
-        Applicative<ListKW.F>,
-        Monad<ListKW.F>,
-        Traverse<ListKW.F> {
+        Functor<ListKWHK>,
+        Applicative<ListKWHK>,
+        Monad<ListKWHK>,
+        Traverse<ListKWHK> {
 
     override fun <A> pure(a: A): ListKW<A> = ListKW.listOfK(a)
 
-    override fun <A, B> flatMap(fa: HK<ListKW.F, A>, f: (A) -> HK<ListKW.F, B>): ListKW<B> = fa.ev().flatMap { f(it).ev() }
+    override fun <A, B> flatMap(fa: HK<ListKWHK, A>, f: (A) -> HK<ListKWHK, B>): ListKW<B> = fa.ev().flatMap { f(it).ev() }
 
-    override fun <A, B> map(fa: HK<ListKW.F, A>, f: (A) -> B): HK<ListKW.F, B> =
-            fa.ev().map(f)
+    override fun <A, B> map(fa: HK<ListKWHK, A>, f: (A) -> B): HK<ListKWHK, B> = fa.ev().map(f)
 
-    override fun <A, B, Z> map2(fa: HK<ListKW.F, A>, fb: HK<ListKW.F, B>, f: (Tuple2<A, B>) -> Z): HK<ListKW.F, Z> =
-            fa.ev().flatMap { a -> fb.ev().map { b -> f(Tuple2(a, b)) } }
+    override fun <A, B, Z> map2(fa: HK<ListKWHK, A>, fb: HK<ListKWHK, B>, f: (Tuple2<A, B>) -> Z): HK<ListKWHK, Z> =
+        fa.ev().flatMap { a ->
+            fb.ev().map { b ->
+                f(Tuple2(a, b))
+            }
+        }
 
-    override fun <A, B> foldL(fa: HK<ListKW.F, A>, b: B, f: (B, A) -> B): B =
-            fa.ev().fold(b, f)
+    override fun <A, B> foldL(fa: HK<ListKWHK, A>, b: B, f: (B, A) -> B): B = fa.ev().fold(b, f)
 
-    override fun <A, B> foldR(fa: HK<ListKW.F, A>, lb: Eval<B>, f: (A, Eval<B>) -> Eval<B>): Eval<B> {
+    override fun <A, B> foldR(fa: HK<ListKWHK, A>, lb: Eval<B>, f: (A, Eval<B>) -> Eval<B>): Eval<B> {
         fun loop(fa_p: ListKW<A>): Eval<B> = when {
             fa_p.isEmpty() -> lb
             else -> f(fa_p.first(), Eval.defer { loop(fa_p.drop(1)) })
@@ -27,7 +29,7 @@ interface ListKWInstances :
         return Eval.defer { loop(fa.ev()) }
     }
 
-    override fun <G, A, B> traverse(fa: HK<ListKW.F, A>, f: (A) -> HK<G, B>, GA: Applicative<G>): HK<G, HK<ListKW.F, B>> =
+    override fun <G, A, B> traverse(fa: HK<ListKWHK, A>, f: (A) -> HK<G, B>, GA: Applicative<G>): HK<G, HK<ListKWHK, B>> =
             foldR(fa, Eval.always { GA.pure(ListKW.listOfK<B>()) }) { a, eval ->
                 GA.map2Eval(f(a), eval) { ListKW.listOfK(it.a) + it.b }
             }.value()
@@ -35,7 +37,7 @@ interface ListKWInstances :
     @Suppress("UNCHECKED_CAST")
     private tailrec fun <A, B> go(
             buf: ArrayList<B>,
-            f: (A) -> HK<ListKW.F, Either<A, B>>,
+            f: (A) -> HK<ListKWHK, Either<A, B>>,
             v: ListKW<Either<A, B>>) {
         if (!v.isEmpty()) {
             val head: Either<A, B> = v.first()
@@ -49,7 +51,7 @@ interface ListKWInstances :
         }
     }
 
-    override fun <A, B> tailRecM(a: A, f: (A) -> HK<ListKW.F, Either<A, B>>): ListKW<B> {
+    override fun <A, B> tailRecM(a: A, f: (A) -> HK<ListKWHK, Either<A, B>>): ListKW<B> {
         val buf = ArrayList<B>()
         go(buf, f, f(a).ev())
         return ListKW.listOfK(buf)
@@ -62,8 +64,8 @@ interface ListKWMonoid<A> : Monoid<ListKW<A>> {
     override fun empty(): ListKW<A> = ListKW.listOfK()
 }
 
-interface ListKWMonoidK : MonoidK<ListKW.F> {
-    override fun <A> combineK(x: HK<ListKW.F, A>, y: HK<ListKW.F, A>): ListKW<A> = x.ev() + y.ev()
+interface ListKWMonoidK : MonoidK<ListKWHK> {
+    override fun <A> combineK(x: HK<ListKWHK, A>, y: HK<ListKWHK, A>): ListKW<A> = x.ev() + y.ev()
 
-    override fun <A> empty(): HK<ListKW.F, A> = ListKW.listOfK()
+    override fun <A> empty(): HK<ListKWHK, A> = ListKW.listOfK()
 }
