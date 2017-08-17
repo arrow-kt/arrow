f7542f6a74052c00a4cd961c6a52f00410489a30
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-08-17 13:50:05 +0200

    move WriterTMonadWriter to object to be able to reify types

--- kategory/src/main/kotlin/kategory/instances/WriterTInstances.kt
+++ kategory/src/main/kotlin/kategory/instances/WriterTInstances.kt
@@ -27,19 +27,20 @@ interface WriterTInstances<F, W> :
 
 }
 
-interface WriterTMonadWriter<F, W> : MonadWriter<WriterTKindPartial<F, W>, W> {
+abstract class WriterTMonadWriter<F, W> : MonadWriter<WriterTKindPartial<F, W>, W> {
 
-    fun MF(): Monad<F>
+    companion object {
 
-    override fun <A> writer(aw: Tuple2<W, A>): WriterT<F, W, A> = WriterT.put(aw.b, aw.a)
+        inline fun <reified F, W, A> writer(aw: Tuple2<W, A>): WriterT<F, W, A> = WriterT.put(aw.b, aw.a)
 
-    override fun <A> listen(fa: HK<WriterTKindPartial<F, W>, A>): HK<WriterTKindPartial<F, W>, Tuple2<W, A>> =
-            WriterT(MF(), MF().flatMap(fa.ev().content(), { a -> MF().map(fa.ev().write(), { l -> Tuple2(l, Tuple2(l, a)) }) }))
+        inline fun <reified F, W, A> listen(fa: HK<WriterTKindPartial<F, W>, A>, MF: Monad<F> = monad()): HK<WriterTKindPartial<F, W>, Tuple2<W, A>> =
+                WriterT(MF, MF.flatMap(fa.ev().content(), { a -> MF.map(fa.ev().write(), { l -> Tuple2(l, Tuple2(l, a)) }) }))
 
-    override fun <A> pass(fa: HK<WriterTKindPartial<F, W>, Tuple2<(W) -> W, A>>): HK<WriterTKindPartial<F, W>, A> =
-            WriterT(MF(), MF().flatMap(fa.ev().content(), { tuple2FA -> MF().map(fa.ev().write(), { l -> Tuple2(tuple2FA.a(l), tuple2FA.b) }) }))
+        inline fun <reified F, W, A> pass(fa: HK<WriterTKindPartial<F, W>, Tuple2<(W) -> W, A>>, MF: Monad<F> = monad()): HK<WriterTKindPartial<F, W>, A> =
+                WriterT(MF, MF.flatMap(fa.ev().content(), { tuple2FA -> MF.map(fa.ev().write(), { l -> Tuple2(tuple2FA.a(l), tuple2FA.b) }) }))
 
-    override fun tell(w: W): HK<WriterTKindPartial<F, W>, Unit> = WriterT.tell(w)
+        inline fun <reified F, W, A> tell(w: W): HK<WriterTKindPartial<F, W>, Unit> = WriterT.tell(w)
+    }
 }
 
 interface WriterTSemigroupK<F, W> : SemigroupK<WriterTKindPartial<F, W>> {
