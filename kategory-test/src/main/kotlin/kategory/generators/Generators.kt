2087325171461b3ecbffa437ff527c62b1e115a4
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-08-18 16:29:29 +0200

    finished writing laws for existent Reducible methods

--- kategory-test/src/main/kotlin/kategory/generators/Generators.kt
+++ kategory-test/src/main/kotlin/kategory/generators/Generators.kt
@@ -16,6 +16,26 @@ fun <A, B> genFunctionAToB(genB: Gen<B>): Gen<(A) -> B> =
             }
         }
 
+fun <A> genFunctionAAToA(genA: Gen<A>): Gen<(A, A) -> A> =
+        object : Gen<(A, A) -> A> {
+            override fun generate(): (A, A) -> A {
+                val v = genA.generate()
+                return { _, _ -> v }
+            }
+        }
+
+fun <A, B> genFunctionABToB(genB: Gen<B>): Gen<(A, B) -> B> =
+        object : Gen<(A, B) -> B> {
+            override fun generate(): (A, B) -> B {
+                val v = genB.generate()
+                return { _, _ -> v }
+            }
+        }
+
+fun <A> genEval(genA: Gen<A>): Gen<Eval<A>> = object : Gen<Eval<A>> {
+    override fun generate(): Eval<A> = Eval.Always({ genA.generate() })
+}
+
 fun genThrowable(): Gen<Throwable> = object : Gen<Throwable> {
     override fun generate(): Throwable =
             Gen.oneOf(listOf(RuntimeException(), NoSuchElementException(), IllegalArgumentException())).generate()
@@ -30,7 +50,7 @@ inline fun <F, A> genConstructor(valueGen: Gen<A>, crossinline cf: (A) -> HK<F, 
 fun genIntSmall(): Gen<Int> =
         Gen.oneOf(Gen.negativeIntegers(), Gen.choose(0, Int.MAX_VALUE / 10000))
 
-fun <A, B> genTuple(genA : Gen<A>, genB: Gen<B>): Gen<Tuple2<A, B>> =
+fun <A, B> genTuple(genA: Gen<A>, genB: Gen<B>): Gen<Tuple2<A, B>> =
         object : Gen<Tuple2<A, B>> {
             override fun generate(): Tuple2<A, B> = Tuple2(genA.generate(), genB.generate())
         }
