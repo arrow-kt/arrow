7cfb00ef78f2db7a7332c814d5d69526bf6f21c4
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-08-20 13:41:19 +0200

    fix detekt

--- kategory/src/main/kotlin/kategory/typeclasses/Eq.kt
+++ kategory/src/main/kotlin/kategory/typeclasses/Eq.kt
@@ -6,7 +6,7 @@ interface Eq<in F> : Typeclass {
     fun neqv(a: F, b: F): Boolean = !eqv(a, b)
 
     companion object {
-        fun  any(): Eq<Any?> = EqAny
+        fun any(): Eq<Any?> = EqAny
 
         object EqAny : Eq<Any?> {
             override fun eqv(a: Any?, b: Any?): Boolean = a == b
@@ -18,6 +18,6 @@ interface Eq<in F> : Typeclass {
 
 inline fun <reified F> eq(): Eq<F> = instance(InstanceParametrizedType(Eq::class.java, listOf(F::class.java)))
 
-inline fun <reified F> F.eqv(EQ: Eq<F> = eq(), b: F) : Boolean = EQ.eqv(this, b)
+inline fun <reified F> F.eqv(EQ: Eq<F> = eq(), b: F): Boolean = EQ.eqv(this, b)
 
-inline fun <reified F> F.neqv(EQ: Eq<F> = eq(), b: F) : Boolean = EQ.neqv(this, b)
+inline fun <reified F> F.neqv(EQ: Eq<F> = eq(), b: F): Boolean = EQ.neqv(this, b)
