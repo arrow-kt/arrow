c99c85eeacb2585021aafc7d99ea472b9629f03f
pakoito <jueces.jus@gmail.com>
2017-08-20 20:04:19 +0100

    Fix detekt

--- kategory/src/main/java/kategory/Convert.kt
+++ kategory/src/main/java/kategory/Convert.kt
@@ -4,11 +4,9 @@ import io.kindedj.HK as HK_J
 
 class ConvertHK private constructor()
 
-fun <F, A> HK<HK<ConvertHK, F>, A>.convert(): HK_J<F, A> =
-        (this as Convert.FromKindedJToKategory<F, A>).asKindedJ()
+fun <F, A> HK<HK<ConvertHK, F>, A>.convert(): HK_J<F, A> = (this as Convert.FromKindedJToKategory<F, A>).asKindedJ()
 
-fun <F, A> HK_J<HK_J<ConvertHK, F>, A>.convert(): HK<F, A> =
-        (this as Convert.FromKategoryToKindedJ<F, A>).asKategory()
+fun <F, A> HK_J<HK_J<ConvertHK, F>, A>.convert(): HK<F, A> = (this as Convert.FromKategoryToKindedJ<F, A>).asKategory()
 
 fun <F, A> HK_J<F, A>.fromKindedJ(): Convert.FromKindedJToKategory<F, A> = Convert.fromKindedJ(this)
 
