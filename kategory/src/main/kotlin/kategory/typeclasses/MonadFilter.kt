160708e483d3cd22e470f58e2c267d9cfb10339d
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-08-23 16:32:43 +0200

    create MonadFilter class

---
+++ kategory/src/main/kotlin/kategory/typeclasses/MonadFilter.kt
@@ -0,0 +1,15 @@
+package kategory
+
+/**
+ * a Monad equipped with an additional method which allows us to create an "empty" value for the Monad
+ * (for whatever "empty" makes sense for that particular monad). This is of particular interest to us
+ * since it allows us to add a filter method to a Monad, which is used when pattern matching or
+ * using guards in for comprehensions.
+ */
+interface MonadFilter<F> : Monad<F>, FunctorFilter<F>, Typeclass {
+
+    fun <A> empty(): HK<F, A>
+
+    override fun <A, B> mapFilter(fa: HK<F, A>, f: (A) -> Option<B>): HK<F, B> =
+            flatMap(fa, { a -> f(a).fold({ empty<B>() }, { pure(it) }) })
+}
