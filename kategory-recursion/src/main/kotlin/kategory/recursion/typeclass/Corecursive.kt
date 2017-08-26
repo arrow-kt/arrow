4c849c1f11220df8c9a8d1f0ebce482f84e5c2f3
pakoito <jueces.jus@gmail.com>
2017-08-26 23:48:36 +0100

    Add cataM and anaM

--- kategory-recursion/src/main/kotlin/kategory/recursion/typeclass/Corecursive.kt
+++ kategory-recursion/src/main/kotlin/kategory/recursion/typeclass/Corecursive.kt
@@ -7,4 +7,7 @@ interface Corecursive<F, G> {
 
     fun <A> ana(a: A, f: Coalgebra<ComposedType<F, G>, A>, FF: Functor<F>): HK<F, G> =
             hylo(a, { embedT(it.lift()) }, f, ComposedFunctor(FF, FG()))
+
+    fun <M, A> anaM(a: A, f: CoalgebraM<M, ComposedType<F, G>, A>, AG: Applicative<G>, TF: Traverse<F>, TG: Traverse<G>, MM: Monad<M>): HK<M, HK<F, G>> =
+            hyloM(a, { MM.pure(embedT(it.lift())) }, f, ComposedTraverse(TF, TG, AG), MM)
 }
--- kategory-recursion/src/main/kotlin/kategory/recursion/typeclass/Recursive.kt
+++ kategory-recursion/src/main/kotlin/kategory/recursion/typeclass/Recursive.kt
@@ -5,4 +5,7 @@ interface Recursive<F, G> : Typeclass {
 
     fun <A> cata(t: HK<F, G>, f: Algebra<ComposedType<F, G>, A>, FF: Functor<F>, FG: Functor<G>): A =
             hylo(t, f, this::projectT, ComposedFunctor(FF, FG))
+
+    fun <M, A> cataM(t: HK<F, G>, f: AlgebraM<M, ComposedType<F, G>, A>, FF: Functor<F>, FG: Monad<G>, TF: Traverse<F>, TG: Traverse<G>, MM: Monad<M>): HK<M, A> =
+            cata(t, { MM.flatMap(ComposedTraverse(TF, TG, FG).sequence(MM, it), f) }, FF, FG)
 }
