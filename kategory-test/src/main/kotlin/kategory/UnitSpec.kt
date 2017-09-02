130e1908dc1a0ec98d7e8294fcb01d38cc0c1917
pakoito <jueces.jus@gmail.com>
2017-09-02 18:37:16 +0100

    Move logged EQ to an extension function available only on the spec

--- kategory-test/src/main/kotlin/kategory/UnitSpec.kt
+++ kategory-test/src/main/kotlin/kategory/UnitSpec.kt
@@ -32,10 +32,19 @@ abstract class UnitSpec : StringSpec() {
     }
 
     fun testLaws(laws: List<Law>): List<TestCase> =
-        laws.map { law ->
-            val tc = TestCase(suite = rootTestSuite, name = law.name, test = law.test, config = defaultTestCaseConfig)
-            rootTestSuite.addTestCase(tc)
-            tc
-        }
+            laws.map { law: Law ->
+                val tc = TestCase(suite = rootTestSuite, name = law.name, test = law.test, config = defaultTestCaseConfig)
+                rootTestSuite.addTestCase(tc)
+                tc
+            }
 
+    inline fun <F> Eq<F>.logged(): Eq<F> = object : Eq<F> {
+        override fun eqv(a: F, b: F): Boolean {
+            val result = this@logged.eqv(a, b)
+            if (!result) {
+                println("$a <---> $b")
+            }
+            return result
+        }
+    }
 }
