9f9a4b83f388857c1ae83392233f4e69445debd6
pakoito <jueces.jus@gmail.com>
2017-09-02 23:13:13 +0100

    Use alias in FixInstances

--- kategory-recursion/src/main/kotlin/kategory/recursion/instances/FixInstances.kt
+++ kategory-recursion/src/main/kotlin/kategory/recursion/instances/FixInstances.kt
@@ -3,9 +3,9 @@ package kategory
 interface FixInstances<F> : Birecursive<FixHK, F> {
     fun FG(): Functor<F>
 
-    override fun projectT(fg: HK<FixHK, F>): HK<Nested<FixHK, F>, FixKind<F>> =
+    override fun projectT(fg: FixKind<F>): HK<Nested<FixHK, F>, FixKind<F>> =
             fg.ev().projectT()
 
-    override fun embedT(compFG: HK<Nested<Nested<FixHK, F>, FixHK>, F>): FixKind<F> =
+    override fun embedT(compFG: NestedType<Nested<FixHK, F>, FixHK, F>): FixKind<F> =
             Fix.embedT(compFG, FG())
 }
