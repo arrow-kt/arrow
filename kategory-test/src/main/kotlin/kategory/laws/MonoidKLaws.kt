aac6e676900a47a40bea3eca3e5fc125055ddda2
Victor Galan <victor.galan@liferay.com>
2017-09-03 20:20:49 +0200

    Update MonoidK and SemigroupK laws to use a HK creation function

--- kategory-test/src/main/kotlin/kategory/laws/MonoidKLaws.kt
+++ kategory-test/src/main/kotlin/kategory/laws/MonoidKLaws.kt
@@ -7,16 +7,21 @@ object MonoidKLaws {
 
     inline fun <reified F> laws(SGK: MonoidK<F>, AP: Applicative<F>, EQ: Eq<HK<F, Int>>): List<Law> =
             SemigroupKLaws.laws(SGK, AP, EQ) + listOf(
-                    Law("MonoidK Laws: Left identity", { monoidKLeftIdentity(SGK, AP, EQ) }),
-                    Law("MonoidK Laws: Right identity", { monoidKRightIdentity(SGK, AP, EQ) }))
+                    Law("MonoidK Laws: Left identity", { monoidKLeftIdentity(SGK, AP::pure, EQ) }),
+                    Law("MonoidK Laws: Right identity", { monoidKRightIdentity(SGK, AP::pure, EQ) }))
 
-    inline fun <reified F> monoidKLeftIdentity(SGK: MonoidK<F>, AP: Applicative<F>, EQ: Eq<HK<F, Int>>): Unit =
-            forAll(genApplicative(Gen.int(), AP), { fa: HK<F, Int> ->
+    inline fun <reified F> laws(SGK: MonoidK<F>, crossinline f: (Int) -> HK<F, Int>, EQ: Eq<HK<F, Int>>): List<Law> =
+            SemigroupKLaws.laws(SGK, f, EQ) + listOf(
+                    Law("MonoidK Laws: Left identity", { monoidKLeftIdentity(SGK, f, EQ) }),
+                    Law("MonoidK Laws: Right identity", { monoidKRightIdentity(SGK, f, EQ) }))
+
+    inline fun <reified F> monoidKLeftIdentity(SGK: MonoidK<F>, crossinline f: (Int) -> HK<F, Int>, EQ: Eq<HK<F, Int>>): Unit =
+            forAll(genConstructor(Gen.int(), f), { fa: HK<F, Int> ->
                 SGK.combineK(SGK.empty<Int>(), fa).equalUnderTheLaw(fa, EQ)
             })
 
-    inline fun <reified F> monoidKRightIdentity(SGK: MonoidK<F>, AP: Applicative<F>, EQ: Eq<HK<F, Int>>): Unit =
-            forAll(genApplicative(Gen.int(), AP), { fa: HK<F, Int> ->
+    inline fun <reified F> monoidKRightIdentity(SGK: MonoidK<F>, crossinline f: (Int) -> HK<F, Int>, EQ: Eq<HK<F, Int>>): Unit =
+            forAll(genConstructor(Gen.int(), f), { fa: HK<F, Int> ->
                 SGK.combineK(fa, SGK.empty<Int>()).equalUnderTheLaw(fa, EQ)
             })
 }
