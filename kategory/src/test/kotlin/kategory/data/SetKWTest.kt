fcc4f467acbe4db91aa9e88074ee85523bd48a2d
Victor Galan <victor.galan@liferay.com>
2017-09-03 20:24:13 +0200

    Update SetKW tests

--- kategory/src/test/kotlin/kategory/data/SetKWTest.kt
+++ kategory/src/test/kotlin/kategory/data/SetKWTest.kt
@@ -5,12 +5,10 @@ import org.junit.runner.RunWith
 
 @RunWith(KTestJUnitRunner::class)
 class SetKWTest : UnitSpec() {
-    val applicative = SetKW.applicative()
 
     init {
-        testLaws(MonadLaws.laws(SetKW.monad(), Eq.any()))
-        testLaws(SemigroupKLaws.laws(SetKW.semigroupK(), applicative, Eq.any()))
-        testLaws(MonoidKLaws.laws(SetKW.monoidK(), applicative, Eq.any()))
-        testLaws(TraverseLaws.laws(SetKW.traverse(), applicative, { n: Int -> SetKW(setOf(n)) }, Eq.any()))
+        testLaws(SemigroupKLaws.laws(SetKW.semigroupK(), { SetKW.pure(it) }, Eq.any()))
+        testLaws(MonoidKLaws.laws(SetKW.monoidK(), { SetKW.pure(it) }, Eq.any()))
+        testLaws(FoldableLaws.laws(SetKW.foldable(), { SetKW.pure(it) }, Eq.any()))
     }
 }
