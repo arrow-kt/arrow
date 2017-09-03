8ceabf66f015e93f5aa31812536758c8399cc24d
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-09-03 17:42:10 +0200

    remove MonadFilter javadoc

--- kategory/src/main/kotlin/kategory/typeclasses/MonadFilter.kt
+++ kategory/src/main/kotlin/kategory/typeclasses/MonadFilter.kt
@@ -1,11 +1,5 @@
 package kategory
 
-/**
- * a Monad equipped with an additional method which allows us to create an "empty" value for the Monad
- * (for whatever "empty" makes sense for that particular monad). This is of particular interest to us
- * since it allows us to add a filter method to a Monad, which is used when pattern matching or
- * using guards in for comprehensions.
- */
 interface MonadFilter<F> : Monad<F>, FunctorFilter<F>, Typeclass {
 
     fun <A> empty(): HK<F, A>
