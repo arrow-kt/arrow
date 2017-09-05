2468241d7d55dbbdc261fdca55937856c371e12c
Raul Raja <raulraja@gmail.com>
2017-09-05 12:05:35 +0200

    `combineAll` can fail in Semigroup for empty collections, moved to Monoid

--- kategory/src/main/kotlin/kategory/typeclasses/Monoid.kt
+++ kategory/src/main/kotlin/kategory/typeclasses/Monoid.kt
@@ -6,8 +6,16 @@ interface Monoid<A> : Semigroup<A> {
      */
     fun empty(): A
 
+    /**
+     * Combine a collection of [A] values.
+     */
+    fun combineAll(elems: Collection<A>): A =
+            elems.fold(empty(), { a, b -> combine(a, b) })
+
 }
 
 inline fun <reified A> A.empty(FT: Monoid<A> = monoid()): A = FT.empty()
+
+inline fun <reified A> Collection<A>.combineAll(FT: Monoid<A> = monoid()): A = FT.combineAll(this)
 
 inline fun <reified A> monoid(): Monoid<A> = instance(InstanceParametrizedType(Monoid::class.java, listOf(A::class.java)))
