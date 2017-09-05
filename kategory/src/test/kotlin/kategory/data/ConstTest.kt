c1edbca430712495e41898b9c3f5d58110565507
Antonio López Marín <tonilopezmr@gmail.com>
2017-09-05 19:49:59 +0200

    TraverseFilterLaws include TraverseLaws

--- kategory/src/test/kotlin/kategory/data/ConstTest.kt
+++ kategory/src/test/kotlin/kategory/data/ConstTest.kt
@@ -7,8 +7,7 @@ import org.junit.runner.RunWith
 @RunWith(KTestJUnitRunner::class)
 class ConstTest : UnitSpec() {
     init {
-        testLaws(TraverseLaws.laws(Const.traverse(IntMonoid), Const.applicative(IntMonoid), { Const(it) }, Eq.any()))
-        testLaws(ApplicativeLaws.laws(Const.applicative(IntMonoid), Eq.any()))
         testLaws(TraverseFilterLaws.laws(Const.traverseFilter(IntMonoid), Const.applicative(IntMonoid), { Const(it) }, Eq.any()))
+        testLaws(ApplicativeLaws.laws(Const.applicative(IntMonoid), Eq.any()))
     }
 }
