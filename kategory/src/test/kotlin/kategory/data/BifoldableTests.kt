bac9d962d55438ac13c3271ae5c1b685335fa1f4
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-09-07 18:45:36 +0200

    write MonadCombine

--- kategory/src/test/kotlin/kategory/data/BifoldableTests.kt
+++ kategory/src/test/kotlin/kategory/data/BifoldableTests.kt
@@ -3,8 +3,6 @@ package kategory
 import io.kotlintest.KTestJUnitRunner
 import org.junit.runner.RunWith
 
-typealias EitherEither<A, B> = Either<Either<A, B>, Either<A, B>>
-
 @RunWith(KTestJUnitRunner::class)
 class BifoldableTests : UnitSpec() {
     init {
@@ -26,27 +24,5 @@ class BifoldableTests : UnitSpec() {
         val eitherComposeEither = eitherBifoldable.compose(eitherBifoldable)
 
         testLaws(BifoldableLaws.laws(eitherComposeEither, { cf: Int -> Either.Right(Either.Right(cf)).liftB() }, Eq.any()))
-
-        /*
-        "Reducible<NonEmptyList> default size implementation" {
-            val nel = NonEmptyList.of(1, 2, 3)
-            nonEmptyReducible.size(LongMonoid, nel) shouldBe nel.size.toLong()
-        }
-
-        "Reducible<NonEmptyList>" {
-            // some basic sanity checks
-            val tail = (2 to 10).toList()
-            val total = 1 + tail.sum()
-            val nel = NonEmptyList(1, tail)
-            nonEmptyReducible.reduceLeft(nel, { a, b -> a + b }) shouldBe total
-            nonEmptyReducible.reduceRight(nel, { x, ly -> ly.map({ x + it }) }).value() shouldBe (total)
-            nonEmptyReducible.reduce(nel) shouldBe total
-
-            // more basic checks
-            val names = NonEmptyList.of("Aaron", "Betty", "Calvin", "Deirdra")
-            val totalLength = names.all.map({ it.length }).sum()
-            nonEmptyReducible.reduceLeftTo(names, { it.length }, { sum, s -> s.length + sum }) shouldBe totalLength
-            nonEmptyReducible.reduceMap(names, { it.length }) shouldBe totalLength
-        }*/
     }
 }
