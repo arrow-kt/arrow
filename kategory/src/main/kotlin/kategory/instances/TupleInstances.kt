3915636df20206ee0774535a6ae6e607e7fd6d4d
Raul Raja <raulraja@gmail.com>
2017-09-07 23:49:40 +0200

    Tuple monoid instance

--- kategory/src/main/kotlin/kategory/instances/TupleInstances.kt
+++ kategory/src/main/kotlin/kategory/instances/TupleInstances.kt
@@ -2,9 +2,9 @@ package kategory
 
 fun <A, B> monoidTuple(MA: Monoid<A>, MB: Monoid<B>): Monoid<Tuple2<A, B>> =
         object: Monoid<Tuple2<A, B>> {
-            override fun combine(x: Tuple2<A, B>, y: Tuple2<A, B>): Tuple2<A, B> {
-                val (xa, xb) = x
-                val (ya, yb) = y
+            override fun combine(a: Tuple2<A, B>, b: Tuple2<A, B>): Tuple2<A, B> {
+                val (xa, xb) = a
+                val (ya, yb) = b
                 return Tuple2(MA.combine(xa, ya), MB.combine(xb, yb))
             }
             override fun empty(): Tuple2<A, B> = Tuple2(MA.empty(), MB.empty())
