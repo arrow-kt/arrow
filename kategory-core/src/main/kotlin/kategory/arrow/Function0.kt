8f0a5bd4479b46a1f7fe8ae88666187b5d39f58d
Raul Raja <raulraja@gmail.com>
2017-09-08 20:35:25 +0200

    `Function0` derivation and implicits pass, take 2

--- kategory-core/src/main/kotlin/kategory/arrow/Function0.kt
+++ kategory-core/src/main/kotlin/kategory/arrow/Function0.kt
@@ -2,7 +2,7 @@ package kategory
 
 fun <A> (() -> A).k(): Function0<A> = Function0(this)
 
-operator fun <A> HK<Function0HK, A>.invoke(): A = (this as Function0<A>).f()
+operator fun <A> Function0Kind<A>.invoke(): A = this.ev().f()
 
 @higherkind
 @deriving(
@@ -16,6 +16,8 @@ data class Function0<out A>(internal val f: () -> A) : Function0Kind<A> {
     fun <B> map(f: (A) -> B): Function0<B> = Function0.pure(f(this()))
 
     fun <B> flatMap(ff: (A) -> Function0Kind<B>): Function0<B> = ff(f()).ev()
+
+    fun <B> coflatMap(f: (Function0Kind<A>) -> B): Function0<B> = { f(this) }.k()
 
     fun <B> ap(ff: Function0Kind<(A) -> B>): Function0<B> = ff.flatMap { f -> map(f) }.ev()
 
@@ -37,9 +39,3 @@ data class Function0<out A>(internal val f: () -> A) : Function0Kind<A> {
 
     }
 }
-
-fun <A, B> Function0<A>.flatMap(f: (A) -> Function0Kind<B>): Function0<B> = Function0(f(this()).ev().f)
-
-fun <A, B> Function0<A>.coflatMap(f: (Function0Kind<A>) -> B): Function0<B> = { f(this) }.k()
-
-fun <A> Function0<A>.extract(): A = this()
