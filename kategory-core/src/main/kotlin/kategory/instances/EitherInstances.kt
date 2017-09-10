b60286e65a7b30636c897cd909be5d54768c2417
Raul Raja <raulraja@gmail.com>
2017-09-11 00:17:52 +0200

    `Either` implicit instances

--- kategory-core/src/main/kotlin/kategory/instances/EitherInstances.kt
+++ kategory-core/src/main/kotlin/kategory/instances/EitherInstances.kt
@@ -1,32 +1,43 @@
 package kategory
 
-interface EitherInstances<L> :
-        Functor<EitherKindPartial<L>>,
-        Applicative<EitherKindPartial<L>>,
-        Monad<EitherKindPartial<L>>,
-        MonadError<EitherKindPartial<L>, L>,
-        Foldable<EitherKindPartial<L>>,
-        Traverse<EitherKindPartial<L>> {
+interface EitherFunctorInstance<L> : Functor<EitherKindPartial<L>> {
+    override fun <A, B> map(fa: EitherKind<L, A>, f: (A) -> B): Either<L, B> = fa.ev().map(f)
+}
 
+object EitherFunctorInstanceImplicits {
+    @JvmStatic fun <L> instance(): EitherFunctorInstance<L> = object: EitherFunctorInstance<L> {}
+}
+
+interface EitherApplicativeInstance<L> : EitherFunctorInstance<L> , Applicative<EitherKindPartial<L>> {
+    
     override fun <A> pure(a: A): Either<L, A> = Either.Right(a)
+    
+    override fun <A, B> map(fa: EitherKind<L, A>, f: (A) -> B): Either<L, B> = fa.ev().map(f)
 
+    override fun <A, B> ap(fa: EitherKind<L, A>, ff: EitherKind<L, (A) -> B>): Either<L, B> =
+            fa.ev().ap(ff)
+}
+
+object EitherApplicativeInstanceImplicits {
+    @JvmStatic fun <L> instance(): EitherApplicativeInstance<L> = object: EitherApplicativeInstance<L> {}
+}
+
+interface EitherMonadInstance<L> : EitherApplicativeInstance<L>, Monad<EitherKindPartial<L>> {
+
+    override fun <A, B> ap(fa: EitherKind<L, A>, ff: EitherKind<L, (A) -> B>): Either<L, B> =
+            fa.ev().ap(ff)
+    
     override fun <A, B> flatMap(fa: EitherKind<L, A>, f: (A) -> EitherKind<L, B>): Either<L, B> = fa.ev().flatMap { f(it).ev() }
 
-    override fun <A, B> map(fa: HK<EitherKindPartial<L>, A>, f: (A) -> B): Either<L, B> = fa.ev().map(f)
+    override fun <A, B> tailRecM(a: A, f: (A) -> HK<EitherKindPartial<L>, Either<A, B>>): Either<L, B> =
+            Either.tailRecM(a, f)
+}
 
-    tailrec override fun <A, B> tailRecM(a: A, f: (A) -> HK<EitherKindPartial<L>, Either<A, B>>): Either<L, B> {
-        val ev: Either<L, Either<A, B>> = f(a).ev()
-        return when (ev) {
-            is Either.Left<L, Either<A, B>> -> ev.a.left()
-            is Either.Right<L, Either<A, B>> -> {
-                val b: Either<A, B> = ev.b
-                when (b) {
-                    is Either.Left<A, B> -> tailRecM(b.a, f)
-                    is Either.Right<A, B> -> b.b.right()
-                }
-            }
-        }
-    }
+object EitherMonadInstanceImplicits {
+    @JvmStatic fun <L> instance(): EitherMonadInstance<L> = object: EitherMonadInstance<L> {}
+}
+
+interface EitherMonadErrorInstance<L> : EitherMonadInstance<L>, MonadError<EitherKindPartial<L>, L> {
 
     override fun <A> raiseError(e: L): Either<L, A> = Either.Left(e)
 
@@ -37,24 +48,41 @@ interface EitherInstances<L> :
             is Either.Right -> fea
         }
     }
+}
 
-    override fun <G, A, B> traverse(fa: HK<EitherKindPartial<L>, A>, f: (A) -> HK<G, B>, GA: Applicative<G>): HK<G, HK<EitherKindPartial<L>, B>> =
-            fa.ev().fold({ GA.pure(it.left()) }, { GA.map(f(it), { Either.Right(it) }) })
+object EitherMonadErrorInstanceImplicits {
+    @JvmStatic fun <L> instance(): EitherMonadErrorInstance<L> = object: EitherMonadErrorInstance<L> {}
+}
+
+interface EitherFoldableInstance<L> : Foldable<EitherKindPartial<L>> {
 
     override fun <A, B> foldL(fa: HK<EitherKindPartial<L>, A>, b: B, f: (B, A) -> B): B =
-            fa.ev().let { either ->
-                when (either) {
-                    is Either.Right -> f(b, either.b)
-                    is Either.Left -> b
-                }
-            }
+            fa.ev().foldL(b, f)
 
     override fun <A, B> foldR(fa: HK<EitherKindPartial<L>, A>, lb: Eval<B>, f: (A, Eval<B>) -> Eval<B>): Eval<B> =
-            fa.ev().let { either ->
-                when (either) {
-                    is Either.Right -> f(either.b, lb)
-                    is Either.Left -> lb
-                }
-            }
+            fa.ev().foldR(lb, f)
+}
 
+object EitherFoldableInstanceImplicits {
+    @JvmStatic fun <L> instance(): EitherFoldableInstance<L> = object: EitherFoldableInstance<L> {}
+}
+
+interface EitherTraverseInstance<L> : EitherFoldableInstance<L>, Traverse<EitherKindPartial<L>> {
+
+    override fun <G, A, B> traverse(fa: HK<EitherKindPartial<L>, A>, f: (A) -> HK<G, B>, GA: Applicative<G>): HK<G, HK<EitherKindPartial<L>, B>> =
+            fa.ev().traverse(f, GA)
+}
+
+object EitherTraverseInstanceImplicits {
+    @JvmStatic fun <L> instance(): EitherTraverseInstance<L> = object: EitherTraverseInstance<L> {}
+}
+
+interface EitherSemigroupKInstance<L> : SemigroupK<EitherKindPartial<L>> {
+
+    override fun <A> combineK(x: EitherKind<L, A>, y: EitherKind<L, A>): Either<L, A> =
+            x.ev().combineK(y)
+}
+
+object EitherSemigroupKInstanceImplicits {
+    @JvmStatic fun <L> instance(): EitherSemigroupKInstance<L> = object: EitherSemigroupKInstance<L> {}
 }
