f0d823080d2a83983860ae5222270893f754613d
Raul Raja <raulraja@gmail.com>
2017-09-11 22:15:39 +0200

    `Number` implicit instances

--- kategory-core/src/main/kotlin/kategory/instances/NumberInstancesJava.kt
+++ kategory-core/src/main/kotlin/kategory/instances/NumberInstancesJava.kt
@@ -1,8 +1,6 @@
 package java_lang
 
-import kategory.IntEqInstance
-import kategory.IntMonoid
-import kategory.Semigroup
+import kategory.*
 
 object IntegerMonoidInstanceImplicits {
     @JvmStatic fun instance(): IntMonoid = IntMonoid
@@ -14,4 +12,44 @@ object IntegerSemigroupInstanceImplicits {
 
 object IntegerEqInstanceImplicits {
     @JvmStatic fun instance(): IntEqInstance = IntEqInstance
+}
+
+object LongMonoidInstanceImplicits {
+    @JvmStatic fun instance(): LongMonoid = LongMonoid
+}
+
+object LongSemigroupInstanceImplicits {
+    @JvmStatic fun instance(): Semigroup<Long> = LongMonoid
+}
+
+object ShortMonoidInstanceImplicits {
+    @JvmStatic fun instance(): ShortMonoid = ShortMonoid
+}
+
+object ShortSemigroupInstanceImplicits {
+    @JvmStatic fun instance(): Semigroup<Short> = ShortMonoid
+}
+
+object ByteMonoidInstanceImplicits {
+    @JvmStatic fun instance(): ByteMonoid = ByteMonoid
+}
+
+object ByteSemigroupInstanceImplicits {
+    @JvmStatic fun instance(): Semigroup<Byte> = ByteMonoid
+}
+
+object DoubleMonoidInstanceImplicits {
+    @JvmStatic fun instance(): DoubleMonoid = DoubleMonoid
+}
+
+object DoubleSemigroupInstanceImplicits {
+    @JvmStatic fun instance(): Semigroup<Double> = DoubleMonoid
+}
+
+object FloatMonoidInstanceImplicits {
+    @JvmStatic fun instance(): FloatMonoid = FloatMonoid
+}
+
+object FloatSemigroupInstanceImplicits {
+    @JvmStatic fun instance(): Semigroup<Float> = FloatMonoid
 }
