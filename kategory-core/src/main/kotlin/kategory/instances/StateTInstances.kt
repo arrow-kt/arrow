4c72029157abdf8aca688a65e4fc7a8439827c8a
Raul Raja <raulraja@gmail.com>
2017-09-12 14:26:30 +0200

    `StateT` implicit instances

--- kategory-core/src/main/kotlin/kategory/instances/StateTInstances.kt
+++ kategory-core/src/main/kotlin/kategory/instances/StateTInstances.kt
@@ -1,53 +1,97 @@
 package kategory
 
-interface StateTInstances<F, S> :
-        Functor<StateTKindPartial<F, S>>,
-        Applicative<StateTKindPartial<F, S>>,
-        Monad<StateTKindPartial<F, S>>,
-        MonadState<StateTKindPartial<F, S>, S> {
+interface StateTFunctorInstance<F, S> : Functor<StateTKindPartial<F, S>> {
 
-    fun MF(): Monad<F>
+    fun FF(): Functor<F>
 
-    override fun <A, B> flatMap(fa: HK<StateTKindPartial<F, S>, A>, f: (A) -> HK<StateTKindPartial<F, S>, B>): StateT<F, S, B> = fa.ev().flatMap(f)
-
-    override fun <A, B> map(fa: HK<StateTKindPartial<F, S>, A>, f: (A) -> B): StateT<F, S, B> = fa.ev().map(f)
-
-    override fun <A> pure(a: A): StateT<F, S, A> = StateT(MF(), MF().pure({ s: S -> MF().pure(Tuple2(s, a)) }))
-
-    override fun <A, B> ap(fa: HK<StateTKindPartial<F, S>, A>, ff: HK<StateTKindPartial<F, S>, (A) -> B>): StateT<F, S, B> =
-            ff.ev().map2(fa.ev()) { f, a -> f(a) }
-
-    override fun <A, B, Z> map2(fa: HK<StateTKindPartial<F, S>, A>, fb: HK<StateTKindPartial<F, S>, B>, f: (Tuple2<A, B>) -> Z): StateT<F, S, Z> =
-            fa.ev().map2(fb.ev(), { a, b -> f(Tuple2(a, b)) })
-
-    @Suppress("UNCHECKED_CAST")
-    override fun <A, B, Z> map2Eval(fa: HK<StateTKindPartial<F, S>, A>, fb: Eval<HK<StateTKindPartial<F, S>, B>>, f: (Tuple2<A, B>) -> Z):
-            Eval<StateT<F, S, Z>> = fa.ev().map2Eval(fb as Eval<StateT<F, S, B>>) { a, b -> f(Tuple2(a, b)) }
-
-    override fun <A, B> product(fa: HK<StateTKindPartial<F, S>, A>, fb: HK<StateTKindPartial<F, S>, B>): HK<StateTKindPartial<F, S>, Tuple2<A, B>> =
-            fa.ev().product(fb.ev())
-
-    override fun <A, B> tailRecM(a: A, f: (A) -> HK<StateTKindPartial<F, S>, Either<A, B>>): StateT<F, S, B> =
-            StateT(MF(), MF().pure({ s: S ->
-                MF().tailRecM(Tuple2(s, a), { (s, a0) ->
-                    MF().map(f(a0).runM(s)) { (s, ab) ->
-                        ab.bimap({ a1 -> Tuple2(s, a1) }, { b -> Tuple2(s, b) })
-                    }
-                })
-            }))
-
-    override fun get(): StateT<F, S, S> = StateT(MF(), MF().pure({ s: S -> MF().pure(Tuple2(s, s)) }))
-
-    override fun set(s: S): StateT<F, S, Unit> = StateT(MF(), MF().pure({ _: S -> MF().pure(Tuple2(s, Unit)) }))
+    override fun <A, B> map(fa: StateTKind<F, S, A>, f: (A) -> B): StateT<F, S, B> = fa.ev().map(f, FF())
 
 }
 
-interface StateTSemigroupK<F, S> : SemigroupK<StateTKindPartial<F, S>> {
+object StateTFunctorInstanceImplicits {
+    @JvmStatic
+    fun <F, S> instance(FF: Functor<F>): StateTFunctorInstance<F, S> = object : StateTFunctorInstance<F, S> {
+        override fun FF(): Functor<F> = FF
+    }
+}
 
-    fun F(): Monad<F>
-    fun G(): SemigroupK<F>
+interface StateTApplicativeInstance<F, S> : StateTFunctorInstance<F, S>, Applicative<StateTKindPartial<F, S>> {
 
-    override fun <A> combineK(x: HK<HK2<StateTHK, F, S>, A>, y: HK<HK2<StateTHK, F, S>, A>): StateT<F, S, A> =
-            StateT(F(), F().pure({ s -> G().combineK(x.ev().run(s), y.ev().run(s)) }))
+    fun MF(): Monad<F>
+
+    override fun <A, B> map(fa: StateTKind<F, S, A>, f: (A) -> B): StateT<F, S, B> = fa.ev().map(f, MF())
+
+    override fun <A> pure(a: A): StateT<F, S, A> = StateT(MF().pure({ s: S -> MF().pure(Tuple2(s, a)) }))
+
+    override fun <A, B> ap(fa: StateTKind<F, S, A>, ff: StateTKind<F, S, (A) -> B>): StateT<F, S, B> =
+            fa.ev().ap(ff, MF())
+
+    override fun <A, B> product(fa: StateTKind<F, S, A>, fb: StateTKind<F, S, B>): StateT<F, S, Tuple2<A, B>> =
+            fa.ev().product(fb.ev(), MF())
+
+}
+
+object StateTApplicativeInstanceImplicits {
+    @JvmStatic
+    fun <F, S> instance(MF: Monad<F>): StateTApplicativeInstance<F, S> = object : StateTApplicativeInstance<F, S> {
+        override fun FF(): Functor<F> = MF
+        override fun MF(): Monad<F> = MF
+    }
+}
+
+interface StateTMonadInstance<F, S> : StateTApplicativeInstance<F, S>, Monad<StateTKindPartial<F, S>> {
+
+    override fun <A, B> flatMap(fa: StateTKind<F, S, A>, f: (A) -> StateTKind<F, S, B>): StateT<F, S, B> =
+            fa.ev().flatMap(f, MF())
+
+    override fun <A, B> tailRecM(a: A, f: (A) -> StateTKind<F, S, Either<A, B>>): StateT<F, S, B> =
+            StateT.tailRecM(a, f, MF())
+
+    override fun <A, B> ap(fa: StateTKind<F, S, A>, ff: StateTKind<F, S, (A) -> B>): StateT<F, S, B> =
+            ff.ev().map2(fa.ev(), { f, a -> f(a) }, MF())
+
+}
+
+object StateTMonadInstanceImplicits {
+    @JvmStatic
+    fun <F, S> instance(MF: Monad<F>): StateTMonadInstance<F, S> = object : StateTMonadInstance<F, S> {
+        override fun FF(): Functor<F> = MF
+        override fun MF(): Monad<F> = MF
+    }
+}
+
+interface StateTMonadStateInstance<F, S> : StateTMonadInstance<F, S>, MonadState<StateTKindPartial<F, S>, S> {
+
+    override fun get(): StateT<F, S, S> = StateT.get(MF())
+
+    override fun set(s: S): StateT<F, S, Unit> = StateT.set(s, MF())
+
+}
+
+object StateTMonadStateInstanceImplicits {
+    @JvmStatic
+    fun <F, S> instance(MF: Monad<F>): StateTMonadStateInstance<F, S> = object : StateTMonadStateInstance<F, S> {
+        override fun FF(): Functor<F> = MF
+        override fun MF(): Monad<F> = MF
+    }
+}
+
+interface StateTSemigroupKInstance<F, S> : SemigroupK<StateTKindPartial<F, S>> {
+
+    fun MF(): Monad<F>
+
+    fun SF(): SemigroupK<F>
+
+    override fun <A> combineK(x: StateTKind<F, S, A>, y: StateTKind<F, S, A>): StateT<F, S, A> =
+            x.ev().combineK(y, MF(), SF())
+
+}
+
+object StateTSemigroupKInstanceImplicits {
+    @JvmStatic
+    fun <F, S> instance(MF: Monad<F>, SF: SemigroupK<F>): StateTSemigroupKInstance<F, S> = object : StateTSemigroupKInstance<F, S> {
+        override fun MF(): Monad<F> = MF
+        override fun SF(): SemigroupK<F> = SF
+    }
 }
 
