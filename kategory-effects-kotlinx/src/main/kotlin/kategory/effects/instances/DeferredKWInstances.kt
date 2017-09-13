114929c9b2a50cc7a75497e1ed948c2322a909c8
pakoito <jueces.jus@gmail.com>
2017-09-13 20:37:43 +0100

    Remove kategory-effects-kotlinx

--- kategory-effects-kotlinx/src/main/kotlin/kategory/effects/instances/DeferredKWInstances.kt
+++
@@ -1,9 +0,0 @@
-package kategory
-
-interface DeferredKWHKMonadErrorInstance : MonadError<DeferredKWHK, Throwable>, DeferredKWHKMonadInstance {
-    override fun <A> raiseError(e: Throwable): HK<DeferredKWHK, A> =
-            DeferredKW.raiseError(e)
-
-    override fun <A> handleErrorWith(fa: HK<DeferredKWHK, A>, f: (Throwable) -> HK<DeferredKWHK, A>): HK<DeferredKWHK, A> =
-            fa.handleErrorWith { f(it).ev() }
-}
