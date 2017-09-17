316c44dad1502e51838add825eb32e3407963841
Raul Raja <raulraja@gmail.com>
2017-09-17 22:18:53 +0200

    ObservableKW cleanup

--- kategory-effects-rx2/src/main/kotlin/kategory/effects/instances/ObservableKWInstances.kt
+++ kategory-effects-rx2/src/main/kotlin/kategory/effects/instances/ObservableKWInstances.kt
@@ -16,10 +16,10 @@ interface ObservableKWFlatMonadInstance :
     override fun <A, B> ap(fa: ObservableKWKind<A>, ff: ObservableKWKind<(A) -> B>): ObservableKW<B> =
             fa.ev().ap(ff)
 
-    override fun <A, B> flatMap(fa: ObservableKWKind<A>, f: (A) -> ObservableKWKind<B>): ObservableKWKind<B> =
+    override fun <A, B> flatMap(fa: ObservableKWKind<A>, f: (A) -> ObservableKWKind<B>): ObservableKW<B> =
             fa.ev().flatMap { f(it).ev() }
 
-    override fun <A, B> tailRecM(a: A, f: (A) -> ObservableKWKind<Either<A, B>>): ObservableKWKind<B> =
+    override fun <A, B> tailRecM(a: A, f: (A) -> ObservableKWKind<Either<A, B>>): ObservableKW<B> =
             f(a).ev().flatMap {
                 it.fold({ tailRecM(a, f).ev() }, { pure(it).ev() })
             }
