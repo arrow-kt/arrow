ce4e813a6f2ec1af1a4f85ed3528ab4439064c45
Raul Raja <raulraja@gmail.com>
2017-09-17 22:06:46 +0200

    `@instance` implicits codegen over `Yoneda`

--- kategory-core/src/main/kotlin/kategory/instances/YonedaInstances.kt
+++ kategory-core/src/main/kotlin/kategory/instances/YonedaInstances.kt
@@ -1,15 +1,9 @@
 package kategory
 
+@instance(Yoneda::class)
 interface YonedaFunctorInstance<U> : Functor<YonedaKindPartial<U>> {
 
     fun FU(): Functor<U>
 
-    override fun <A, B> map(fa: HK<YonedaKindPartial<U>, A>, f: (A) -> B): Yoneda<U, B> = fa.ev().map(f, FU())
-}
-
-object YonedaFunctorInstanceImplicits {
-    @JvmStatic
-    fun <U> instance(FU: Functor<U>): YonedaFunctorInstance<U> = object : YonedaFunctorInstance<U> {
-        override fun FU(): Functor<U> = FU
-    }
+    override fun <A, B> map(fa: YonedaKind<U, A>, f: (A) -> B): Yoneda<U, B> = fa.ev().map(f, FU())
 }
