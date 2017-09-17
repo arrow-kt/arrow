1bea0cb920ece9fa46e17c6047c3df3f40cb716f
Marcel Holter <marcel.holter+github@gmail.com>
2017-09-17 13:10:38 +0300

    #280: Add Flowable wrapper

---
+++ kategory-effects-rx2/src/main/kotlin/kategory/effects/data/FlowableKW.kt
@@ -0,0 +1,85 @@
+package kategory
+
+import io.reactivex.*
+
+fun <A> Flowable<A>.k(): FlowableKW<A> = FlowableKW(this)
+
+fun <A> FlowableKWKind<A>.value(): Flowable<A> = this.ev().flowable
+
+@higherkind
+@deriving(Functor::class, Applicative::class)
+data class FlowableKW<A>(val flowable: Flowable<A>) : FlowableKWKind<A> {
+
+    fun <B> map(f: (A) -> B): FlowableKW<B> =
+            flowable.map(f).k()
+
+    fun <B> ap(fa: FlowableKWKind<(A) -> B>): FlowableKW<B> =
+            flatMap { a -> fa.ev().map { ff -> ff(a) } }
+
+    fun <B> flatMap(f: (A) -> FlowableKW<B>): FlowableKW<B> =
+            flowable.flatMap { f(it).flowable }.k()
+
+    fun <B> concatMap(f: (A) -> FlowableKW<B>): FlowableKW<B> =
+            flowable.concatMap { f(it).flowable }.k()
+
+    fun <B> switchMap(f: (A) -> FlowableKW<B>): FlowableKW<B> =
+            flowable.switchMap { f(it).flowable }.k()
+
+    companion object {
+        fun <A> pure(a: A): FlowableKW<A> =
+                Flowable.just(a).k()
+
+        fun <A> raiseError(t: Throwable): FlowableKW<A> =
+                Flowable.error<A>(t).k()
+
+        fun <A> runAsync(fa: Proc<A>, mode: BackpressureStrategy = BackpressureStrategy.BUFFER): FlowableKW<A> =
+                Flowable.create({ emitter: FlowableEmitter<A> ->
+                    fa { either: Either<Throwable, A> ->
+                        either.fold({
+                            emitter.onError(it)
+                        }, {
+                            emitter.onNext(it)
+                            emitter.onComplete()
+                        })
+
+                    }
+                }, mode).k()
+
+        fun monadFlat(): FlowableKWFlatMonadInstance = FlowableKWFlatMonadInstanceImplicits.instance()
+
+        fun monadConcat(): FlowableKWConcatMonadInstance = FlowableKWConcatMonadInstanceImplicits.instance()
+
+        fun monadSwitch(): FlowableKWSwitchMonadInstance = FlowableKWSwitchMonadInstanceImplicits.instance()
+
+        fun monadErrorFlat(): FlowableKWFlatMonadErrorInstance = FlowableKWFlatMonadErrorInstanceImplicits.instance()
+
+        fun monadErrorConcat(): FlowableKWConcatMonadErrorInstance = FlowableKWConcatMonadErrorInstanceImplicits.instance()
+
+        fun monadErrorSwitch(): FlowableKWSwitchMonadErrorInstance = FlowableKWSwitchMonadErrorInstanceImplicits.instance()
+
+        fun asyncContext(): FlowableKWAsyncContextInstance = FlowableKWAsyncContextInstanceImplicits.instance()
+
+        fun asyncContextBuffer(): FlowableKWAsyncContextInstance = object : FlowableKWAsyncContextInstance {
+            override fun BS(): BackpressureStrategy = BackpressureStrategy.BUFFER
+        }
+
+        fun asyncContextDrop(): FlowableKWAsyncContextInstance = object : FlowableKWAsyncContextInstance {
+            override fun BS(): BackpressureStrategy = BackpressureStrategy.DROP
+        }
+
+        fun asyncContextError(): FlowableKWAsyncContextInstance = object : FlowableKWAsyncContextInstance {
+            override fun BS(): BackpressureStrategy = BackpressureStrategy.ERROR
+        }
+
+        fun asyncContextLatest(): FlowableKWAsyncContextInstance = object : FlowableKWAsyncContextInstance {
+            override fun BS(): BackpressureStrategy = BackpressureStrategy.LATEST
+        }
+
+        fun asyncContextMissing(): FlowableKWAsyncContextInstance = object : FlowableKWAsyncContextInstance {
+            override fun BS(): BackpressureStrategy = BackpressureStrategy.MISSING
+        }
+    }
+}
+
+fun <A> FlowableKW<A>.handleErrorWith(function: (Throwable) -> FlowableKW<A>): FlowableKW<A> =
+        this.flowable.onErrorResumeNext { t: Throwable -> function(t).flowable }.k()
---
+++ kategory-effects-rx2/src/test/kotlin/kategory/effects/data/FlowableKWTests.kt
@@ -0,0 +1,110 @@
+package kategory.effects.data
+
+import io.kotlintest.KTestJUnitRunner
+import io.kotlintest.matchers.shouldNotBe
+import io.reactivex.Flowable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subscribers.TestSubscriber
+import kategory.*
+import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
+
+@RunWith(KTestJUnitRunner::class)
+class FlowableKWTests : UnitSpec() {
+
+    fun <T> EQ(): Eq<FlowableKWKind<T>> = object : Eq<FlowableKWKind<T>> {
+        override fun eqv(a: FlowableKWKind<T>, b: FlowableKWKind<T>): Boolean =
+                try {
+                    a.value().blockingFirst() == b.value().blockingFirst()
+                } catch (throwable: Throwable) {
+                    val errA = try {
+                        a.value().blockingFirst()
+                        throw IllegalArgumentException()
+                    } catch (err: Throwable) {
+                        err
+                    }
+                    val errB = try {
+                        b.value().blockingFirst()
+                        throw IllegalStateException()
+                    } catch (err: Throwable) {
+                        err
+                    }
+                    errA == errB
+                }
+
+    }
+
+    init {
+        "instances can be resolved implicitly" {
+            functor<FlowableKWHK>() shouldNotBe null
+            applicative<FlowableKWHK>() shouldNotBe null
+            monad<FlowableKWHK>() shouldNotBe null
+            monadError<FlowableKWHK, Unit>() shouldNotBe null
+            asyncContext<FlowableKWHK>() shouldNotBe null
+        }
+
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContext(), FlowableKW.monadErrorFlat(), EQ(), EQ()))
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContext(), FlowableKW.monadErrorConcat(), EQ(), EQ()))
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContext(), FlowableKW.monadErrorSwitch(), EQ(), EQ()))
+
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextBuffer(), FlowableKW.monadErrorFlat(), EQ(), EQ()))
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextBuffer(), FlowableKW.monadErrorConcat(), EQ(), EQ()))
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextBuffer(), FlowableKW.monadErrorSwitch(), EQ(), EQ()))
+
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextDrop(), FlowableKW.monadErrorFlat(), EQ(), EQ()))
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextDrop(), FlowableKW.monadErrorConcat(), EQ(), EQ()))
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextDrop(), FlowableKW.monadErrorSwitch(), EQ(), EQ()))
+
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextError(), FlowableKW.monadErrorFlat(), EQ(), EQ()))
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextError(), FlowableKW.monadErrorConcat(), EQ(), EQ()))
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextError(), FlowableKW.monadErrorSwitch(), EQ(), EQ()))
+
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextLatest(), FlowableKW.monadErrorFlat(), EQ(), EQ()))
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextLatest(), FlowableKW.monadErrorConcat(), EQ(), EQ()))
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextLatest(), FlowableKW.monadErrorSwitch(), EQ(), EQ()))
+
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextMissing(), FlowableKW.monadErrorFlat(), EQ(), EQ()))
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextMissing(), FlowableKW.monadErrorConcat(), EQ(), EQ()))
+        testLaws(AsyncLaws.laws(FlowableKW.asyncContextMissing(), FlowableKW.monadErrorSwitch(), EQ(), EQ()))
+
+        "Multi-thread Flowables finish correctly" {
+            val value: Flowable<Long> = FlowableKW.monadErrorFlat().bindingE {
+                val a = Flowable.timer(2, TimeUnit.SECONDS).k().bind()
+                yields(a)
+            }.value()
+            val test: TestSubscriber<Long> = value.test()
+            test.awaitDone(5, TimeUnit.SECONDS)
+            test.assertTerminated().assertComplete().assertNoErrors().assertValue(0)
+        }
+
+        "Multi-thread Observables should run on their required threads" {
+            val originalThread: Thread = Thread.currentThread()
+            var nextThread: Thread? = null
+            val value: Flowable<Long> = FlowableKW.monadErrorFlat().bindingE {
+                val a = Flowable.timer(2, TimeUnit.SECONDS).k().bind()
+                nextThread = Thread.currentThread()
+                val b = Flowable.just(a).observeOn(Schedulers.io()).k().bind()
+                yields(b)
+            }.value()
+            val test: TestSubscriber<Long> = value.test()
+            val lastThread: Thread = test.awaitDone(5, TimeUnit.SECONDS).lastThread()
+            nextThread shouldNotBe originalThread
+            lastThread shouldNotBe originalThread
+            lastThread shouldNotBe nextThread
+        }
+
+        "Flowable cancellation forces binding to cancel without completing too" {
+            val value: Flowable<Long> = FlowableKW.monadErrorFlat().bindingE {
+                val a = Flowable.timer(3, TimeUnit.SECONDS).k().bind()
+                yields(a)
+            }.value()
+            val test : TestSubscriber<Long> = value.doOnSubscribe { subscription ->
+                Flowable.timer(1, TimeUnit.SECONDS).subscribe {
+                    subscription.cancel()
+                }
+            }.test()
+            test.awaitTerminalEvent(10, TimeUnit.SECONDS)
+            test.assertNotTerminated().assertNotComplete().assertNoErrors().assertNoValues()
+        }
+    }
+}
