5c8dcd87883a9bacdbebd814528a1354149173ac
Jorge Castillo <jorge.castillo.prz@gmail.com>
2017-09-17 16:05:05 +0200

    add extension functions to forward to Kleisli using IdHK

--- kategory-core/src/main/kotlin/kategory/data/Reader.kt
+++ kategory-core/src/main/kotlin/kategory/data/Reader.kt
@@ -8,6 +8,20 @@ fun <D, A> ((D) -> A).reader(): ReaderT<IdHK, D, A> = Reader(this)
 
 fun <D, A> ReaderT<IdHK, D, A>.runId(d: D): A = this.run(d).value()
 
+fun <D, A, B> ReaderT<IdHK, D, A>.map(f: (A) -> B): ReaderT<IdHK, D, B> = map(f, Id.functor())
+
+fun <D, A, B> ReaderT<IdHK, D, A>.flatMap(f: (A) -> ReaderT<IdHK, D, B>): ReaderT<IdHK, D, B> = flatMap(f, Id.monad())
+
+fun <D, A, B> ReaderT<IdHK, D, A>.ap(ff: KleisliKind<IdHK, D, (A) -> B>): ReaderT<IdHK, D, B> = ap(ff, Id.applicative())
+
+fun <D, A, B> ReaderT<IdHK, D, A>.zip(o: ReaderT<IdHK, D, B>): ReaderT<IdHK, D, Tuple2<A, B>> = zip(o, Id.monad())
+
+fun <D, A, C> ReaderT<IdHK, D, A>.andThen(f: ReaderT<IdHK, A, C>): ReaderT<IdHK, D, C> = andThen(f, Id.monad())
+
+fun <D, A, B> ReaderT<IdHK, D, A>.andThen(f: (A) -> HK<IdHK, B>): ReaderT<IdHK, D, B> = andThen(f, Id.monad())
+
+fun <D, A, B> ReaderT<IdHK, D, A>.andThen(a: HK<IdHK, B>): ReaderT<IdHK, D, B> = andThen(a, Id.monad())
+
 object Reader {
 
     operator fun <D, A> invoke(run: (D) -> A): ReaderT<IdHK, D, A> = Kleisli(run.andThen { Id(it) })
