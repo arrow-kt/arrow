f4c3c1d5e8cd71d7e13d3432381d7d0089868421
Raul Raja <raulraja@gmail.com>
2017-09-17 20:53:50 +0200

    `@instance` implicits codegen over `NonEmptyList`

--- kategory-core/src/main/kotlin/kategory/instances/ListKWInstances.kt
+++ kategory-core/src/main/kotlin/kategory/instances/ListKWInstances.kt
@@ -1,19 +1,11 @@
 package kategory
 
+@instance(ListKW::class)
 interface ListKWSemigroupInstance<A> : Semigroup<ListKW<A>> {
     override fun combine(a: ListKW<A>, b: ListKW<A>): ListKW<A> = (a + b).k()
 }
 
-object ListKWSemigroupInstanceImplicits {
-    @JvmStatic
-    fun <A> instance(): ListKWSemigroupInstance<A> = object : ListKWSemigroupInstance<A> {}
-}
-
+@instance(ListKW::class)
 interface ListKWMonoidInstance<A> : ListKWSemigroupInstance<A>, Monoid<ListKW<A>> {
     override fun empty(): ListKW<A> = emptyList<A>().k()
-}
-
-object ListKWMonoidInstanceImplicits {
-    @JvmStatic
-    fun <A> instance(): ListKWMonoidInstance<A> = object : ListKWMonoidInstance<A> {}
 }
