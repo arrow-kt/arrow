abca71c1931055ade1957374a366f21614fec0eb
Simon Vergauwen <vergauwen.simon@gmail.com>
2017-09-18 08:49:28 +0200

    Number instances

--- kategory-core/src/main/kotlin/kategory/instances/NumberInstancesJava.kt
+++ kategory-core/src/main/kotlin/kategory/instances/NumberInstancesJava.kt
@@ -14,12 +14,20 @@ object IntegerEqInstanceImplicits {
     @JvmStatic fun instance(): IntEqInstance = IntEqInstance
 }
 
+object IntegerOrderInstanceImplicits {
+    @JvmStatic fun instance(): IntOrderInstance = IntOrderInstance
+}
+
 object LongMonoidInstanceImplicits {
     @JvmStatic fun instance(): LongMonoid = LongMonoid
 }
 
 object LongSemigroupInstanceImplicits {
     @JvmStatic fun instance(): Semigroup<Long> = LongMonoid
+}
+
+object LongOrderInstanceImplicits {
+    @JvmStatic fun instance(): LongOrderInstance = LongOrderInstance
 }
 
 object ShortMonoidInstanceImplicits {
@@ -30,12 +38,20 @@ object ShortSemigroupInstanceImplicits {
     @JvmStatic fun instance(): Semigroup<Short> = ShortMonoid
 }
 
+object ShortOrderInstanceImplicits {
+    @JvmStatic fun instance(): ShortOrderInstance = ShortOrderInstance
+}
+
 object ByteMonoidInstanceImplicits {
     @JvmStatic fun instance(): ByteMonoid = ByteMonoid
 }
 
 object ByteSemigroupInstanceImplicits {
     @JvmStatic fun instance(): Semigroup<Byte> = ByteMonoid
+}
+
+object ByteOrderInstanceImplicits {
+    @JvmStatic fun instance(): ByteOrderInstance = ByteOrderInstance
 }
 
 object DoubleMonoidInstanceImplicits {
@@ -46,10 +62,18 @@ object DoubleSemigroupInstanceImplicits {
     @JvmStatic fun instance(): Semigroup<Double> = DoubleMonoid
 }
 
+object DoubleOrderInstanceImplicits {
+    @JvmStatic fun instance(): DoubleOrderInstance = DoubleOrderInstance
+}
+
 object FloatMonoidInstanceImplicits {
     @JvmStatic fun instance(): FloatMonoid = FloatMonoid
 }
 
 object FloatSemigroupInstanceImplicits {
     @JvmStatic fun instance(): Semigroup<Float> = FloatMonoid
+}
+
+object FloatOrderInstanceImplicits {
+    @JvmStatic fun instance(): FloatOrderInstance = FloatOrderInstance
 }
