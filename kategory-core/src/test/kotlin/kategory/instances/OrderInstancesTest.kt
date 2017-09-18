9a0ee460f63f510b7501daab5c2705929ad4eb54
Simon Vergauwen <vergauwen.simon@gmail.com>
2017-09-18 10:09:58 +0200

    Order laws

--- kategory-core/src/test/kotlin/kategory/instances/OrderInstancesTest.kt
+++ kategory-core/src/test/kotlin/kategory/instances/OrderInstancesTest.kt
@@ -2,13 +2,25 @@ package kategory.instances
 
 import io.kotlintest.KTestJUnitRunner
 import io.kotlintest.matchers.shouldNotBe
+import io.kotlintest.properties.Gen
+import kategory.OrderLaws
 import kategory.UnitSpec
+import kategory.genFunctionAToB
 import kategory.order
 import org.junit.runner.RunWith
 
 @RunWith(KTestJUnitRunner::class)
 class OrderInstancesTest : UnitSpec() {
     init {
+
+        testLaws(
+                OrderLaws.laws(
+                        O = order(),
+                        fGen = Gen.int(),
+                        funcGen = genFunctionAToB(Gen.int())
+                )
+        )
+
         "instances can be resolved implicitly" {
             order<Byte>() shouldNotBe null
             order<Short>() shouldNotBe null
