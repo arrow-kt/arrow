nomisRev/kategory

Update docs to reflect polymorphic optics

kategory-optics/src/main/kotlin/kategory/optics/Getter.kt
@@ -10,96 +10,105 @@ import kategory.some
 import kategory.toT
 
 /**
- * A [Getter] can be seen as a glorified get method between a type A and a type B.
+ * A [Getter] is an optic that allows to see into a structure and getting a target.
  *
- * @param A the source of a [Getter]
- * @param B the target of a [Getter]
+ * A [Getter] can be seen as a get function:
+ * - `get: (S) -> A` meaning we can look into an `S` and get an `A`
+ *
+ * @param S the source of a [Getter]
+ * @param A the target of a [Getter]
  */
-abstract class Getter<A, B> {
+abstract class Getter<S, A> {
     /**
      * Get the target of a [Getter]
      */
-    abstract fun get(a: A): B
+    abstract fun get(a: S): A
 
     companion object {
 
-        fun <A> id() = Iso.id<A>().asGetter()
+        fun <S> id() = Iso.id<S>().asGetter()
 
-        fun <A> codiagonal(): Getter<Either<A, A>, A> = Getter { aa -> aa.fold(::identity, ::identity) }
+        /**
+         * [Getter] that takes either A or A and strips the choice of A.
+         */
+        fun <S> codiagonal(): Getter<Either<S, S>, S> = Getter { aa -> aa.fold(::identity, ::identity) }
 
-        operator fun <A, B> invoke(get: (A) -> B) = object : Getter<A, B>() {
-            override fun get(a: A): B = get(a)
+        /**
+         * Invoke operator overload to create a [Getter] of type `S` with target `A`.
+         */
+        operator fun <S, A> invoke(get: (S) -> A) = object : Getter<S, A>() {
+            override fun get(a: S): A = get(a)
         }
     }
 
     /**
      * Find if the target satisfies the predicate.
      */
-    inline fun find(a: A, crossinline p: (B) -> Boolean): Option<B> = get(a).let { b ->
+    inline fun find(a: S, crossinline p: (A) -> Boolean): Option<A> = get(a).let { b ->
             if (p(b)) b.some() else none()
         }
 
     /**
      * Check if the target satisfies the predicate
      */
-    inline fun exist(a: A, crossinline p: (B) -> Boolean): Boolean = p(get(a))
+    inline fun exist(a: S, crossinline p: (A) -> Boolean): Boolean = p(get(a))
 
     /**
      * join two [Getter] with the same target
      */
-    fun <C> choice(other: Getter<C, B>): Getter<Either<A, C>, B> = Getter { a ->
+    fun <C> choice(other: Getter<C, A>): Getter<Either<S, C>, A> = Getter { a ->
         a.fold(this::get, other::get)
     }
 
     /**
      * Pair two disjoint [Getter]
      */
-    fun <C, D> split(other: Getter<C, D>): Getter<Tuple2<A, C>, Tuple2<B, D>> = Getter { (a, c) ->
+    fun <C, D> split(other: Getter<C, D>): Getter<Tuple2<S, C>, Tuple2<A, D>> = Getter { (a, c) ->
         get(a) toT other.get(c)
     }
 
-    fun <C> zip(other: Getter<A, C>): Getter<A, Tuple2<B, C>> = Getter { a ->
+    fun <C> zip(other: Getter<S, C>): Getter<S, Tuple2<A, C>> = Getter { a ->
         get(a) toT other.get(a)
     }
 
-    fun <C> first(): Getter<Tuple2<A, C>, Tuple2<B, C>> = Getter { (a, c) ->
+    fun <C> first(): Getter<Tuple2<S, C>, Tuple2<A, C>> = Getter { (a, c) ->
         get(a) toT c
     }
 
-    fun <C> second(): Getter<Tuple2<C, A>, Tuple2<C, B>> = Getter { (c, a) ->
+    fun <C> second(): Getter<Tuple2<C, S>, Tuple2<C, A>> = Getter { (c, a) ->
         c toT get(a)
     }
 
-    fun <C> left(): Getter<Either<A, C>, Either<B, C>> = Getter { ac ->
+    fun <C> left(): Getter<Either<S, C>, Either<A, C>> = Getter { ac ->
         ac.bimap(this::get, ::identity)
     }
 
-    fun <C> right(): Getter<Either<C, A>, Either<C, B>> = Getter { ca ->
+    fun <C> right(): Getter<Either<C, S>, Either<C, A>> = Getter { ca ->
         ca.map(this::get)
     }
 
     /**
      * Compose a [Getter] with a [Getter]
      */
-    infix fun <C> composeGetter(other: Getter<B, C>): Getter<A, C> = Getter(other::get compose this::get)
+    infix fun <C> composeGetter(other: Getter<A, C>): Getter<S, C> = Getter(other::get compose this::get)
 
     /**
      * Compose a [Getter] with a [Lens]
      */
-    infix fun <C> composeLens(other: Lens<B,C>): Getter<A,C> = Getter(other::get compose this::get)
+    infix fun <C> composeLens(other: Lens<A,C>): Getter<S,C> = Getter(other::get compose this::get)
 
     /**
      * Compose a [Getter] with a [Iso]
      */
-    infix fun <C> composeIso(other: Iso<B,C>): Getter<A,C> = Getter(other::get compose this::get)
+    infix fun <C> composeIso(other: Iso<A,C>): Getter<S,C> = Getter(other::get compose this::get)
 
     /**
      * Plus operator overload to compose optionals
      */
-    operator fun <C> plus(other: Getter<B, C>): Getter<A, C> = composeGetter(other)
+    operator fun <C> plus(other: Getter<A, C>): Getter<S, C> = composeGetter(other)
 
-    operator fun <C> plus(other: Lens<B,C>): Getter<A, C> = composeLens(other)
+    operator fun <C> plus(other: Lens<A,C>): Getter<S, C> = composeLens(other)
 
-    operator fun <C> plus(other: Iso<B,C>): Getter<A, C> = composeIso(other)
+    operator fun <C> plus(other: Iso<A,C>): Getter<S, C> = composeIso(other)
 
 }

kategory-optics/src/main/kotlin/kategory/optics/Prism.kt
@@ -5,32 +5,41 @@ import kategory.Either
 import kategory.Eq
 import kategory.HK
 import kategory.Option
+import kategory.PartialFunction
 import kategory.Tuple2
 import kategory.compose
 import kategory.eq
 import kategory.flatMap
 import kategory.identity
 import kategory.left
+import kategory.lift
 import kategory.none
 import kategory.right
 import kategory.some
 import kategory.toT
 
+/**
+ * [Prism] is a type alias for [PPrism] which fixes the type arguments
+ * and restricts the [PPrism] to monomorphic updates.
+ */
 typealias Prism<S, A> = PPrism<S, S, A, A>
 
 /**
- * A [PPrism] can be seen as a pair of functions: `reverseGet : B -> A` and `getOrModify: A -> Either<A,B>`
+ * A [Prism] is a lossless invertible optic that can look into a structure and reach either no targets or exactly one target.
+ * i.e. generalization of finding a target in a sum type (sealed class) Prism<SumType, SumType.SubType>
+ * or safe casting Prism<Double, Int>.
  *
- * - `reverseGet : B -> A` get the source type of a [PPrism]
- * - `getOrModify: A -> Either<A,B>` get the target of a [PPrism] or return the original value
+ * A (polymorphic) [PPrism] is useful when setting or modifying a value for a polymorphic sum type
+ * i.e. PPrism<Try<Sting>, Try<Int>, String, Int>
  *
- * It encodes the relation between a Sum or CoProduct type (sealed class) and one of its element.
+ * A [PPrism] gathers the two concepts of pattern matching and constructor and thus can be seen as a pair of functions:
+ * - `getOrModify: A -> Either<A, B>` meaning we can get the target of a [PPrism] OR return the original value
+ * - `reverseGet : B -> A` meaning we can construct the source type of a [PPrism] from a `B`
  *
- * @param A the source of a [PPrism]
- * @param B the target of a [PPrism]
- * @property getOrModify from an `B` we can produce an `A`
- * @property reverseGet get the target of a [PPrism] or return the original value
- * @constructor Creates a Lens of type `A` with target `B`
+ * @param S the source of a [PPrism]
+ * @param T the modified source of a [PPrism]
+ * @param A the target of a [PPrism]
+ * @param B the modified target of a [PPrism]
  */
 abstract class PPrism<S, T, A, B> {
 
@@ -41,6 +50,10 @@ abstract class PPrism<S, T, A, B> {
 
         fun <A> id() = Iso.id<A>().asPrism()
 
+        /**
+         * Invoke operator overload to create a [PPrism] of type `S` with target `A`.
+         * Can also be used to construct [Prism]
+         */
         operator fun <S, T, A, B> invoke(getOrModify: (S) -> Either<T, A>, reverseGet: (B) -> T) = object : PPrism<S, T, A, B>() {
             override fun getOrModify(s: S): Either<T, A> = getOrModify(s)
 
@@ -48,7 +61,7 @@ abstract class PPrism<S, T, A, B> {
         }
 
         /**
-         * a [PPrism] that checks for equality with a given value
+         * A [PPrism] that checks for equality with a given value
          */
         inline fun <reified A> only(a: A, EQA: Eq<A> = eq()): Prism<A, Unit> = Prism(
                 getOrModify = { a2 -> (if (EQA.eqv(a, a2)) a.left() else Unit.right()) },
@@ -174,7 +187,7 @@ abstract class PPrism<S, T, A, B> {
     /**
      * View a [PPrism] as a [PSetter]
      */
-    fun asSetter(): PSetter<S, T, A, B> = PSetter { f -> { s -> modify(s,f) } }
+    fun asSetter(): PSetter<S, T, A, B> = PSetter { f -> { s -> modify(s, f) } }
 
 }
 

arrow-kt/arrow

kategory-optics/src/main/kotlin/kategory/optics/Getter.kt
@@ -10,96 +10,120 @@ import kategory.some
 import kategory.toT
 
 /**
- * A [Getter] can be seen as a glorified get method between a type A and a type B.
+ * A [Getter] is an optic that allows to see into a structure and getting a target.
  *
- * @param A the source of a [Getter]
- * @param B the target of a [Getter]
+ * A [Getter] can be seen as a get function:
+ * - `get: (S) -> A` meaning we can look into an `S` and get an `A`
+ *
+ * @param S the source of a [Getter]
+ * @param A the target of a [Getter]
  */
-abstract class Getter<A, B> {
+abstract class Getter<S, A> {
     /**
      * Get the target of a [Getter]
      */
-    abstract fun get(a: A): B
+    abstract fun get(s: S): A
 
     companion object {
 
-        fun <A> id() = Iso.id<A>().asGetter()
+        fun <S> id() = Iso.id<S>().asGetter()
 
-        fun <A> codiagonal(): Getter<Either<A, A>, A> = Getter { aa -> aa.fold(::identity, ::identity) }
+        /**
+         * [Getter] that takes either [S] or [S] and strips the choice of [S].
+         */
+        fun <S> codiagonal(): Getter<Either<S, S>, S> = Getter { aa -> aa.fold(::identity, ::identity) }
 
-        operator fun <A, B> invoke(get: (A) -> B) = object : Getter<A, B>() {
-            override fun get(a: A): B = get(a)
+        /**
+         * Invoke operator overload to create a [Getter] of type `S` with target `A`.
+         */
+        operator fun <S, A> invoke(get: (S) -> A) = object : Getter<S, A>() {
+            override fun get(s: S): A = get(s)
         }
     }
 
     /**
      * Find if the target satisfies the predicate.
      */
-    inline fun find(a: A, crossinline p: (B) -> Boolean): Option<B> = get(a).let { b ->
+    inline fun find(s: S, crossinline p: (A) -> Boolean): Option<A> = get(s).let { b ->
             if (p(b)) b.some() else none()
         }
 
     /**
      * Check if the target satisfies the predicate
      */
-    inline fun exist(a: A, crossinline p: (B) -> Boolean): Boolean = p(get(a))
+    inline fun exist(s: S, crossinline p: (A) -> Boolean): Boolean = p(get(s))
 
     /**
-     * join two [Getter] with the same target
+     * Join two [Getter] with the same target
      */
-    fun <C> choice(other: Getter<C, B>): Getter<Either<A, C>, B> = Getter { a ->
-        a.fold(this::get, other::get)
+    fun <C> choice(other: Getter<C, A>): Getter<Either<S, C>, A> = Getter { s ->
+        s.fold(this::get, other::get)
     }
 
     /**
      * Pair two disjoint [Getter]
      */
-    fun <C, D> split(other: Getter<C, D>): Getter<Tuple2<A, C>, Tuple2<B, D>> = Getter { (a, c) ->
-        get(a) toT other.get(c)
+    fun <C, D> split(other: Getter<C, D>): Getter<Tuple2<S, C>, Tuple2<A, D>> = Getter { (s, c) ->
+        get(s) toT other.get(c)
     }
 
-    fun <C> zip(other: Getter<A, C>): Getter<A, Tuple2<B, C>> = Getter { a ->
-        get(a) toT other.get(a)
+    /**
+     * Zip two [Getter] optics with the same source [S]
+     */
+    fun <C> zip(other: Getter<S, C>): Getter<S, Tuple2<A, C>> = Getter { s ->
+        get(s) toT other.get(s)
     }
 
-    fun <C> first(): Getter<Tuple2<A, C>, Tuple2<B, C>> = Getter { (a, c) ->
-        get(a) toT c
+    /**
+     * Create a product of the [Getter] and a type [C]
+     */
+    fun <C> first(): Getter<Tuple2<S, C>, Tuple2<A, C>> = Getter { (s, c) ->
+        get(s) toT c
     }
 
-    fun <C> second(): Getter<Tuple2<C, A>, Tuple2<C, B>> = Getter { (c, a) ->
-        c toT get(a)
+    /**
+     * Create a product of type [C] and the [Getter]
+     */
+    fun <C> second(): Getter<Tuple2<C, S>, Tuple2<C, A>> = Getter { (c, s) ->
+        c toT get(s)
     }
 
-    fun <C> left(): Getter<Either<A, C>, Either<B, C>> = Getter { ac ->
-        ac.bimap(this::get, ::identity)
+    /**
+     * Create a sum of the [Getter] and type [C]
+     */
+    fun <C> left(): Getter<Either<S, C>, Either<A, C>> = Getter { sc ->
+        sc.bimap(this::get, ::identity)
     }
 
-    fun <C> right(): Getter<Either<C, A>, Either<C, B>> = Getter { ca ->
-        ca.map(this::get)
+    /**
+     * Create a sum of type [C] and the [Getter]
+     */
+    fun <C> right(): Getter<Either<C, S>, Either<C, A>> = Getter { cs ->
+        cs.map(this::get)
     }
 
     /**
      * Compose a [Getter] with a [Getter]
      */
-    infix fun <C> composeGetter(other: Getter<B, C>): Getter<A, C> = Getter(other::get compose this::get)
+    infix fun <C> composeGetter(other: Getter<A, C>): Getter<S, C> = Getter(other::get compose this::get)
 
     /**
      * Compose a [Getter] with a [Lens]
      */
-    infix fun <C> composeLens(other: Lens<B,C>): Getter<A,C> = Getter(other::get compose this::get)
+    infix fun <C> composeLens(other: Lens<A,C>): Getter<S,C> = Getter(other::get compose this::get)
 
     /**
      * Compose a [Getter] with a [Iso]
      */
-    infix fun <C> composeIso(other: Iso<B,C>): Getter<A,C> = Getter(other::get compose this::get)
+    infix fun <C> composeIso(other: Iso<A,C>): Getter<S,C> = Getter(other::get compose this::get)
 
     /**
      * Plus operator overload to compose optionals
      */
-    operator fun <C> plus(other: Getter<B, C>): Getter<A, C> = composeGetter(other)
+    operator fun <C> plus(other: Getter<A, C>): Getter<S, C> = composeGetter(other)
 
-    operator fun <C> plus(other: Lens<B,C>): Getter<A, C> = composeLens(other)
+    operator fun <C> plus(other: Lens<A,C>): Getter<S, C> = composeLens(other)
 
-    operator fun <C> plus(other: Iso<B,C>): Getter<A, C> = composeIso(other)
+    operator fun <C> plus(other: Iso<A,C>): Getter<S, C> = composeIso(other)
 
 }

kategory-optics/src/main/kotlin/kategory/optics/Prism.kt
@@ -6,6 +6,7 @@ import kategory.Eq
 import kategory.HK
 import kategory.Option
 import kategory.Tuple2
+import kategory.applicative
 import kategory.compose
 import kategory.eq
 import kategory.flatMap
@@ -16,21 +17,28 @@ import kategory.right
 import kategory.some
 import kategory.toT
 
+/**
+ * [Prism] is a type alias for [PPrism] which fixes the type arguments
+ * and restricts the [PPrism] to monomorphic updates.
+ */
 typealias Prism<S, A> = PPrism<S, S, A, A>
 
 /**
- * A [PPrism] can be seen as a pair of functions: `reverseGet : B -> A` and `getOrModify: A -> Either<A,B>`
+ * A [Prism] is a lossless invertible optic that can look into a structure and reach either no targets or exactly one target.
+ * i.e. generalization of finding a target in a sum type (sealed class) Prism<SumType, SumType.SubType>
+ * or safe casting Prism<Double, Int>.
  *
- * - `reverseGet : B -> A` get the source type of a [PPrism]
- * - `getOrModify: A -> Either<A,B>` get the target of a [PPrism] or return the original value
+ * A (polymorphic) [PPrism] is useful when setting or modifying a value for a polymorphic sum type
+ * i.e. PPrism<Try<Sting>, Try<Int>, String, Int>
  *
- * It encodes the relation between a Sum or CoProduct type (sealed class) and one of its element.
+ * A [PPrism] gathers the two concepts of pattern matching and constructor and thus can be seen as a pair of functions:
+ * - `getOrModify: A -> Either<A, B>` meaning we can get the target of a [PPrism] OR return the original value
+ * - `reverseGet : B -> A` meaning we can construct the source type of a [PPrism] from a `B`
  *
- * @param A the source of a [PPrism]
- * @param B the target of a [PPrism]
- * @property getOrModify from an `B` we can produce an `A`
- * @property reverseGet get the target of a [PPrism] or return the original value
- * @constructor Creates a Lens of type `A` with target `B`
+ * @param S the source of a [PPrism]
+ * @param T the modified source of a [PPrism]
+ * @param A the target of a [PPrism]
+ * @param B the modified target of a [PPrism]
  */
 abstract class PPrism<S, T, A, B> {
 
@@ -39,8 +47,12 @@ abstract class PPrism<S, T, A, B> {
 
     companion object {
 
-        fun <A> id() = Iso.id<A>().asPrism()
+        fun <S> id() = Iso.id<S>().asPrism()
 
+        /**
+         * Invoke operator overload to create a [PPrism] of type `S` with target `A`.
+         * Can also be used to construct [Prism]
+         */
         operator fun <S, T, A, B> invoke(getOrModify: (S) -> Either<T, A>, reverseGet: (B) -> T) = object : PPrism<S, T, A, B>() {
             override fun getOrModify(s: S): Either<T, A> = getOrModify(s)
 
@@ -48,7 +60,7 @@ abstract class PPrism<S, T, A, B> {
         }
 
         /**
-         * a [PPrism] that checks for equality with a given value
+         * A [PPrism] that checks for equality with a given value
          */
         inline fun <reified A> only(a: A, EQA: Eq<A> = eq()): Prism<A, Unit> = Prism(
                 getOrModify = { a2 -> (if (EQA.eqv(a, a2)) a.left() else Unit.right()) },
@@ -58,14 +70,14 @@ abstract class PPrism<S, T, A, B> {
     }
 
     /**
-     * Get the target or nothing if `A` does not match the target
+     * Get the target or nothing if [S] does not match the target
      */
-    fun getOption(a: S): Option<A> = getOrModify(a).toOption()
+    fun getOption(s: S): Option<A> = getOrModify(s).toOption()
 
     /**
      * Modify the target of a [PPrism] with an Applicative function
      */
-    inline fun <reified F> modifyF(FA: Applicative<F> = kategory.applicative(), crossinline f: (A) -> HK<F, B>, s: S): HK<F, T> = getOrModify(s).fold(
+    inline fun <reified F> modifyF(FA: Applicative<F> = applicative(), s: S, crossinline f: (A) -> HK<F, B>): HK<F, T> = getOrModify(s).fold(
             FA::pure,
             { FA.map(f(it), this::reverseGet) }
     )
@@ -101,7 +113,7 @@ abstract class PPrism<S, T, A, B> {
     fun isEmpty(s: S): Boolean = !nonEmpty(s)
 
     /**
-     * Find if the target satisfies the predicate
+     * Find the target that satisfies the predicate
      */
     inline fun find(s: S, crossinline p: (A) -> Boolean): Option<A> = getOption(s).flatMap { a -> if (p(a)) a.some() else none() }
 
@@ -116,7 +128,7 @@ abstract class PPrism<S, T, A, B> {
     inline fun all(s: S, crossinline p: (A) -> Boolean): Boolean = getOption(s).fold({ true }, p)
 
     /**
-     * Create a product of the target and a type C
+     * Create a product of the [PPrism] and a type [C]
      */
     fun <C> first(): PPrism<Tuple2<S, C>, Tuple2<T, C>, Tuple2<A, C>, Tuple2<B, C>> = PPrism(
             { (s, c) -> getOrModify(s).bimap({ it toT c }, { it toT c }) },
@@ -124,7 +136,7 @@ abstract class PPrism<S, T, A, B> {
     )
 
     /**
-     * Create a product of a type C and the target
+     * Create a product of a type [C] and the [PPrism]
      */
     fun <C> second(): PPrism<Tuple2<C, S>, Tuple2<C, T>, Tuple2<C, A>, Tuple2<C, B>> = PPrism(
             { (c, s) -> getOrModify(s).bimap({ c toT it }, { c toT it }) },
@@ -174,12 +186,12 @@ abstract class PPrism<S, T, A, B> {
     /**
      * View a [PPrism] as a [PSetter]
      */
-    fun asSetter(): PSetter<S, T, A, B> = PSetter { f -> { s -> modify(s,f) } }
+    fun asSetter(): PSetter<S, T, A, B> = PSetter { f -> { s -> modify(s, f) } }
 
 }
 
 /**
- * Create a sum of the target and a type C
+ * Create a sum of the [PPrism] and a type [C]
  */
 fun <S, T, A, B, C> PPrism<S, T, A, B>.left(): PPrism<Either<S, C>, Either<T, C>, Either<A, C>, Either<B, C>> = Prism(
         { it.fold({ a -> getOrModify(a).bimap({ it.left() }, { it.left() }) }, { c -> Either.Right(c.right()) }) },
@@ -192,7 +204,7 @@ fun <S, T, A, B, C> PPrism<S, T, A, B>.left(): PPrism<Either<S, C>, Either<T, C>
 )
 
 /**
- * Create a sum of a type C and the target
+ * Create a sum of a type [C] and the [PPrism]
  */
 fun <S, T, A, B, C> PPrism<S, T, A, B>.right(): PPrism<Either<C, S>, Either<C, T>, Either<C, A>, Either<C, B>> = Prism(
         { it.fold({ c -> Either.Right(c.left()) }, { s -> getOrModify(s).bimap({ it.right() }, { it.right() }) }) },