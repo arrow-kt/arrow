6eab419c4f34f68c9a417d52d1e260c1b2e50f5a
pakoito <jueces.jus@gmail.com>
2017-09-25 22:11:39 +0100

    Re-enable test

--- kategory-core/src/test/kotlin/kategory/data/ListKWTest.kt
+++ kategory-core/src/test/kotlin/kategory/data/ListKWTest.kt
@@ -29,12 +29,12 @@ class ListKWTest : UnitSpec() {
         testLaws(MonoidKLaws.laws(ListKW.monoidK(), applicative, Eq.any()))
         testLaws(TraverseLaws.laws(ListKW.traverse(), applicative, { n: Int -> ListKW(listOf(n)) }, Eq.any()))
 
-        /*testLaws(MonadCombineLaws.laws(ListKW.monadCombine(),
+        testLaws(MonadCombineLaws.laws(ListKW.monadCombine(),
                 { n -> ListKW(listOf(n)) },
                 { n -> ListKW(listOf({ s: Int -> n * s })) },
                 object : Eq<HK<ListKWHK, Int>> {
                     override fun eqv(a: HK<ListKWHK, Int>, b: HK<ListKWHK, Int>): Boolean =
                             a.ev().list == b.ev().list
-                }))*/
+                }))
     }
 }
