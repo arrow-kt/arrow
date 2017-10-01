28bbcaeb0a622c43232eb11da000f4f19d7aff72
pakoito <jueces.jus@gmail.com>
2017-10-01 21:06:14 +0100

    Add identity law for Eq

--- kategory-test/src/main/kotlin/kategory/laws/EqLaws.kt
+++ kategory-test/src/main/kotlin/kategory/laws/EqLaws.kt
@@ -8,8 +8,15 @@ object EqLaws {
 
     inline fun <reified F> laws(EQ: Eq<F> = eq<F>(), noinline cf: (Int) -> F): List<Law> =
             listOf(
+                    Law("Eq Laws: identity", { identityEquality(EQ, cf) }),
                     Law("Eq Laws: commutativity", { commutativeEquality(EQ, cf) })
             )
+
+    inline fun <reified F> identityEquality(EQ: Eq<F> = eq(), crossinline cf: (Int) -> F): Unit =
+            forAll(Gen.int(), { int: Int ->
+                val a = cf(int)
+                EQ.eqv(a, a) == EQ.eqv(a, a)
+            })
 
     inline fun <reified F> commutativeEquality(EQ: Eq<F> = eq(), crossinline cf: (Int) -> F): Unit =
             forAll(Gen.int(), { int: Int ->
