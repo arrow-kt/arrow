a1fa8e31bc95f672a979cc42b0afa9632d5c27b2
Simon Vergauwen <vergauwen.simon@gmail.com>
2017-10-22 21:10:12 +0200

    Iso instances Option

--- kategory-optics/src/test/kotlin/kategory/optics/data/OptionInstancesTest.kt
+++ kategory-optics/src/test/kotlin/kategory/optics/data/OptionInstancesTest.kt
@@ -2,15 +2,25 @@ package kategory.optics
 
 import io.kotlintest.KTestJUnitRunner
 import io.kotlintest.properties.Gen
+import kategory.Either
 import kategory.Eq
 import kategory.IntMonoid
 import kategory.IsoLaws
+import kategory.Monoid
 import kategory.OptionMonoidInstanceImplicits
 import kategory.PrismLaws
 import kategory.UnitSpec
+import kategory.applicative
+import kategory.binding
+import kategory.ev
+import kategory.flatMap
+import kategory.genEither
 import kategory.genFunctionAToB
 import kategory.genNullable
 import kategory.genOption
+import kategory.monad
+import kategory.pure
+import kategory.right
 import org.junit.runner.RunWith
 
 @RunWith(KTestJUnitRunner::class)
@@ -46,6 +56,21 @@ class OptionInstancesTest : UnitSpec() {
                 EQA = Eq.any(),
                 EQB = Eq.any(),
                 EQOptionB = Eq.any()
+        ))
+
+        testLaws(IsoLaws.laws(
+                iso = optionToEither(),
+                aGen = genOption(Gen.int()),
+                bGen = genEither(Gen.create { Unit }, Gen.int()),
+                funcGen = genFunctionAToB(genEither(Gen.create { Unit }, Gen.int())),
+                EQA = Eq.any(),
+                EQB = Eq.any(),
+                bMonoid = object : Monoid<Either<Unit, Int>> {
+                    override fun combine(a: Either<Unit, Int>, b: Either<Unit, Int>): Either<Unit, Int> =
+                            Either.applicative<Unit>().map2(a, b) { (a, b) -> a + b }.ev()
+
+                    override fun empty(): Either<Unit, Int> = 0.right()
+                }
         ))
 
     }
