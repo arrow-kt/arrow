5b3b2a97d747c91e01c4a4acb1930f6d8f9f4317
pakoito <jueces.jus@gmail.com>
2017-11-24 23:53:23 +0000

    Fixes all around

--- kategory-effects-android-architecture-components/src/main/kotlin/kategory/effects/data/LiveDataKW.kt
+++ kategory-effects-android-architecture-components/src/main/kotlin/kategory/effects/data/LiveDataKW.kt
@@ -3,10 +3,11 @@ package kategory.effects
 import kategory.*
 import kotlin.coroutines.experimental.CoroutineContext
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 
 fun <A> LiveData<A>.k() = LiveDataKW(this)
 
-fun <A> LiveDataKWKind<A>.value() = (this as LiveDataKW<A>).getValue()
+fun <A> LiveDataKWKind<A>.value(): LiveData<A> = (this as LiveDataKW<A>).liveData
 
 @higherkind
 @deriving(
@@ -14,23 +15,23 @@ fun <A> LiveDataKWKind<A>.value() = (this as LiveDataKW<A>).getValue()
         Applicative::class,
         Monad::class
 )
-data class LiveDataKW<A>(val liveData: LiveData<A>) : LiveDataKWKind<A>, LiveData<A>() by liveData {
+data class LiveDataKW<A>(val liveData: LiveData<A>) : LiveDataKWKind<A> {
 
     fun <B> map(f: (A) -> B): LiveDataKW<B> =
             android.arch.lifecycle.Transformations.map<A, B>(liveData, { f(it) }).k()
 
-    fun <B> ap(fa: LiveDataKW<(A) -> B>): LiveDataKW<B> =
+    fun <B> ap(fa: LiveDataKWKind<(A) -> B>): LiveDataKW<B> =
             this.flatMap { a: A -> fa.ev().map { ff: (A) -> B -> ff(a) } }
 
     fun <B> flatMap(f: (A) -> LiveDataKWKind<B>): LiveDataKW<B> =
-            android.arch.lifecycle.Transformations.switchMap<A, B>(this, { value: A -> f(value).ev() }).k()
+            android.arch.lifecycle.Transformations.switchMap<A, B>(liveData, { value: A -> f(value).value() }).k()
 
     companion object {
         fun <A> pure(a: A): LiveDataKW<A> =
-                LiveDataKW<A>(LiveData()).apply { setValue(a) }
+                LiveDataKW<A>(MutableLiveData<A>().apply { postValue(a) })
 
         fun <A, B> tailRecM(a: A, f: (A) -> LiveDataKWKind<Either<A, B>>): LiveDataKW<B> =
-                LiveDataKW()
+                LiveDataKW(MutableLiveData())
         /*        f(a).value().let { initial: LiveData<Either<A, B>> ->
                     var current: LiveData<Either<A, B>> = initial
                     async(Unconfined, CoroutineStart.LAZY) {
