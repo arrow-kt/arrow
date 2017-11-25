068ac7dd5c2bc4f986818276f2a630b3119abc89
pakoito <jueces.jus@gmail.com>
2017-11-25 02:37:36 +0000

    Add implementation of tailRecM

--- kategory-effects-android-architecture-components/src/main/kotlin/kategory/effects/data/LiveDataKW.kt
+++ kategory-effects-android-architecture-components/src/main/kotlin/kategory/effects/data/LiveDataKW.kt
@@ -4,6 +4,7 @@ import kategory.*
 import kotlin.coroutines.experimental.CoroutineContext
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
 
 fun <A> LiveData<A>.k() = LiveDataKW(this)
 
@@ -18,36 +19,21 @@ fun <A> LiveDataKWKind<A>.value(): LiveData<A> = (this as LiveDataKW<A>).liveDat
 data class LiveDataKW<A>(val liveData: LiveData<A>) : LiveDataKWKind<A> {
 
     fun <B> map(f: (A) -> B): LiveDataKW<B> =
-            android.arch.lifecycle.Transformations.map<A, B>(liveData, { f(it) }).k()
+            Transformations.map(liveData, { f(it) }).k()
 
     fun <B> ap(fa: LiveDataKWKind<(A) -> B>): LiveDataKW<B> =
             this.flatMap { a: A -> fa.ev().map { ff: (A) -> B -> ff(a) } }
 
     fun <B> flatMap(f: (A) -> LiveDataKWKind<B>): LiveDataKW<B> =
-            android.arch.lifecycle.Transformations.switchMap<A, B>(liveData, { value: A -> f(value).value() }).k()
+            Transformations.switchMap(liveData, { value: A -> f(value).value() }).k()
 
     companion object {
         fun <A> pure(a: A): LiveDataKW<A> =
-                LiveDataKW<A>(MutableLiveData<A>().apply { postValue(a) })
+                LiveDataKW(MutableLiveData<A>().apply { setValue(a) })
 
         fun <A, B> tailRecM(a: A, f: (A) -> LiveDataKWKind<Either<A, B>>): LiveDataKW<B> =
-                LiveDataKW(MutableLiveData())
-        /*        f(a).value().let { initial: LiveData<Either<A, B>> ->
-                    var current: LiveData<Either<A, B>> = initial
-                    async(Unconfined, CoroutineStart.LAZY) {
-                        val result: B
-                        while (true) {
-                            val actual: Either<A, B> = current.await()
-                            if (actual is Right) {
-                                result = actual.b
-                                break
-                            } else if (actual is Left) {
-                                current = f(actual.a).ev()
-                            }
-                        }
-                        result
-                    }.k()
+                f(a).ev().flatMap {
+                    it.fold({ tailRecM(a, f).ev() }, { LiveDataKW.pure(it).ev() })
                 }
-                */
     }
 }
