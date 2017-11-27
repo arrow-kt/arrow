0e9eeaa080794dacde4ee21d1e662b77f39e8af2
Pablisco <pabs87@gmail.com>
2017-11-27 14:05:06 +0000

    Inlined pattern

--- deploy-scripts/deploy.sh
+++ deploy-scripts/deploy.sh
@@ -3,14 +3,11 @@
 
 echo "Branch '$TRAVIS_BRANCH'"
 
-VERSION_PATTERN_RELEASE=^[0-9]+\.[0-9]+\.[0-9]+$
-VERSION_PATTERN_SNAPSHOT=^[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT$
-
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    if [[ "$VERSION_NAME" =~ "$VERSION_PATTERN_RELEASE" ]]; then
+    if [[ "$VERSION_NAME" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
         echo "Starting script for Release $VERSION_NAME"
         . $(dirname $0)/deploy_release.sh
-    elif [[ "$VERSION_NAME" =~ "$VERSION_PATTERN_SNAPSHOT" ]]; then
+    elif [[ "$VERSION_NAME" =~ ^[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT$ ]]; then
         echo "Starting script for Snapshot Release $VERSION_NAME"
         . $(dirname $0)/deploy_snapshot.sh
     else
