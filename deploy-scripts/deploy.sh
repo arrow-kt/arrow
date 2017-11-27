56453c4d210c407d18e5890a2bca60739655eaac
Pablisco <pabs87@gmail.com>
2017-11-27 13:14:47 +0000

    Added extra debug logs for the deploy script

--- deploy-scripts/deploy.sh
+++ deploy-scripts/deploy.sh
@@ -8,9 +8,13 @@ VERSION_PATTERN_SNAPSHOT=^[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT$
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     if [[ "$VERSION_NAME" =~ "$VERSION_PATTERN_RELEASE" ]]; then
+        echo "Starting script for Release $VERSION_NAME"
         . $(dirname $0)/deploy_release.sh
     elif [[ "$VERSION_NAME" =~ "$VERSION_PATTERN_SNAPSHOT" ]]; then
+        echo "Starting script for Snapshot Release $VERSION_NAME"
         . $(dirname $0)/deploy_snapshot.sh
+    else
+        echo "No deploy script matched version '$VERSION_NAME' on master"
     fi
 else
     echo "Skipped deployment in branch '$TRAVIS_BRANCH'"
