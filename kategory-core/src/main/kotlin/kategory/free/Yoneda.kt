1f742d0108e5c8bf21474ae81e7b30c1f5bb73f5
pakoito <jueces.jus@gmail.com>
2017-12-10 17:56:22 +0000

    Add fixes to several warnings

--- kategory-core/src/main/kotlin/kategory/free/Yoneda.kt
+++ kategory-core/src/main/kotlin/kategory/free/Yoneda.kt
@@ -1,12 +1,14 @@
 package kategory
 
-@higherkind abstract class Yoneda<F, A> : YonedaKind<F, A> {
+@higherkind
+@deriving(Functor::class)
+abstract class Yoneda<F, A> : YonedaKind<F, A> {
 
     abstract operator fun <B> invoke(f: (A) -> B): HK<F, B>
 
     fun lower(): HK<F, A> = invoke { a -> a }
 
-    fun <B> map(ff: (A) -> B, FF: Functor<F>): Yoneda<F, B> =
+    fun <B> map(ff: (A) -> B): Yoneda<F, B> =
             object : Yoneda<F, B>() {
                 override fun <C> invoke(f: (B) -> C): HK<F, C> = this@Yoneda({ f(ff(it)) })
             }
--- kategory-core/src/main/kotlin/kategory/typeclasses/Bifoldable.kt
+++ kategory-core/src/main/kotlin/kategory/typeclasses/Bifoldable.kt
@@ -10,7 +10,7 @@ interface Bifoldable<F> : Typeclass {
             bifoldLeft(fab, MC.empty(), { c, a -> MC.combine(c, f(a)) }, { c, b -> MC.combine(c, g(b)) })
 }
 
-inline fun <F, A, B, reified C> Bifoldable<in F>.bifoldMap(fab: HK2<F, A, B>, noinline f: (A) -> C, noinline g: (B) -> C, MC: Monoid<C> = monoid()) =
+inline fun <F, A, B, reified C> Bifoldable<in F>.bifoldMap(MC: Monoid<C> = monoid(), fab: HK2<F, A, B>, noinline f: (A) -> C, noinline g: (B) -> C) =
         bifoldMap(fab, f, g, MC)
 
 inline fun <reified F> bifoldable(): Bifoldable<F> = instance(InstanceParametrizedType(Bifoldable::class.java, listOf(F::class.java)))
