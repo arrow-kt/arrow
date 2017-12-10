2529760eae8243a465e84e8bd33067b615e080fe
pakoito <jueces.jus@gmail.com>
2017-12-10 17:59:13 +0000

    Add default runner

--- kategory-kindedj/src/test/java/kategory/kindedj/fromkategory/KatDataclassTestJava.java
+++ kategory-kindedj/src/test/java/kategory/kindedj/fromkategory/KatDataclassTestJava.java
@@ -2,6 +2,8 @@ package kategory.kindedj.fromkategory;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import kategory.kindedj.Convert;
 import kategory.kindedj.KatDataclass1;
@@ -12,6 +14,7 @@ import kategory.kindedj.KatDataclass5;
 import kategory.kindedj.KatDataclassHK;
 import kategory.kindedj.KatDataclassKategoryShow;
 
+@RunWith(JUnit4.class)
 public class KatDataclassTestJava {
 
     private final KatDataclass1<Integer> kinded = new KatDataclass1<>(0);
