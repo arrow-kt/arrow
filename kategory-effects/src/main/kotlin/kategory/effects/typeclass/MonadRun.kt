c096b803380fadb18b29ba5ebb8b358725ff472a
pakoito <jueces.jus@gmail.com>
2017-12-15 20:03:00 +0000

    MonadRun is also an AsyncContext

--- kategory-effects/src/main/kotlin/kategory/effects/typeclass/MonadRun.kt
+++ kategory-effects/src/main/kotlin/kategory/effects/typeclass/MonadRun.kt
@@ -1,14 +1,14 @@
 package kategory.effects
 
-import kategory.Either
-import kategory.HK
-import kategory.MonadError
-import kategory.Typeclass
+import kategory.*
 
-interface MonadRun<F, E> : MonadError<F, E>, Typeclass {
+interface MonadRun<F, E> : AsyncContext<F>, MonadError<F, E>, Typeclass {
     fun <A> unsafeRunSync(fa: HK<F, A>): A
 
     fun <A> runAsync(fa: HK<F, A>, cb: (Either<E, A>) -> HK<F, Unit>): HK<F, Unit>
 
     fun <A> unsafeRunAsync(fa: HK<F, A>, cb: (Either<E, A>) -> Unit): Unit
 }
+
+inline fun <reified F, reified E> monadRun(): MonadRun<F, E> =
+        instance(InstanceParametrizedType(MonadRun::class.java, listOf(typeLiteral<F>(), typeLiteral<E>())))
