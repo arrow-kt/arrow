13653bae40f25f5e758d2a5d6f1a50e4393a04ec
pakoito <jueces.jus@gmail.com>
2017-12-15 19:54:55 +0000

    Remove RunAsync and RunSync

--- kategory-effects/src/main/kotlin/kategory/effects/typeclass/MonadRun.kt
+++ kategory-effects/src/main/kotlin/kategory/effects/typeclass/MonadRun.kt
@@ -1,6 +1,14 @@
 package kategory.effects
 
+import kategory.Either
+import kategory.HK
 import kategory.MonadError
 import kategory.Typeclass
 
-interface MonadRun<F, E> : MonadError<F, E>, RunAsync<F, E>, RunSync<F, E>, Typeclass
+interface MonadRun<F, E> : MonadError<F, E>, Typeclass {
+    fun <A> unsafeRunSync(fa: HK<F, A>): A
+
+    fun <A> runAsync(fa: HK<F, A>, cb: (Either<E, A>) -> HK<F, Unit>): HK<F, Unit>
+
+    fun <A> unsafeRunAsync(fa: HK<F, A>, cb: (Either<E, A>) -> Unit): Unit
+}
--- kategory-effects/src/main/kotlin/kategory/effects/typeclass/RunSync.kt
+++
@@ -1,9 +0,0 @@
-package kategory.effects
-
-import kategory.HK
-import kategory.MonadError
-import kategory.Typeclass
-
-interface RunSync<F, E> : MonadError<F, E>, Typeclass {
-    fun <A> unsafeRunSync(fa: HK<F, A>): A
-}
