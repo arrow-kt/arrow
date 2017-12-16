afbc5be4bb2c11558cf2e5ccfd4086866f0d2da2
pakoito <jueces.jus@gmail.com>
2017-12-16 15:07:53 +0000

    Add suspend, invoke, and lazy to MonadRun

--- kategory-effects/src/main/kotlin/kategory/effects/typeclass/MonadRun.kt
+++ kategory-effects/src/main/kotlin/kategory/effects/typeclass/MonadRun.kt
@@ -8,6 +8,13 @@ interface MonadRun<F, E> : MonadError<F, E>, Typeclass {
     fun <A> runAsync(fa: HK<F, A>, cb: (Either<E, A>) -> HK<F, Unit>): HK<F, Unit>
 
     fun <A> unsafeRunAsync(fa: HK<F, A>, cb: (Either<E, A>) -> Unit): Unit
+
+    fun <A> suspend(f: () -> HK<F, A>): HK<F, A>
+
+    operator fun <A> invoke(f: () -> A): HK<F, A> =
+            suspend { pure(f()) }
+
+    fun lazy(): HK<F, Unit> = invoke { }
 }
 
 inline fun <reified F, reified E> monadRun(): MonadRun<F, E> =
