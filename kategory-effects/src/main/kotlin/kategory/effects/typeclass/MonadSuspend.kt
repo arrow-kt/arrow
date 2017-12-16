a4c803893527c7eb217f5f7093ec2b5cf81712f3
pakoito <jueces.jus@gmail.com>
2017-12-16 15:40:58 +0000

    Move bindFiber to MonadSuspend

--- kategory-effects/src/main/kotlin/kategory/effects/typeclass/MonadSuspend.kt
+++ kategory-effects/src/main/kotlin/kategory/effects/typeclass/MonadSuspend.kt
@@ -1,6 +1,8 @@
 package kategory.effects
 
 import kategory.*
+import kategory.effects.data.internal.BindingCancellationException
+import kotlin.coroutines.experimental.startCoroutine
 
 interface MonadSuspend<F, E> : MonadError<F, E>, Typeclass {
     fun <A> unsafeRunSync(fa: HK<F, A>): A
@@ -15,6 +17,24 @@ interface MonadSuspend<F, E> : MonadError<F, E>, Typeclass {
             suspend { pure(f()) }
 
     fun lazy(): HK<F, Unit> = invoke { }
+}
+
+/**
+ * Entry point for monad bindings which enables for comprehensions. The underlying impl is based on coroutines.
+ * A coroutines is initiated and inside [MonadSuspendContinuation] suspended yielding to [Monad.flatMap]. Once all the flatMap binds are completed
+ * the underlying monad is returned from the act of executing the coroutine
+ *
+ * This one operates over [MonadSuspend] instances that can support [Throwable] in their error type automatically lifting
+ * errors as failed computations in their monadic context and not letting exceptions thrown as the regular monad binding does.
+ *
+ * This operation is cancellable by calling invoke on the [Fiber].
+ * If [invoke] is called the binding result will become a lifted [BindingCancellationException].
+ *
+ */
+fun <F, B> MonadSuspend<F, Throwable>.bindingFiber(AC: AsyncContext<F>, c: suspend MonadSuspendContinuation<F, *>.() -> HK<F, B>): Fiber<F, B> {
+    val continuation = MonadSuspendContinuation<F, B>(this, AC)
+    c.startCoroutine(continuation, continuation)
+    return Fiber(continuation.returnedMonad(), continuation.disposable())
 }
 
 inline fun <reified F, reified E> monadSuspend(): MonadSuspend<F, E> =
