57bb3ebeee4db757da529f5f9ded7c2015fbc6cb
Raul Raja <raulraja@gmail.com>
2017-12-18 16:13:06 +0100

    compile and test `funktionale-currying`

--- funktionale-currying/src/test/kotlin/org/funktionale/currying/CurryingTest.kt
+++ funktionale-currying/src/test/kotlin/org/funktionale/currying/CurryingTest.kt
@@ -16,29 +16,37 @@
 
 package org.funktionale.currying
 
-import org.testng.Assert.assertEquals
-import org.testng.Assert.assertTrue
-import org.testng.annotations.Test
+import io.kotlintest.KTestJUnitRunner
+import io.kotlintest.matchers.shouldBe
+import kategory.UnitSpec
+import org.junit.runner.RunWith
 
-class CurryingTest {
-    @Test fun testCurrying() {
-        val sum2ints = { x: Int, y: Int -> x + y }
-        val curried = sum2ints.curried()
-        assertEquals(curried(2)(4), 6)
-        val add5 = curried(5)
-        assertEquals(add5(7), 12)
-    }
+@RunWith(KTestJUnitRunner::class)
+class CurryingTest : UnitSpec() {
 
-    @Test fun testUncurrying() {
-        val sum2ints: (Int, Int) -> Int = { x, y -> x + y }
-        val curried: (Int) -> (Int) -> Int = sum2ints.curried()
-        assertEquals(curried(2)(4), 6)
-        //same type as sum2ints,
-        assertEquals(curried.uncurried()(2, 4), 6)
-        assertEquals(sum2ints(2, 4), 6)
+    init {
 
-        val sum3ints: (Int, Int, Int) -> Int = { x, y, _ -> x + y }
-        val f: (Int) -> (Int) -> (Int) -> Int = sum3ints.curried()
-        assertTrue(f.uncurried() is (Int, Int, Int) -> Int)
+        "testCurrying" {
+            val sum2ints = { x: Int, y: Int -> x + y }
+            val curried = sum2ints.curried()
+            curried(2)(4) shouldBe 6
+            val add5 = curried(5)
+            add5(7) shouldBe 12
+        }
+
+        "testUncurrying" {
+            val sum2ints: (Int, Int) -> Int = { x, y -> x + y }
+            val curried: (Int) -> (Int) -> Int = sum2ints.curried()
+            curried(2)(4) shouldBe 6
+            //same type as sum2ints,
+            curried.uncurried()(2, 4) shouldBe 6
+            sum2ints(2, 4) shouldBe 6
+
+            val sum3ints: (Int, Int, Int) -> Int = { x, y, _ -> x + y }
+            val f: (Int) -> (Int) -> (Int) -> Int = sum3ints.curried()
+            (f.uncurried() is (Int, Int, Int) -> Int) shouldBe true
+        }
+
     }
 }
+
