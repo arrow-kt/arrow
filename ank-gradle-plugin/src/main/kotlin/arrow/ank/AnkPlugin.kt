52712986bae90e6f02ef21fc908c2b4b7eec9a3d
Raul Raja <raulraja@gmail.com>
2018-01-02 23:24:28 +0100

    Bump version to 0.5.2 and require always ank-core where needed

--- ank-gradle-plugin/src/main/kotlin/arrow/ank/AnkPlugin.kt
+++ ank-gradle-plugin/src/main/kotlin/arrow/ank/AnkPlugin.kt
@@ -11,23 +11,12 @@ class AnkPlugin : Plugin<Project> {
     companion object {
         private const val EXTENSION_NAME = "ank"
         private const val TASK_NAME = "runAnk"
-        private const val ANK_CORE_DEPENDENCY = "io.arrow-kt:ank-core:0.5.1"
     }
 
     override fun apply(target: Project) {
         val compileDeps = target.configurations.getByName("compile").dependencies
         val extension = AnkExtension()
         target.extensions.add(EXTENSION_NAME, extension)
-        target.gradle.addListener(object : DependencyResolutionListener {
-            override fun beforeResolve(resolvableDependencies: ResolvableDependencies) {
-                if (extension.includeAnkCore) {
-                    compileDeps.add(target.dependencies.create(ANK_CORE_DEPENDENCY))
-                }
-                target.gradle.removeListener(this)
-            }
-
-            override fun afterResolve(resolvableDependencies: ResolvableDependencies) {}
-        })
         target.afterEvaluate {
             target.tasks.create(TASK_NAME, JavaExec::class.java).apply {
                 classpath = extension.classpath
