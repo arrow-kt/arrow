e5eb5f06ffcc03f1fd28e9d437f506416b255ec3
Cotel <miguelcoletomunoz@hotmail.com>
2018-01-19 20:00:39 +0100

    Passing EQ through paramenters

--- arrow-test/src/main/kotlin/arrow/test/laws/ShowLaws.kt
+++ arrow-test/src/main/kotlin/arrow/test/laws/ShowLaws.kt
@@ -1,5 +1,6 @@
 package arrow.test.laws
 
+import arrow.typeclasses.Eq
 import arrow.typeclasses.Show
 import arrow.typeclasses.eq
 import arrow.typeclasses.show
@@ -8,16 +9,16 @@ import io.kotlintest.properties.forAll
 
 object ShowLaws {
 
-    inline fun <reified F> laws(S: Show<F> = show<F>(), noinline cf: (Int) -> F): List<Law> =
-            EqLaws.laws(eq<F>(), cf) + listOf(
-                    Law("Show Laws: equality", { equalShow(S, cf) })
+    inline fun <reified F> laws(S: Show<F> = show<F>(), EQ: Eq<F> = eq<F>(), noinline cf: (Int) -> F): List<Law> =
+            EqLaws.laws(EQ, cf) + listOf(
+                    Law("Show Laws: equality", { equalShow(S, EQ, cf) })
             )
 
-    inline fun <reified F> equalShow(S: Show<F> = show(), crossinline cf: (Int) -> F): Unit =
+    inline fun <reified F> equalShow(S: Show<F> = show(), EQ: Eq<F> = eq(), crossinline cf: (Int) -> F): Unit =
             forAll(Gen.int(), { int: Int ->
                 val a = cf(int)
                 val b = cf(int)
-                eq<F>().eqv(a, b) && S.show(a) == S.show(b)
+                EQ.eqv(a, b) && S.show(a) == S.show(b)
             })
 
 }
