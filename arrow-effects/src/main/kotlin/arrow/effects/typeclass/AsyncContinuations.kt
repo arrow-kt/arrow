fd806b1d723671b547f2ac70bbfd8dde8f78712f
pakoito <jueces.jus@gmail.com>
2018-01-20 19:05:26 +0000

    Fix continuations

--- arrow-effects/src/main/kotlin/arrow/effects/typeclass/AsyncContinuations.kt
+++ arrow-effects/src/main/kotlin/arrow/effects/typeclass/AsyncContinuations.kt
@@ -7,4 +7,4 @@ suspend inline fun <reified F, A, B> MonadContinuation<F, A>.bindAsync(AC: Async
         AC(f).bind()
 
 suspend inline fun <reified F, A, B> MonadContinuation<F, A>.bindAsyncUnsafe(AC: Async<F> = async(), noinline f: () -> Either<Throwable, B>): B =
-        AC.invokeUnsafe(f).bind()
+        AC.deferUnsafe(f).bind()
