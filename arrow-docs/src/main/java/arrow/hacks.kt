5914260f4924ad43eaf255ed02fc3ab2e076826d
pakoito <jueces.jus@gmail.com>
2018-01-20 18:13:39 +0000

    Fix changes in Async in docs

--- arrow-docs/src/main/java/arrow/hacks.kt
+++ arrow-docs/src/main/java/arrow/hacks.kt
@@ -5,6 +5,6 @@ import arrow.effects.Proc
 import kotlinx.coroutines.experimental.CoroutineStart
 import kotlinx.coroutines.experimental.DefaultDispatcher
 
-// [showInstances] can't detect the version of runAsync with default parameters we're using
+// [showInstances] can't detect the version of async with default parameters we're using
 fun <A> DeferredKW.Companion.async(fa: Proc<A>): DeferredKW<A> =
         async(DefaultDispatcher, CoroutineStart.DEFAULT, fa)
