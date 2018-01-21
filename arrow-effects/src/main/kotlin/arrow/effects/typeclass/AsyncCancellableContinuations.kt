f8b604aee207fe71ad80161ee3d23ee81a4cefb9
pakoito <jueces.jus@gmail.com>
2018-01-21 16:59:18 +0000

    Rename bindingECancellable to bindingCancellable. Require Async instead of MonadError.

--- arrow-effects/src/main/kotlin/arrow/effects/typeclass/AsyncCancellableContinuations.kt
+++ arrow-effects/src/main/kotlin/arrow/effects/typeclass/AsyncCancellableContinuations.kt
@@ -1,6 +1,7 @@
 package arrow.effects
 
 import arrow.HK
+import arrow.core.Either
 import arrow.core.Tuple2
 import arrow.core.toT
 import arrow.effects.data.internal.BindingCancellationException
@@ -18,14 +19,20 @@ import kotlin.coroutines.experimental.startCoroutine
 typealias Disposable = () -> Unit
 
 @RestrictsSuspension
-open class MonadErrorCancellableContinuation<F, A>(ME: MonadError<F, Throwable>, override val context: CoroutineContext = EmptyCoroutineContext) :
-        MonadErrorContinuation<F, A>(ME) {
+open class AsyncCancellableContinuation<F, A>(val AC: Async<F>, override val context: CoroutineContext = EmptyCoroutineContext) :
+        MonadErrorContinuation<F, A>(AC) {
 
     protected val cancelled: AtomicBoolean = AtomicBoolean(false)
 
     fun disposable(): Disposable = { cancelled.set(true) }
 
     override fun returnedMonad(): HK<F, A> = returnedMonad
+
+    suspend fun <B> bindAsync(f: () -> B): B =
+            AC(f).bind()
+
+    suspend fun <B> bindAsyncUnsafe(f: () -> Either<Throwable, B>): B =
+            AC.deferUnsafe(f).bind()
 
     override suspend fun <B> bind(m: () -> HK<F, B>): B = suspendCoroutineOrReturn { c ->
         val labelHere = c.stackLabels // save the whole coroutine stack labels
@@ -40,10 +47,15 @@ open class MonadErrorCancellableContinuation<F, A>(ME: MonadError<F, Throwable>,
         COROUTINE_SUSPENDED
     }
 
-    override suspend fun <B> bindInM(context: CoroutineContext, m: () -> HK<F, B>): B = suspendCoroutineOrReturn { c ->
+    override suspend fun <B> bindIn(context: CoroutineContext, m: () -> B): B = suspendCoroutineOrReturn { c ->
         val labelHere = c.stackLabels // save the whole coroutine stack labels
         val monadCreation: suspend () -> HK<F, A> = {
-            flatMap(m(), { xx: B ->
+            val datatype = try {
+                pure(m())
+            } catch (t: Throwable) {
+                ME.raiseError<B>(t)
+            }
+            flatMap(datatype, { xx: B ->
                 c.stackLabels = labelHere
                 if (cancelled.get()) {
                     throw BindingCancellationException()
@@ -67,7 +79,7 @@ open class MonadErrorCancellableContinuation<F, A>(ME: MonadError<F, Throwable>,
 
 /**
  * Entry point for monad bindings which enables for comprehensions. The underlying impl is based on coroutines.
- * A coroutines is initiated and inside [MonadErrorCancellableContinuation] suspended yielding to [Monad.flatMap]. Once all the flatMap binds are completed
+ * A coroutines is initiated and inside [AsyncCancellableContinuation] suspended yielding to [Monad.flatMap]. Once all the flatMap binds are completed
  * the underlying monad is returned from the act of executing the coroutine
  *
  * This one operates over [MonadError] instances that can support [Throwable] in their error type automatically lifting
@@ -76,8 +88,8 @@ open class MonadErrorCancellableContinuation<F, A>(ME: MonadError<F, Throwable>,
  * This operation is cancellable by calling invoke on the [Disposable] return.
  * If [Disposable.invoke] is called the binding result will become a lifted [BindingCancellationException].
  */
-fun <F, B> MonadError<F, Throwable>.bindingECancellable(c: suspend MonadErrorCancellableContinuation<F, *>.() -> HK<F, B>): Tuple2<HK<F, B>, Disposable> {
-    val continuation = MonadErrorCancellableContinuation<F, B>(this)
+fun <F, B> Async<F>.bindingCancellable(c: suspend AsyncCancellableContinuation<F, *>.() -> HK<F, B>): Tuple2<HK<F, B>, Disposable> {
+    val continuation = AsyncCancellableContinuation<F, B>(this)
     c.startCoroutine(continuation, continuation)
     return continuation.returnedMonad() toT continuation.disposable()
 }
