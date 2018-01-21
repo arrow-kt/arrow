dca897d7dbd529a2d0767ade0e56d86d84f8542b
pakoito <jueces.jus@gmail.com>
2018-01-21 17:00:14 +0000

    Move bindingCancellable to Async

--- arrow-effects/src/main/kotlin/arrow/effects/typeclass/AsyncCancellableContinuations.kt
+++ arrow-effects/src/main/kotlin/arrow/effects/typeclass/AsyncCancellableContinuations.kt
@@ -2,11 +2,8 @@ package arrow.effects
 
 import arrow.HK
 import arrow.core.Either
-import arrow.core.Tuple2
-import arrow.core.toT
 import arrow.effects.data.internal.BindingCancellationException
 import arrow.effects.internal.stackLabels
-import arrow.typeclasses.MonadError
 import arrow.typeclasses.MonadErrorContinuation
 import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.coroutines.experimental.CoroutineContext
@@ -75,21 +72,4 @@ open class AsyncCancellableContinuation<F, A>(val AC: Async<F>, override val con
         })
         COROUTINE_SUSPENDED
     }
-}
-
-/**
- * Entry point for monad bindings which enables for comprehensions. The underlying impl is based on coroutines.
- * A coroutines is initiated and inside [AsyncCancellableContinuation] suspended yielding to [Monad.flatMap]. Once all the flatMap binds are completed
- * the underlying monad is returned from the act of executing the coroutine
- *
- * This one operates over [MonadError] instances that can support [Throwable] in their error type automatically lifting
- * errors as failed computations in their monadic context and not letting exceptions thrown as the regular monad binding does.
- *
- * This operation is cancellable by calling invoke on the [Disposable] return.
- * If [Disposable.invoke] is called the binding result will become a lifted [BindingCancellationException].
- */
-fun <F, B> Async<F>.bindingCancellable(c: suspend AsyncCancellableContinuation<F, *>.() -> HK<F, B>): Tuple2<HK<F, B>, Disposable> {
-    val continuation = AsyncCancellableContinuation<F, B>(this)
-    c.startCoroutine(continuation, continuation)
-    return continuation.returnedMonad() toT continuation.disposable()
 }
