b53357a19fa07e67dac6cc587f4ab4bfc9956604
Simon Vergauwen <vergauwen.simon@gmail.com>
2018-01-24 19:09:18 +0100

    Fix incorrect OptionalLaw law

--- arrow-optics/src/test/kotlin/arrow/optics/data/ListInstancesTest.kt
+++ arrow-optics/src/test/kotlin/arrow/optics/data/ListInstancesTest.kt
@@ -26,22 +26,20 @@ class ListInstancesTest : UnitSpec() {
 
         testLaws(
             OptionalLaws.laws(
-                optional = listHead(),
-                aGen = Gen.list(Gen.int()),
-                bGen = Gen.int(),
-                funcGen = genFunctionAToB(Gen.int()),
-                EQA = Eq.any(),
-                EQB = Eq.any(),
-                EQOptionB = Eq.any()),
+                    optional = listHead(),
+                    aGen = Gen.list(Gen.int()),
+                    bGen = Gen.int(),
+                    funcGen = genFunctionAToB(Gen.int()),
+                    EQA = Eq.any(),
+                    EQOptionB = Eq.any()),
 
             OptionalLaws.laws(
-                optional = listTail(),
-                aGen = Gen.list(Gen.int()),
-                bGen = Gen.list(Gen.int()),
-                funcGen = genFunctionAToB(Gen.list(Gen.int())),
-                EQA = Eq.any(),
-                EQB = Eq.any(),
-                EQOptionB = Eq.any()),
+                    optional = listTail(),
+                    aGen = Gen.list(Gen.int()),
+                    bGen = Gen.list(Gen.int()),
+                    funcGen = genFunctionAToB(Gen.list(Gen.int())),
+                    EQA = Eq.any(),
+                    EQOptionB = Eq.any()),
 
             IsoLaws.laws(
                 iso = listToOptionNel(),
