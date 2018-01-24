dc7950bd5a032f52feeacaa1113edf7b6e360d93
Simon Vergauwen <vergauwen.simon@gmail.com>
2018-01-24 22:17:55 +0100

    Fix TraversalLaw

--- arrow-optics/src/test/kotlin/arrow/optics/data/TupleInstancesTest.kt
+++ arrow-optics/src/test/kotlin/arrow/optics/data/TupleInstancesTest.kt
@@ -1,5 +1,9 @@
 package arrow.optics
 
+import arrow.core.Option
+import arrow.core.eq
+import arrow.data.ListKW
+import arrow.data.eq
 import io.kotlintest.KTestJUnitRunner
 import io.kotlintest.properties.Gen
 import arrow.typeclasses.Eq
@@ -28,7 +32,7 @@ class TupleInstancesTest : UnitSpec() {
                 MB = IntMonoid
         ))
 
-        testLaws( LensLaws.laws(
+        testLaws(LensLaws.laws(
                 lens = secondTuple2(),
                 aGen = genTuple(Gen.int(), Gen.string()),
                 bGen = Gen.string(),
@@ -38,79 +42,95 @@ class TupleInstancesTest : UnitSpec() {
                 MB = StringMonoidInstance
         ))
 
-        testLaws(
-                TraversalLaws.laws(
-                        traversal = traversalTuple2(),
-                        aGen = genTuple(Gen.int(), Gen.int()),
-                        bGen = Gen.int(),
-                        funcGen = genFunctionAToB(Gen.int()),
-                        EQA = Eq.any(),
-                        EQB = Eq.any()),
+        testLaws(TraversalLaws.laws(
+                traversal = traversalTuple2(),
+                aGen = genTuple(Gen.int(), Gen.int()),
+                bGen = Gen.int(),
+                funcGen = genFunctionAToB(Gen.int()),
+                EQA = Eq.any(),
+                EQOptionB = Option.eq(Eq.any()),
+                EQListB = ListKW.eq(Eq.any())
+        ))
 
-                TraversalLaws.laws(
-                        traversal = traversalTuple3(),
-                        aGen = genTuple(Gen.int(), Gen.int(), Gen.int()),
-                        bGen = Gen.int(),
-                        funcGen = genFunctionAToB(Gen.int()),
-                        EQA = Eq.any(),
-                        EQB = Eq.any()),
+        testLaws(TraversalLaws.laws(
+                traversal = traversalTuple3(),
+                aGen = genTuple(Gen.int(), Gen.int(), Gen.int()),
+                bGen = Gen.int(),
+                funcGen = genFunctionAToB(Gen.int()),
+                EQA = Eq.any(),
+                EQOptionB = Option.eq(Eq.any()),
+                EQListB = ListKW.eq(Eq.any())
+        ))
 
-                TraversalLaws.laws(
-                        traversal = traversalTuple4(),
-                        aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int()),
-                        bGen = Gen.int(),
-                        funcGen = genFunctionAToB(Gen.int()),
-                        EQA = Eq.any(),
-                        EQB = Eq.any()),
+        testLaws(TraversalLaws.laws(
+                traversal = traversalTuple4(),
+                aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int()),
+                bGen = Gen.int(),
+                funcGen = genFunctionAToB(Gen.int()),
+                EQA = Eq.any(),
+                EQOptionB = Option.eq(Eq.any()),
+                EQListB = ListKW.eq(Eq.any())
+        ))
 
-                TraversalLaws.laws(
-                        traversal = traversalTuple5(),
-                        aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int()),
-                        bGen = Gen.int(),
-                        funcGen = genFunctionAToB(Gen.int()),
-                        EQA = Eq.any(),
-                        EQB = Eq.any()),
+        testLaws(TraversalLaws.laws(
+                traversal = traversalTuple5(),
+                aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int()),
+                bGen = Gen.int(),
+                funcGen = genFunctionAToB(Gen.int()),
+                EQA = Eq.any(),
+                EQOptionB = Option.eq(Eq.any()),
+                EQListB = ListKW.eq(Eq.any())
+        ))
 
-                TraversalLaws.laws(
-                        traversal = traversalTuple6(),
-                        aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int()),
-                        bGen = Gen.int(),
-                        funcGen = genFunctionAToB(Gen.int()),
-                        EQA = Eq.any(),
-                        EQB = Eq.any()),
+        testLaws(TraversalLaws.laws(
+                traversal = traversalTuple6(),
+                aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int()),
+                bGen = Gen.int(),
+                funcGen = genFunctionAToB(Gen.int()),
+                EQA = Eq.any(),
+                EQOptionB = Option.eq(Eq.any()),
+                EQListB = ListKW.eq(Eq.any())
+        ))
 
-                TraversalLaws.laws(
-                        traversal = traversalTuple7(),
-                        aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int()),
-                        bGen = Gen.int(),
-                        funcGen = genFunctionAToB(Gen.int()),
-                        EQA = Eq.any(),
-                        EQB = Eq.any()),
+        testLaws(TraversalLaws.laws(
+                traversal = traversalTuple7(),
+                aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int()),
+                bGen = Gen.int(),
+                funcGen = genFunctionAToB(Gen.int()),
+                EQA = Eq.any(),
+                EQOptionB = Option.eq(Eq.any()),
+                EQListB = ListKW.eq(Eq.any())
+        ))
 
-                TraversalLaws.laws(
-                        traversal = traversalTuple8(),
-                        aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int()),
-                        bGen = Gen.int(),
-                        funcGen = genFunctionAToB(Gen.int()),
-                        EQA = Eq.any(),
-                        EQB = Eq.any()),
+        testLaws(TraversalLaws.laws(
+                traversal = traversalTuple8(),
+                aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int()),
+                bGen = Gen.int(),
+                funcGen = genFunctionAToB(Gen.int()),
+                EQA = Eq.any(),
+                EQOptionB = Option.eq(Eq.any()),
+                EQListB = ListKW.eq(Eq.any())
+        ))
 
-                TraversalLaws.laws(
-                        traversal = traversalTuple9(),
-                        aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int()),
-                        bGen = Gen.int(),
-                        funcGen = genFunctionAToB(Gen.int()),
-                        EQA = Eq.any(),
-                        EQB = Eq.any()),
+        testLaws(TraversalLaws.laws(
+                traversal = traversalTuple9(),
+                aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int()),
+                bGen = Gen.int(),
+                funcGen = genFunctionAToB(Gen.int()),
+                EQA = Eq.any(),
+                EQOptionB = Option.eq(Eq.any()),
+                EQListB = ListKW.eq(Eq.any())
+        ))
 
-                TraversalLaws.laws(
-                        traversal = traversalTuple10(),
-                        aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int()),
-                        bGen = Gen.int(),
-                        funcGen = genFunctionAToB(Gen.int()),
-                        EQA = Eq.any(),
-                        EQB = Eq.any())
-        )
+        testLaws(TraversalLaws.laws(
+                traversal = traversalTuple10(),
+                aGen = genTuple(Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int(), Gen.int()),
+                bGen = Gen.int(),
+                funcGen = genFunctionAToB(Gen.int()),
+                EQA = Eq.any(),
+                EQOptionB = Option.eq(Eq.any()),
+                EQListB = ListKW.eq(Eq.any())
+        ))
 
     }
 
