1d1bfad21224f6322d101357b1000496cc714e55
Andrzej Ressel <jereksel@gmail.com>
2018-01-25 18:18:52 +0100

    Clean example

--- arrow-examples/src/main/kotlin/com/jereksel/test/OptionalTest.kt
+++
@@ -1,9 +0,0 @@
-package com.jereksel.test
-
-import arrow.core.Option
-import arrow.lenses
-import arrow.optionals
-
-@optionals
-@lenses data class OptionalTest(val name: String, val company: Int, val optionalName: String?, val optionalCompany: Option<String>)
-
