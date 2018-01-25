11a975ae0f55acf50433fdfdc4c0b2280634596d
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-01-25 23:47:26 +0000

    Extracted methods and added more documentation to code

--- arrow-weak/src/main/kotlin/arrow/weak/Weak.kt
+++ arrow-weak/src/main/kotlin/arrow/weak/Weak.kt
@@ -19,6 +19,9 @@ import arrow.weak.internal.WeakRef
  *
  *   - `weakObject.eval`: [Eval] property that provides an [Option] with the result or lack thereof.
  *   - `weakObject.option()`: to get the [Option] directly.
+ *
+ *
+ *   Basically, this is a Schrodinger's cat box.
  */
 @higherkind
 class Weak<out A> private constructor(val eval: Eval<Option<A>>) : WeakKind<A> {
@@ -30,6 +33,9 @@ class Weak<out A> private constructor(val eval: Eval<Option<A>>) : WeakKind<A> {
         @Suppress("UNCHECKED_CAST")
         fun <B> emptyWeak(): Weak<B> = EMPTY
 
+        /**
+         * Main public factory method
+         */
         operator fun <A> invoke(a: A): Weak<A> {
             val reference = WeakRef(a)
             return Weak(Eval.always { Option.fromNullable(reference.get()) })// { reference.get() }
@@ -50,49 +56,74 @@ class Weak<out A> private constructor(val eval: Eval<Option<A>>) : WeakKind<A> {
         }
     }
 
-    fun option(): Option<A> = eval.value()
-
-    inline fun <B> fold(fn: () -> B, f: (A) -> B): B = option().fold(fn, f)
-
-    inline fun <B> map(crossinline f: (A) -> B): Weak<B> = fold({ emptyWeak() }, { f(it).weak() })
-
-    inline fun <B> flatMap(crossinline f: (A) -> WeakKind<B>): Weak<B> = fold({ emptyWeak() }, { f(it).ev() })
-
     /**
-     * Returns this Weak as long as the provided predicate confirms the value is to be kept
-     *
-     * @param p Predicate used for testing
+     * Evaluates the contents of this weak and returns [Some] is the subject is in memory or [None] otherwise
      */
-    inline fun filter(crossinline p: (A) -> Boolean): Weak<A> = fold({ emptyWeak() }, { a -> if (p(a)) a.weak() else emptyWeak() })
-
-    fun <B> ap(ff: WeakKind<(A) -> B>): Weak<B> = ff.ev().flatMap { f -> map(f) }.ev()
-
-    fun exists(predicate: Predicate<A>): Boolean = fold({ false }, { predicate(it) })
-
-    fun <B> foldLeft(b: B, f: (B, A) -> B): B = ev().fold({ b }, { f(b, it) })
-
-    fun <B> foldRight(lb: Eval<B>, f: (A, Eval<B>) -> Eval<B>): Eval<B> = ev().fold({ lb }, { f(it, lb) })
-
-    fun forall(predicate: Predicate<A>): Boolean = fold({ false }, { predicate(it) })
-
-    fun isEmpty(): Boolean = fold({ true }, { false })
-
-    fun nonEmpty(): Boolean = fold({ false }, { true })
+    fun option(): Option<A> = eval.value()
 }
+
+fun <A, B> WeakKind<A>.ap(ff: WeakKind<(A) -> B>): Weak<B> = ff.ev().flatMap(::map).ev()
+
+/**
+ * If subject is not in memory this will return the result of [f], otherwise it returns the computed result from [fn]
+ */
+inline fun <A, B> WeakKind<A>.fold(fn: () -> B, f: (A) -> B): B = ev().option().fold(fn, f)
+
+/**
+ * Returns a new [Weak] instance from the result of [f] if the current subject is present or empty [Weak] otherwise.
+ */
+inline fun <A, B> WeakKind<A>.map(crossinline f: (A) -> B): Weak<B> = fold({ Weak.emptyWeak() }, { f(it).weak() })
+
+/**
+ * Similar to [map] but [f] is expected to return a [Weak] instance instead of a subject object.
+ */
+inline fun <A, B> WeakKind<A>.flatMap(crossinline f: (A) -> WeakKind<B>): Weak<B> = fold({ Weak.emptyWeak() }, { f(it).ev() })
+
+/**
+ * Returns this Weak as long as the provided predicate confirms the value is to be kept
+ *
+ * @param predicate Predicate used for testing
+ */
+inline fun <A> WeakKind<A>.filter(predicate: Predicate<A>): Weak<A> = ev().fold({ Weak.emptyWeak() }, { a -> if (predicate(a)) a.weak() else Weak.emptyWeak() })
+
+
+/**
+ * Returns true if the subject is in memory and passes the provided predicate
+ */
+inline fun <A> WeakKind<A>.exists(predicate: Predicate<A>): Boolean = ev().fold({ false }, predicate)
+
+inline fun <A, B> WeakKind<A>.foldLeft(b: B, f: (B, A) -> B): B = ev().fold({ b }, { f(b, it) })
+
+inline fun <A, B> WeakKind<A>.foldRight(lb: Eval<B>, f: (A, Eval<B>) -> Eval<B>): Eval<B> = ev().fold({ lb }, { f(it, lb) })
+
+/**
+ * Returns true if the subject is available and passes the provided predicate
+ */
+inline fun <A> WeakKind<A>.forall(predicate: Predicate<A>): Boolean = ev().fold({ false }, predicate)
+
+/**
+ * Returns true if we have lost the reference to the subject
+ */
+fun <A> WeakKind<A>.isEmpty(): Boolean = ev().option().isEmpty()
+
+/**
+ * Returns true if we still have the reference to the subject
+ */
+fun <A> WeakKind<A>.nonEmpty(): Boolean = ev().option().nonEmpty()
 
 /**
  * Returns the internal value or an alternative if we've lost it.
  *
  * @param fallback provides a new value if we have lost the current one.
  */
-fun <B> WeakKind<B>.getOrElse(fallback: () -> B): B = ev().fold({ fallback() }, { it })
+inline fun <B> WeakKind<B>.getOrElse(fallback: () -> B): B = ev().fold(fallback, { it })
 
 /**
  * Returns this Weak instance if present or an alternative.
  *
  * @param fallback provides a new value if we have lost the current one.
  */
-fun <A, B : A> WeakKind<B>.orElse(fallback: () -> Weak<B>): Weak<B> = ev().fold({ fallback() }, { it.weak() })
+inline fun <A, B : A> WeakKind<B>.orElse(fallback: () -> Weak<B>): Weak<B> = ev().fold(fallback, { it.weak() })
 
 /**
  * Creates a new Weak instance. Alias of [Weak.invoke].
