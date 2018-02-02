74bc110ec7fefee87000e282daff10a7fa199d42
Simon Vergauwen <vergauwen.simon@gmail.com>
2018-02-02 12:40:28 +0100

    Add Setter law test for optic conversion

--- arrow-optics/src/test/kotlin/arrow/optics/SetterTest.kt
+++ arrow-optics/src/test/kotlin/arrow/optics/SetterTest.kt
@@ -1,6 +1,7 @@
 package arrow.optics
 
 import arrow.core.getOrElse
+import arrow.data.k
 import io.kotlintest.KTestJUnitRunner
 import io.kotlintest.properties.Gen
 import io.kotlintest.properties.forAll
@@ -11,12 +12,14 @@ import arrow.test.generators.genFunctionAToB
 import arrow.test.generators.genOption
 import arrow.syntax.either.left
 import arrow.syntax.either.right
+import arrow.syntax.foldable.combineAll
 import org.junit.runner.RunWith
 
 @RunWith(KTestJUnitRunner::class)
 class SetterTest : UnitSpec() {
 
     init {
+
         testLaws(SetterLaws.laws(
                 setter = Setter.id(),
                 aGen = Gen.int(),
@@ -40,6 +43,54 @@ class SetterTest : UnitSpec() {
                 funcGen = genFunctionAToB(Gen.string()),
                 EQA = Eq.any()
         ))
+
+        "asFold should behave as valid Fold: size" {
+            forAll(SumGen) { sum: SumType ->
+                sumPrism.asFold().size(sum) == sumPrism.getOption(sum).map { 1 }.getOrElse { 0 }
+            }
+        }
+
+        "asFold should behave as valid Fold: nonEmpty" {
+            forAll(SumGen) { sum: SumType ->
+                sumPrism.asFold().nonEmpty(sum) == sumPrism.getOption(sum).nonEmpty()
+            }
+        }
+
+        "asFold should behave as valid Fold: isEmpty" {
+            forAll(SumGen) { sum: SumType ->
+                sumPrism.asFold().isEmpty(sum) == sumPrism.getOption(sum).isEmpty()
+            }
+        }
+
+        "asFold should behave as valid Fold: getAll" {
+            forAll(SumGen) { sum: SumType ->
+                sumPrism.asFold().getAll(sum) == sumPrism.getOption(sum).toList().k()
+            }
+        }
+
+        "asFold should behave as valid Fold: combineAll" {
+            forAll(SumGen) { sum: SumType ->
+                sumPrism.asFold().combineAll(sum) == sumPrism.getOption(sum).combineAll()
+            }
+        }
+
+        "asFold should behave as valid Fold: fold" {
+            forAll(SumGen) { sum: SumType ->
+                sumPrism.asFold().fold(sum) == sumPrism.getOption(sum).combineAll()
+            }
+        }
+
+        "asFold should behave as valid Fold: headOption" {
+            forAll(SumGen) { sum: SumType ->
+                sumPrism.asFold().headOption(sum) == sumPrism.getOption(sum)
+            }
+        }
+
+        "asFold should behave as valid Fold: lastOption" {
+            forAll(SumGen) { sum: SumType ->
+                sumPrism.asFold().lastOption(sum) == sumPrism.getOption(sum)
+            }
+        }
 
         "Joining two lenses together with same target should yield same result" {
             val userTokenStringSetter = userSetter compose tokenSetter
