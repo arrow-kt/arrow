e1ae53b24dbdb069c8e0af38ee0a5ba83089a8bc
Simon Vergauwen <vergauwen.simon@gmail.com>
2018-02-02 08:28:33 +0100

    Add Missing map optic

--- arrow-optics/src/main/kotlin/arrow/optics/std/map.kt
+++ arrow-optics/src/main/kotlin/arrow/optics/std/map.kt
@@ -21,6 +21,14 @@ fun <K, A> mapToMapKW(): Iso<Map<K, A>, MapKW<K, A>> = pMapToMapKW()
  * [Iso] that defines the equality between a Unit value [Map] and a [Set] with its keys
  */
 fun <K> mapToSet(): Iso<Map<K, Unit>, Set<K>> = Iso(
-        get= { it.keys },
+        get = { it.keys },
         reverseGet = { keys -> keys.map { it to Unit }.toMap() }
+)
+
+/**
+ * [Iso] that defines the equality between a [MapKW] and a [ListKW] of key value products.
+ */
+fun <K, A> mapToList(): Iso<MapKW<K, A>, ListKW<Pair<K, A>>> = Iso(
+        get = { it.toList().k() },
+        reverseGet = { it.toMap().k() }
 )
