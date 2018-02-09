0c6135a584fa3142b257ed1e4288f948f2d7fd00
Raul Raja <raulraja@gmail.com>
2018-02-09 01:10:14 +0100

    Added remaining dagger syntax instances for effects type classes

--- modules/dagger/arrow-dagger-effects/src/main/kotlin/arrow.dagger.effects.instances/syntax.kt
+++ modules/dagger/arrow-dagger-effects/src/main/kotlin/arrow.dagger.effects.instances/syntax.kt
@@ -6,3 +6,11 @@ import javax.inject.Inject
 class DaggerMonadSuspendSyntaxInstance<F> @Inject constructor(val monadSuspend: MonadSuspend<F>) : MonadSuspendSyntax<F> {
     override fun monadSuspend(): MonadSuspend<F> = monadSuspend
 }
+
+class DaggerAsyncSyntaxInstance<F> @Inject constructor(val async: Async<F>) : AsyncSyntax<F> {
+    override fun async(): Async<F> = async
+}
+
+class DaggerEffectSyntaxInstance<F> @Inject constructor(val effect: Effect<F>) : EffectSyntax<F> {
+    override fun effect(): Effect<F> = effect
+}
