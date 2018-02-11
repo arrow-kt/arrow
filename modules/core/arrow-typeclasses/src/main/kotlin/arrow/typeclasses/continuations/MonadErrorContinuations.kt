d33d5ccc35ceddcb6f210b6c1ece38cd22d83fed
pakoito <jueces.jus@gmail.com>
2018-02-11 23:02:07 +0000

    Remove redundant RestrictsSuspension

--- modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/continuations/MonadErrorContinuations.kt
+++ modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/continuations/MonadErrorContinuations.kt
@@ -4,9 +4,7 @@ import arrow.Kind
 import arrow.typeclasses.Awaitable
 import arrow.typeclasses.MonadError
 import kotlin.coroutines.experimental.CoroutineContext
-import kotlin.coroutines.experimental.RestrictsSuspension
 
-@RestrictsSuspension
 open class MonadErrorBlockingContinuation<F, E, A>(ME: MonadError<F, E>, latch: Awaitable<Kind<F, A>>, override val context: CoroutineContext, private val convertError: (Throwable) -> E) :
         MonadBlockingContinuation<F, A>(ME, latch, context), MonadError<F, E> by ME, BindingCatchContinuation<F, E, A> {
 
