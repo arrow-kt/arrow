afd6ea1c6cd0dd3f7ec6a6990f5879f111a11520
Israel Fdez <israel.fernandez@monsanto.com>
2018-02-12 00:28:22 -0300

    #638
    MapKW tests migrated to use `Laws`

--- arrow-data/src/test/kotlin/arrow/data/MapKWTest.kt
+++ arrow-data/src/test/kotlin/arrow/data/MapKWTest.kt
@@ -14,11 +14,6 @@ import org.junit.runner.RunWith
 @RunWith(KTestJUnitRunner::class)
 class MapKWTest : UnitSpec() {
 
-    val SG: Semigroup<Int> = object : Semigroup<Int> {
-        override fun combine(a: Int, b: Int): Int =
-                a * b
-    }
-
     val EQ: Eq<HK2<MapKWHK, String, Int>> = object : Eq<HK2<MapKWHK, String, Int>> {
         override fun eqv(a: HK2<MapKWHK, String, Int>, b: HK2<MapKWHK, String, Int>): Boolean =
                 a.ev()["key"] == b.ev()["key"]
