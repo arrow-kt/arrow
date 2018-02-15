350fcf976542cb7ce10e17a99ecbf18cc45e19d0
Simon Vergauwen <vergauwen.simon@gmail.com>
2018-02-15 09:38:31 +0100

    Add Kind4

--- modules/core/arrow-annotations/src/main/java/arrow/hks.kt
+++ modules/core/arrow-annotations/src/main/java/arrow/hks.kt
@@ -5,3 +5,5 @@ interface Kind<out F, out A>
 typealias Kind2<F, A, B> = Kind<Kind<F, A>, B>
 
 typealias Kind3<F, A, B, C> = Kind<Kind2<F, A, B>, C>
+
+typealias Kind4<F, A, B, C, D> = Kind<Kind3<F, A, B, C>, D>
