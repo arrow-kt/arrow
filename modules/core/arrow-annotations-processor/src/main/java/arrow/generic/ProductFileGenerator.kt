7e7024b693836acc91d09078b7bb7de681325a92
Raul Raja <raulraja@gmail.com>
2018-03-05 22:11:03 +0100

    First draft of generic syntax enha Deriving `tupledLabelled`

--- modules/core/arrow-annotations-processor/src/main/java/arrow/generic/ProductFileGenerator.kt
+++ modules/core/arrow-annotations-processor/src/main/java/arrow/generic/ProductFileGenerator.kt
@@ -50,18 +50,22 @@ fun ${product.sourceName}Product(): arrow.products.Product<${product.sourceClass
             |package ${product.classData.`package`.escapedClassName}
             |
             |import arrow.syntax.applicative.*
+            |import arrow.core.toT
             |
             |fun ${product.sourceClassName}.tupled(): ${focusType(product)} =
             |  ${tupleConstructor(product)}
             |
+            |fun ${product.sourceClassName}.tupledLabelled(): ${labelledFocusType(product)} =
+            |  ${product.targets.joinToString(" toT ") { """("${it.paramName}" toT ${it.paramName})""" }}
+            |
             |fun ${focusType(product)}.to${product.sourceSimpleName}(): ${product.sourceClassName} =
             |  ${classConstructorFromTuple(product.sourceClassName, product.focusSize)}
             |
-            |interface ${product.sourceSimpleName}Syntax<F> : arrow.typeclasses.ApplicativeSyntax<F> {
+            |interface ${product.sourceSimpleName}ApplicativeSyntax<F> : arrow.typeclasses.ApplicativeSyntax<F> {
             |  fun arrow.Kind<F, ${focusType(product)}>.to${product.sourceSimpleName}(): arrow.Kind<F, ${product.sourceClassName}> =
             |    this.map { it.to${product.sourceSimpleName}() }
             |
-            |  fun arrow.typeclasses.Applicative<F>.mapTo${product.sourceSimpleName}${kindedProperties("F", product)}: arrow.Kind<F, ${product.sourceClassName}> =
+            |  fun mapTo${product.sourceSimpleName}${kindedProperties("F", product)}: arrow.Kind<F, ${product.sourceClassName}> =
             |    applicative().tupled(${product.targets.joinToString(", ") { it.paramName }}).to${product.sourceSimpleName}()
             |}
             |
@@ -77,6 +81,10 @@ fun ${product.sourceName}Product(): arrow.products.Product<${product.sourceClass
 
     private fun tupleConstructor(product: AnnotatedGeneric): String =
             product.targets.joinToString(prefix = "$tuple${product.focusSize}(", postfix = ")", transform = { "this.${it.paramName.plusIfNotBlank(prefix = "`", postfix = "`")}" })
+
+    private fun labelledFocusType(product: AnnotatedGeneric): String =
+            if (product.hasTupleFocus) product.targetNames.map { "${tuple}2<String, $it>" }.joinToString(prefix = "$tuple${product.targets.size}<", postfix = ">")
+            else product.targetNames.first()
 
     private fun focusType(product: AnnotatedGeneric): String =
             if (product.hasTupleFocus) product.targetNames.joinToString(prefix = "$tuple${product.targets.size}<", postfix = ">")
