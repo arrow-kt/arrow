302e3ef719caa05da1846d8b1dfc7e1943790c24
pakoito <jueces.jus@gmail.com>
2018-03-08 01:18:02 +0100

    Remove MonadSuspend comprehensions

--- modules/effects/arrow-effects/src/main/kotlin/arrow/effects/MonadSuspend.kt
+++ modules/effects/arrow-effects/src/main/kotlin/arrow/effects/MonadSuspend.kt
@@ -3,13 +3,9 @@ package arrow.effects
 import arrow.Kind
 import arrow.TC
 import arrow.core.Either
-import arrow.core.Tuple2
-import arrow.core.toT
-import arrow.effects.data.internal.BindingCancellationException
 import arrow.typeclass
 import arrow.typeclasses.MonadError
 import arrow.typeclasses.MonadErrorSyntax
-import kotlin.coroutines.experimental.startCoroutine
 
 /** The context required to defer evaluating a safe computation. **/
 @typeclass(syntax = false)
@@ -17,7 +13,7 @@ interface MonadSuspend<F> : MonadError<F, Throwable>, TC {
     fun <A> suspend(fa: () -> Kind<F, A>): Kind<F, A>
 
     operator fun <A> invoke(fa: () -> A): Kind<F, A> =
-            suspend {
+            suspend() {
                 try {
                     pure(fa())
                 } catch (t: Throwable) {
@@ -28,7 +24,7 @@ interface MonadSuspend<F> : MonadError<F, Throwable>, TC {
     fun lazy(): Kind<F, Unit> = invoke { }
 
     fun <A> deferUnsafe(f: () -> Either<Throwable, A>): Kind<F, A> =
-            suspend { f().fold({ raiseError<A>(it) }, { pure(it) }) }
+            suspend() { f().fold({ raiseError<A>(it) }, { pure(it) }) }
 }
 
 interface MonadSuspendSyntax<F> : MonadErrorSyntax<F, Throwable> {
@@ -54,21 +50,3 @@ inline fun <reified F, A> (() -> A).defer(SC: MonadSuspend<F> = monadSuspend()):
 
 inline fun <reified F, A> (() -> Either<Throwable, A>).deferUnsafe(SC: MonadSuspend<F> = monadSuspend()): Kind<F, A> =
         SC.deferUnsafe(this)
-
-/**
- * Entry point for monad bindings which enables for comprehensions. The underlying impl is based on coroutines.
- * A coroutines is initiated and inside [MonadSuspendCancellableContinuation] suspended yielding to [Monad.flatMap]. Once all the flatMap binds are completed
- * the underlying monad is returned from the act of executing the coroutine
- *
- * This one operates over [MonadError] instances that can support [Throwable] in their error type automatically lifting
- * errors as failed computations in their monadic context and not letting exceptions thrown as the regular monad binding does.
- *
- * This operation is cancellable by calling invoke on the [Disposable] return.
- * If [Disposable.invoke] is called the binding result will become a lifted [BindingCancellationException].
- */
-fun <F, B> MonadSuspend<F>.bindingCancellable(c: suspend MonadSuspendCancellableContinuation<F, *>.() -> B): Tuple2<Kind<F, B>, Disposable> {
-    val continuation = MonadSuspendCancellableContinuation<F, B>(this)
-    val wrapReturn: suspend MonadSuspendCancellableContinuation<F, *>.() -> Kind<F, B> = { pure(c()) }
-    wrapReturn.startCoroutine(continuation, continuation)
-    return continuation.returnedMonad() toT continuation.disposable()
-}
