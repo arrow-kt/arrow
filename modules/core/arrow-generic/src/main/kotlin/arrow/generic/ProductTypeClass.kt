41b2463fbf621b3bf9adb589e480f0803ac27747
Raul Raja <raulraja@gmail.com>
2018-03-26 19:06:26 +0200

    LabelledProductTypeClass

--- modules/core/arrow-generic/src/main/kotlin/arrow/generic/ProductTypeClass.kt
+++ modules/core/arrow-generic/src/main/kotlin/arrow/generic/ProductTypeClass.kt
@@ -32,6 +32,31 @@ interface ProductTypeClassCompanion<C> {
             typeClass.product(ch, ct)
 
     fun <F, G> deriveInstance(gen: Generic<F, G>, cg: Kind<C, G>): Kind<C, F> =
-            typeClass.project({ cg } , gen::to, gen::from)
+            typeClass.project({ cg }, gen::to, gen::from)
 }
 
+/**
+ * A type class abstracting over the `product` operation of type classes over
+ * types of kind `*`, as well as deriving instances using an isomorphism.
+ * Refines ProductTypeClass with the addition of runtime `String` labels
+ * corresponding to the names of the product elements.
+ */
+interface LabelledProductTypeClass<C> {
+    /**
+     * Given a type class instance for `H`, and a type class instance for a
+     * product, produce a type class instance for the product prepended with `H`.
+     */
+    fun <H, T : HList> product(name: String, ch: Kind<C, H>, ct: Kind<C, T>): Kind<C, HCons<H, T>>
+
+    /**
+     * The empty product.
+     */
+    fun emptyProduct(): Kind<C, HNil>
+
+    /**
+     * Given an isomorphism between `F` and `G`, and a type class instance for `G`,
+     * produce a type class instance for `F`.
+     */
+    fun <F, G> project(instance: () -> Kind<C, G>, to: (F) -> G, from: (G) -> F): Kind<C, F>
+
+}
