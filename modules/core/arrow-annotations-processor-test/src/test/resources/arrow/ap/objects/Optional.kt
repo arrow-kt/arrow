1cc53ecccbc9a64ff9e7612f977f3756049fd69a
Raul Raja <raulraja@gmail.com>
2018-04-06 23:31:30 +0200

    revert test formatting

--- modules/core/arrow-annotations-processor-test/src/test/resources/arrow/ap/objects/Optional.kt
+++ modules/core/arrow-annotations-processor-test/src/test/resources/arrow/ap/objects/Optional.kt
@@ -12,11 +12,8 @@ fun optionalNullableOptional(): arrow.optics.Optional<`arrow`.`ap`.`objects`.`Op
     }
   }
 )
-
 fun optionalOptionOptional(): arrow.optics.Optional<`arrow`.`ap`.`objects`.`Optional`, `kotlin`.`String`> = arrow.optics.Optional(
-  getOrModify = { optional: `arrow`.`ap`.`objects`.`Optional` ->
-    optional.`option`.orNull()?.right() ?: optional.left()
-  },
+  getOrModify = { optional: `arrow`.`ap`.`objects`.`Optional` -> optional.`option`.orNull()?.right() ?: optional.left() },
   set = { value: `kotlin`.`String` ->
     { optional: `arrow`.`ap`.`objects`.`Optional` ->
       optional.copy(`option` = value.toOption())
