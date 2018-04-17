267389cbd45faa797bd2dbb98d98c7d4b2c7fe9b
Simon Vergauwen <vergauwen.simon@gmail.com>
2018-04-17 19:04:24 +0200

    Generate DSL for sealed classes and improve error messages

--- modules/core/arrow-annotations-processor/src/main/java/arrow/optics/BoundSetterGenerator.kt
+++ modules/core/arrow-annotations-processor/src/main/java/arrow/optics/BoundSetterGenerator.kt
@@ -41,7 +41,7 @@ class BoundSetterGenerator(
                |import $packageName.*
                |""".trimMargin()
 
-  fun createDslFunction(annotatedOptic: AnnotatedOptic): String = """
+  private fun createDslFunction(annotatedOptic: AnnotatedOptic): String = """
         |/**
         | * @receiver [${annotatedOptic.sourceClassName.removeBackticks()}] the instance you want to bind the dsl on.
         | * @return [$boundSetter] an intermediate optics that is bound to the instance.
@@ -49,8 +49,8 @@ class BoundSetterGenerator(
         |fun ${annotatedOptic.sourceClassName}.setter() = $boundSetter(this, arrow.optics.PSetter.id())
         |""".trimMargin()
 
-  fun processBoundSetter(sourceClassName: String, targetName: String, targetClassName: String, sourceName: String) = """
-      |inline val <T> $boundSetter<T, $sourceClassName>.$targetName: $boundSetter<T, $targetClassName>
+  private fun processBoundSetter(sourceClassName: String, targetName: String, targetClassName: String, sourceName: String) = """
+      |inline val <T> $boundSetter<T, $sourceClassName>.${targetName.decapitalize()}: $boundSetter<T, $targetClassName>
       |    get() = this.compose($sourceName${targetName.toUpperCamelCase()}())
       |""".trimMargin()
 
--- modules/core/arrow-annotations-processor/src/test/java/arrow/ap/tests/PrismTest.kt
+++ modules/core/arrow-annotations-processor/src/test/java/arrow/ap/tests/PrismTest.kt
@@ -7,17 +7,22 @@ class PrismTest : APTest("arrow.ap.objects.prism") {
   init {
 
     testProcessor(AnnotationProcessor(
-      name = "Prisms cannot be generated for data class",
-      sourceFile = "PrismDataClass.java",
-      errorMessage = "Prisms can only be generated for sealed classes",
-      processor = OpticsProcessor()
+            name = "Prisms cannot be generated for data class",
+            sourceFile = "PrismDataClass.java",
+            errorMessage ="""
+      |Cannot generate arrow.optics.Prism for arrow.ap.objects.prism.PrismDataClass
+      |                                          ^
+      |  arrow.optics.OpticsTarget.PRISM is an invalid @optics argument for arrow.ap.objects.prism.PrismDataClass.
+      |  It is only valid for sealed classes.
+      """.trimMargin(),
+            processor = OpticsProcessor()
     ))
 
     testProcessor(AnnotationProcessor(
-      name = "Prisms are generated for sealed class",
-      sourceFile = "Prism.java",
-      destFile = "Prism.kt",
-      processor = OpticsProcessor()
+            name = "Prisms are generated for sealed class",
+            sourceFile = "Prism.java",
+            destFile = "Prism.kt",
+            processor = OpticsProcessor()
     ))
 
 
