4e5b515ff1d5fe0d1c63dcf0f4c1ddfd63ab22dd
Simon Vergauwen <vergauwen.simon@gmail.com>
2018-04-18 09:06:06 +0200

    Move errors to seperate file

--- modules/core/arrow-annotations-processor/src/main/java/arrow/optics/OpticsProcessor.kt
+++ modules/core/arrow-annotations-processor/src/main/java/arrow/optics/OpticsProcessor.kt
@@ -123,60 +123,6 @@ class OpticsProcessor : AbstractProcessor() {
     else -> null.also { logE(element.isoErrorMessage, element) }
   }
 
-  private val Element.otherClassTypeErrorMessage
-    get() = """
-      |$this cannot be annotated with @optics
-      | ^
-      |
-      |Only data and sealed classes can be annotated with @optics annotation""".trimMargin()
-
-  private val Element.lensErrorMessage
-    get() = """
-      |Cannot generate arrow.optics.Lens for $this
-      |                                       ^
-      |arrow.optics.OpticsTarget.LENS is an invalid @optics argument for $this.
-      |It is only valid for data classes.
-      """.trimMargin()
-
-  private val Element.optionalErrorMessage
-    get() = """
-      |Cannot generate arrow.optics.Optional for $this
-      |                                           ^
-      |arrow.optics.OpticsTarget.OPTIONAL is an invalid @optics argument for $this.
-      |It is only valid for data classes.
-      """.trimMargin()
-
-  private val Element.prismErrorMessage
-    get() = """
-      |Cannot generate arrow.optics.Prism for $this
-      |                                        ^
-      |arrow.optics.OpticsTarget.PRISM is an invalid @optics argument for $this.
-      |It is only valid for sealed classes.
-      """.trimMargin()
-
-  private val Element.isoErrorMessage
-    get() = """
-      |Cannot generate arrow.optics.Iso for $this
-      |                                      ^
-      |arrow.optics.OpticsTarget.ISO is an invalid @optics argument for $this.
-      |It is only valid for data classes.
-      """.trimMargin()
-
-  private val Element.isoTooBigErrorMessage
-    get() = """
-      |Cannot generate arrow.optics.Iso for $this
-      |                                      ^
-      |Iso generation is supported for data classes with up to 22 constructor parameters.
-      """.trimMargin()
-
-  private val Element.dslErrorMessage
-    get() = """
-      |Cannot generate DSL (arrow.optics.BoundSetter) for $this
-      |                                           ^
-      |arrow.optics.OpticsTarget.DSL is an invalid @optics argument for $this.
-      |It is only valid for data classes and sealed classes.
-      """.trimMargin()
-
   private enum class ClassType {
     DATA_CLASS,
     SEALED_CLASS,
