a29958a2ba111f4ec4c2b3cb63aebcea008d5638
raulraja <raulraja@gmail.com>
2018-04-22 21:56:16 +0200

    mtl: `Function1<I>().run { ... }` + `with(Function1<I>()) { ... }`

--- modules/core/arrow-mtl/src/main/kotlin/arrow/mtl/instances/function1.kt
+++ modules/core/arrow-mtl/src/main/kotlin/arrow/mtl/instances/function1.kt
@@ -15,3 +15,16 @@ interface Function1MonadReaderInstance<I> : Function1MonadInstance<I>, MonadRead
 
   override fun <A> Kind<Function1PartialOf<I>, A>.local(f: (I) -> I): Function1<I, A> = fix().local(f)
 }
+
+class Function1MtlContext<A> : Function1MonadReaderInstance<A>
+
+class Function1MtlContextPartiallyApplied<L> {
+  fun <A> run(f: Function1MtlContext<L>.() -> A): A =
+    f(Function1MtlContext())
+}
+
+fun <L> Function1(): Function1MtlContextPartiallyApplied<L> =
+  Function1MtlContextPartiallyApplied()
+
+fun <L, A> with(c: Function1MtlContextPartiallyApplied<L>, f: Function1MtlContext<L>.() -> A): A =
+  c.run(f)
