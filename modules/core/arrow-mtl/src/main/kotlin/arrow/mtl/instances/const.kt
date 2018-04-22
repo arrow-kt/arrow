6ba5348ea9723ebc8245e96a0b65fb7345d6e5be
raulraja <raulraja@gmail.com>
2018-04-22 21:52:22 +0200

    mtl: `Const(MA).run { ... }` + `with(Const(MA)) { ... }`

--- modules/core/arrow-mtl/src/main/kotlin/arrow/mtl/instances/const.kt
+++ modules/core/arrow-mtl/src/main/kotlin/arrow/mtl/instances/const.kt
@@ -3,6 +3,7 @@ package arrow.mtl.instances
 import arrow.Kind
 import arrow.core.Option
 import arrow.instance
+import arrow.instances.ConstApplicativeInstance
 import arrow.instances.ConstTraverseInstance
 import arrow.mtl.typeclasses.TraverseFilter
 import arrow.typeclasses.*
@@ -15,3 +16,21 @@ interface ConstTraverseFilterInstance<X> : ConstTraverseInstance<X>, TraverseFil
   override fun <G, A, B> Kind<ConstPartialOf<X>, A>.traverseFilter(AP: Applicative<G>, f: (A) -> Kind<G, Option<B>>): Kind<G, ConstOf<X, B>> =
     fix().traverseFilter(AP, f)
 }
+
+class ConstMtlContext<A>(val MA: Monoid<A>) : ConstApplicativeInstance<A>, ConstTraverseFilterInstance<A> {
+  override fun MA(): Monoid<A> = MA
+
+  override fun <T, U> Kind<ConstPartialOf<A>, T>.map(f: (T) -> U): Const<A, U> =
+    fix().map(f)
+}
+
+class ConstMtlContextPartiallyApplied<L>(val MA: Monoid<L>) {
+  fun <A> run(f: ConstMtlContext<L>.() -> A): A =
+    f(ConstMtlContext(MA))
+}
+
+fun <L> Const(MA: Monoid<L>): ConstMtlContextPartiallyApplied<L> =
+  ConstMtlContextPartiallyApplied(MA)
+
+fun <L, A> with(c: ConstMtlContextPartiallyApplied<L>, f: ConstMtlContext<L>.() -> A): A =
+  c.run(f)
