f1d057a3f71bfa3f04c73089d9aca2a3eee77536
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-04-25 16:51:55 +0200

    delete renzu infographic after running tests, not before

--- modules/core/arrow-annotations-processor/src/test/java/arrow/ap/tests/RenzuTest.kt
+++ modules/core/arrow-annotations-processor/src/test/java/arrow/ap/tests/RenzuTest.kt
@@ -6,13 +6,13 @@ import java.io.File
 class RenzuTest : APTest("arrow.ap.objects.renzu") {
 
   init {
-    File("./infographic/", "").listFiles()?.forEach { it.delete() }
-
     testProcessor(AnnotationProcessor(
       name = "Generates UML for nomnoml",
       sourceFiles = listOf("OptionMonoidInstance.java", "OptionSemigroupInstance.java"),
       destFile = "SampleUML.txt",
       processor = RenzuProcessor()
     ), generationDir = File("./infographic/", "").also { it.mkdirs() })
+
+    File("./infographic/", "").listFiles()?.forEach { it.delete() }
   }
 }
