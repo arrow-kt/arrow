54f2ad63099f74235222d25a83a883a9de2120ee
Andrzej Ressel <jereksel@gmail.com>
2018-04-27 18:13:32 +0200

    Update Kotlin to 1.2.40

--- modules/core/arrow-annotations-processor/src/main/java/arrow/common/utils/ProcessorUtils.kt
+++ modules/core/arrow-annotations-processor/src/main/java/arrow/common/utils/ProcessorUtils.kt
@@ -2,10 +2,10 @@ package arrow.common.utils
 
 import me.eugeniomarletti.kotlin.metadata.*
 import me.eugeniomarletti.kotlin.metadata.jvm.getJvmMethodSignature
-import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.java.MethodElement
-import org.jetbrains.kotlin.serialization.ProtoBuf
-import org.jetbrains.kotlin.serialization.deserialization.TypeTable
-import org.jetbrains.kotlin.serialization.deserialization.supertypes
+import me.eugeniomarletti.kotlin.metadata.shadow.metadata.ProtoBuf
+import me.eugeniomarletti.kotlin.metadata.shadow.metadata.deserialization.TypeTable
+import me.eugeniomarletti.kotlin.metadata.shadow.metadata.deserialization.supertypes
+import me.eugeniomarletti.kotlin.metadata.shadow.serialization.deserialization.getName
 import java.io.File
 import javax.lang.model.element.Element
 import javax.lang.model.element.ExecutableElement
@@ -55,8 +55,8 @@ interface ProcessorUtils : KotlinMetadataUtils {
       ?: knownError("Arrow's annotation can't be used on $classElement")
   }
 
-  fun TypeElement.methods(): List<MethodElement> =
-    enclosedElements.mapNotNull { it as? MethodElement }
+//  fun TypeElement.methods(): List<MethodElement> =
+//    enclosedElements.mapNotNull { it as? MethodElement }
 
   fun ClassOrPackageDataWrapper.getFunction(methodElement: ExecutableElement) =
     getFunctionOrNull(methodElement, nameResolver, functionList)
@@ -82,9 +82,9 @@ interface ProcessorUtils : KotlinMetadataUtils {
   }
 
   fun recurseTypeclassInterfaces(
-    current: ClassOrPackageDataWrapper.Class,
-    typeTable: TypeTable,
-    acc: List<ClassOrPackageDataWrapper>): List<ClassOrPackageDataWrapper> {
+          current: ClassOrPackageDataWrapper.Class,
+          typeTable: TypeTable,
+          acc: List<ClassOrPackageDataWrapper>): List<ClassOrPackageDataWrapper> {
     val interfaces = current.classProto.supertypes(typeTable).map {
       it.extractFullName(current)
     }.filter {
