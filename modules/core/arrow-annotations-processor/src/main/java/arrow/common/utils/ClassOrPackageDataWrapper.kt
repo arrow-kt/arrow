c53c15d09db5d104c248d6802872df564defeeff
Andrzej Ressel <jereksel@gmail.com>
2018-04-28 23:48:39 +0200

    Revert

--- modules/core/arrow-annotations-processor/src/main/java/arrow/common/utils/ClassOrPackageDataWrapper.kt
+++ modules/core/arrow-annotations-processor/src/main/java/arrow/common/utils/ClassOrPackageDataWrapper.kt
@@ -2,40 +2,43 @@ package arrow.common.utils
 
 import me.eugeniomarletti.kotlin.metadata.ClassData
 import me.eugeniomarletti.kotlin.metadata.PackageData
-import me.eugeniomarletti.kotlin.metadata.shadow.metadata.ProtoBuf
+import me.eugeniomarletti.kotlin.metadata.shadow.metadata.ProtoBuf.Constructor
+import me.eugeniomarletti.kotlin.metadata.shadow.metadata.ProtoBuf.Function
+import me.eugeniomarletti.kotlin.metadata.shadow.metadata.ProtoBuf.Property
+import me.eugeniomarletti.kotlin.metadata.shadow.metadata.ProtoBuf.TypeParameter
 import me.eugeniomarletti.kotlin.metadata.shadow.metadata.deserialization.NameResolver
 
 sealed class ClassOrPackageDataWrapper {
   abstract val `package`: String
   abstract val nameResolver: NameResolver
-  abstract val constructorList: List<ProtoBuf.Constructor>
-  abstract val functionList: List<ProtoBuf.Function>
-  abstract val propertyList: List<ProtoBuf.Property>
-  abstract val typeParameters: List<ProtoBuf.TypeParameter>
-  abstract fun getTypeParameter(typeParameterIndex: Int): ProtoBuf.TypeParameter?
+  abstract val constructorList: List<Constructor>
+  abstract val functionList: List<Function>
+  abstract val propertyList: List<Property>
+  abstract val typeParameters: List<TypeParameter>
+  abstract fun getTypeParameter(typeParameterIndex: Int): TypeParameter?
 
   class Package(
     override val nameResolver: NameResolver,
-    val packageProto: ProtoBuf.Package,
+    val packageProto: me.eugeniomarletti.kotlin.metadata.shadow.metadata.ProtoBuf.Package,
     override val `package`: String
   ) : ClassOrPackageDataWrapper() {
-    override val constructorList: List<ProtoBuf.Constructor> get() = emptyList()
-    override val functionList: List<ProtoBuf.Function> get() = packageProto.functionList
-    override val propertyList: List<ProtoBuf.Property> get() = packageProto.propertyList
-    override val typeParameters: List<ProtoBuf.TypeParameter> = emptyList()
-    override fun getTypeParameter(typeParameterIndex: Int): ProtoBuf.TypeParameter? = null
+    override val constructorList: List<Constructor> get() = emptyList()
+    override val functionList: List<Function> get() = packageProto.functionList
+    override val propertyList: List<Property> get() = packageProto.propertyList
+    override val typeParameters: List<TypeParameter> = emptyList()
+    override fun getTypeParameter(typeParameterIndex: Int): TypeParameter? = null
   }
 
   class Class(
     override val nameResolver: NameResolver,
-    val classProto: ProtoBuf.Class,
+    val classProto: me.eugeniomarletti.kotlin.metadata.shadow.metadata.ProtoBuf.Class,
     override val `package`: String
   ) : ClassOrPackageDataWrapper() {
-    override val constructorList: List<ProtoBuf.Constructor> get() = classProto.constructorList
-    override val functionList: List<ProtoBuf.Function> get() = classProto.functionList
-    override val propertyList: List<ProtoBuf.Property> get() = classProto.propertyList
-    override val typeParameters: List<ProtoBuf.TypeParameter> = classProto.typeParameterList
-    override fun getTypeParameter(typeParameterIndex: Int): ProtoBuf.TypeParameter? = classProto.getTypeParameter(typeParameterIndex)
+    override val constructorList: List<Constructor> get() = classProto.constructorList
+    override val functionList: List<Function> get() = classProto.functionList
+    override val propertyList: List<Property> get() = classProto.propertyList
+    override val typeParameters: List<TypeParameter> = classProto.typeParameterList
+    override fun getTypeParameter(typeParameterIndex: Int): TypeParameter? = classProto.getTypeParameter(typeParameterIndex)
   }
 }
 
