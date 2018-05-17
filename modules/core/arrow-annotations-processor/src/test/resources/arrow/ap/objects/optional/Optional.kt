0cf749fc785861b72b189012cc1e15b461f9033e
Simon Vergauwen <vergauwen.simon@gmail.com>
2018-05-17 13:19:40 +0200

    Fix Optional processor tests

--- modules/core/arrow-annotations-processor/src/test/resources/arrow/ap/objects/optional/Optional.kt
+++ modules/core/arrow-annotations-processor/src/test/resources/arrow/ap/objects/optional/Optional.kt
@@ -12,6 +12,15 @@ inline val `arrow`.`ap`.`objects`.`optional`.`Optional`.Companion.nullable: arro
     }
   }
 )
+
+inline val <S> arrow.optics.Iso<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.nullable: arrow.optics.Optional<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.nullable
+inline val <S> arrow.optics.Lens<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.nullable: arrow.optics.Optional<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.nullable
+inline val <S> arrow.optics.Optional<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.nullable: arrow.optics.Optional<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.nullable
+inline val <S> arrow.optics.Prism<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.nullable: arrow.optics.Optional<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.nullable
+inline val <S> arrow.optics.Setter<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.nullable: arrow.optics.Setter<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.nullable
+inline val <S> arrow.optics.Traversal<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.nullable: arrow.optics.Traversal<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.nullable
+inline val <S> arrow.optics.Fold<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.nullable: arrow.optics.Fold<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.nullable
+
 inline val `arrow`.`ap`.`objects`.`optional`.`Optional`.Companion.option: arrow.optics.Optional<`arrow`.`ap`.`objects`.`optional`.`Optional`, `kotlin`.`String`> get()= arrow.optics.Optional(
   getOrModify = { optional: `arrow`.`ap`.`objects`.`optional`.`Optional` -> optional.`option`.orNull()?.right() ?: optional.left() },
   set = { value: `kotlin`.`String` ->
@@ -20,3 +29,11 @@ inline val `arrow`.`ap`.`objects`.`optional`.`Optional`.Companion.option: arrow.
     }
   }
 )
+
+inline val <S> arrow.optics.Iso<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.option: arrow.optics.Optional<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.option
+inline val <S> arrow.optics.Lens<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.option: arrow.optics.Optional<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.option
+inline val <S> arrow.optics.Optional<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.option: arrow.optics.Optional<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.option
+inline val <S> arrow.optics.Prism<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.option: arrow.optics.Optional<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.option
+inline val <S> arrow.optics.Setter<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.option: arrow.optics.Setter<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.option
+inline val <S> arrow.optics.Traversal<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.option: arrow.optics.Traversal<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.option
+inline val <S> arrow.optics.Fold<S, `arrow`.`ap`.`objects`.`optional`.`Optional`>.option: arrow.optics.Fold<S, `kotlin`.`String`> inline get() = this + `arrow`.`ap`.`objects`.`optional`.`Optional`.option
