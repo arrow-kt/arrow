5b11f27237b469f67e438bbeb96d9b5a8da88238
Cotel <miguelcoletomunoz@hotmail.com>
2018-06-16 00:10:39 +0200

    Updating FluxK

--- modules/effects/arrow-effects-reactor/src/main/kotlin/arrow/effecs/FluxK.kt
+++ modules/effects/arrow-effects-reactor/src/main/kotlin/arrow/effecs/FluxK.kt
@@ -3,122 +3,118 @@ package arrow.effecs
 import arrow.Kind
 import arrow.core.Either
 import arrow.core.Eval
-import arrow.core.Right
 import arrow.core.Left
-import arrow.deriving
-import arrow.effects.Proc
+import arrow.core.Right
+import arrow.core.identity
+import arrow.effects.typeclasses.Proc
 import arrow.higherkind
-import arrow.typeclasses.*
+import arrow.typeclasses.Applicative
 import reactor.core.publisher.Flux
 import reactor.core.publisher.FluxSink
 
 fun <A> Flux<A>.k(): FluxK<A> = FluxK(this)
 
 fun <A> FluxKOf<A>.value(): Flux<A> =
-        this.fix().flux
+    this.fix().flux
 
 @higherkind
-@deriving(
-        Functor::class,
-        Applicative::class,
-        Monad::class,
-        Foldable::class,
-        Traverse::class
-)
 data class FluxK<A>(val flux: Flux<A>) : FluxKOf<A>, FluxKKindedJ<A> {
+  fun <B> map(f: (A) -> B): FluxK<B> =
+      flux.map(f).k()
 
-    fun <B> map(f: (A) -> B): FluxK<B> =
-            flux.map(f).k()
+  fun <B> ap(fa: FluxKOf<(A) -> B>): FluxK<B> =
+      flatMap { a -> fa.fix().map { ff -> ff(a) } }
 
-    fun <B> ap(fa: FluxKOf<(A) -> B>): FluxK<B> =
-            flatMap { a -> fa.fix().map { ff -> ff(a) } }
+  fun <B> flatMap(f: (A) -> FluxKOf<B>): FluxK<B> =
+      flux.flatMap { f(it).fix().flux }.k()
 
-    fun <B> flatMap(f: (A) -> FluxKOf<B>): FluxK<B> =
-            flux.flatMap { f(it).fix().flux }.k()
+  fun <B> concatMap(f: (A) -> FluxKOf<B>): FluxK<B> =
+      flux.concatMap { f(it).fix().flux }.k()
 
-    fun <B> concatMap(f: (A) -> FluxKOf<B>): FluxK<B> =
-            flux.concatMap { f(it).fix().flux }.k()
+  fun <B> switchMap(f: (A) -> FluxKOf<B>): FluxK<B> =
+      flux.switchMap { f(it).fix().flux }.k()
 
-    fun <B> switchMap(f: (A) -> FluxKOf<B>): FluxK<B> =
-            flux.switchMap { f(it).fix().flux }.k()
+  fun <B> foldLeft(b: B, f: (B, A) -> B): B = flux.reduce(b, f).block()
 
-    fun <B> foldLeft(b: B, f: (B, A) -> B): B = flux.reduce(b, f).block()
-
-    fun <B> foldRight(lb: Eval<B>, f: (A, Eval<B>) -> Eval<B>): Eval<B> {
-        fun loop(fa_p: FluxK<A>): Eval<B> = when {
-            fa_p.flux.hasElements().block() -> lb
-            else -> f(fa_p.flux.blockFirst(), Eval.defer { loop(fa_p.flux.skip(1).k()) })
-        }
-
-        return Eval.defer { loop(this) }
+  fun <B> foldRight(lb: Eval<B>, f: (A, Eval<B>) -> Eval<B>): Eval<B> {
+    fun loop(fa_p: FluxK<A>): Eval<B> = when {
+      fa_p.flux.hasElements().block() -> lb
+      else -> f(fa_p.flux.blockFirst(), Eval.defer { loop(fa_p.flux.skip(1).k()) })
     }
 
-    fun <G, B> traverse(f: (A) -> Kind<G, B>, GA: Applicative<G>): Kind<G, FluxK<B>> =
-            foldRight(Eval.always { GA.pure(Flux.empty<B>().k()) }) { a, eval ->
-                GA.map2Eval(f(a), eval) { Flux.concat(Flux.just<B>(it.a), it.b.flux).k() }
-            }.value()
+    return Eval.defer { loop(this) }
+  }
 
-    fun runAsync(cb: (Either<Throwable, A>) -> FluxKOf<Unit>): FluxK<Unit> =
-            flux.flatMap { cb(Right(it)).value() }.onErrorResume { cb(Left(it)).value() }.k()
+  fun <G, B> traverse(GA: Applicative<G>, f: (A) -> Kind<G, B>): Kind<G, FluxK<B>> = GA.run {
+    foldRight(Eval.always { GA.just(Flux.empty<B>().k()) }) { a, eval ->
+      f(a).map2Eval(eval) { Flux.concat(Flux.just<B>(it.a), it.b.flux).k() }
+    }.value()
+  }
 
-    companion object {
-        fun <A> pure(a: A): FluxK<A> =
-                Flux.just(a).k()
+  fun handleErrorWith(function: (Throwable) -> FluxK<A>): FluxK<A> =
+      this.fix().flux.onErrorResume { t: Throwable -> function(t).flux }.k()
 
-        fun <A> raiseError(t: Throwable): FluxK<A> =
-                Flux.error<A>(t).k()
+  fun runAsync(cb: (Either<Throwable, A>) -> FluxKOf<Unit>): FluxK<Unit> =
+      flux.flatMap { cb(Right(it)).value() }.onErrorResume { cb(Left(it)).value() }.k()
 
-        operator fun <A> invoke(fa: () -> A): FluxK<A> =
-                suspend { pure(fa()) }
+  companion object {
+    fun <A> just(a: A): FluxK<A> =
+        Flux.just(a).k()
 
-        fun <A> suspend(fa: () -> FluxKOf<A>): FluxK<A> =
-                Flux.defer { fa().value() }.k()
+    fun <A> raiseError(t: Throwable): FluxK<A> =
+        Flux.error<A>(t).k()
 
-        fun <A> runAsync(fa: Proc<A>): FluxK<A> =
-                Flux.create { emitter: FluxSink<A> ->
-                    fa { either: Either<Throwable, A> ->
-                        either.fold({
-                            emitter.error(it)
-                        }, {
-                            emitter.next(it)
-                            emitter.complete()
-                        })
-                    }
-                }.k()
+    operator fun <A> invoke(fa: () -> A): FluxK<A> =
+        defer { just(fa()) }
 
-        tailrec fun <A, B> tailRecM(a: A, f: (A) -> FluxKOf<Either<A, B>>): FluxK<B> {
-            val either = f(a).fix().value().blockFirst()
-            return when (either) {
-                is Either.Left -> tailRecM(either.a, f)
-                is Either.Right -> Flux.just(either.b).k()
-            }
-        }
+    fun <A> defer(fa: () -> FluxKOf<A>): FluxK<A> =
+        Flux.defer { fa().value() }.k()
 
-        fun monadFlat(): FluxKMonadInstance = FluxKMonadInstanceImplicits.instance()
+    fun <A> runAsync(fa: Proc<A>): FluxK<A> =
+        Flux.create { emitter: FluxSink<A> ->
+          fa { either: Either<Throwable, A> ->
+            either.fold({
+              emitter.error(it)
+            }, {
+              emitter.next(it)
+              emitter.complete()
+            })
+          }
+        }.k()
 
-        fun monadConcat(): FluxKMonadInstance = object : FluxKMonadInstance {
-            override fun <A, B> flatMap(fa: FluxKOf<A>, f: (A) -> FluxKOf<B>): FluxK<B> =
-                    fa.fix().concatMap { f(it).fix() }
-        }
-
-        fun monadSwitch(): FluxKMonadInstance = object : FluxKMonadErrorInstance {
-            override fun <A, B> flatMap(fa: FluxKOf<A>, f: (A) -> FluxKOf<B>): FluxK<B> =
-                    fa.fix().switchMap { f(it).fix() }
-        }
-
-        fun monadErrorFlat(): FluxKMonadErrorInstance = FluxKMonadErrorInstanceImplicits.instance()
-
-        fun monadErrorConcat(): FluxKMonadErrorInstance = object : FluxKMonadErrorInstance {
-            override fun <A, B> flatMap(fa: FluxKOf<A>, f: (A) -> FluxKOf<B>): FluxK<B> =
-                    fa.fix().concatMap { f(it).fix() }
-        }
-
-        fun monadErrorSwitch(): FluxKMonadErrorInstance = object : FluxKMonadErrorInstance {
-            override fun <A, B> flatMap(fa: FluxKOf<A>, f: (A) -> FluxKOf<B>): FluxK<B> =
-                    fa.fix().switchMap { f(it).fix() }
-        }
+    tailrec fun <A, B> tailRecM(a: A, f: (A) -> FluxKOf<Either<A, B>>): FluxK<B> {
+      val either = f(a).fix().value().blockFirst()
+      return when (either) {
+        is Either.Left -> tailRecM(either.a, f)
+        is Either.Right -> Flux.just(either.b).k()
+      }
     }
+
+    fun monadFlat(): FluxKMonadInstance = monad()
+
+    fun monadConcat(): FluxKMonadInstance = object : FluxKMonadInstance {
+      override fun <A, B> Kind<ForFluxK, A>.flatMap(f: (A) -> Kind<ForFluxK, B>): FluxK<B> =
+          fix().concatMap { f(it).fix() }
+    }
+
+    fun monadSwitch(): FluxKMonadInstance = object : FluxKMonadErrorInstance {
+      override fun <A, B> Kind<ForFluxK, A>.flatMap(f: (A) -> Kind<ForFluxK, B>): FluxK<B> =
+          fix().switchMap { f(it).fix() }
+    }
+
+    fun monadErrorFlat(): FluxKMonadErrorInstance = monadError()
+
+    fun monadErrorConcat(): FluxKMonadErrorInstance = object : FluxKMonadErrorInstance {
+      override fun <A, B> Kind<ForFluxK, A>.flatMap(f: (A) -> Kind<ForFluxK, B>): FluxK<B> =
+          fix().concatMap { f(it).fix() }
+    }
+
+    fun monadErrorSwitch(): FluxKMonadErrorInstance = object : FluxKMonadErrorInstance {
+      override fun <A, B> Kind<ForFluxK, A>.flatMap(f: (A) -> Kind<ForFluxK, B>): FluxK<B> =
+          fix().switchMap { f(it).fix() }
+    }
+  }
 }
 
-fun <A> FluxKOf<A>.handleErrorWith(function: (Throwable) -> FluxK<A>): FluxK<A> =
-        this.fix().flux.onErrorResume { t: Throwable -> function(t).flux }.k()
+inline fun <A, G> FluxKOf<Kind<G, A>>.sequence(GA: Applicative<G>): Kind<G, FluxK<A>> =
+    fix().traverse(GA, ::identity)
--- modules/effects/arrow-effects-reactor/src/test/kotlin/arrow/effects/FluxKTests.kt
+++ modules/effects/arrow-effects-reactor/src/test/kotlin/arrow/effects/FluxKTests.kt
@@ -1,13 +1,20 @@
 package arrow.effects
 
-import arrow.effecs.*
+import arrow.effecs.FluxK
+import arrow.effecs.FluxKOf
+import arrow.effecs.async
+import arrow.effecs.foldable
+import arrow.effecs.functor
+import arrow.effecs.k
+import arrow.effecs.traverse
+import arrow.effecs.value
 import arrow.test.UnitSpec
 import arrow.test.laws.AsyncLaws
 import arrow.test.laws.FoldableLaws
 import arrow.test.laws.TraverseLaws
-import arrow.typeclasses.*
+import arrow.typeclasses.Eq
+import arrow.typeclasses.bindingCatch
 import io.kotlintest.KTestJUnitRunner
-import io.kotlintest.matchers.shouldNotBe
 import org.junit.runner.RunWith
 import reactor.core.publisher.Flux
 import reactor.test.test
@@ -16,73 +23,60 @@ import java.time.Duration
 @RunWith(KTestJUnitRunner::class)
 class FluxKTest : UnitSpec() {
 
-    fun <T> EQ(): Eq<FluxKOf<T>> = object : Eq<FluxKOf<T>> {
-        override fun eqv(a: FluxKOf<T>, b: FluxKOf<T>): Boolean =
-                try {
-                    a.value().blockFirst() == b.value().blockFirst()
-                } catch (throwable: Throwable) {
-                    val errA = try {
-                        a.value().blockFirst()
-                        throw IllegalArgumentException()
-                    } catch (err: Throwable) {
-                        err
-                    }
+  fun <T> EQ(): Eq<FluxKOf<T>> = object : Eq<FluxKOf<T>> {
+    override fun FluxKOf<T>.eqv(b: FluxKOf<T>): Boolean =
+        try {
+          this.value().blockFirst() == b.value().blockFirst()
+        } catch (throwable: Throwable) {
+          val errA = try {
+            this.value().blockFirst()
+            throw IllegalArgumentException()
+          } catch (err: Throwable) {
+            err
+          }
 
-                    val errB = try {
-                        b.value().blockFirst()
-                        throw IllegalStateException()
-                    } catch (err: Throwable) {
-                        err
-                    }
+          val errB = try {
+            b.value().blockFirst()
+            throw IllegalStateException()
+          } catch (err: Throwable) {
+            err
+          }
 
-                    errA == errB
-                }
+          errA == errB
+        }
+  }
+
+  init {
+
+    testLaws(
+        AsyncLaws.laws(FluxK.async(), EQ(), EQ()),
+        FoldableLaws.laws(FluxK.foldable(), { FluxK.just(it) }, Eq.any()),
+        TraverseLaws.laws(FluxK.traverse(), FluxK.functor(), { FluxK.just(it) }, EQ())
+    )
+
+    "Multi-thread Fluxes finish correctly" {
+      val value: Flux<Int> = FluxK.monadErrorFlat().bindingCatch {
+        val a = Flux.just(0).delayElements(Duration.ofSeconds(2)).k().bind()
+        a
+      }.value()
+
+      value.test()
+          .expectNext(0)
+          .verifyComplete()
     }
 
-    init {
+    "Flux cancellation forces binding to cancel without completing too" {
+      val value = FluxK.monadErrorFlat().bindingCatch {
+        val a = Flux.just(0).delayElements(Duration.ofSeconds(3)).k().bind()
+        a
+      }.value()
 
-        "instances can be resolved implicitly" {
-            functor<ForFluxK>() shouldNotBe null
-            applicative<ForFluxK>() shouldNotBe null
-            monad<ForFluxK>() shouldNotBe null
-            applicativeError<ForFluxK, Unit>() shouldNotBe null
-            monadError<ForFluxK, Unit>() shouldNotBe null
-            monadSuspend<ForFluxK>() shouldNotBe null
-            async<ForFluxK>() shouldNotBe null
-            effect<ForFluxK>() shouldNotBe null
-            foldable<ForFluxK>() shouldNotBe null
-            traverse<ForFluxK>() shouldNotBe null
-        }
-
-        testLaws(
-                AsyncLaws.laws(FluxK.async(), EQ(), EQ()),
-                FoldableLaws.laws(FluxK.foldable(), { FluxK.pure(it) }, Eq.any()),
-                TraverseLaws.laws(FluxK.traverse(), FluxK.functor(), { FluxK.pure(it) }, EQ())
-        )
-
-        "Multi-thread Fluxes finish correctly" {
-            val value: Flux<Int> = FluxK.monadErrorFlat().bindingCatch {
-                val a = Flux.just(0).delayElements(Duration.ofSeconds(2)).k().bind()
-                yields(a)
-            }.value()
-
-            value.test()
-                    .expectNext(0)
-                    .verifyComplete()
-        }
-
-        "Flux cancellation forces binding to cancel without completing too" {
-            val value = FluxK.monadErrorFlat().bindingCatch {
-                val a = Flux.just(0).delayElements(Duration.ofSeconds(3)).k().bind()
-                a
-            }.value()
-
-            val test = value.doOnSubscribe { subscription -> Flux.just(0).delayElements(Duration.ofSeconds(1)).subscribe { subscription.cancel() } }.test()
-            test
-                    .expectNextCount(0)
-                    .verifyComplete()
-        }
-
+      val test = value.doOnSubscribe { subscription -> Flux.just(0).delayElements(Duration.ofSeconds(1)).subscribe { subscription.cancel() } }.test()
+      test
+          .expectNextCount(0)
+          .verifyComplete()
     }
+
+  }
 
 }
