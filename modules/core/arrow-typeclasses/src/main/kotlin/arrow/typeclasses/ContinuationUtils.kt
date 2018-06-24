89cdb891172005286eda26fa497ee0c94d66bb17
Paco <pakoito@users.noreply.github.com>
2018-06-24 20:28:30 +0100

    Fix comprehension crash  (#905)
    * Remove duplicate ContinuationUtils
    
    * Add new ContinuationUtils from h0tk3y/probabilistik
    
    * Amend all ContinuationUtils callsites

--- modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/ContinuationUtils.kt
+++ modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/ContinuationUtils.kt
@@ -5,21 +5,31 @@ import kotlin.coroutines.experimental.Continuation
 private val coroutineImplClass by lazy { Class.forName("kotlin.coroutines.experimental.jvm.internal.CoroutineImpl") }
 
 private val labelField by lazy { coroutineImplClass.getDeclaredField("label").apply { isAccessible = true } }
-private val completionField by lazy { coroutineImplClass.getDeclaredField("completion").apply { isAccessible = true } }
 
-private var <T> Continuation<T>.label
-  get() = labelField.get(this)
-  set(value) = labelField.set(this@label, value)
+private val completionField by lazy { coroutineImplClass.getDeclaredField("completion").apply { isAccessible = true } }
 
 private var <T> Continuation<T>.completion: Continuation<*>?
   get() = completionField.get(this) as Continuation<*>
   set(value) = completionField.set(this@completion, value)
 
-var <T> Continuation<T>.stackLabels: List<Any>
-  get() = if (coroutineImplClass.isInstance(this)) listOf(label) + completion?.stackLabels.orEmpty() else emptyList()
+var <T> Continuation<T>.stateStack: List<Map<String, *>>
+  get() {
+    if (!coroutineImplClass.isInstance(this)) return emptyList()
+    val resultForThis = (this.javaClass.declaredFields + labelField)
+      .associate { it.isAccessible = true; it.name to it.get(this@stateStack) }
+      .let(::listOf)
+    val resultForCompletion = completion?.stateStack
+    return resultForCompletion?.let { resultForThis + it } ?: resultForThis
+  }
   set(value) {
-    if (coroutineImplClass.isInstance(this)) {
-      label = value.first()
-      completion?.stackLabels = value.subList(1, value.size)
+    if (!coroutineImplClass.isInstance(this)) return
+    val mapForThis = value.first()
+    (this.javaClass.declaredFields + labelField).forEach {
+      if (it.name in mapForThis) {
+        it.isAccessible = true
+        val fieldValue = mapForThis[it.name]
+        it.set(this@stateStack, fieldValue)
+      }
     }
+    completion?.stateStack = value.subList(1, value.size)
   }
