41dd06b2e2faad866078c2bfd92ec9ecc60e79a2
Simon Vergauwen <vergauwen.simon@gmail.com>
2018-06-24 14:44:05 +0200

    Add missing test NonEmptyList

--- modules/optics/arrow-optics/src/test/kotlin/arrow/optics/std/NonEmptyListTest.kt
+++ modules/optics/arrow-optics/src/test/kotlin/arrow/optics/std/NonEmptyListTest.kt
@@ -1,12 +1,13 @@
 package arrow.optics
 
+import arrow.data.ListK
 import arrow.data.NonEmptyList
+import arrow.data.monoid
+import arrow.instances.ListInstances
 import arrow.instances.monoid
 import arrow.test.UnitSpec
 import arrow.test.generators.genFunctionAToB
 import arrow.test.generators.genNonEmptyList
-import arrow.test.generators.genOption
-import arrow.test.laws.IsoLaws
 import arrow.test.laws.LensLaws
 import arrow.typeclasses.Eq
 import arrow.typeclasses.Monoid
@@ -28,6 +29,19 @@ class NonEmptyListTest : UnitSpec() {
       EQB = Eq.any(),
       MB = String.monoid())
     )
+
+    testLaws(LensLaws.laws(
+      lens = NonEmptyList.tail(),
+      aGen = genNonEmptyList(Gen.string()),
+      bGen = Gen.list(Gen.string()),
+      funcGen = genFunctionAToB(Gen.list(Gen.string())),
+      EQA = Eq.any(),
+      EQB = Eq.any(),
+      MB = object : Monoid<List<String>> {
+        override fun empty(): List<String> = ListK.empty<String>().list
+        override fun List<String>.combine(b: List<String>): List<String> = this + b
+      }
+    ))
 
   }
 
