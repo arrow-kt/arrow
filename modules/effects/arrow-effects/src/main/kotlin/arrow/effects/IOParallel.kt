bc19d808cb4ac9c48348d03bbaf9c7429acc76aa
pakoito <jueces.jus@gmail.com>
2018-07-22 19:18:55 +0100

    Cleanup of unnecessary lambdas

--- modules/effects/arrow-effects/src/main/kotlin/arrow/effects/IOParallel.kt
+++ modules/effects/arrow-effects/src/main/kotlin/arrow/effects/IOParallel.kt
@@ -21,18 +21,18 @@ fun <A, B, C, D, E> IO.Companion.parMapN(ctx: CoroutineContext, ioA: IO<A>, ioB:
 fun <A, B, C, D, E, F> IO.Companion.parMapN(ctx: CoroutineContext, ioA: IO<A>, ioB: IO<B>, ioC: IO<C>, ioD: IO<D>, ioE: IO<E>, f: (A, B, C, D, E) -> F): IO<F> =
   parMapN(ctx,
     parMapN(ctx, ioA, ioB, ::Tuple2),
-    parMapN(ctx, ioC, ioD, ioE, { c, d, e -> Tuple3(c, d, e) }),
+    parMapN(ctx, ioC, ioD, ioE, ::Tuple3),
     { ab, cde -> f(ab.a, ab.b, cde.a, cde.b, cde.c) })
 
 fun <A, B, C, D, E, F, G> IO.Companion.parMapN(ctx: CoroutineContext, ioA: IO<A>, ioB: IO<B>, ioC: IO<C>, ioD: IO<D>, ioE: IO<E>, ioF: IO<F>, f: (A, B, C, D, E, F) -> G): IO<G> =
   parMapN(ctx,
-    parMapN(ctx, ioA, ioB, ioC, { a, b, c -> Tuple3(a, b, c) }),
-    parMapN(ctx, ioD, ioE, ioF, { d, e, ff -> Tuple3(d, e, ff) }),
+    parMapN(ctx, ioA, ioB, ioC, ::Tuple3),
+    parMapN(ctx, ioD, ioE, ioF, ::Tuple3),
     { abc, def -> f(abc.a, abc.b, abc.c, def.a, def.b, def.c) })
 
 fun <A, B, C, D, E, F, G, H> IO.Companion.parMapN(ctx: CoroutineContext, ioA: IO<A>, ioB: IO<B>, ioC: IO<C>, ioD: IO<D>, ioE: IO<E>, ioF: IO<F>, ioG: IO<G>, f: (A, B, C, D, E, F, G) -> H): IO<H> =
   parMapN(ctx,
-    parMapN(ctx, ioA, ioB, ioC, { a, b, c -> Tuple3(a, b, c) }),
+    parMapN(ctx, ioA, ioB, ioC, ::Tuple3),
     parMapN(ctx, ioD, ioE, ::Tuple2),
     parMapN(ctx, ioF, ioG, ::Tuple2),
     { abc, de, fg -> f(abc.a, abc.b, abc.c, de.a, de.b, fg.a, fg.b) })
