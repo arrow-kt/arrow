acdbfe4705a44961991dc12410b6588aa09576eb
pakoito <jueces.jus@gmail.com>
2018-07-31 22:25:13 +0100

    RightFunctor

--- modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/Bifunctor.kt
+++ modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/Bifunctor.kt
@@ -7,18 +7,17 @@ import arrow.core.identity
 interface Bifunctor<F> {
   fun <A, B, C, D> Kind2<F, A, B>.bimap(fl: (A) -> C, fr: (B) -> D): Kind2<F, C, D>
 
-  fun <A, B, C> Kind2<F, A, B>.mapLeft(f: (A) -> C): Kind2<F, C, B> = bimap(f, ::identity)
+  fun <A, B, C> Kind2<F, A, B>.mapLeft(f: (A) -> C): Kind2<F, C, B> =
+    bimap(f, ::identity)
 
-  fun <X> leftFunctor(): Functor<Kind<F, X>> = object : LeftFunctor<F, X>() {
+  fun <X> rightFunctor(): Functor<Kind<F, X>> = object : RightFunctor<F, X> {
     override val F: Bifunctor<F> = this@Bifunctor
   }
-
 }
 
-private abstract class LeftFunctor<F, X> : Functor<Kind<F, X>> {
-  abstract val F: Bifunctor<F>
+private interface RightFunctor<F, X> : Functor<Kind<F, X>> {
+  val F: Bifunctor<F>
 
-  override fun <A, B> Kind<Kind<F, X>, A>.map(f: (A) -> B): Kind<Kind<F, X>, B> {
-    return F.run { bimap(f, ::identity) }
-  }
+  override fun <A, B> Kind2<F, X, A>.map(f: (A) -> B): Kind2<F, X, B> =
+    F.run { bimap(::identity, f) }
 }
