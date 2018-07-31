11ac028089b84175efcfef7273e6a0807cf5cd1d
pakoito <jueces.jus@gmail.com>
2018-07-31 19:59:18 +0100

    Fix Cokleisli

--- modules/core/arrow-instances-data/src/main/kotlin/arrow/instances/cokleisli.kt
+++ modules/core/arrow-instances-data/src/main/kotlin/arrow/instances/cokleisli.kt
@@ -1,13 +1,15 @@
 package arrow.instances
 
-import arrow.Kind2
+import arrow.Kind
 import arrow.data.Cokleisli
+import arrow.data.CokleisliOf
 import arrow.data.ForCokleisli
+import arrow.data.fix
 import arrow.instance
 import arrow.typeclasses.Profunctor
 
 @instance(Cokleisli::class)
-interface CokleisliProfunctorInstance : Profunctor<ForCokleisli> {
-    override fun <A, B, C, D> Kind2<ForCokleisli, A, B>.dimap(fl: (C) -> A, fr: (B) -> D): Kind2<ForCokleisli, C, D> =
-        fix().bimap(fl, fr)
+interface CokleisliProfunctorInstance<F> : Profunctor<Kind<ForCokleisli, F>> {
+  override fun <A, B, C, D> CokleisliOf<F, A, B>.dimap(fl: (C) -> A, fr: (B) -> D): Cokleisli<F, C, D> =
+    fix().bimap(fl, fr)
 }
