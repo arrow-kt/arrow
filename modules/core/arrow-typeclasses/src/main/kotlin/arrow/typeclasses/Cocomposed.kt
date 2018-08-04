a8520b4a50f44fc72b9c4ed33009641676cf87b0
pakoito <jueces.jus@gmail.com>
2018-08-04 18:38:32 +0100

    LeftFunctor for Bifunctor

--- modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/Cocomposed.kt
+++ modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/Cocomposed.kt
@@ -1,6 +1,7 @@
 package arrow.typeclasses
 
 import arrow.Kind
+import arrow.Kind2
 
 /**
  * A type to represent λ[α => Kind[F, α, C]]
@@ -18,3 +19,20 @@ fun <F, A, B> CounnestedType<F, A, B>.conest(): ConestedType<F, A, B> = this as 
 
 @Suppress("UNCHECKED_CAST")
 fun <F, A, B> ConestedType<F, A, B>.counnest(): CounnestedType<F, A, B> = this as CounnestedType<F, A, B>
+
+interface CocomposedFunctor<F, X> : Functor<Conested<F, X>> {
+  fun F(): Bifunctor<F>
+
+  override fun <A, B> Kind<Conested<F, X>, A>.map(f: (A) -> B): Kind<Conested<F, X>, B> =
+    F().run { counnest().mapLeft(f) }.conest()
+
+  fun <A, B> CounnestedType<F, A, X>.mapC(f: (A) -> B): Kind2<F, B, X> =
+    F().run { mapLeft(f) }
+
+  companion object {
+    operator fun <F, X> invoke(BF: Bifunctor<F>): Functor<Conested<F, X>> =
+      object : CocomposedFunctor<F, X> {
+        override fun F(): Bifunctor<F> = BF
+      }
+  }
+}
