5d6ad0892e84949f1fff21648996a889ab7af768
Cotel <miguelcoletomunoz@hotmail.com>
2018-08-04 21:53:38 +0200

    Instances of Contravariant for Kleisli and Cokleisli and laws for Kleisli

--- modules/core/arrow-data/src/test/kotlin/arrow/data/KleisliTest.kt
+++ modules/core/arrow-data/src/test/kotlin/arrow/data/KleisliTest.kt
@@ -1,10 +1,15 @@
 package arrow.data
 
+import arrow.Kind
 import arrow.core.*
 import arrow.instances.ForKleisli
 import arrow.test.UnitSpec
+import arrow.test.laws.ContravariantLaws
 import arrow.test.laws.MonadErrorLaws
+import arrow.typeclasses.Conested
 import arrow.typeclasses.Eq
+import arrow.typeclasses.conest
+import arrow.typeclasses.counnest
 import io.kotlintest.KTestJUnitRunner
 import io.kotlintest.matchers.shouldBe
 import org.junit.runner.RunWith
@@ -15,10 +20,17 @@ class KleisliTest : UnitSpec() {
     a.fix().run(1) == b.fix().run(1)
   }
 
+  private fun <A> ConestEQ(): Eq<Kind<Conested<Kind<ForKleisli, ForTry>, A>, Int>> = Eq { a, b ->
+    a.counnest().fix().run(1) == b.counnest().fix().run(1)
+  }
+
   init {
 
     ForKleisli<ForTry, Int, Throwable>(Try.monadError()) extensions {
-      testLaws(MonadErrorLaws.laws(this, EQ(), EQ()))
+      testLaws(
+        ContravariantLaws.laws(Kleisli.contravariant(), { Kleisli { x: Int -> Try.just(x) }.conest() }, ConestEQ()),
+        MonadErrorLaws.laws(this, EQ(), EQ())
+      )
     }
 
     "andThen should continue sequence" {
