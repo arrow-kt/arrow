b855ef317d587ed0df6bfc5210aff1a1f9c66a29
pakoito <jueces.jus@gmail.com>
2018-08-11 03:25:12 +0100

    Races up to 9

--- modules/effects/arrow-effects/src/main/kotlin/arrow/effects/IOParallel.kt
+++ modules/effects/arrow-effects/src/main/kotlin/arrow/effects/IOParallel.kt
@@ -53,7 +53,7 @@ fun <A, B, C, D, E, F, G, H, I, J> IO.Companion.parallelMapN(ctx: CoroutineConte
     parallelMapN(ctx, ioG, ioH, ioI, ::Tuple3)
   ) { abc, def, ghi -> f(abc.a, abc.b, abc.c, def.a, def.b, def.c, ghi.a, ghi.b, ghi.c) }
 
-fun <A, B> IO.Companion.race(a: IO<A>, b: IO<B>): IO<Either<A, B>> =
+fun <A, B> IO.Companion.raceN(a: IO<A>, b: IO<B>): IO<Either<A, B>> =
   IO.async { asyncCb ->
     val cancellation: Future<Tuple2<Disposable, Disposable>> = Future()
 
@@ -69,6 +69,66 @@ fun <A, B> IO.Companion.race(a: IO<A>, b: IO<B>): IO<Either<A, B>> =
       ).fix().unsafeRunSync()
     )
   }
+
+fun <A, B, C> IO.Companion.raceN(a: IO<A>, b: IO<B>, c: IO<C>): IO<Either<A, Either<B, C>>> =
+  IO.async { asyncCb ->
+    val cancellation: Future<Tuple3<Disposable, Disposable, Disposable>> = Future()
+
+    val cb = onceOnly { result: Either<Throwable, Either<A, Either<B, C>>> ->
+      cancellation.unsafeGet().let { (da, db, dc) -> da(); db(); dc() }
+      asyncCb(result)
+    }
+
+    cancellation.set(
+      IO.applicative().tupled(
+        a.runAsyncCancellable { IO { it.fold({ cb(it.left()) }, { cb(it.left().right()) }) } },
+        b.runAsyncCancellable { IO { it.fold({ cb(it.left()) }, { cb(it.left().right().right()) }) } },
+        c.runAsyncCancellable { IO { it.fold({ cb(it.left()) }, { cb(it.right().right().right()) }) } }
+      ).fix().unsafeRunSync()
+    )
+  }
+
+fun <A, B, C, D> IO.Companion.raceN(a: IO<A>, b: IO<B>, c: IO<C>, d: IO<D>): IO<Either<Either<A, B>, Either<C, D>>> =
+  raceN(
+    raceN(a, b),
+    raceN(c, d)
+  )
+
+fun <A, B, C, D, E> IO.Companion.raceN(a: IO<A>, b: IO<B>, c: IO<C>, d: IO<D>, e: IO<E>): IO<Either<Either<A, Either<B, C>>, Either<D, E>>> =
+  raceN(
+    raceN(a, b, c),
+    raceN(d, e)
+  )
+
+fun <A, B, C, D, E, F> IO.Companion.raceN(a: IO<A>, b: IO<B>, c: IO<C>, d: IO<D>, e: IO<E>, f: IO<F>): IO<Either<Either<A, B>, Either<Either<C, D>, Either<E, F>>>> =
+  raceN(
+    raceN(a, b),
+    raceN(c, d),
+    raceN(e, f)
+  )
+
+fun <A, B, C, D, E, F, G> IO.Companion.raceN(a: IO<A>, b: IO<B>, c: IO<C>, d: IO<D>, e: IO<E>, f: IO<F>, g: IO<G>): IO<Either<Either<A, Either<B, C>>, Either<Either<D, E>, Either<F, G>>>> =
+  raceN(
+    raceN(a, b, c),
+    raceN(d, e),
+    raceN(f, g)
+  )
+
+fun <A, B, C, D, E, F, G, H> IO.Companion.raceN(a: IO<A>, b: IO<B>, c: IO<C>, d: IO<D>, e: IO<E>, f: IO<F>, g: IO<G>, h: IO<H>): IO<Either<Either<Either<A, B>, Either<C, D>>, Either<Either<E, F>, Either<G, H>>>> =
+  raceN(
+    raceN(a, b),
+    raceN(c, d),
+    raceN(e, f),
+    raceN(g, h)
+  )
+
+fun <A, B, C, D, E, F, G, H, I> IO.Companion.raceN(a: IO<A>, b: IO<B>, c: IO<C>, d: IO<D>, e: IO<E>, f: IO<F>, g: IO<G>, h: IO<H>, i: IO<I>): IO<Either<Either<Either<A, Either<B, C>>, Either<D, E>>, Either<Either<F, G>, Either<H, I>>>> =
+  raceN(
+    raceN(a, b, c),
+    raceN(d, e),
+    raceN(f, g),
+    raceN(h, i)
+  )
 
 fun <A> IO.Companion.race(l: List<IO<A>>): IO<A> =
   IO.async { asyncCb ->
