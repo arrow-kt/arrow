4292defd25a6b0c0d922cf639b914c7fa28dd583
Paco <pakoito@users.noreply.github.com>
2018-08-15 21:37:13 +0100

    Add ApplicativeError constructors from the Core types

--- modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/ApplicativeError.kt
+++ modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/ApplicativeError.kt
@@ -2,8 +2,11 @@ package arrow.typeclasses
 
 import arrow.Kind
 import arrow.core.Either
+import arrow.core.EitherOf
 import arrow.core.Left
+import arrow.core.OptionOf
 import arrow.core.Right
+import arrow.core.TryOf
 import arrow.core.identity
 
 interface ApplicativeError<F, E> : Applicative<F> {
@@ -14,6 +17,15 @@ interface ApplicativeError<F, E> : Applicative<F> {
 
   fun <A> E.raiseError(dummy: Unit = Unit): Kind<F, A> =
     raiseError(this)
+  
+  fun <A> OptionOf<A>.lift(f: () -> E): Kind<F, A> =
+    fix().fold({ raiseError<A>(f()) }, { just(it) })
+  
+  fun <A> EitherOf<E, A>.lift(): Kind<F, A> =
+    fix().fold({ raiseError<A>(it) }, { just(it) })
+  
+  fun <A> TryOf<A>.lift(f: (Throwable) -> E): Kind<F, A> =
+    fix().fold({ raiseError<A>(f(it)) }, { just(it) })
 
   fun <A> Kind<F, A>.handleError(f: (E) -> A): Kind<F, A> =
     handleErrorWith { just(f(it)) }
@@ -22,9 +34,6 @@ interface ApplicativeError<F, E> : Applicative<F> {
     map { Right(it) }.handleErrorWith {
       just(Left(it))
     }
-
-  fun <A> fromEither(fab: Either<E, A>): Kind<F, A> =
-    fab.fold({ raiseError<A>(it) }, { just(it) })
 
   fun <A> catch(f: () -> A, recover: (Throwable) -> E): Kind<F, A> =
     try {
