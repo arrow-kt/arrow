18c7d739a58cf51431a6c3fc0b952970dde6a498
Paul Cadman <git@paulcadman.io>
2018-08-25 17:53:55 +0100

    Makes Function1 an instance of the Category typeclass
    Function1 is a category where:
    * The identity morphism is the identity function
    * Morphism composition is function composition
    
    Tests are added to check the instance obeys CategoryLaws.

--- modules/core/arrow-data/src/test/kotlin/arrow/data/Function1Test.kt
+++ modules/core/arrow-data/src/test/kotlin/arrow/data/Function1Test.kt
@@ -2,10 +2,12 @@ package arrow.data
 
 import arrow.core.Function1
 import arrow.core.Function1Of
+import arrow.core.category
 import arrow.core.invoke
 import arrow.core.profunctor
 import arrow.instances.ForFunction1
 import arrow.test.UnitSpec
+import arrow.test.laws.CategoryLaws
 import arrow.test.laws.MonadLaws
 import arrow.test.laws.ProfunctorLaws
 import arrow.typeclasses.Eq
@@ -22,7 +24,8 @@ class Function1Test : UnitSpec() {
     ForFunction1<Int>() extensions {
       testLaws(
         ProfunctorLaws.laws(Function1.profunctor(), { Function1.just(it) }, EQ),
-        MonadLaws.laws(this, EQ)
+        MonadLaws.laws(this, EQ),
+        CategoryLaws.laws(Function1.category(), { Function1.just(it) }, EQ)
       )
     }
   }
