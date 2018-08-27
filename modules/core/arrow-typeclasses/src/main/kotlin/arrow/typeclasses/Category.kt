a5542fa4743da04eee80c9a99fc8e7b12297b211
Paul Cadman <git@paulcadman.io>
2018-08-27 12:58:57 +0100

    Adds `andThen`, the dual of `compose`, to Category
    As suggested by @pakoito

--- modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/Category.kt
+++ modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/Category.kt
@@ -7,4 +7,6 @@ interface Category<F> {
   fun <A> id(): Kind2<F, A, A>
 
   fun <A, B, C> Kind2<F, B, C>.compose(arr: Kind2<F, A, B>): Kind2<F, A, C>
+
+  fun <A, B, C> Kind2<F, A, B>.andThen(arr: Kind2<F, B, C>): Kind2<F, A, C> = arr.compose(this)
 }
