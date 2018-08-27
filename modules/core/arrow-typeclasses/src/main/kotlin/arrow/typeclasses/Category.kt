e5f68bd85ba6f55c0a389e03aaf4f2b2023b54e6
Paul Cadman <git@paulcadman.io>
2018-08-27 12:54:49 +0100

    Fix whitespace typo

--- modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/Category.kt
+++ modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/Category.kt
@@ -4,7 +4,7 @@ import arrow.Kind2
 
 interface Category<F> {
 
-  fun <A> id(): Kind2<F, A , A>
+  fun <A> id(): Kind2<F, A, A>
 
   fun <A, B, C> Kind2<F, B, C>.compose(arr: Kind2<F, A, B>): Kind2<F, A, C>
 }
