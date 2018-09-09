f4603a17e0df268d32cbb1e19274060c49d542ac
Cotel <miguelcoletomunoz@hotmail.com>
2018-09-09 12:20:26 +0200

    Adding Store datatype with tests and instances

---
+++ modules/core/arrow-data/src/main/kotlin/arrow/data/Store.kt
@@ -0,0 +1,16 @@
+package arrow.data
+
+import arrow.higherkind
+
+@higherkind
+data class Store<S, V>(val state: S, private val render: (S) -> V) : StoreOf<S, V> {
+  fun <A> map(f: (V) -> A): Store<S, A> =
+      Store(state) { state -> f(render(state)) }
+
+  fun <A> extend(f: (Store<S, V>) -> A): Store<S, A> =
+      Store(state) { next: S -> f(Store(next, render)) }
+
+  fun extract(): V = render(state)
+
+  companion object
+}
