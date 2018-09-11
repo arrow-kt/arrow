335751219bfaa8e9c70f4c7c88e0000a77344ebf
Cotel <miguelcoletomunoz@hotmail.com>
2018-09-11 20:37:34 +0200

    Adding Moore datatype with tests and instances

---
+++ modules/core/arrow-instances-data/src/main/kotlin/arrow/instances/moore.kt
@@ -0,0 +1,37 @@
+package arrow.instances
+
+import arrow.Kind
+import arrow.data.Moore
+import arrow.data.MoorePartialOf
+import arrow.data.fix
+import arrow.instance
+import arrow.typeclasses.Comonad
+import arrow.typeclasses.Functor
+
+@instance(Moore::class)
+interface MooreComonadInstance<V> : Comonad<MoorePartialOf<V>> {
+  override fun <A, B> Kind<MoorePartialOf<V>, A>.coflatMap(f: (Kind<MoorePartialOf<V>, A>) -> B): Moore<V, B> =
+      fix().extend(f)
+
+  override fun <A> Kind<MoorePartialOf<V>, A>.extract(): A =
+      fix().extract()
+
+  override fun <A, B> Kind<MoorePartialOf<V>, A>.map(f: (A) -> B): Moore<V, B> =
+      fix().map(f)
+}
+
+@instance(Moore::class)
+interface MooreFunctorInstance<V> : Functor<MoorePartialOf<V>> {
+  override fun <A, B> Kind<MoorePartialOf<V>, A>.map(f: (A) -> B): Moore<V, B> =
+      fix().map(f)
+}
+
+class MooreContext<S> : MooreComonadInstance<S>
+
+class MooreContextPartiallyApplied<S> {
+  infix fun <A> extensions(f: MooreContext<S>.() -> A): A =
+      f(MooreContext())
+}
+
+fun <S> ForMoore(): MooreContextPartiallyApplied<S> =
+    MooreContextPartiallyApplied()
