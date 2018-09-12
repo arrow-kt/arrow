ea97c47918924f5ba217202bffdad1b35872260f
Cotel <miguelcoletomunoz@hotmail.com>
2018-09-12 19:48:15 +0200

    Improving Sum and adding tests

--- modules/core/arrow-data/src/main/kotlin/arrow/data/Sum.kt
+++ modules/core/arrow-data/src/main/kotlin/arrow/data/Sum.kt
@@ -6,26 +6,30 @@ import arrow.typeclasses.Comonad
 import arrow.typeclasses.Functor
 
 @higherkind
-data class Sum<F, G, V>(private val side: Side, val left: Kind<F, V>, val right: Kind<G, V>) : SumOf<F, G, V> {
+data class Sum<F, G, V>(
+    val left: Kind<F, V>,
+    val right: Kind<G, V>,
+    val side: Side = Side.Left
+) : SumOf<F, G, V> {
 
   sealed class Side {
     object Left : Side()
     object Right : Side()
   }
 
-  fun <A> extend(f: (Sum<F, G, V>) -> A, CF: Comonad<F>, CG: Comonad<G>): Sum<F, G, A> = CF.run {
+  fun <A> extend(CF: Comonad<F>, CG: Comonad<G>, f: (Sum<F, G, V>) -> A): Sum<F, G, A> = CF.run {
     CG.run {
       Sum(
-          side,
-          left.coflatMap { f(Sum(Side.Left, left, right)) },
-          right.coflatMap { f(Sum(Side.Right, left, right)) }
+          left.coflatMap { f(Sum(left, right, Side.Left)) },
+          right.coflatMap { f(Sum(left, right, Side.Right)) },
+          side
       )
     }
   }
 
-  fun <A> map(f: (V) -> A, FF: Functor<F>, FG: Functor<G>): Sum<F, G, A> = FF.run {
+  fun <A> map(FF: Functor<F>, FG: Functor<G>, f: (V) -> A): Sum<F, G, A> = FF.run {
     FG.run {
-      Sum(side, left.map(f), right.map(f))
+      Sum(left.map(f), right.map(f), side)
     }
   }
 
@@ -38,4 +42,11 @@ data class Sum<F, G, V>(private val side: Side, val left: Kind<F, V>, val right:
     }
   }
 
+  fun changeSide(side: Side): Sum<F, G, V> = Sum(left, right, side)
+
+  companion object {
+    fun <F, G, V> left(ls: Kind<F, V>, rs: Kind<G, V>): Sum<F, G, V> = Sum(ls, rs, Side.Left)
+
+    fun <F, G, V> right(ls: Kind<F, V>, rs: Kind<G, V>): Sum<F, G, V> = Sum(ls, rs, Side.Right)
+  }
 }
--- modules/core/arrow-instances-data/src/main/kotlin/arrow/instances/sum.kt
+++ modules/core/arrow-instances-data/src/main/kotlin/arrow/instances/sum.kt
@@ -15,14 +15,14 @@ interface ComonadSumInstance<F, G> : Comonad<SumPartialOf<F, G>> {
 
   fun CG(): Comonad<G>
 
-  override fun <A, B> Kind<SumPartialOf<F, G>, A>.coflatMap(f: (Kind<SumPartialOf<F, G>, A>) -> B): Kind<SumPartialOf<F, G>, B> =
-      fix().extend(f, CF(), CG())
+  override fun <A, B> Kind<SumPartialOf<F, G>, A>.coflatMap(f: (Kind<SumPartialOf<F, G>, A>) -> B): Sum<F, G, B> =
+      fix().extend(CF(), CG(), f)
 
   override fun <A> Kind<SumPartialOf<F, G>, A>.extract(): A =
       fix().extract(CF(), CG())
 
-  override fun <A, B> Kind<SumPartialOf<F, G>, A>.map(f: (A) -> B): Kind<SumPartialOf<F, G>, B> =
-      fix().map(f, CF(), CG())
+  override fun <A, B> Kind<SumPartialOf<F, G>, A>.map(f: (A) -> B): Sum<F, G, B> =
+      fix().map(CF(), CG(), f)
 }
 
 @instance(Sum::class)
@@ -32,6 +32,19 @@ interface FunctorSumInstance<F, G> : Functor<SumPartialOf<F, G>> {
 
   fun FG(): Functor<G>
 
-  override fun <A, B> Kind<SumPartialOf<F, G>, A>.map(f: (A) -> B): Kind<SumPartialOf<F, G>, B> =
-      fix().map(f, FF(), FG())
+  override fun <A, B> Kind<SumPartialOf<F, G>, A>.map(f: (A) -> B): Sum<F, G, B> =
+      fix().map(FF(), FG(), f)
 }
+
+class SumContext<F, G>(val CF: Comonad<F>, val CG: Comonad<G>) : ComonadSumInstance<F, G> {
+  override fun CF(): Comonad<F> = CF
+  override fun CG(): Comonad<G> = CG
+}
+
+class SumContextPartiallyApplied<F, G>(val CF: Comonad<F>, val CG: Comonad<G>) {
+  infix fun <A> extensions(f: SumContext<F, G>.() -> A): A =
+      f(SumContext(CF, CG))
+}
+
+fun <F, G> ForSum(CF: Comonad<F>, CG: Comonad<G>): SumContextPartiallyApplied<F, G> =
+    SumContextPartiallyApplied(CF, CG)
