ea2f7887db318fe91d26e308d4d815c34c1e9765
Paco <pakoito@users.noreply.github.com>
2018-09-15 22:58:42 +0100

    Add CancellableEffect typeclass. Use it to give races to IO (#984)
    * Add implementation of cancellation in the loop
    
    * Fix logic
    
    * Add tests for cancellation
    
    * Fix detekt
    
    * Re-enable IO Laws
    
    * FIx missing AsyncLaws import
    
    * Formatting
    
    * Add tests for boundary cancellation
    
    * Formatting
    
    * Fix bug where IOCancellationException thrown by users were supressed
    
    * Change wording on test
    
    * Add minimal Race implementation for IO
    
    * Races up to 9
    
    * Future isn't the solution
    
    * Add correct implementation of `race`
    
    * Add some first tests
    
    * Add other cancellation methods
    
    * Make errors cancel the chain...just in case
    
    * Second attempt at races
    
    * Successful pass for racing!
    
    * Fix copypasta
    
    * Typo
    
    * Replace IOCancellationException with OnCancel.CancellationException
    
    * Adapt cancellation tests to new paradigm
    
    * Fix unsafe cancellation
    
    * Simplify implementation of Cancellable IO
    
    * Add laws
    
    * When cancelling or fail a parallel IO, cancel all execution
    
    * Fix copypaste
    
    * Documentation I guess
    
    * Make IOContext extend CancellableEffect
    
    * Add instances for effects integration
    
    * Rename CancellableEffect to ConcurrentEffect
    
    * Remove dependency on Concurrent
    
    * Fix concurrentEffect
    
    * Fix detekt

--- modules/effects/arrow-effects/src/main/kotlin/arrow/effects/data/internal/IOCancellationException.kt
+++ modules/effects/arrow-effects/src/main/kotlin/arrow/effects/data/internal/IOCancellationException.kt
@@ -2,4 +2,4 @@ package arrow.effects.data.internal
 
 import arrow.effects.internal.JavaCancellationException
 
-data class IOCancellationException(override val message: String? = null) : JavaCancellationException()
+object IOCancellationException : JavaCancellationException("User cancellation")
