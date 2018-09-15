f4cbdee0eb9bbc0b68d7f9a662efc5898193c4d1
pakoito <jueces.jus@gmail.com>
2018-09-15 02:44:16 +0100

    Cleanup

--- modules/core/arrow-data/src/main/kotlin/arrow/data/Day.kt
+++ modules/core/arrow-data/src/main/kotlin/arrow/data/Day.kt
@@ -9,10 +9,10 @@ import arrow.typeclasses.Comonad
 /*
  * This whole file is a world of gotchas.
  *
- * It's an abstract class with a hidden constructor to prevent people from using runDay.
+ * It's an abstract class with a hidden constructor to prevent people from using `runDay` directly.
  *
- * `runDay` is an encoding of Existential function, enabled by inputs directly piped to outputs.
- * The inputs and outputs are both `Any?` to avoid type annotations that won't be used.
+ * `runDay` is an encoding of an Existential function for <R>, meaning that it can't be passed as a constructor parameter.
+ * The inputs and outputs are both `Any?` to avoid type annotations that won't be used externally.
  *
  * The other functions start by creating their own private `Day` object so they don't eagerly invoke `runDay`.
  *
@@ -36,6 +36,7 @@ abstract class Day<F, G, A> private constructor() : DayOf<F, G, A> {
       }
   }
 
+  @Suppress("UNCHECKED_CAST")
   fun <B> ap(AF: Applicative<F>, AG: Applicative<G>, f: DayOf<F, G, (A) -> B>): Day<F, G, B> = object : Day<F, G, B>() {
     override fun <R> runDay(ff: (Kind<F, *>, Kind<G, *>, (Any?, Any?) -> B) -> R): R =
       this@Day.runDay { left, right, get ->
@@ -44,15 +45,15 @@ abstract class Day<F, G, A> private constructor() : DayOf<F, G, A> {
           val r = AG.run { tupled(right, rf) }
           ff(l, r) { x, y ->
             // Kapachao? This is a safe cast because you can see l and r just above
-            // Kotlin allows me to use x and y now that they're narrowed
-            val _x = x as Tuple2<*, *>
-            val _y = y as Tuple2<*, *>
-            getf(x.b, y.b).invoke(get(x.a, y.a))
+            val xx = x as Tuple2<*, *>
+            val yy = y as Tuple2<*, *>
+            getf(xx.b, yy.b).invoke(get(xx.a, yy.a))
           }
         }
       }
   }
 
+  @Suppress("UNCHECKED_CAST")
   fun <B> coflatMap(CF: Comonad<F>, CG: Comonad<G>, f: (DayOf<F, G, A>) -> B): Day<F, G, B> = object : Day<F, G, B>() {
     override fun <R> runDay(ff: (Kind<F, *>, Kind<G, *>, (Any?, Any?) -> B) -> R): R =
       this@Day.runDay { left, right, get ->
@@ -60,10 +61,9 @@ abstract class Day<F, G, A> private constructor() : DayOf<F, G, A> {
         val r = CG.run { right.duplicate() }
         ff(l, r) { x, y ->
           // Kapachao? This is a safe cast because you can see l and r just above
-          // Kotlin allows me to use x and y now that they're narrowed
-          val _x = x as Kind<F, *>
-          val _y = y as Kind<G, *>
-          f(Day(x, y, get))
+          val xx = x as Kind<F, *>
+          val yy = y as Kind<G, *>
+          f(Day(xx, yy, get))
         }
       }
   }
@@ -78,6 +78,7 @@ abstract class Day<F, G, A> private constructor() : DayOf<F, G, A> {
     @Suppress("UNCHECKED_CAST")
     operator fun <F, G, X, Y, A> invoke(left: Kind<F, X>, right: Kind<G, Y>, f: (X, Y) -> A): Day<F, G, A> = object : Day<F, G, A>() {
       override fun <R> runDay(ff: (Kind<F, *>, Kind<G, *>, (Any?, Any?) -> A) -> R): R =
+      // The cast is implementation-safe, as it'll only ever be used with the inputs passed
         ff(left, right, f as (Any?, Any?) -> A)
     }
   }
