8742f2127e74dd64026d553ccfbdc4be9595099b
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-10-06 11:06:08 +0200

    fix kleisli bracket test

--- modules/core/arrow-data/src/test/kotlin/arrow/data/KleisliTest.kt
+++ modules/core/arrow-data/src/test/kotlin/arrow/data/KleisliTest.kt
@@ -1,6 +1,7 @@
 package arrow.data
 
 import arrow.Kind
+import arrow.core.Either
 import arrow.core.ForId
 import arrow.core.ForTry
 import arrow.core.Id
@@ -25,6 +26,7 @@ import io.kotlintest.KTestJUnitRunner
 import io.kotlintest.matchers.shouldBe
 import org.junit.runner.RunWith
 
+@Suppress("TestFunctionName")
 @RunWith(KTestJUnitRunner::class)
 class KleisliTest : UnitSpec() {
   private fun <A> EQ(): Eq<KleisliOf<ForTry, Int, A>> = Eq { a, b ->
@@ -35,8 +37,12 @@ class KleisliTest : UnitSpec() {
     a.counnest().fix().run(1) == b.counnest().fix().run(1)
   }
 
-  private fun <A> ConextIOEQ(): Eq<Kind<Conested<Kind<ForKleisli, ForIO>, A>, Int>> = Eq { a, b ->
-    a.counnest().fix().run(1) == b.counnest().fix().run(1)
+  private fun EQBracket(): Eq<Kind<KleisliPartialOf<ForIO, Int>, Int>> = Eq { a, b ->
+    a.fix().run(1) == b.fix().run(1)
+  }
+
+  private fun EQBracketError(): Eq<Kind<KleisliPartialOf<ForIO, Int>, Either<Throwable, Int>>> = Eq { a, b ->
+    a.fix().run(1) == b.fix().run(1)
   }
 
   init {
@@ -48,7 +54,12 @@ class KleisliTest : UnitSpec() {
 
     ForKleisli<ForIO, Int, Throwable>(IO.monadError()) extensions {
       testLaws(
-        BracketLaws.laws(Kleisli.bracket(IO.bracket()), { Kleisli { x: Int -> IO.just(x) }.conest() }, ConextIOEQ<Int>(), ConextIOEQ<Int>())
+        BracketLaws.laws(
+          Kleisli.bracket<ForIO, Int, Throwable>(IO.bracket()),
+          { Kleisli { x: Int -> IO.just(x) } },
+          EQBracket(),
+          EQBracketError(),
+          EQBracket())
       )
     }
 
