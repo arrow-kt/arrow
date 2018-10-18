1a7c2cd13fe05413523cbae719ce913c0b987b04
Leandro Borges Ferreira <lehen01@gmail.com>
2018-10-18 20:40:08 -0300

    refactor to extension functions

--- modules/integrations/arrow-integrations-retrofit-adapter/src/main/kotlin/arrow/integrations/retrofit/adapter/CallK.kt
+++ modules/integrations/arrow-integrations-retrofit-adapter/src/main/kotlin/arrow/integrations/retrofit/adapter/CallK.kt
@@ -8,20 +8,23 @@ import retrofit2.Call
 import retrofit2.Response
 
 data class CallK<R>(val call: Call<R>) {
-  fun <F> async(async: Async<F>): Kind<F, Response<R>> =
-    async.async { proc ->
-      call.enqueue(ResponseCallback(proc))
-    }
+  fun <F> async(AC: Async<F>): Kind<F, Response<R>> = call.runInAsyncContext(AC)
 
-  fun <F> defer(defer: MonadDefer<F>): Kind<F, Response<R>> =
-    defer {
-      call.execute()
-    }
+  fun <F> defer(defer: MonadDefer<F>): Kind<F, Response<R>> = call.runSyncDeferred(defer)
 
-  fun <F> catch(defer: MonadError<F, Throwable>): Kind<F, Response<R>> =
-    defer.run {
-      catch {
-        call.execute()
-      }
-    }
+  fun <F> catch(monadError: MonadError<F, Throwable>): Kind<F, Response<R>> = call.runSyncCatch(monadError)
 }
+
+fun <F, A> Call<A>.runInAsyncContext(AC: Async<F>): Kind<F, Response<A>> =
+  AC.async { callback ->
+    enqueue(ResponseCallback(callback))
+  }
+
+fun <F, A> Call<A>.runSyncDeferred(defer: MonadDefer<F>): Kind<F, Response<A>> = defer { execute() }
+
+fun <F, A> Call<A>.runSyncCatch(monadError: MonadError<F, Throwable>): Kind<F, Response<A>> =
+  monadError.run {
+    catch {
+      execute()
+    }
+  }
