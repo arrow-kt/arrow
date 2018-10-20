ef019a6c6df15d4937a349fd562817f529cf9106
Jeffrey Starke <starkej2@gmail.com>
2018-10-20 10:22:22 -0400

    Replace BooleanInstances with Boolean companion object

--- modules/core/arrow-instances-core/src/main/kotlin/arrow/instances/boolean.kt
+++ modules/core/arrow-instances-core/src/main/kotlin/arrow/instances/boolean.kt
@@ -1,6 +1,5 @@
 package arrow.instances
 
-import arrow.core.BooleanInstances
 import arrow.core.DeprecatedAmbiguity
 import arrow.typeclasses.Eq
 import arrow.typeclasses.Show
@@ -14,17 +13,8 @@ interface BooleanEqInstance : Eq<Boolean> {
   override fun Boolean.eqv(b: Boolean): Boolean = this == b
 }
 
-// FIXME
-//fun Boolean.Companion.show(): Show<Boolean> =
-//  object : BooleanShowInstance {}
-//
-//fun Boolean.Companion.eq(): Eq<Boolean> =
-//  object : BooleanEqInstance {}
-
-@Deprecated(DeprecatedAmbiguity, ReplaceWith("Boolean.show()"))
-fun BooleanInstances.show(): Show<Boolean> =
+fun Boolean.Companion.show(): Show<Boolean> =
   object : BooleanShowInstance {}
 
-@Deprecated(DeprecatedAmbiguity, ReplaceWith("Boolean.eq()"))
-fun BooleanInstances.eq(): Eq<Boolean> =
+fun Boolean.Companion.eq(): Eq<Boolean> =
   object : BooleanEqInstance {}
