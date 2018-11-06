b3145409096a3db64c983c54334a48569405a6f5
Leandro Borges Ferreira <lehen01@gmail.com>
2018-11-06 08:43:41 -0200

    changing visibility for Call inside CallK

--- modules/integrations/arrow-integrations-retrofit-adapter/src/main/kotlin/arrow/integrations/retrofit/adapter/CallK.kt
+++ modules/integrations/arrow-integrations-retrofit-adapter/src/main/kotlin/arrow/integrations/retrofit/adapter/CallK.kt
@@ -7,7 +7,7 @@ import arrow.typeclasses.MonadError
 import retrofit2.Call
 import retrofit2.Response
 
-data class CallK<R>(val call: Call<R>) {
+data class CallK<R>(private val call: Call<R>) {
   fun <F> async(AC: Async<F>): Kind<F, Response<R>> = call.runAsync(AC)
 
   fun <F> defer(defer: MonadDefer<F>): Kind<F, Response<R>> = call.runSyncDeferred(defer)
