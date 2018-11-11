6f28c7d464945135326684b9e319d0202db416f2
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-11-11 12:12:13 +0100

    fix Framework async calls

--- modules/docs/arrow-examples/src/test/kotlin/arrow/typeclasses/solved/Framework.kt
+++ modules/docs/arrow-examples/src/test/kotlin/arrow/typeclasses/solved/Framework.kt
@@ -7,7 +7,13 @@ import arrow.core.Try
 import arrow.core.left
 import arrow.core.right
 import arrow.effects.typeclasses.Async
-import arrow.typeclasses.*
+import arrow.typeclasses.Applicative
+import arrow.typeclasses.ApplicativeError
+import arrow.typeclasses.DaoDatabase
+import arrow.typeclasses.Index
+import arrow.typeclasses.NetworkModule
+import arrow.typeclasses.UserDao
+import arrow.typeclasses.UserDto
 
 // Step 0 - extract interface
 
