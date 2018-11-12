467a8f7d7a437977ece9aeafb916f7a325946720
Cotel <miguelcoletomunoz@hotmail.com>
2018-11-12 21:16:38 +0100

    Introducing Refinement typeclass and NonZero refined type example

---
+++ modules/core/arrow-validation/src/main/kotlin/arrow/validation/refinedTypes/NonZero.kt
@@ -0,0 +1,47 @@
+package arrow.validation.refinedTypes
+
+import arrow.Kind
+import arrow.core.Either
+import arrow.core.EitherPartialOf
+import arrow.data.Nel
+import arrow.data.Validated
+import arrow.data.ValidatedPartialOf
+import arrow.extension
+import arrow.instances.either.applicativeError.applicativeError
+import arrow.instances.nonemptylist.semigroup.semigroup
+import arrow.instances.validated.applicativeError.applicativeError
+import arrow.typeclasses.ApplicativeError
+import arrow.validation.RefinedPredicateException
+import arrow.validation.Refinement
+
+interface NonZero<F, A : Number> : Refinement<F, A> {
+
+  override fun A.refinement(): Boolean = this != 0
+
+  fun A.nonZero(): Kind<F, A> = refine(this)
+
+  fun <B> A.nonZero(f: A.() -> B): Kind<F, B> = refine(this, f)
+
+  companion object {
+    fun errorMsg(x: Number): String = "$x cannot be 0"
+  }
+
+}
+
+@extension
+interface ValidatedNonZero<A : Number> : NonZero<ValidatedPartialOf<Nel<RefinedPredicateException>>, A> {
+  override fun applicativeError(): ApplicativeError<ValidatedPartialOf<Nel<RefinedPredicateException>>,
+    Nel<RefinedPredicateException>> =
+    Validated.applicativeError(Nel.semigroup())
+
+  override fun invalidValueMsg(a: A): String = NonZero.errorMsg(a)
+}
+
+@extension
+interface EitherNonZero<A : Number> : NonZero<EitherPartialOf<Nel<RefinedPredicateException>>, A> {
+  override fun applicativeError(): ApplicativeError<EitherPartialOf<Nel<RefinedPredicateException>>,
+    Nel<RefinedPredicateException>> =
+    Either.applicativeError()
+
+  override fun invalidValueMsg(a: A): String = NonZero.errorMsg(a)
+}
