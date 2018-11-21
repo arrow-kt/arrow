537c35d5aa6d764e2f168f54528e3deef5e0d2fd
Leandro Borges Ferreira <lehen01@gmail.com>
2018-11-20 23:23:16 -0200

    creating lift for bifunctor (#1147)
    * creating lift for bifunctor
    
    * adding documentation

--- modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/Bifunctor.kt
+++ modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/Bifunctor.kt
@@ -4,16 +4,88 @@ import arrow.Kind
 import arrow.Kind2
 import arrow.core.identity
 
+/**
+ * [Bifunctor] has the same properties of [Functor], but acting onto two types, instead of one.
+ *
+ * It can be used to map both of its inside types.
+ */
 interface Bifunctor<F> {
+  /**
+   * Maps both types of Kind2.
+   *
+   * ```kotlin:ank
+   * {: data-executable='true'}
+   *
+   * import arrow.core.Tuple2
+   *
+   * fun main() {
+   *   //sampleStart
+   *   val tuple = Tuple2(2, 2).bimap({ num -> num * 2 }, { num -> num + 5})
+   *   println("First value: ${tuple.a}")
+   *   println("Second value: ${tuple.b}")
+   *   //sampleEnd
+   * }
+   * ```
+   */
   fun <A, B, C, D> Kind2<F, A, B>.bimap(fl: (A) -> C, fr: (B) -> D): Kind2<F, C, D>
 
+  /**
+   * Lifts two functions to the Bifunctor type.
+   * ```kotlin:ank
+   * {: data-executable='true'}
+   *
+   * import arrow.core.Tuple2
+   * import arrow.instances.tuple2.bifunctor.bifunctor
+   *
+   * val sumFive : (Int) -> Int = { it + 5 }
+   * val sumTwo : (Int) -> Int = { it + 2 }
+   *
+   * fun main() {
+   *   //sampleStart
+   *   val tuple : Tuple2<Int, Int> = Tuple2.bifunctor().lift(sumFive, sumTwo).invoke(Tuple2(2, 2))
+   *   println("First value: ${tuple.a}")
+   *   println("Second value: ${tuple.b}")
+   *   //sampleEnd
+   * }
+   *
+   * ```
+   */
+  fun <A, B, C, D> lift(fl: (A) -> C, fr: (B) -> D): (Kind2<F, A, B>) -> Kind2<F, C, D> =
+    { kind2 ->
+      kind2.bimap(fl, fr)
+    }
+
+  /**
+   * Map the left side type of Kind2
+   *
+   * ```kotlin:ank
+   * {: data-executable='true'}
+   *
+   * import arrow.core.Tuple2
+   *
+   * fun main() {
+   *   //sampleStart
+   *   val tuple = Tuple2(2, 2).mapLeft { num -> num * 2 }
+   *   println("First value: ${tuple.a}")
+   *   println("Second value: ${tuple.b}")
+   *   //sampleEnd
+   * }
+   * ```
+   *
+   */
   fun <A, B, C> Kind2<F, A, B>.mapLeft(f: (A) -> C): Kind2<F, C, B> =
     bimap(f, ::identity)
 
+  /**
+   * Returns a [Functor] acting on the type of the right side.
+   */
   fun <X> rightFunctor(): Functor<Kind<F, X>> = object : RightFunctor<F, X> {
     override val F: Bifunctor<F> = this@Bifunctor
   }
 
+  /**
+   * Returns a [Functor] acting on the type of the left side.
+   */
   fun <X> leftFunctor(): Functor<Conested<F, X>> = CocomposedFunctor(this@Bifunctor)
 
   fun <AA, B, A : AA> Kind2<F, A, B>.leftWiden(): Kind2<F, AA, B> = this
