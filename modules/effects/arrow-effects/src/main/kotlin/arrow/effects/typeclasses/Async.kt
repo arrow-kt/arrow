c869843d2e983f05032170b38fcc1ce5cbf3b722
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-11-22 12:14:15 +0100

    add asyncF to Async

--- modules/effects/arrow-effects/src/main/kotlin/arrow/effects/typeclasses/Async.kt
+++ modules/effects/arrow-effects/src/main/kotlin/arrow/effects/typeclasses/Async.kt
@@ -8,6 +8,8 @@ import kotlin.coroutines.CoroutineContext
 /** An asynchronous computation that might fail. **/
 typealias Proc<A> = ((Either<Throwable, A>) -> Unit) -> Unit
 
+typealias ProcF<F, A> = ((Either<Throwable, A>) -> Unit) -> Kind<F, Unit>
+
 /**
  * ank_macro_hierarchy(arrow.effects.typeclasses.Async)
  *
@@ -15,6 +17,13 @@ typealias Proc<A> = ((Either<Throwable, A>) -> Unit) -> Unit
  **/
 interface Async<F> : MonadDefer<F> {
   fun <A> async(fa: Proc<A>): Kind<F, A>
+
+  /**
+   * [[async]] variant that can suspend side effects in the provided registration function. On this variant, the passed
+   * in function is injected with a side-effectful callback for signaling the final result of an asynchronous process.
+   * Its returned result needs to be a pure `F[Unit]` that gets evaluated by the runtime.
+   */
+  fun <A> asyncF(k: ProcF<F, A>): Kind<F, A>
 
   fun <A> Kind<F, A>.continueOn(ctx: CoroutineContext): Kind<F, A>
 
