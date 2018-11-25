fde64064f1d7dd42d185a50272bf2c78e44e62af
Jannis <overesch.jannis@gmail.com>
2018-11-25 16:22:04 +0100

    Add some kdoc to some methods
    Added only to methods that may need it, mainly methods creating an instance of DeferredK since that may produce unexpected results as in memoization

--- modules/effects/arrow-effects-kotlinx-coroutines/src/main/kotlin/arrow/effects/DeferredK.kt
+++ modules/effects/arrow-effects-kotlinx-coroutines/src/main/kotlin/arrow/effects/DeferredK.kt
@@ -8,14 +8,75 @@ import arrow.higherkind
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
+/**
+ * Wraps a [Deferred] with [DeferredK]
+ *
+ * Note: Using this extension means that the resulting [DeferredK] will use a memoized version of [Deferred].
+ *
+ * {: data-executable='true'}
+ *
+ * ```kotlin:ank
+ * import arrow.effects.DeferredK
+ * import arrow.effects.k
+ * import arrow.effects.unsafeRunSync
+ * import kotlinx.coroutines.GlobalScope
+ * import kotlinx.coroutines.async
+ *
+ * fun main(args: Array<String>) {
+ *   //sampleStart
+ *   val result: DeferredK<String> = GlobalScope.async {
+ *     // some computation ...
+ *     "Done"
+ *   }.k()
+ *   //sampleEnd
+ *   println(result.unsafeRunSync())
+ * }
+ *  ```
+ */
 fun <A> Deferred<A>.k(): DeferredK<A> =
   DeferredK.Wrapped(memoized = this)
 
+/**
+ * Wrap a suspend function in a [DeferredK] given a context and a start method
+ *
+ * Note: Using this extension means that the resulting [DeferredK] will rerun f on every await, hence
+ *  awaiting a new coroutine every time. This means it won't be memoized and as such can be used for repeated/retried actions and
+ *  it will properly re-execute side-effects.
+ *
+ * {: data-executable='true'}
+ *
+ * ```kotlin:ank
+ * import arrow.effects.DeferredK
+ * import arrow.effects.asyncK
+ * import arrow.effects.unsafeRunSync
+ * import kotlinx.coroutines.GlobalScope
+ *
+ * fun main(args: Array<String>) {
+ *   //sampleStart
+ *   val result: DeferredK<String> = GlobalScope.asyncK {
+ *     // some computation ...
+ *     "Done"
+ *   }
+ *   //sampleEnd
+ *   println(result.unsafeRunSync())
+ * }
+ * ```
+ */
 fun <A> CoroutineScope.asyncK(ctx: CoroutineContext = Dispatchers.Default, start: CoroutineStart = CoroutineStart.LAZY, f: suspend CoroutineScope.() -> A): DeferredK<A> =
   DeferredK.Generated(ctx, start, this) { f() }
 
+/**
+ * Return the wrapped [Deferred] from a [DeferredK]
+ *
+ * Note: `aDeferredK.value().await()` does not necessarily equal `aDeferredK.await()`. That is because [DeferredK] will attempt to rerun
+ *  all computation and if the code executed is not pure it may change on every await. This is important because otherwise impure code would not
+ *  rerun, and its side effects never happen. This only applies to [DeferredK]'s created without `Deferred.k()`
+ */
 fun <A> DeferredKOf<A>.value(): Deferred<A> = this.fix().memoized
 
+/**
+ * Returns the [CoroutineScope] the [DeferredK] operates on
+ */
 fun <A> DeferredKOf<A>.scope(): CoroutineScope = this.fix().scope
 
 @higherkind
@@ -26,25 +87,28 @@ sealed class DeferredK<A>(
 ) : DeferredKOf<A>, Deferred<A> by memoized {
 
   /**
-   * Pure wrapper for already constructed deferred instances, does nothing special really
+   * Pure wrapper for already constructed [Deferred] instances. Created solely by `Deferred.k()` extension method
    */
-  class Wrapped<A>(scope: CoroutineScope = GlobalScope, memoized: Deferred<A>) : DeferredK<A>(scope = scope, memoized = memoized)
+  internal class Wrapped<A>(scope: CoroutineScope = GlobalScope, memoized: Deferred<A>) : DeferredK<A>(scope = scope, memoized = memoized)
 
   /**
-   * Represents a DeferredK that can generate an instance of deferred on every await
+   * Represents a [DeferredK] that can generate an instance of [Deferred] on every await
    *
-   * It does not memoize results and thus can be rerun just as expected from a MonadDefer
+   * It does not memoize results and thus can be rerun just as expected from a [MonadDefer]
    * However one can still break this system by ie returning or using a deferred in one of the functions
    *  only when creating all deferred instances inside DeferredK or using DeferredK's methods
    *  one can guarantee not having memoization
    */
-  class Generated<A>(
+  internal class Generated<A>(
     val ctx: CoroutineContext = Dispatchers.Default,
     val coroutineStart: CoroutineStart = CoroutineStart.LAZY,
     scope: CoroutineScope = GlobalScope,
     val generator: suspend () -> A
   ) : DeferredK<A>(scope, scope.async(ctx, coroutineStart) { generator() }) {
 
+    /**
+     * Returns either the memoized [Deferred] if it has not been run yet. Or creates a new one.
+     */
     override suspend fun await(): A =
       if (memoized.isCompleted || memoized.isActive || memoized.isCancelled) {
         scope.async(ctx, coroutineStart) { generator() }.await()
@@ -101,13 +165,92 @@ sealed class DeferredK<A>(
   override fun hashCode(): Int = memoized.hashCode()
 
   companion object {
+
+    /**
+     * [DeferredK] that just contains [Unit]
+     *
+     * Useful for comprehensions to avoid executing code at the beginning of a binding.
+     *
+     * This below executes `println("Stuff")` despite the resulting [DeferredK] never being executed itself.
+     * To avoid that call [bind] on a [DeferredK] before that. The second binding won't print a thing before run.
+     *
+     * {: data-executable='true'}
+     *
+     * ```kotlin:ank
+     * import arrow.effects.DeferredK
+     * import arrow.effects.deferredk.monad.monad
+     *
+     * fun main(args: Array<String>) {
+     *   //sampleStart
+     *   DeferredK.monad().binding {
+     *     println("Stuff")
+     *     DeferredK { 10 }.bind()
+     *   }
+     *
+     *   DeferredK.monad().binding {
+     *     DeferredK.unit().bind()
+     *     println("Other Stuff")
+     *     DeferredK { 10 }.bind()
+     *   }
+     *   //sampleEnd
+     * }
+     * ```
+     */
     fun unit(): DeferredK<Unit> = just(Unit)
 
     fun <A> just(a: A): DeferredK<A> = CompletableDeferred(a).k()
 
-    fun <A> defer(scope: CoroutineScope = GlobalScope, ctx: CoroutineContext = Dispatchers.Default, start: CoroutineStart = CoroutineStart.LAZY, fa: () -> DeferredKOf<A>): DeferredK<A> =
+    /**
+     * Wraps a function that returns a [DeferredK] in a [DeferredK] together with a specific [CoroutineScope], a [CoroutineContext] and a
+     *  [CoroutineStart]. All those parameters (except f) have defaults.
+     *
+     * Note: Using this method means the resulting [DeferredK] will rerun fa on await, not memoizing its result.
+     *  As long as the result of fa is also re-runnable, this [DeferredK] this [DeferredK] will correctly re-run.
+     *
+     * {: data-executable='true'}
+     *
+     * ```kotlin:ank
+     * import arrow.effects.DeferredK
+     * import arrow.effects.unsafeRunSync
+     *
+     * fun main(args: Array<String>) {
+     *   //sampleStart
+     *   val result = DeferredK.defer {
+     *     println("Calculating solution:")
+     *     DeferredK.just(42)
+     *   }
+     *   //sampleEnd
+     *   println(result.unsafeRunSync())
+     * }
+     * ```
+     */
+    fun <A> defer(scope: CoroutineScope = GlobalScope, ctx: CoroutineContext = Dispatchers.Unconfined, start: CoroutineStart = CoroutineStart.LAZY, fa: () -> DeferredKOf<A>): DeferredK<A> =
       Generated(ctx, start, scope) { fa().await() }
 
+    /**
+     * Wraps a suspend function in a [DeferredK] together with a specific [CoroutineScope], a [CoroutineContext] and a
+     *  [CoroutineStart]. All those parameters (except f) have defaults.
+     *
+     * Note: Using this method means the resulting [DeferredK] will rerun f on await. Making this [DeferredK] re-runnable as long as f itself does not use a non re-runnable
+     *  [Deferred] or [DeferredK].
+     *
+     * {: data-executable='true'}
+     *
+     * ```kotlin:ank
+     * import arrow.effects.DeferredK
+     * import arrow.effects.unsafeRunSync
+     *
+     * fun main(args: Array<String>) {
+     *   //sampleStart
+     *   val result = DeferredK {
+     *     println("Calculating solution:")
+     *     42
+     *   }
+     *   //sampleEnd
+     *   println(result.unsafeRunSync())
+     * }
+     * ```
+     */
     operator fun <A> invoke(scope: CoroutineScope = GlobalScope, ctx: CoroutineContext = Dispatchers.Default, start: CoroutineStart = CoroutineStart.LAZY, f: suspend () -> A): DeferredK<A> =
       Generated(ctx, start, scope, f)
 
@@ -119,7 +262,7 @@ sealed class DeferredK<A>(
      *
      * Matching the behavior of [async],
      * its [CoroutineContext] is set to [DefaultDispatcher]
-     * and its [CoroutineStart] is [CoroutineStart.DEFAULT].
+     * and its [CoroutineStart] is [CoroutineStart.LAZY].
      */
     fun <A> async(scope: CoroutineScope = GlobalScope, ctx: CoroutineContext = Dispatchers.Default, start: CoroutineStart = CoroutineStart.LAZY, fa: Proc<A>): DeferredK<A> =
       Generated(ctx, start, scope) {
