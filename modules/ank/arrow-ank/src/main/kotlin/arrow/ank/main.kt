45f2f0f6e740a51b09e5d67593f7c767dcb2d7cf
Jannis <overesch.jannis@gmail.com>
2018-11-26 23:35:10 +0100

    Test different ank implementation based on nio insted of io

--- modules/ank/arrow-ank/src/main/kotlin/arrow/ank/main.kt
+++ modules/ank/arrow-ank/src/main/kotlin/arrow/ank/main.kt
@@ -3,16 +3,19 @@
 package arrow.ank
 
 import arrow.core.Try
-import java.io.File
+import arrow.effects.IO
+import arrow.effects.fix
+import arrow.effects.instances.io.monadDefer.monadDefer
+import java.nio.file.Paths
 
 fun main(vararg args: String) {
   when {
     args.size > 1 -> {
-      Try { ank(File(args[0]), File(args[1]), args.drop(2)) }
+      Try { ank(Paths.get(args[0]), Paths.get(args[1]), args.drop(2), monadDeferInterpreter(IO.monadDefer())).fix().unsafeRunSync() }
         .fold({ ex ->
           throw ex
         }, { files ->
-          println("ΛNK Generated ${files.size} files")
+          println("ΛNK Generated $files files")
         })
     }
     else -> throw IllegalArgumentException("Required first 2 args as directory paths in this order: <required: source> <required: destination> <optional: classpath entries, one per arg..>")
