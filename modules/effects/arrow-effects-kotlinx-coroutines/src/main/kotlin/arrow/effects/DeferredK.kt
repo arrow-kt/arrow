2570effec6ab75120db8fb509d1e097d0e745f28
pakoito <jueces.jus@gmail.com>
2018-12-14 23:26:19 +0000

    Remove annotation and add fixme

--- modules/effects/arrow-effects-kotlinx-coroutines/src/main/kotlin/arrow/effects/DeferredK.kt
+++ modules/effects/arrow-effects-kotlinx-coroutines/src/main/kotlin/arrow/effects/DeferredK.kt
@@ -119,6 +119,11 @@ fun <A> DeferredKOf<A>.value(): Deferred<A> = this.fix().memoized
  */
 fun <A> DeferredKOf<A>.scope(): CoroutineScope = this.fix()._scope
 
+// FIXME (0.8.1)
+// The calls to `Unconfined` ask for @ExperimentalCoroutinesApi, which makes using DeferredK light up
+// with warnings like a Xmas tree. Unconfined or an alternative will always exist.
+//
+// Please avoid adding the annotation for now.
 /**
  * A wrapper class for [Deferred] that either memoizes its result or re-runs the computation each time, based on how it is constructed.
  */
@@ -742,40 +747,3 @@ private fun Deferred<*>.forceExceptionPropagation(): Unit =
   }.let { }
 
 suspend fun <A> DeferredKOf<A>.await(): A = this.fix().await()
-
-typealias DeferredKConnection = KindConnection<ForDeferredK>
-typealias DeferredKProc<A> = (DeferredKConnection, (Either<Throwable, A>) -> Unit) -> Unit
-
-/**
- * Connection for [DeferredK].
- *
- * A connection is represented by a composite of `cancel` functions,
- * [KindConnection.cancel] is idempotent and all methods are thread-safe & atomic.
- *
- * The cancellation functions are maintained in a stack and executed in a FIFO order.
- *
- * @see DeferredK.async
- */
-@Suppress("FunctionName")
-fun DeferredKConnection(dummy: Unit = Unit): KindConnection<ForDeferredK> = KindConnection(object : MonadDefer<ForDeferredK> {
-  override fun <A> defer(fa: () -> DeferredKOf<A>): DeferredK<A> =
-    DeferredK.defer(fa = fa)
-
-  override fun <A> raiseError(e: Throwable): DeferredK<A> =
-    DeferredK.raiseError(e)
-
-  override fun <A> DeferredKOf<A>.handleErrorWith(f: (Throwable) -> DeferredKOf<A>): DeferredK<A> =
-    fix().handleErrorWith(f)
-
-  override fun <A> just(a: A): DeferredK<A> =
-    DeferredK.just(a)
-
-  override fun <A, B> DeferredKOf<A>.flatMap(f: (A) -> DeferredKOf<B>): DeferredK<B> =
-    fix().flatMap(f)
-
-  override fun <A, B> tailRecM(a: A, f: (A) -> DeferredKOf<Either<A, B>>): DeferredK<B> =
-    DeferredK.tailRecM(a, f)
-
-  override fun <A, B> DeferredKOf<A>.bracketCase(release: (A, ExitCase<Throwable>) -> DeferredKOf<Unit>, use: (A) -> DeferredKOf<B>): DeferredK<B> =
-    fix().bracketCase(release = release, use = use)
-}) { it.unsafeRunAsync { } }
