58c2e5ba010e57de8334a6348043e7ebc2c367b0
Simon Vergauwen <vergauwen.simon@gmail.com>
2018-12-14 17:50:55 +0100

    Add some documentation for async

--- modules/effects/arrow-effects/src/main/kotlin/arrow/effects/typeclasses/Async.kt
+++ modules/effects/arrow-effects/src/main/kotlin/arrow/effects/typeclasses/Async.kt
@@ -3,7 +3,6 @@ package arrow.effects.typeclasses
 import arrow.Kind
 import arrow.core.Either
 import arrow.core.Right
-import arrow.core.left
 import arrow.effects.*
 import arrow.effects.internal.CancelToken
 import arrow.effects.internal.asyncIOContinuation
@@ -22,19 +21,58 @@ typealias ProcF<F, A> = ((Either<Throwable, A>) -> Unit) -> Kind<F, Unit>
 /**
  * ank_macro_hierarchy(arrow.effects.typeclasses.Async)
  *
- * The context required to run an asynchronous computation that may fail.
+ * [Async] models how a data type runs an asynchronous computation that may fail.
+ * Defined by the [Proc] signature, which is the consumption of a callback.
  **/
 interface Async<F> : MonadDefer<F> {
-  fun <A> async(fa: Proc<A>): Kind<F, A>
+
+  /**
+   * Creates an instance of [F] that executes an asynchronous process on evaluation.
+   *
+   * This combinator can be used to wrap callbacks or other similar impure code.
+   *
+   * @param fa an asynchronous computation that might fail typed as [Proc].
+   *
+   * import arrow.Kind
+   * import arrow.core.*
+   * import arrow.effects.*
+   * import arrow.effects.instances.io.async.async
+   * import arrow.effects.typeclasses.Async
+   * import java.lang.RuntimeException
+   *
+   *
+   * object GithubService {
+   *   fun getUsernames(callback: (List<String>?, Throwable?) -> Unit): Unit =
+   *     callback(listOf("nomisRev", "raulraja", "pacoworks", "jorgecastilloprz"), null)
+   * }
+   *
+   * fun main(args: Array<String>) {
+   *   //sampleStart
+   *   fun <F> Async<F>.getUsernames(): Kind<F, List<String>> =
+   *     async { cb ->
+   *       GithubService.getUsernames { names, throwable ->
+   *         when {
+   *           names != null -> cb(Right(names))
+   *           throwable != null -> cb(Left(throwable))
+   *           else -> cb(Left(RuntimeException("Null result and no exception")))
+   *         }
+   *       }
+   *     }
+   *
+   *   val result = IO.async().getUsernames()
+   *     .fix()
+   *     .unsafeRunSync()
+   *   //sampleEnd
+   *   println(result)
+   * }
+   */
+  fun <A> async(fa: Proc<A>): Kind<F, A> =
+    asyncF { cb -> delay { fa(cb) } }
 
   /**
    * [async] variant that can suspend side effects in the provided registration function. On this variant, the passed
    * in function is injected with a side-effectful callback for signaling the final result of an asynchronous process.
    * Its returned result needs to be a pure `F[Unit]` that gets evaluated by the runtime.
-   *
-   * ```kotlin
-   *
-   * ```
    */
   fun <A> asyncF(k: ProcF<F, A>): Kind<F, A>
 
