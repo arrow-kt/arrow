2a8b5755d6db610310f7a4a5b8c4e02f298cd270
Miguel Coleto <miguelcoletomunoz@hotmail.com>
2018-12-19 18:02:34 +0100

    Refined types (#1118)
    * Introducing Refinement typeclass and NonZero refined type example
    
    * Adding Less refined type
    
    * Adding Not and GreaterEqual refined types
    
    * Introducing Greater and LessEqual refined types
    
    * Refactoring GreaterEqual and LessEqual
    
    * Negative refined type added
    
    * Positive refined type added
    
    * NonEmptyString refined type added
    
    * Fixing unused import
    
    * Adding NonPositive and NonNegative refined types
    
    * Transforming NonEmptyString into NonEmpty
    
    * Adding some documentation on most important methods for each refined type
    
    * Adding some documentation on most important methods for each refined type
    
    * Fixing code snippets
    
    * Solving ank problems

---
+++ modules/core/arrow-validation/src/main/kotlin/arrow/validation/refinedTypes/bool/Not.kt
@@ -0,0 +1,16 @@
+package arrow.validation.refinedTypes.bool
+
+import arrow.Kind
+import arrow.validation.Refinement
+
+interface Not<F, B> : Refinement<F, B> {
+
+  fun REF(): Refinement<F, B>
+
+  override fun B.refinement(): Boolean = REF().run { !refinement() }
+
+  fun B.not(): Kind<F, B> = refine(this)
+
+  fun <A> B.not(f: (B) -> A): Kind<F, A> = refine(this ,f)
+
+}
