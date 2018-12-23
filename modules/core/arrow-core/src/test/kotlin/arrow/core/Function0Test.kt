cf175100f3bb66d2b3512c110272a402af98c725
Jacob Bass <jacob@jacobbass.net>
2018-12-23 16:26:32 +1100

    semigroup and monoid instances for function0

--- modules/core/arrow-core/src/test/kotlin/arrow/core/Function0Test.kt
+++ modules/core/arrow-core/src/test/kotlin/arrow/core/Function0Test.kt
@@ -5,9 +5,15 @@ import arrow.core.*
 import arrow.instances.extensions
 import arrow.instances.function0.comonad.comonad
 import arrow.instances.function0.monad.monad
+import arrow.instances.function0.monoid.monoid
+import arrow.instances.function0.semigroup.semigroup
+import arrow.instances.monoid
+import arrow.instances.semigroup
 import arrow.test.UnitSpec
 import arrow.test.laws.ComonadLaws
 import arrow.test.laws.MonadLaws
+import arrow.test.laws.SemigroupLaws
+import arrow.test.laws.MonoidLaws
 import arrow.typeclasses.Eq
 import io.kotlintest.KTestJUnitRunner
 import org.junit.runner.RunWith
@@ -20,6 +26,8 @@ class Function0Test : UnitSpec() {
 
   init {
       testLaws(
+        SemigroupLaws.laws(Function0.semigroup(Int.semigroup()), { 1 }.k(), { 2 }.k(), { 3 }.k(), EQ),
+        MonoidLaws.laws(Function0.monoid(Int.monoid()), { 1 }.k(), EQ),
         MonadLaws.laws(Function0.monad(), EQ),
         ComonadLaws.laws(Function0.comonad(), { { it }.k() }, EQ)
       )
