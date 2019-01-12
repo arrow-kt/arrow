c88561376077335741eba15c613bb87b5d10f0fe
Simon Vergauwen <vergauwen.simon@gmail.com>
2019-01-12 18:14:48 +0100

    Rewrite PromiseTest to fix flakiness build, inliner issues

--- modules/effects/arrow-effects-data/src/test/kotlin/arrow/effects/PromiseTest.kt
+++ modules/effects/arrow-effects-data/src/test/kotlin/arrow/effects/PromiseTest.kt
@@ -1,175 +1,159 @@
 package arrow.effects
 
 import arrow.core.*
+import arrow.effects.extensions.io.applicative.product
+import arrow.effects.extensions.io.applicativeError.attempt
 import arrow.effects.extensions.io.async.async
-import arrow.effects.extensions.io.bracket.guarantee
 import arrow.effects.extensions.io.concurrent.concurrent
-import arrow.effects.extensions.io.monad.binding
+import arrow.effects.extensions.io.functor.tupleLeft
 import arrow.effects.extensions.io.monad.flatMap
+import arrow.effects.extensions.io.monad.followedBy
 import arrow.effects.extensions.io.monadDefer.monadDefer
 import arrow.test.UnitSpec
 import arrow.test.generators.genThrowable
-import arrow.test.laws.equalUnderTheLaw
 import io.kotlintest.KTestJUnitRunner
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.properties.Gen
 import io.kotlintest.properties.forAll
 import kotlinx.coroutines.Dispatchers
 import org.junit.runner.RunWith
+import java.lang.RuntimeException
 import kotlin.coroutines.CoroutineContext
 
 @RunWith(KTestJUnitRunner::class)
 class PromiseTest : UnitSpec() {
 
   init {
-    fun tests(label: String,
-              ctx: CoroutineContext = Dispatchers.Default,
-              promise: () -> IOOf<Promise<ForIO, Int>>): Unit {
+
+    fun tests(
+      label: String,
+      ctx: CoroutineContext = Dispatchers.Default,
+      promise: IO<Promise<ForIO, Int>>
+    ): Unit {
 
       "$label - complete" {
-        forAll(Gen.int()) { i ->
-          promise().flatMap { p ->
-            p.complete(i).flatMap {
+        forAll(Gen.int()) { a ->
+          promise.flatMap { p ->
+            p.complete(a).flatMap {
               p.get()
             }
-          }.equalUnderTheLaw(IO.just(i), EQ())
+          }.unsafeRunSync() == a
         }
       }
 
-      "$label - complete blocks"{
-        fun loop(times: Int): IO<Boolean> {
-          fun foreverAsync(i: Int): IO<Unit> =
-            if (i == 512) IO.async<Unit> { _, cb -> cb(Right(Unit)) }.flatMap { foreverAsync(0) }
-            else IO.unit.flatMap { foreverAsync(i + 1) }
-
-          val task = binding {
-            val p = Promise.uncancelable<ForIO, Unit>(IO.async()).bind()
-            val latch = Promise.uncancelable<ForIO, Unit>(IO.async()).bind()
-            val fb = latch.complete(Unit).flatMap { p.get().flatMap { foreverAsync(0) } }.startF(Dispatchers.Default).bind()
-            latch.get().bind()
-            p.complete(Unit).guarantee(fb.cancel()).bind()
-            true
-          }
-
-          return task.flatMap { r ->
-            if (times > 0) loop(times - 1)
-            else IO.just(r)
-          }
-        }
-
-        loop(100).unsafeRunSync() shouldBe true
-      }
-
-      "$label - complete twice results in AlreadyFulfilled" {
+      "$label - complete twice should result in Promise.AlreadyFulfilled" {
         forAll(Gen.int(), Gen.int()) { a, b ->
-          binding {
-            val p = promise().bind()
-            p.complete(a).bind()
-            p.complete(b).bind()
-            p.get().bind()
-          }.equalUnderTheLaw(IO.raiseError(Promise.AlreadyFulfilled), EQ())
+          promise.flatMap { p ->
+            p.complete(a).flatMap {
+              p.complete(b)
+                .attempt()
+                .product(p.get())
+            }
+          }.unsafeRunSync() == Tuple2(Left(Promise.AlreadyFulfilled), a)
         }
       }
 
-      "$label - get() blocks until set" {
-        binding {
-          val state = Ref.of(0, IO.monadDefer()).bind()
-          val modifyGate = promise().bind()
-          val readGate = promise().bind()
-          modifyGate.get().flatMap { state.update { i -> i * 2 }.flatMap { readGate.complete(1) } }.startF(ctx).bind()
-          state.set(1).flatMap { modifyGate.complete(1) }.startF(ctx).bind()
-          readGate.get().bind()
-          state.get().bind()
+      "$label - tryComplete" {
+        forAll(Gen.int()) { a ->
+          promise.flatMap { p ->
+            p.tryComplete(a).flatMap { didComplete ->
+              p.get().tupleLeft(didComplete)
+            }
+          }.unsafeRunSync() == Tuple2(true, a)
+        }
+      }
+
+      "$label - tryComplete twice returns false" {
+        forAll(Gen.int(), Gen.int()) { a, b ->
+          promise.flatMap { p ->
+            p.tryComplete(a).flatMap {
+              p.tryComplete(b).flatMap { didComplete ->
+                p.get().tupleLeft(didComplete)
+              }
+            }
+          }.unsafeRunSync() == Tuple2(false, a)
+        }
+      }
+
+      "$label - error" {
+        forAll(genThrowable()) { error ->
+          promise.flatMap { p ->
+            p.error(error).flatMap {
+              p.get().attempt()
+            }
+          }.unsafeRunSync() == Left(error)
+        }
+      }
+
+      "$label - error twice should result in Promise.AlreadyFulfilled" {
+        forAll(genThrowable()) { error ->
+          promise.flatMap { p ->
+            p.error(error).flatMap {
+              p.error(RuntimeException("Boom!")).attempt()
+                .product(p.get().attempt())
+            }
+          }.unsafeRunSync() == Tuple2(Left(Promise.AlreadyFulfilled), Left(error))
+        }
+      }
+
+      "$label - tryError" {
+        forAll(genThrowable()) { error ->
+          promise.flatMap { p ->
+            p.tryError(error).flatMap { didError ->
+              p.get().attempt()
+                .tupleLeft(didError)
+            }
+          }.unsafeRunSync() == Tuple2(true, Left(error))
+        }
+      }
+
+      "$label - tryError twice returns false" {
+        forAll(genThrowable()) { error ->
+          promise.flatMap { p ->
+            p.tryError(error).flatMap {
+              p.tryError(RuntimeException("Boom!")).flatMap { didComplete ->
+                p.get().attempt()
+                  .tupleLeft(didComplete)
+              }
+            }
+          }.unsafeRunSync() == Tuple2(false, Left(error))
+        }
+      }
+
+      "$label - get blocks until set" {
+        Ref.of(0, IO.monadDefer()).flatMap { state ->
+          promise.flatMap { modifyGate ->
+            promise.flatMap { readGate ->
+              modifyGate.get().flatMap { state.update { i -> i * 2 }.flatMap { readGate.complete(0) } }.startF(ctx).flatMap {
+                state.set(1).flatMap { modifyGate.complete(0) }.startF(ctx).flatMap {
+                  readGate.get().flatMap {
+                    state.get()
+                  }
+                }
+              }
+            }
+          }
         }.unsafeRunSync() shouldBe 2
       }
 
-      "$label - tryGet before completing" {
-        promise().flatMap { p ->
-          p.tryGet()
-        }.equalUnderTheLaw(IO.just(None), EQ())
+      "$label - tryGet returns None for empty Promise" {
+        promise.flatMap { p -> p.tryGet() }.unsafeRunSync() shouldBe None
       }
 
-      "$label - tryGet after completing" {
-        forAll(Gen.int()) { i ->
-          promise().flatMap { p ->
-            p.complete(i).flatMap {
+      "$label - tryGet returns Some for completed promise" {
+        forAll(Gen.int()) { a ->
+          promise.flatMap { p ->
+            p.complete(a).flatMap {
               p.tryGet()
             }
-          }.equalUnderTheLaw(IO.just(Some(i)), EQ())
+          }.unsafeRunSync() == Some(a)
         }
       }
-
-      "$label - tryComplete returns true and sets value when not completed" {
-        forAll(Gen.int()) { i ->
-          binding {
-            val p = promise().bind()
-            p.tryComplete(i).bind() toT p.get().bind()
-          }.equalUnderTheLaw(IO.just(true toT i), EQ())
-        }
-      }
-
-      "$label - tryComplete returns false if already completed" {
-        forAll(Gen.int(), Gen.int()) { a, b ->
-          binding {
-            val p = promise().bind()
-            p.complete(a).bind()
-            p.tryComplete(b).bind() toT p.get().bind()
-          }.equalUnderTheLaw(IO.just(false toT a), EQ())
-        }
-      }
-
-      "$label - error is raised when not completed" {
-        val error = RuntimeException("Boom")
-        promise().flatMap { p ->
-          p.error(error)
-        }.equalUnderTheLaw(IO.raiseError(error), EQ())
-      }
-
-      "$label - error after completion results in AlreadyFulfilled" {
-        forAll(Gen.int(), genThrowable()) { i, t ->
-          binding {
-            val p = promise().bind()
-            p.complete(i).bind()
-            p.error(t).bind()
-            p.get().bind()
-          }.equalUnderTheLaw(IO.raiseError(Promise.AlreadyFulfilled), EQ())
-        }
-      }
-
-      "$label - tryError returns false if already completed" {
-        forAll(Gen.int(), genThrowable()) { i, t ->
-          binding {
-            val p = promise().bind()
-            p.complete(i).bind()
-            p.tryError(t).bind() toT p.get().bind()
-          }.equalUnderTheLaw(IO.just(false toT i), EQ())
-        }
-      }
-
-      "$label - tryError raises error if not completed" {
-        forAll(genThrowable()) { t ->
-          binding {
-            val p = promise().bind()
-            p.tryError(t).bind()
-            p.get().bind()
-          }.equalUnderTheLaw(IO.raiseError(t), EQ())
-        }
-      }
-
     }
 
-    tests("UncancelablePromise") { Promise.uncancelable(IO.async()) }
-    tests("CancelablePromise") { Promise(IO.concurrent()) }
-
-    "CancelablePromise - supports cancellation of get()" {
-      Promise<ForIO, Unit>(IO.concurrent()).flatMap { p ->
-        p.get()
-      }
-        .unsafeRunAsyncCancellable { }
-        .invoke()
-    }
+    tests("CancelablePromise", promise = Promise<ForIO, Int>(IO.concurrent()).fix())
+    tests("UncancelablePromise", promise = Promise.uncancelable<ForIO, Int>(IO.async()).fix())
 
   }
 
 }
-
