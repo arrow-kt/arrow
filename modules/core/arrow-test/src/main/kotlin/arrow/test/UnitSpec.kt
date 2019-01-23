3c3ff6bb136a315c71c5a33b815bdb2370c7a233
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2019-01-23 19:38:12 +0100

    set as expression body

--- modules/core/arrow-test/src/main/kotlin/arrow/test/UnitSpec.kt
+++ modules/core/arrow-test/src/main/kotlin/arrow/test/UnitSpec.kt
@@ -12,16 +12,14 @@ abstract class UnitSpec : AbstractStringSpec() {
 
   private val lawTestCases = mutableListOf<TestCase>()
 
-  fun testLaws(vararg laws: List<Law>): List<TestCase> {
-    return laws
-     .flatMap { list: List<Law> -> list.asIterable() }
-     .distinctBy { law: Law -> law.name }
-     .map { law: Law ->
-       val lawTestCase = createTestCase(law.name, law.test, defaultTestCaseConfig, TestType.Test)
-       lawTestCases.add(lawTestCase)
-       lawTestCase
-     }
-  }
+  fun testLaws(vararg laws: List<Law>): List<TestCase> = laws
+    .flatMap { list: List<Law> -> list.asIterable() }
+    .distinctBy { law: Law -> law.name }
+    .map { law: Law ->
+      val lawTestCase = createTestCase(law.name, law.test, defaultTestCaseConfig, TestType.Test)
+      lawTestCases.add(lawTestCase)
+      lawTestCase
+    }
 
   override fun testCases(): List<TestCase> = super.testCases() + lawTestCases
 
