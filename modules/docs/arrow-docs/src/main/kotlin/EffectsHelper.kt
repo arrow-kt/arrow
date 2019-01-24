6fd968f352e40b2cffdeab95562d13cd12923e78
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2019-01-24 11:10:55 +0100

    delete unused deps

--- modules/docs/arrow-docs/src/main/kotlin/EffectsHelper.kt
+++
@@ -1,8 +0,0 @@
-package arrow.effects
-
-import arrow.effects.extensions.io.monad.flatMap
-import arrow.effects.extensions.io.monad.map
-import kotlinx.coroutines.Dispatchers.Default
-import kotlinx.coroutines.Dispatchers.Unconfined
-
-val UI = Unconfined
