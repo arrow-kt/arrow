fb2968732e4aed7e1949a4427dc10cb469ac4158
Simon Vergauwen <vergauwen.simon@gmail.com>
2019-01-24 10:28:29 +0100

    Rewrote Fiber docs, added coroutines-core to docs module

---
+++ modules/docs/arrow-docs/src/main/kotlin/EffectsHelper.kt
@@ -0,0 +1,8 @@
+package arrow.effects
+
+import arrow.effects.extensions.io.monad.flatMap
+import arrow.effects.extensions.io.monad.map
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.Unconfined
+
+val UI = Unconfined
