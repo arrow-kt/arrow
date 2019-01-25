89c314bf8d1683b6cc555f7b8fff04f8d9896059
Julian Kotrba <me@juliankotrba.xyz>
2019-01-25 16:15:02 +0100

    Add Semiring type class

---
+++ modules/core/arrow-typeclasses/src/main/kotlin/arrow/typeclasses/Semiring.kt
@@ -0,0 +1,19 @@
+package arrow.typeclasses
+
+import arrow.core.Option
+
+interface Semiring<A> : Monoid<A> {
+
+    fun zero(): A
+    fun one(): A
+
+    fun A.product(b: A): A
+
+    override fun empty(): A = one()
+
+    operator fun A.times(b: A): A =
+            this.product(b)
+
+    fun A.maybeProduct(b: A?): A =
+            Option.fromNullable(b).fold({ this }, { product(it) })
+}
