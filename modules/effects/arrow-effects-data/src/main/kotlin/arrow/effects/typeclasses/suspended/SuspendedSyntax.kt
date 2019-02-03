847fe3859ec6cd169e0aebde1345f032f3bd04fc
raulraja <raulraja@gmail.com>
2019-02-03 23:48:31 +0100

    thanks Simon! @nomisrev

--- modules/effects/arrow-effects-data/src/main/kotlin/arrow/effects/typeclasses/suspended/SuspendedSyntax.kt
+++ modules/effects/arrow-effects-data/src/main/kotlin/arrow/effects/typeclasses/suspended/SuspendedSyntax.kt
@@ -2,25 +2,20 @@ package arrow.effects.typeclasses.suspended
 
 import arrow.Kind
 import arrow.core.Either
-import arrow.core.identity
 import arrow.core.left
 import arrow.core.right
-import arrow.effects.CancelToken
 import arrow.effects.KindConnection
-import arrow.effects.OnCancel
 import arrow.effects.internal.Platform
 import arrow.effects.typeclasses.*
-import arrow.effects.typeclasses.suspended.ForwardCancelable.Companion.State.Active
-import arrow.effects.typeclasses.suspended.ForwardCancelable.Companion.State.Empty
+import arrow.effects.typeclasses.suspended.fx.dispatchers.dispatchers
 import arrow.extension
 import arrow.typeclasses.*
+import arrow.typeclasses.Continuation
 import arrow.unsafe
 import java.util.concurrent.CancellationException
+import java.util.concurrent.ForkJoinPool
 import java.util.concurrent.atomic.AtomicReference
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
-import kotlin.coroutines.RestrictsSuspension
-import kotlin.coroutines.startCoroutine
+import kotlin.coroutines.*
 
 class ForFx private constructor() {
   companion object
@@ -111,6 +106,8 @@ class Fx<A>(internal val fa: suspend () -> A) : FxOf<A> {
     b
   }
 
+  fun continueOn(ctx: CoroutineContext): Fx<A> =
+    unit().map { foldContinuation(ctx) { throw it } }
 
   @RestrictsSuspension
   companion object {
@@ -120,6 +117,9 @@ class Fx<A>(internal val fa: suspend () -> A) : FxOf<A> {
 
     fun <A> just(a: A): Fx<A> =
       Fx { a }
+
+    fun unit(): Fx<Unit> =
+      Fx { Unit }
 
     fun <A> raiseError(e: Throwable): Fx<A> =
       Fx { throw e }
@@ -132,26 +132,16 @@ class Fx<A>(internal val fa: suspend () -> A) : FxOf<A> {
         result.fold({ tailRecM(it, f) }, { just(it) })
       }
 
-    fun <A> asyncF(fa: ProcF<ForFx, A>): Fx<A> =
-      Fx<A> {
-
-        val x =
-        fa(x)
+    fun <A> asyncF(fa: ProcF<ForFx, A>): Fx<A> = Fx<A> {
+      suspendCoroutine { continuation ->
         val conn = FxConnection()
-        //On disposing of the upstream stream this will be called by `setCancellable` so check if upstream is already disposed or not because
-        //on disposing the stream will already be in a terminated state at this point so calling onError, in a terminated state, will blow everything up.
-        conn.push(FluxK { if (!sink.isCancelled) sink.error(OnCancel.CancellationException) })
-        sink.onCancel { conn.cancel().value().subscribe() }
-
-        fa(conn) { callback: Either<Throwable, A> ->
-          callback.fold({
-            sink.error(it)
-          }, {
-            sink.next(it)
-            sink.complete()
-          })
-        }
+        //Is CancellationException from kotlin in kotlinx package???
+        conn.push(Fx { continuation.resumeWith(Result.failure(CancellationException())) })
+        fa { either ->
+          continuation.resumeWith(either.fold(Result.Companion::failure, Result.Companion::success))
+        }.fix().foldContinuation(EmptyCoroutineContext, mapUnit)
       }
+    }
 
     fun <A> unsafe.runBlocking(fa: Fx<A>): A {
       val value = AtomicReference<A>()
@@ -247,12 +237,65 @@ interface FxAsync : Async<ForFx>, FxMonadDefer {
   override fun <A> asyncF(k: ProcF<ForFx, A>): Fx<A> =
     Fx.asyncF(k)
 
-  override fun <A> Kind<ForFx, A>.continueOn(ctx: CoroutineContext): Kind<ForFx, A> {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  override fun <A> FxOf<A>.continueOn(ctx: CoroutineContext): Fx<A> =
+    fix().continueOn(ctx)
+}
+
+private class Pool(val pool: ForkJoinPool) : AbstractCoroutineContextElement(ContinuationInterceptor), ContinuationInterceptor {
+  override fun <T> interceptContinuation(continuation: kotlin.coroutines.Continuation<T>): kotlin.coroutines.Continuation<T> =
+    PoolContinuation(pool, continuation.context.fold(continuation) { cont, element ->
+      if (element != this@Pool && element is ContinuationInterceptor)
+        element.interceptContinuation(cont) else cont
+    })
+}
+
+private class PoolContinuation<T>(
+  val pool: ForkJoinPool,
+  val cont: kotlin.coroutines.Continuation<T>
+) : kotlin.coroutines.Continuation<T> {
+  override val context: CoroutineContext = cont.context
+
+  override fun resumeWith(result: Result<T>) {
+    pool.execute { cont.resumeWith(result) }
   }
 }
 
-private fun <A> Fx<A>.foldContinuation(onError: (Throwable) -> A): A {
+val NonBlocking: CoroutineContext = EmptyCoroutineContext + Pool(ForkJoinPool())
+
+@extension
+interface FxDispatchers : Dispatchers<ForFx> {
+  override fun default(): CoroutineContext =
+    NonBlocking
+}
+
+@extension
+interface FxConcurrent : Concurrent<ForFx>, FxAsync {
+  override fun dispatchers(): Dispatchers<ForFx> = Fx.dispatchers()
+
+  override fun <A> asyncF(fa: ConnectedProcF<ForFx, A>): Fx<A> {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun <A> CoroutineContext.startFiber(kind: FxOf<A>): Fx<Fiber<ForFx, A>> {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun <A, B> CoroutineContext.racePair(fa: FxOf<A>, fb: FxOf<B>): Fx<RacePair<ForFx, A, B>> {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun <A, B, C> CoroutineContext.raceTriple(fa: FxOf<A>, fb: FxOf<B>, fc: FxOf<C>): Fx<RaceTriple<ForFx, A, B, C>> {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun <A> asyncF(k: ProcF<ForFx, A>): Fx<A> =
+    Fx.asyncF(k)
+}
+
+private fun <A> Fx<A>.foldContinuation(
+  context: CoroutineContext = EmptyCoroutineContext,
+  onError: (Throwable) -> A
+): A {
   val result: AtomicReference<A> = AtomicReference()
   fa.startCoroutine(object : Continuation<A> {
     override fun resume(value: A) {
@@ -264,14 +307,13 @@ private fun <A> Fx<A>.foldContinuation(onError: (Throwable) -> A): A {
     }
 
     override val context: CoroutineContext
-      get() = EmptyCoroutineContext
+      get() = context
   })
   return result.get()
 }
 
 fun FxConnection(): KindConnection<ForFx> =
-  KindConnection(object : FxMonadDefer {}) { it.fix().fa.invoke() }
-
+  KindConnection(object : FxMonadDefer {}) { it.fix().foldContinuation { e -> throw e } }
 
 fun main() {
   unsafe { Fx.runBlocking(program) }
