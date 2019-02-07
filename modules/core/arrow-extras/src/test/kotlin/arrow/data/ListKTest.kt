823662a86ac6756ecc784bf88876c75049f7bd59
Julian Kotrba <me@juliankotrba.xyz>
2019-02-07 15:13:47 +0100

    Add Semigroupal ListK instance

--- modules/core/arrow-extras/src/test/kotlin/arrow/data/ListKTest.kt
+++ modules/core/arrow-extras/src/test/kotlin/arrow/data/ListKTest.kt
@@ -1,15 +1,16 @@
 package arrow.data
 
+import arrow.Kind
+import arrow.core.*
 import arrow.core.extensions.eq
 import arrow.core.extensions.hash
-import arrow.core.identity
-import arrow.core.toT
 import arrow.data.extensions.list.fx.fx
 import arrow.data.extensions.listk.applicative.applicative
 import arrow.data.extensions.listk.eq.eq
 import arrow.data.extensions.listk.hash.hash
 import arrow.data.extensions.listk.monoidK.monoidK
 import arrow.data.extensions.listk.semigroupK.semigroupK
+import arrow.data.extensions.listk.semigroupal.semigroupal
 import arrow.data.extensions.listk.show.show
 import arrow.data.extensions.listk.traverse.traverse
 import arrow.mtl.extensions.listk.monadCombine.monadCombine
@@ -24,6 +25,10 @@ import org.junit.runner.RunWith
 class ListKTest : UnitSpec() {
   val applicative = ListK.applicative()
 
+  val A: ListK<Int> = listOf(1,2).k()
+  val B: ListK<Int> = listOf(3,4).k()
+  val C: ListK<Int> = listOf(5).k()
+
   init {
 
     val EQ: Eq<ListKOf<Int>> = ListK.eq(Eq.any())
@@ -31,6 +36,7 @@ class ListKTest : UnitSpec() {
     testLaws(
       ShowLaws.laws(ListK.show(), EQ) { listOf(it).k() },
       SemigroupKLaws.laws(ListK.semigroupK(), applicative, Eq.any()),
+      SemigroupalLaws.laws(ListK.semigroupal(), A, B, C, this::bijection),
       MonoidKLaws.laws(ListK.monoidK(), applicative, Eq.any()),
       TraverseLaws.laws(ListK.traverse(), applicative, { n: Int -> ListK(listOf(n)) }, Eq.any()),
       MonadCombineLaws.laws(ListK.monadCombine(),
@@ -45,4 +51,8 @@ class ListKTest : UnitSpec() {
     }
 
   }
+
+  private fun bijection(from: Kind<ForListK, Tuple2<Tuple2<Int, Int>, Int>>): ListK<Tuple2<Int, Tuple2<Int, Int>>> =
+          from.fix().map { Tuple2(it.a.a, Tuple2(it.a.b, it.b)) }.k()
+
 }
