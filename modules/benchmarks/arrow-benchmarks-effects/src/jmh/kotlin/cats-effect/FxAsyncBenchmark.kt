44fe6371e976b4b7344cbd3c2e46f389962eaafb
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2019-02-15 21:28:25 +0100

    add Handler Error benchmark

--- modules/benchmarks/arrow-benchmarks-effects/src/jmh/kotlin/cats-effect/FxAsyncBenchmark.kt
+++ modules/benchmarks/arrow-benchmarks-effects/src/jmh/kotlin/cats-effect/FxAsyncBenchmark.kt
@@ -9,21 +9,6 @@ import arrow.unsafe
 import org.openjdk.jmh.annotations.*
 import java.util.concurrent.TimeUnit
 
-/**
- * To do comparative benchmarks between versions:
- *
- *     benchmarks/run-benchmark AsyncBenchmark
- *
- * This will generate results in `benchmarks/results`.
- *
- * Or to run the benchmark from within SBT:
- *
- *     jmh:run -i 10 -wi 10 -f 2 -t 1 cats.effect.benchmarks.AsyncBenchmark
- *
- * Which means "10 iterations", "10 warm-up iterations", "2 forks", "1 thread".
- * Please note that benchmarks should be usually executed at least in
- * 10 iterations (as a rule of thumb), but more is better.
- */
 @State(Scope.Thread)
 @Fork(2)
 @Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
