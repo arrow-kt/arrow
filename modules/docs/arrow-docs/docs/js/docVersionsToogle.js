c5bcc4590b7eb4873a2481bd14c2dbc579dd6cb7
AntonioMateoGomez <antonio.mateo@47deg.com>
2019-02-26 17:43:16 +0100

    Doc version dropdown in home section

---
+++ modules/docs/arrow-docs/docs/js/docVersionsToogle.js
@@ -0,0 +1,17 @@
+function displayToogle() {
+  document.getElementById("docVersionDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
