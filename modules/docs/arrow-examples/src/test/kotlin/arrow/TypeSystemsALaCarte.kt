21e6077e2c0f0f80b944f1f3779f524b1f91b62e
pakoito <jueces.jus@gmail.com>
2019-03-02 20:41:42 +0000

    Fix topological to be a Scheduler

--- modules/docs/arrow-examples/src/test/kotlin/arrow/TypeSystemsALaCarte.kt
+++ modules/docs/arrow-examples/src/test/kotlin/arrow/TypeSystemsALaCarte.kt
@@ -6,6 +6,7 @@ import arrow.core.toOption
 import arrow.core.toT
 import arrow.mtl.typeclasses.MonadState
 import arrow.typeclasses.Eq
+import arrow.typeclasses.Hash
 import arrow.typeclasses.Order
 import arrow.typeclasses.suspended.monad.Fx
 
@@ -25,15 +26,15 @@ data class Store<I, K, V>(val information: I, private val get: (K) -> V) {
     }
 }
 
-data class Hash<V> private constructor(private val valueHash: Int) {
+data class Hashable<V> private constructor(private val valueHash: Int) {
 
   companion object {
-    operator fun <V> invoke(hashable: arrow.typeclasses.Hash<V>, value: V) = hashable.run {
-      Hash<V>(value.hash())
+    operator fun <V> invoke(hashable: Hash<V>, value: V) = hashable.run {
+      Hashable<V>(value.hash())
     }
   }
 
-  fun <I, K> getHash(hashable: arrow.typeclasses.Hash<V>, key: K, store: Store<I, K, V>) = Hash(hashable, store.getValue(key))
+  fun <I, K> getHash(hashable: arrow.typeclasses.Hash<V>, key: K, store: Store<I, K, V>) = Hashable(hashable, store.getValue(key))
 }
 
 interface Task<F, K, V> {
@@ -46,32 +47,36 @@ typealias Build<F, I, K, V> = BuildSystem<K, F>.(tasks: Tasks<F, K, V>, key: K, 
 
 typealias Rebuilder<F, IR, K, V> = BuildComponents<K, F, IR>.(key: K, value: V, task: Task<F, K, V>) -> Kind<F, Task<F, K, V>>
 
-typealias Scheduler<F, I, IR, K, V> = BuildComponents<K, F, IR>.(rebuilder: Rebuilder<F, IR, K, V>) -> Kind<F, Build<F, I, K, V>>
+typealias Scheduler<F, I, IR, K, V> = BuildComponents<K, F, IR>.(rebuilder: Rebuilder<F, IR, K, V>) -> Build<F, I, K, V>
 
 interface BuildSystem<K, F> : Order<K>, Fx<F>
 
 interface BuildComponents<K, F, IR> : Order<K>, Fx<F>, MonadState<F, IR>, BuildSystem<K, F>
 
-/* Build types */
+/** Scheduling algorithms */
 
-fun <F, I, K, V> BuildComponents<K, F, I>.topological(rebuilder: Rebuilder<F, I, K, V>): Build<F, I, K, V> = { tasks: Tasks<F, K, V>, target: K, store: Store<I, K, V> ->
-  fx {
-    val dep: (K) -> Graph<K> = { k: K -> tasks(k).fold({ emptyList() }, { dependencies(it) }) }
-    val order: List<K> = topSort(reachable(dep, target))
-    order.foldRight(store) { currTarget: K, acc: Store<I, K, V> ->
-      tasks(currTarget).fold({
-        acc
-      }, { task ->
-        val value: V = store.getValue(currTarget)
-        val newTask: Task<F, K, V> = !rebuilder(currTarget, value, task)
-        val newValue: V = !newTask.run { just(store.getValue(it)) }
-        store.putValue(this@topological, currTarget, newValue)
-      })
+fun <F, I, K, V> BuildComponents<K, F, I>.topological(): Scheduler<F, I, I, K, V> = { rebuilder: Rebuilder<F, I, K, V> ->
+  { tasks: Tasks<F, K, V>, target: K, store: Store<I, K, V> ->
+    fx {
+      val dep: (K) -> Graph<K> = { k: K -> tasks(k).fold({ emptyList() }, { dependencies(it) }) }
+      val order: List<K> = topSort(reachable(dep, target))
+      order.foldRight(store) { currTarget: K, acc: Store<I, K, V> ->
+        tasks(currTarget).fold({
+          acc
+        }, { task ->
+          val value: V = store.getValue(currTarget)
+          val newTask: Task<F, K, V> = !rebuilder(currTarget, value, task)
+          val newValue: V = !newTask.run { just(store.getValue(it)) }
+          store.putValue(this@topological, currTarget, newValue)
+        })
+      }
     }
   }
 }
 
-/** The MAKE build system */
+/** Rebuilding Strategies */
+
+/* The MAKE build system */
 
 typealias Time = Int
 
@@ -93,38 +98,13 @@ fun <F, K, V> modTimeRebuilder(): Rebuilder<F, MakeInfo<K>, K, V> = { key, value
   }
 }
 
+/** Full examples **/
+
 fun <F, K, V> BuildComponents<K, F, MakeInfo<K>>.make(): Build<F, MakeInfo<K>, K, V> = { tasks: Tasks<F, K, V>, key: K, store: Store<MakeInfo<K>, K, V> ->
-  fx {
-    !topological<F, MakeInfo<K>, K, V>(modTimeRebuilder())(tasks, key, store)
-  }
+  val topological: Scheduler<F, MakeInfo<K>, MakeInfo<K>, K, V> = topological()
+  val build: Build<F, MakeInfo<K>, K, V> = topological(modTimeRebuilder())
+  build(tasks, key, store)
 }
-
-// -- A restarting task scheduler
-// restarting :: Ord k => Scheduler Monad (ir, Chain k) ir k v
-// restarting rebuilder tasks target = execState $ do
-//     chain <- gets (snd . getInfo)
-//     newChain <- liftChain $ go Set.empty $ chain ++ [target | target `notElem` chain]
-//     modify $ mapInfo $ \(ir, _) -> (ir, newChain)
-//   where
-//     go :: Set k -> Chain k -> State (Store ir k v) (Chain k)
-//     go _    []         = return []
-//     go done (key:keys) = case tasks key of
-//       Nothing -> (key :) <$> go (Set.insert key done) keys
-//       Just task -> do
-//         store <- get
-//         let newTask :: Task (MonadState ir) k (Either k v)
-//             newTask = try $ rebuilder key (getValue key store) task
-//             fetch :: k -> State ir (Either k v)
-//             fetch k | k `Set.member` done = return $ Right (getValue k store)
-//                     | otherwise = return $ Left k
-//         result <- liftStore (run newTask fetch) -- liftStore is defined in Fig. 7
-//         case result of
-//           Left dep -> go done $ dep: filter (/= dep) keys ++ [key]
-//           Right newValue -> do modify $ putValue key newValue
-//                                (key :) <$> go (Set.insert key done) keys
-
-fun <F, IR> restarting(ord: Order<F>) = 1
-
 
 /* Helpers */
 
