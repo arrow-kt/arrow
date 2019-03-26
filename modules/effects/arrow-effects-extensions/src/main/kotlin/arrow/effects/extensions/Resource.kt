a9556d1c333aecd1467e2480ba29b532d6d000f1
Jannis <overesch.jannis@gmail.com>
2019-03-26 14:06:38 +0100

    Change docs to fully kdoc

--- modules/effects/arrow-effects-extensions/src/main/kotlin/arrow/effects/extensions/Resource.kt
+++ modules/effects/arrow-effects-extensions/src/main/kotlin/arrow/effects/extensions/Resource.kt
@@ -8,17 +8,21 @@ import arrow.effects.fix
 import arrow.effects.typeclasses.Bracket
 import arrow.extension
 import arrow.typeclasses.*
+import arrow.undocumented
 
 @extension
+@undocumented
 interface ResourceFunctor<F, E> : Functor<ResourcePartialOf<F, E>> {
-  override fun <A, B> Kind<ResourcePartialOf<F, E>, A>.map(f: (A) -> B): Kind<ResourcePartialOf<F, E>, B> = fix().map(f)
+  fun BR(): Bracket<F, E>
+  override fun <A, B> Kind<ResourcePartialOf<F, E>, A>.map(f: (A) -> B): Kind<ResourcePartialOf<F, E>, B> =
+    fix().map(BR(), f)
 }
 
 @extension
 interface ResourceApplicative<F, E> : Applicative<ResourcePartialOf<F, E>> {
   fun BR(): Bracket<F, E>
   override fun <A, B> Kind<ResourcePartialOf<F, E>, A>.ap(ff: Kind<ResourcePartialOf<F, E>, (A) -> B>): Kind<ResourcePartialOf<F, E>, B> =
-    fix().ap(ff.fix())
+    fix().ap(BR(), ff.fix())
 
   override fun <A> just(a: A): Kind<ResourcePartialOf<F, E>, A> = Resource.just(a, BR())
 }
@@ -31,7 +35,7 @@ interface ResourceSelective<F, E> : Selective<ResourcePartialOf<F, E>> {
     fix().ap(ff.fix())
 
   override fun <A, B> Kind<ResourcePartialOf<F, E>, Either<A, B>>.select(f: Kind<ResourcePartialOf<F, E>, (A) -> B>): Kind<ResourcePartialOf<F, E>, B> =
-    fix().flatMap { it.fold({ Resource.just(it, BR()).ap(f.fix()) }, { Resource.just(it, BR()) }) }
+    fix().flatMap { it.fold({ Resource.just(it, BR()).ap(BR(), f.fix()) }, { Resource.just(it, BR()) }) }
 }
 
 @extension
@@ -54,13 +58,14 @@ interface ResourceMonad<F, E> : Monad<ResourcePartialOf<F, E>> {
 @extension
 interface ResourceSemigroup<F, E, A> : Semigroup<Resource<F, E, A>> {
   fun SR(): Semigroup<A>
-  override fun Resource<F, E, A>.combine(b: Resource<F, E, A>): Resource<F, E, A> = combine(b, SR())
+  fun BR(): Bracket<F, E>
+  override fun Resource<F, E, A>.combine(b: Resource<F, E, A>): Resource<F, E, A> = combine(b, SR(), BR())
 }
 
 @extension
 interface ResourceMonoid<F, E, A> : Monoid<Resource<F, E, A>>, ResourceSemigroup<F, E, A> {
   fun MR(): Monoid<A>
-  fun BR(): Bracket<F, E>
+  override fun BR(): Bracket<F, E>
   override fun SR(): Semigroup<A> = MR()
   override fun empty(): Resource<F, E, A> = Resource.empty(MR(), BR())
 }
