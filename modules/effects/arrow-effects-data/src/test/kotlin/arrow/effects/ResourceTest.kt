5a476927563d79118a808bf11ab21e22cd77fb82
Jannis <overesch.jannis@gmail.com>
2019-03-30 17:59:00 +0100

    `Resource` data type (#1376)
    * Add ManagedT data-type
    
    * Use interface instead of abstract class
    
    * Add initial docs
    
    * Rename to Resource and add some documentation
    
    * Add menu link
    
    * Remove @documented
    
    * Change docs to fully kdoc
    
    * Use ! operator instead of bind
    
    * Include all context in playground

---
+++ modules/effects/arrow-effects-data/src/test/kotlin/arrow/effects/ResourceTest.kt
@@ -0,0 +1,46 @@
+package arrow.effects
+
+import arrow.Kind
+import arrow.core.extensions.monoid
+import arrow.data.extensions.list.traverse.traverse
+import arrow.effects.extensions.io.bracket.bracket
+import arrow.effects.extensions.resource.applicative.applicative
+import arrow.effects.extensions.resource.monad.monad
+import arrow.effects.extensions.resource.monoid.monoid
+import arrow.effects.typeclasses.seconds
+import arrow.test.UnitSpec
+import arrow.test.laws.MonadLaws
+import arrow.test.laws.MonoidLaws
+import arrow.test.laws.forFew
+import arrow.typeclasses.Eq
+import io.kotlintest.properties.Gen
+import io.kotlintest.runner.junit4.KotlinTestRunner
+import org.junit.runner.RunWith
+
+@RunWith(KotlinTestRunner::class)
+class ResourceTest : UnitSpec() {
+  init {
+
+    val EQ = Eq<Kind<ResourcePartialOf<ForIO, Throwable>, Int>> { a, b ->
+      a.fix().invoke { IO.just(1) }.fix().unsafeRunTimed(60.seconds) ==
+        b.fix().invoke { IO.just(1) }.fix().unsafeRunTimed(60.seconds)
+    }
+
+    testLaws(
+      MonadLaws.laws(Resource.monad(IO.bracket()), EQ),
+      MonoidLaws.laws(Resource.monoid(Int.monoid(), IO.bracket()), Gen.int().map { Resource.just(it, IO.bracket()) }, EQ)
+    )
+
+    "Resource releases resources in reverse order of acquisation" {
+      forFew(5, Gen.list(Gen.string())) { l ->
+        val released = mutableListOf<String>()
+        l.traverse(Resource.applicative(IO.bracket())) {
+          Resource({ IO { it } }, { r -> IO { released.add(r); Unit } }, IO.bracket())
+        }.fix().invoke { IO.unit }.fix().unsafeRunSync()
+
+        // This looks confusing but is correct, traverse is a rightFold => l is already "reversed"
+        l == released
+      }
+    }
+  }
+}
