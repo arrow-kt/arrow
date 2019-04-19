0a3a3d3128792ffefb21c0234c63bcdd59a4d568
Julian Kotrba <me@juliankotrba.xyz>
2019-04-19 12:28:45 +0900

    Improve formatting

--- modules/core/arrow-extras-data/src/main/kotlin/arrow/data/fingertree/internal/FingerTreeInternal.kt
+++ modules/core/arrow-extras-data/src/main/kotlin/arrow/data/fingertree/internal/FingerTreeInternal.kt
@@ -18,63 +18,67 @@ internal sealed class FingerTreeInternal<T> {
   ) : FingerTreeInternal<T>()
 
 
-  fun prepend(item: T): FingerTreeInternal<T> = when (val tree = this) {
+  fun prepend(item: T): FingerTreeInternal<T> =
+    when (val tree = this) {
 
-    is Empty -> Single(item)
-    is Single -> Deep(Affix.One(item), Empty(), Affix.One(tree.a))
-    is Deep -> {
-      when (val prefix = tree.prefix) {
-        is Affix.Four ->
-          Deep(
-            Affix.Two(item, prefix.a),
-            tree.deeper.prepend(Node.Branch3(prefix.b, prefix.c, prefix.d)),
-            tree.suffix
+      is Empty -> Single(item)
+      is Single -> Deep(Affix.One(item), Empty(), Affix.One(tree.a))
+      is Deep -> {
+        when (val prefix = tree.prefix) {
+          is Affix.Four ->
+            Deep(
+              Affix.Two(item, prefix.a),
+              tree.deeper.prepend(Node.Branch3(prefix.b, prefix.c, prefix.d)),
+              tree.suffix
+            )
+          else -> Deep(tree.prefix.prepend(item), tree.deeper, tree.suffix)
+        }
+      }
+    }
+
+  fun append(item: T): FingerTreeInternal<T> =
+    when (val tree = this) {
+
+      is Empty -> Single(item)
+      is Single -> Deep(Affix.One(tree.a), Empty(), Affix.One(item))
+      is Deep -> {
+        when (val suffix = tree.suffix) {
+          is Affix.Four ->
+            Deep(
+              tree.prefix,
+              tree.deeper.append(Node.Branch3(suffix.a, suffix.b, suffix.c)),
+              Affix.Two(suffix.d, item)
+            )
+
+          else -> Deep(tree.prefix, tree.deeper, tree.suffix.append(item))
+        }
+      }
+    }
+
+  fun viewL(): Option<Tuple2<T, FingerTreeInternal<T>>> =
+    when (this) {
+      is Empty -> Option.empty()
+      is Single -> Option.just(Tuple2(this.a, Empty()))
+      is Deep -> when (this.prefix) {
+        is Affix.One -> {
+
+          this.deeper.viewL().fold(
+            { Option.just(Tuple2(this.prefix.a, suffix.toFingerTree())) },
+            { (node, tree) -> Option.just(Tuple2(this.prefix.a, Deep(node.toAffix(), tree, this.suffix))) }
           )
-        else -> Deep(tree.prefix.prepend(item), tree.deeper, tree.suffix)
-      }
-    }
-  }
+        }
 
-  fun append(item: T): FingerTreeInternal<T> = when (val tree = this) {
-
-    is Empty -> Single(item)
-    is Single -> Deep(Affix.One(tree.a), Empty(), Affix.One(item))
-    is Deep -> {
-      when (val suffix = tree.suffix) {
-        is Affix.Four ->
-          Deep(
-            tree.prefix,
-            tree.deeper.append(Node.Branch3(suffix.a, suffix.b, suffix.c)),
-            Affix.Two(suffix.d, item)
-          )
-
-        else -> Deep(tree.prefix, tree.deeper, tree.suffix.append(item))
-      }
-    }
-  }
-
-  fun viewL(): Option<Tuple2<T, FingerTreeInternal<T>>> = when (this) {
-    is Empty -> Option.empty()
-    is Single -> Option.just(Tuple2(this.a, Empty()))
-    is Deep -> when (this.prefix) {
-      is Affix.One -> {
-
-        this.deeper.viewL().fold(
-          { Option.just(Tuple2(this.prefix.a, suffix.toFingerTree())) },
-          { (node, tree) -> Option.just(Tuple2(this.prefix.a, Deep(node.toAffix(), tree, this.suffix))) }
-        )
+        else -> Option.just(
+          Tuple2(this.prefix.head(),
+            Deep(this.prefix.dropHead(), this.deeper, this.suffix)
+          ))
       }
 
-      else -> Option.just(
-        Tuple2(this.prefix.head(),
-          Deep(this.prefix.dropHead(), this.deeper, this.suffix)
-        ))
     }
 
-  }
 
-
-  fun viewR(): Option<Tuple2<T, FingerTreeInternal<T>>> = when (this) {
+  fun viewR(): Option<Tuple2<T, FingerTreeInternal<T>>> =
+    when (this) {
       is Empty -> Option.empty()
       is Single -> Option.just(Tuple2(this.a, Empty()))
       is Deep -> when (this.suffix) {
