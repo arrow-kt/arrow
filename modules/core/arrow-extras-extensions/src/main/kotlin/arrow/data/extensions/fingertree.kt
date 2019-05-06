2e953057efd132d281435d8f8ee3a6e86a8c4847
Julian Kotrba <me@juliankotrba.xyz>
2019-05-06 16:23:35 +0900

    Add finger tree functor instance

--- modules/core/arrow-extras-extensions/src/main/kotlin/arrow/data/extensions/fingertree.kt
+++ modules/core/arrow-extras-extensions/src/main/kotlin/arrow/data/extensions/fingertree.kt
@@ -1,7 +1,11 @@
 package arrow.data.extensions
 
+import arrow.Kind
 import arrow.data.fingertree.FingerTree
+import arrow.data.fingertree.ForFingerTree
+import arrow.data.fingertree.fix
 import arrow.extension
+import arrow.typeclasses.Functor
 import arrow.typeclasses.Monoid
 
 @extension
@@ -9,4 +13,10 @@ interface FingerTreeMonoid<T> : Monoid<FingerTree<T>> {
   override fun empty(): FingerTree<T> = FingerTree.empty()
 
   override fun FingerTree<T>.combine(b: FingerTree<T>): FingerTree<T> = this.concat(b)
+}
+
+@extension
+interface FingerTreeFunctor<T> : Functor<ForFingerTree> {
+  override fun <A, B> Kind<ForFingerTree, A>.map(f: (A) -> B): Kind<ForFingerTree, B> =
+    FingerTree.fromList(fix().asList().map(f))
 }
