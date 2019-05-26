fa75fc35ec5cd027007ab04531ba2a036f2e911b
Raul Raja <raulraja@gmail.com>
2019-05-26 23:49:10 +0200

    Deprecate Legacy Validation utils
    The Validation utils have been assimilated in the `Apply.map` features which abstract over arity for `Validated`, `Either` and in general all data types that provide an instance of `ApplicativeError`.
    Additionally it's possible to abstract over validation strategies and error handling in general https://arrow-kt.io/docs/patterns/error_handling/#example--alternative-validation-strategies-using-applicativeerror
    The current utilities abuse unsafe APIs and bails with exceptions being inconsistent with the `Left` short-circuit monad extension for `Either` and models error accumulation with a data type that may be empty.

--- modules/validation/arrow-validation/src/main/kotlin/arrow/validation/Validation.kt
+++ modules/validation/arrow-validation/src/main/kotlin/arrow/validation/Validation.kt
@@ -6,6 +6,7 @@ import arrow.core.identity
 private fun <A, B> Either<A, B>.get(): B =
   fold({ throw NoSuchElementException("Either.get on a Left value") }, ::identity)
 
+@Deprecated(message = "This data type has been replaced by arrow.core.Validated, arrow.core.Either and their `map` methods equivalent to the `validate` API")
 class Validation<out E : Any>(vararg disjunctionSequence: Either<E, *>) {
 
   val failures: List<E> = disjunctionSequence.filter { it.isLeft() }.map { it.swap().get() }
@@ -13,6 +14,15 @@ class Validation<out E : Any>(vararg disjunctionSequence: Either<E, *>) {
   val hasFailures: Boolean = failures.isNotEmpty()
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2) { (a, b) -> ifValid(a, b) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -26,6 +36,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any> validate(
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3) { (a, b, c) -> ifValid(a, b, c) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -40,6 +59,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any> validate(
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4) { (a, b, c, d) -> ifValid(a, b, c, d) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -55,6 +83,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any> validate(
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5) { (a, b, c, d, e) -> ifValid(a, b, c, d, e) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -71,6 +108,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any> validat
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6) { (a, b, c, d, e, f) -> ifValid(a, b, c, d, e, f) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -88,6 +134,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7) { (a, b, c, d, e, f, g) -> ifValid(a, b, c, d, e, f, g) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -106,6 +161,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8) { (a, b, c, d, e, f, g, h) -> ifValid(a, b, c, d, e, f, g, h) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -125,6 +189,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9) { (a, b, c, d, e, f, g, h, i) -> ifValid(a, b, c, d, e, f, g, h, i) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -145,6 +218,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10) { (a, b, c, d, e, f, g, h, i, j) -> ifValid(a, b, c, d, e, f, g, h, i, j) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any, R10 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -166,6 +248,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11) { (a, b, c, d, e, f, g, h, i, j, k) -> ifValid(a, b, c, d, e, f, g, h, i, j, k) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any, R10 : Any, R11 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -188,6 +279,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12) { (a, b, c, d, e, f, g, h, i, j, k, l) -> ifValid(a, b, c, d, e, f, g, h, i, j, k, l) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any, R10 : Any, R11 : Any, R12 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -211,6 +311,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13) { (a, b, c, d, e, f, g, h, i, j, k, l, m) -> ifValid(a, b, c, d, e, f, g, h, i, j, k, l, m) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any, R10 : Any, R11 : Any, R12 : Any, R13 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -235,6 +344,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14) { (a, b, c, d, e, f, g, h, i, j, k, l, m, n) -> ifValid(a, b, c, d, e, f, g, h, i, j, k, l, m, n) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any, R10 : Any, R11 : Any, R12 : Any, R13 : Any, R14 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -260,6 +378,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15) { (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o) -> ifValid(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any, R10 : Any, R11 : Any, R12 : Any, R13 : Any, R14 : Any, R15 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -286,6 +413,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16) { (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o) -> ifValid(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any, R10 : Any, R11 : Any, R12 : Any, R13 : Any, R14 : Any, R15 : Any, R16 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -313,6 +449,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17) { (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p) -> ifValid(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any, R10 : Any, R11 : Any, R12 : Any, R13 : Any, R14 : Any, R15 : Any, R16 : Any, R17 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -341,6 +486,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18) { (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q) -> ifValid(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any, R10 : Any, R11 : Any, R12 : Any, R13 : Any, R14 : Any, R15 : Any, R16 : Any, R17 : Any, R18 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -370,6 +524,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19) { (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r) -> ifValid(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any, R10 : Any, R11 : Any, R12 : Any, R13 : Any, R14 : Any, R15 : Any, R16 : Any, R17 : Any, R18 : Any, R19 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -400,6 +563,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20) { (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s) -> ifValid(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any, R10 : Any, R11 : Any, R12 : Any, R13 : Any, R14 : Any, R15 : Any, R16 : Any, R17 : Any, R18 : Any, R19 : Any, R20 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -431,6 +603,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21) { (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t) -> ifValid(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any, R10 : Any, R11 : Any, R12 : Any, R13 : Any, R14 : Any, R15 : Any, R16 : Any, R17 : Any, R18 : Any, R19 : Any, R20 : Any, R21 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
@@ -463,6 +644,15 @@ fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : An
   }
 }
 
+@Deprecated(
+  message = "This is deprecated in favor of the more general map(either, either, ...) { (a, b, ...) -> result }",
+  replaceWith = ReplaceWith(
+    expression = "map(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22) { (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u) -> ifValid(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u) }",
+    imports = arrayOf(
+      "arrow.core.extensions.either.apply.map"
+    )
+  )
+)
 fun <L : Any, R : Any, R1 : Any, R2 : Any, R3 : Any, R4 : Any, R5 : Any, R6 : Any, R7 : Any, R8 : Any, R9 : Any, R10 : Any, R11 : Any, R12 : Any, R13 : Any, R14 : Any, R15 : Any, R16 : Any, R17 : Any, R18 : Any, R19 : Any, R20 : Any, R21 : Any, R22 : Any> validate(
   p1: Either<L, R1>,
   p2: Either<L, R2>,
