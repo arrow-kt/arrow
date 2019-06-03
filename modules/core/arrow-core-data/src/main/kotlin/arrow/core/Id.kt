24326f06796b346ba741faf01394462f18bd3672
Miguel Angel Ruiz Lopez <maruiz81@gmail.com>
2019-06-03 23:58:50 +0200

    Fixing the comments of id class

--- modules/core/arrow-core-data/src/main/kotlin/arrow/core/Id.kt
+++ modules/core/arrow-core-data/src/main/kotlin/arrow/core/Id.kt
@@ -10,36 +10,29 @@ fun <A> IdOf<A>.value(): A = this.fix().extract()
  * The identity monad can be seen as the ambient monad that encodes the effect of having no effect.
  * It is ambient in the sense that plain pure values are values of `Id`.
  *
- * ```kotlin:ank:playground
- *  //sampleStart
- * import arrow.*
- * import arrow.core.*
- *
+ * ```
  * Id("hello")
- * //sampleEnd
  * ```
  *
  * Using this type declaration, we can treat our Id type constructor as a `Monad` and as a `Comonad`.
  * The `just` method, which has type `A -> Id<A>` just becomes the identity function. The `map` method
  * from `Functor` just becomes function application
  *
- * ``kotlin:ank:playground
+ * ```kotlin:ank:playground
+ * import arrow.core.Id
+ *
+ * fun main() {
  * //sampleStart
  * val id: Id<Int> = Id.just(3)
  * id.map{it + 3}
  * //sampleEnd
+ * System.out.print(id.map{it + 3}.extract())
+ * }
  * ```
  *
  * ### Supported type classes
- *
- * ``kotlin:ank:playground
- * //sampleStart
- * import arrow.reflect.*
- * import arrow.data.*
- * import arrow.core.*
- *
+ * ```
  * DataType(Id::class).tcMarkdownList()
- * //sampleEnd
  * ```
  */
 
