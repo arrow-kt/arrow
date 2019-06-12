0f549f347369c8358cd325c76f49cc1d7b5102ea
Miguel Angel Ruiz Lopez <maruiz81@gmail.com>
2019-06-13 00:03:43 +0200

    Fix Bitraverse law

--- modules/core/arrow-test/src/main/kotlin/arrow/test/laws/BitraverseLaws.kt
+++ modules/core/arrow-test/src/main/kotlin/arrow/test/laws/BitraverseLaws.kt
@@ -8,22 +8,21 @@ import arrow.core.extensions.id.applicative.applicative
 import arrow.core.extensions.id.comonad.extract
 import arrow.test.generators.functionAToB
 import arrow.test.generators.intSmall
-import arrow.typeclasses.Bifunctor
 import arrow.typeclasses.Bitraverse
 import arrow.typeclasses.Eq
 import io.kotlintest.properties.Gen
 import io.kotlintest.properties.forAll
 
 object BitraverseLaws {
-  fun <F> laws(BT: Bitraverse<F>, BF: Bifunctor<F>, cf: (Int) -> Kind2<F, Int, Int>, EQ: Eq<Kind2<F, Int, Int>>): List<Law> =
-    listOf(Law("Bitraverse Laws: Identity") { BT.identityBitraverse(BF, cf, EQ) })
+  fun <F> laws(BT: Bitraverse<F>, cf: (Int) -> Kind2<F, Int, Int>, EQ: Eq<Kind2<F, Int, Int>>): List<Law> =
+    listOf(Law("Bitraverse Laws: Identity") { BT.identityBitraverse(BT, cf, EQ) })
 
-  fun <F> Bitraverse<F>.identityBitraverse(BF: Bifunctor<F>, cf: (Int) -> Kind2<F, Int, Int>, EQ: Eq<Kind2<F, Int, Int>>) =
+  fun <F> Bitraverse<F>.identityBitraverse(BT: Bitraverse<F>, cf: (Int) -> Kind2<F, Int, Int>, EQ: Eq<Kind2<F, Int, Int>>) =
     Id.applicative().run {
       val idApp = this
       forAll(Gen.functionAToB<Int, Kind<ForId, Int>>(Gen.intSmall().map(::Id)),
         Gen.functionAToB<Int, Kind<ForId, Int>>(Gen.intSmall().map(::Id)), Gen.intSmall().map(cf)) { f, g, fa ->
-        fa.bitraverse(idApp, f, g).extract().equalUnderTheLaw(BF.run { fa.bimap({ f(it).extract() }, { g(it).extract() }) }, EQ)
+        fa.bitraverse(idApp, f, g).extract().equalUnderTheLaw(BT.run { fa.bimap({ f(it).extract() }, { g(it).extract() }) }, EQ)
       }
     }
 }
