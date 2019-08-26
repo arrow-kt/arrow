08fb7f315b7e685e32574e21fe482eb6a8b0d20b
i-walker <46971368+i-walker@users.noreply.github.com>
2019-08-26 10:58:05 +0200

    cleanUp

--- modules/core/arrow-core-data/src/main/kotlin/arrow/core/Try.kt
+++ modules/core/arrow-core-data/src/main/kotlin/arrow/core/Try.kt
@@ -7,7 +7,7 @@ typealias Success<A> = Try.Success<A>
 
 @Deprecated(
   "Try will be deleted soon as it promotes eager execution of effects, so it’s better if you work with Either’s suspend constructors or a an effect handler like IO",
-  ReplaceWith("Either<Nothing, A>")
+  ReplaceWith("Either<Throwable, A>")
 )
 @higherkind
 sealed class Try<out A> : TryOf<A> {
