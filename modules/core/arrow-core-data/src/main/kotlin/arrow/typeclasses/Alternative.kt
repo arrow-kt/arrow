e87b5df954e0e2b79ae081e445880f4345ad42ee
Cotel <miguelcoletomunoz@hotmail.com>
2019-10-01 14:43:36 +0200

    Adding some, many and alt functions to Alternative and instances

--- modules/core/arrow-core-data/src/main/kotlin/arrow/typeclasses/Alternative.kt
+++ modules/core/arrow-core-data/src/main/kotlin/arrow/typeclasses/Alternative.kt
@@ -1,6 +1,16 @@
 package arrow.typeclasses
 
+import arrow.Kind
+import arrow.core.ListK
+import arrow.core.k
+
 /**
  * ank_macro_hierarchy(arrow.typeclasses.Alternative)
  */
-interface Alternative<F> : Applicative<F>, MonoidK<F>
+interface Alternative<F> : Applicative<F>, MonoidK<F> {
+  fun <A> Kind<F, A>.some(): Kind<F, ListK<A>> = map(this, many()) { (v, vs) -> (listOf(v) + vs).k() }
+
+  fun <A> Kind<F, A>.many(): Kind<F, ListK<A>> = some() alt just(emptyList<A>().k())
+
+  infix fun <A> Kind<F, A>.alt(b: Kind<F, A>): Kind<F, A>
+}
