3c0f040d10cfb2f763cdf89b69c73a30fcc4ee47
franciscodr <francisco.d@47deg.com>
2019-10-01 16:11:35 +0200

    Uses Eq typeclass in validation NonZero class

--- modules/validation/arrow-validation/src/main/kotlin/arrow/validation/refinedTypes/numeric/NonZero.kt
+++ modules/validation/arrow-validation/src/main/kotlin/arrow/validation/refinedTypes/numeric/NonZero.kt
@@ -11,17 +11,20 @@ import arrow.core.extensions.either.applicativeError.applicativeError
 import arrow.core.extensions.nonemptylist.semigroup.semigroup
 import arrow.core.extensions.validated.applicativeError.applicativeError
 import arrow.typeclasses.ApplicativeError
+import arrow.typeclasses.Eq
 import arrow.validation.RefinedPredicateException
 import arrow.validation.Refinement
 
-internal fun <A : Number> isNonZero(a: A): Boolean = a != 0
+internal fun <A : Number> isNonZero(EQ: Eq<A>, a: A): Boolean =
+  EQ.run { a.neqv(zero()) }
 
 /**
  * `NonZero` defines a subset of Numbers which are not 0
  */
 interface NonZero<F, A : Number> : Refinement<F, A> {
+  fun EQ(): Eq<A>
 
-  override fun A.refinement(): Boolean = isNonZero(this)
+  override fun A.refinement(): Boolean = isNonZero(EQ(), this)
 
   /**
    * Commented method or class
@@ -52,6 +55,8 @@ interface NonZero<F, A : Number> : Refinement<F, A> {
 @extension
 interface ValidatedNonZero<A : Number> :
   NonZero<ValidatedPartialOf<Nel<RefinedPredicateException>>, A> {
+  override fun EQ(): Eq<A>
+
   override fun applicativeError(): ApplicativeError<ValidatedPartialOf<Nel<RefinedPredicateException>>,
     Nel<RefinedPredicateException>> =
     Validated.applicativeError(Nel.semigroup())
@@ -62,6 +67,8 @@ interface ValidatedNonZero<A : Number> :
 @extension
 interface EitherNonZero<A : Number> :
   NonZero<EitherPartialOf<Nel<RefinedPredicateException>>, A> {
+  override fun EQ(): Eq<A>
+
   override fun applicativeError(): ApplicativeError<EitherPartialOf<Nel<RefinedPredicateException>>,
     Nel<RefinedPredicateException>> =
     Either.applicativeError()
