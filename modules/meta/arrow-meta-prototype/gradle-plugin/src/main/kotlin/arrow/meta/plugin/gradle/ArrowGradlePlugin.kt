031d6f6f37269f155cf751fb511d4ab1afd41163
i-walker <46971368+i-walker@users.noreply.github.com>
2019-10-04 15:34:10 +0200

    adds the compiler-plugin automatically

--- modules/meta/arrow-meta-prototype/gradle-plugin/src/main/kotlin/arrow/meta/plugin/gradle/ArrowGradlePlugin.kt
+++ modules/meta/arrow-meta-prototype/gradle-plugin/src/main/kotlin/arrow/meta/plugin/gradle/ArrowGradlePlugin.kt
@@ -2,10 +2,7 @@ package arrow.meta.plugin.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.jetbrains.kotlin.gradle.tasks.AbstractKotlinCompileTool
-import java.io.File
-import java.util.*
-
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 /**
  * The project-level Gradle plugin behavior that is used specifying the plugin's configuration through the
@@ -23,65 +20,10 @@ class ArrowGradlePlugin : Plugin<Project> {
 
   override fun apply(project: Project): Unit {
     project.extensions.create("arrow", ArrowExtension::class.java)
-    project.tasks.withType(AbstractKotlinCompileTool::class.java).all {
-      (it.defaultSerializedCompilerArguments as ArrayList) + File("")
-      println(it.compilerClasspath)
-    }
-
-    /*project.afterEvaluate { p ->
-      if (isEnabled(p)) {
-        project.co
-        tasks.withType(JavaExec::class.java).all {
-          it.setArgsString("-Xplugin=${project.rootDir}/modules/meta/arrow-meta-prototype/compiler-plugin/build/libs/compiler-plugin.jar")
-        }
-      }
-    }*/
-  }
-  /*project.pluginManager.withPlugin("arrow.meta.gradle.plugin") {
-    val kExtension = checkNotNull(project.extensions.findByType(KotlinJvmProjectExtension::class.java))
-    kExtension.target {
-
-    }
-  }
-  project.tasks.withType(AbstractKotlinCompile::class.java).all {
-  }
-  project.tasks.withType(KotlinCompile::class.java).all {
-    val mutableList = mutableListOf(it.kotlinOptions.freeCompilerArgs, listOf())
-    //it.kotlinOptions.freeCompilerArgs.apply {
-    //val a = this + "-Xplugin=${project.rootDir}/modules/meta/arrow-meta-prototype/compiler-plugin/build/libs/compiler-plugin.jar"
-    //it.kotlinOptions.freeCompilerArgs + "BLA"
-    //a
-    it.kotlinOptions.copyFreeCompilerArgsToArgs(
-      objec
-    )
-    println(it.kotlinOptions.freeCompilerArgs)
-  }*/
-
-  /*project.afterEvaluate { p ->
-    if (isEnabled(p)) {
-      println("Add compiler-plugin to freeCompilerArgs")
-      p.tasks.withType(KotlinCompile::class.java).all {
-        it.kotlinOptions.freeCompilerArgs.run {
-          this + "-Xplugin=${project.rootDir.toString() + "/modules/meta/arrow-meta-prototype/compiler-plugin/build/libs/compiler-plugin.jar"}"
-        }
+    project.afterEvaluate { p ->
+      p.tasks.withType(KotlinCompile::class.java).configureEach {
+        it.kotlinOptions.freeCompilerArgs += "-Xplugin=${project.rootDir}/modules/meta/arrow-meta-prototype/compiler-plugin/build/libs/compiler-plugin.jar"
       }
     }
-  }*/
-/*project.pluginManager.withPlugin("arrow.meta.plugin.gradle") {
-  val kExtension: KotlinProjectExtension = checkNotNull(project.extensions.findByType(KotlinProjectExtension::class.java))
-  val gradlePlugin: ArrowGradlePlugin = project.extensions.create("Arrow_Plugin", ArrowGradlePlugin::class.java, project.objects)
-  project.configureDefaults(gradlePlugin, kExtension)
-}*/
-// println("Try add the Plugin")
-// project.addGradleDependency(PLUGIN_CLASSPATH_CONFIGURATION_NAME, ArrowKotlinGradleSubplugin().getPluginArtifact())
-//}
+  }
 }
-
-/*fun Project.configureDefaults(gradlePlugin: ArrowGradlePlugin, kExtension: KotlinProjectExtension): Unit? =
-  !gradlePlugin.en*/
-
-/*fun Project.addGradleDependency(configuration: String, artifact: SubpluginArtifact): Unit {
-  val artifactVersion = artifact.version ?: "0.0.1"
-  val gradleCoordinate = "${artifact.groupId}:${artifact.artifactId}:$artifactVersion"
-  project.dependencies.add(configuration, gradleCoordinate)
-}*/
