4cacf3d511bb177e77774f40c3f2283fe3e18532
Paco <pakoito@users.noreply.github.com>
2019-10-09 12:24:29 +0100

    Add easy constructor for FunctionK

--- modules/core/arrow-core-data/src/main/kotlin/arrow/core/FunctionK.kt
+++ modules/core/arrow-core-data/src/main/kotlin/arrow/core/FunctionK.kt
@@ -13,5 +13,9 @@ interface FunctionK<F, G> {
     fun <F> id(): FunctionK<F, F> = object : FunctionK<F, F> {
       override fun <A> invoke(fa: Kind<F, A>): Kind<F, A> = fa
     }
+    
+    operator fun <F, G> invoke(f: (Kind<F, A>) -> Kind<G, A>): FunctionK<F, G> = object : FunctionK<F, G> {
+      override fun <A> invoke(fa: Kind<F, A>): Kind<G, A> = f(fa)
+    }
   }
 }
