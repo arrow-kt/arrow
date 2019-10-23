f93651fe1d63494c8041a084a05c7fc39eecaff7
Amanda Hinchman-Dominguez <ahinchman1@gmail.com>
2019-10-23 08:57:47 -0500

    pair programming session - still need to generate simpleIrType to feed into the new IrCall

--- modules/meta/arrow-meta-prototype/compiler-plugin/src/main/kotlin/arrow/meta/plugins/dummy/EqOperatorPlugin.kt
+++ modules/meta/arrow-meta-prototype/compiler-plugin/src/main/kotlin/arrow/meta/plugins/dummy/EqOperatorPlugin.kt
@@ -4,17 +4,23 @@ import arrow.meta.Meta
 import arrow.meta.Plugin
 import arrow.meta.invoke
 import arrow.meta.phases.codegen.ir.IrUtils
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+import org.jetbrains.kotlin.descriptors.PropertyDescriptor
+import org.jetbrains.kotlin.descriptors.resolveClassByFqName
+import org.jetbrains.kotlin.incremental.components.NoLookupLocation
 import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
+import org.jetbrains.kotlin.ir.descriptors.IrSimpleBuiltinOperatorDescriptorImpl
 import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.expressions.impl.IrCallImpl
 import org.jetbrains.kotlin.ir.symbols.IrFunctionSymbol
-import org.jetbrains.kotlin.ir.types.IrType
-import org.jetbrains.kotlin.ir.types.impl.IrSimpleTypeImpl
 import org.jetbrains.kotlin.ir.util.referenceFunction
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
-import org.jetbrains.kotlin.types.KotlinType
+import org.jetbrains.kotlin.resolve.descriptorUtil.module
+import org.jetbrains.kotlin.types.replace
+import org.jetbrains.kotlin.types.typeUtil.asTypeProjection
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 const val EQEQ = "EQEQ"
 
@@ -22,46 +28,43 @@ val Meta.eq: Plugin
   get() =
     "Eq" {
       meta(
+        /**
+         * Need to create a new kotlintype that is of Eq with type arguments A for whatever A is in your comparison with  ==
+         *
+         * If the member descriptor fqNameSafe matches EQEQ, then:
+         * 1. Find the type of the descriptor's arguments to use in new IrSimpleType
+         * 2. Create IrSimpleType for Arrow.Eq
+         * 3. Get the EQ descriptor from the external symbol table. Create a new descriptor with new IrType "Eq<A>"
+         *    ^^ Wait, why is the EQ descriptor needed
+         * 4. Replace descriptor of the current IrCall with new descriptor
+         */
         irFunctionAccess {
-          /**
-           * Need to create a new kotlintype that is of Eq with type arguments A for whatever A is in your comparison with  ==
-           *
-           * If the member descriptor fqNameSafe matches EQEQ, then:
-           * 1. Find the type of the descriptor's arguments to use in new IrSimpleType
-           * 2. Create IrSimpleType for Arrow.Eq
-           * 3. Get the EQ descriptor from the external symbol table. Create a new descriptor with new IrType "Eq<A>"
-           *    ^^ Wait, why is the EQ descriptor needed
-           * 4. Replace descriptor of the current IrCall with new descriptor
-           */
-          if (it.descriptor.fqNameSafe == FqName("kotlin.ir.expressions.IrStatementOrigin.$EQEQ")) {
-            val kotlinType: KotlinType = it.descriptor.typeParameters.first().defaultType // making a terrible assumption the types match
-            // ^ can be used for the meta dsl
-            val irType: IrType? = it.getValueArgument(0)?.type
-            val eqDescriptor : FunctionDescriptor = it.descriptor
-
-
-            val newEqDescriptor: FunctionDescriptor = it.descriptor.newCopyBuilder()
-              .setDescriptor(       // create IrSimpleType for Arrow.Eq - Eq<A>
-              IrSimpleTypeImpl(
-                classifier = irType?.classifier.symbol, // can you downcast irType to irSimpleType?
-                hasQuestionMark = false,
-                arguments = arguments.map {
-                  // need more serious consideration for what the arguments are for here
-                },
-                annotations = emptyList()
-              ).descriptor
-            ).build()
-
-            val result = compilerContext.findExtension(kotlinType)
-
-            // change IrCall
-            return irCall( it.descriptor, newEqDescriptor)
+          if (it.descriptor.name.asString().contains(EQEQ)) {
+            it.descriptor.safeAs<IrSimpleBuiltinOperatorDescriptorImpl>()?.let { operator ->
+              val first = operator.valueParameters[0]
+              val eqTypeArg = first.type
+              val module = it.descriptor.module
+              val eqClassDescriptor = module.resolveClassByFqName(FqName("arrow.typeclasses.Eq"), NoLookupLocation.FROM_BACKEND)
+              val extensionType = eqClassDescriptor?.defaultType?.replace(newArguments = listOf(eqTypeArg.asTypeProjection()))
+              val typeClassFactory = extensionType?.let { type ->
+                findExtension(type)
+              }
+              val call = when (val extension = typeClassFactory) {
+                is FunctionDescriptor -> extension.irCall()
+                is ClassDescriptor -> extension.irConstructorCall()
+                is PropertyDescriptor -> extension.irGetterCall()
+                else -> null
+              }
+              // eqInt . eqv(a, b)
+              // eqInt.run { a.eqv(b) }
+              println("call: $call")
+            }
+            println("Found EQEQ: ${it.descriptor.fqNameSafe}")
           }
-          else it
+          it
         }
       )
     }
-}
 
 fun IrUtils.irCall(oldEqDescriptor: FunctionDescriptor, newEqDescriptor: FunctionDescriptor): IrCall {
   val irFunctionSymbol: IrFunctionSymbol = backendContext.ir.symbols.externalSymbolTable.referenceFunction(oldEqDescriptor)
