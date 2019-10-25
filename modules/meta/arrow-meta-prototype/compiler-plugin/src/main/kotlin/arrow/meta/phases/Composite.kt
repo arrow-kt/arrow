fcea26cb5a8658fa92b6814fc2425b7e4273e6e7
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2019-10-25 15:46:03 +0200

    Remove arrow-meta-prototype
    Move to another repository temporarily.

--- modules/meta/arrow-meta-prototype/compiler-plugin/src/main/kotlin/arrow/meta/phases/Composite.kt
+++
@@ -1,8 +0,0 @@
-package arrow.meta.phases
-
-data class Composite(val phases: List<ExtensionPhase>): ExtensionPhase {
-  companion object {
-    operator fun invoke(vararg phases: ExtensionPhase): ExtensionPhase =
-      Composite(phases.toList())
-  }
-}
