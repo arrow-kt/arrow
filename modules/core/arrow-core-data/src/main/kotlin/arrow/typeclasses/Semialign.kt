ea411b9929fe82847c93cd2b7708d2bcb164058b
Alphonse Bendt <alphonse.bendt@gmail.com>
2019-11-16 12:25:08 +0100

    fix doc

--- modules/core/arrow-core-data/src/main/kotlin/arrow/typeclasses/Semialign.kt
+++ modules/core/arrow-core-data/src/main/kotlin/arrow/typeclasses/Semialign.kt
@@ -13,11 +13,12 @@ interface Semialign<F> : Functor<F> {
   /**
    * Combines two structures by taking the union of their shapes and using Ior to hold the elements.
    *
-   *  * {: data-executable='true'}
+   *  {: data-executable='true'}
    *
    * ```kotlin:ank
    * import arrow.core.extensions.*
    * import arrow.core.extensions.listk.semialign.semialign
+   * import arrow.core.*
    *
    * fun main(args: Array<String>) {
    *   //sampleStart
@@ -34,11 +35,12 @@ interface Semialign<F> : Functor<F> {
   /**
    * Combines two structures by taking the union of their shapes and combining the elements with the given function.
    *
-   *    *  * {: data-executable='true'}
+   * {: data-executable='true'}
    *
    * ```kotlin:ank
    * import arrow.core.extensions.*
    * import arrow.core.extensions.listk.semialign.semialign
+   * import arrow.core.*
    *
    * fun main(args: Array<String>) {
    *   //sampleStart
