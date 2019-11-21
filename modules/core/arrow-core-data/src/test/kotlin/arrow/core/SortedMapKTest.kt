d529abc47c6b9b4c5de02b937d4858b0681e2fba
Alphonse Bendt <alphonse.bendt@gmail.com>
2019-11-21 11:50:38 +0100

    add test for proper alignment of maps

--- modules/core/arrow-core-data/src/test/kotlin/arrow/core/SortedMapKTest.kt
+++ modules/core/arrow-core-data/src/test/kotlin/arrow/core/SortedMapKTest.kt
@@ -22,6 +22,7 @@ import arrow.test.laws.TraverseLaws
 import arrow.typeclasses.Eq
 import arrow.typeclasses.EqK
 import io.kotlintest.properties.Gen
+import io.kotlintest.properties.forAll
 
 class SortedMapKTest : UnitSpec() {
 
@@ -50,5 +51,48 @@ class SortedMapKTest : UnitSpec() {
         EQK,
         SortedMapK.foldable<String>()
       ))
+
+    "can align maps" {
+      val gen = Gen.sortedMapK(Gen.string(), Gen.bool())
+
+      // aligned keySet is union of a's and b's keys
+      forAll(gen, gen) { a, b ->
+        SortedMapK.semialign<String>().run {
+          val aligned = align(a, b).fix()
+
+          aligned.size == (a.keys + b.keys).size
+        }
+      }
+
+      // aligned map contains Both for all entries existing in a and b
+      forAll(gen, gen) { a, b ->
+        SortedMapK.semialign<String>().run {
+          val aligned = align(a, b).fix()
+          a.keys.intersect(b.keys).all {
+            aligned[it]?.isBoth ?: false
+          }
+        }
+      }
+
+      // aligned map contains Left for all entries existing only in a
+      forAll(gen, gen) { a, b ->
+        SortedMapK.semialign<String>().run {
+          val aligned = align(a, b).fix()
+          (a.keys - b.keys).all { key ->
+            aligned[key]?.let { it.isLeft } ?: false
+          }
+        }
+      }
+
+      // aligned map contains Right for all entries existing only in b
+      forAll(gen, gen) { a, b ->
+        SortedMapK.semialign<String>().run {
+          val aligned = align(a, b).fix()
+          (b.keys - a.keys).all { key ->
+            aligned[key]?.let { it.isRight } ?: false
+          }
+        }
+      }
+    }
   }
 }
