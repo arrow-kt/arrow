e4be1bed61ee0dc607480adfa24ad96a9f4a50e3
Alphonse Bendt <alphonse.bendt@gmail.com>
2019-11-24 16:33:20 +0100

    add EqK instance for MapK

--- modules/core/arrow-core-data/src/test/kotlin/arrow/core/MapKTest.kt
+++ modules/core/arrow-core-data/src/test/kotlin/arrow/core/MapKTest.kt
@@ -6,6 +6,7 @@ import arrow.core.extensions.eq
 import arrow.core.extensions.hash
 import arrow.core.extensions.mapk.align.align
 import arrow.core.extensions.mapk.eq.eq
+import arrow.core.extensions.mapk.eqK.eqK
 import arrow.core.extensions.mapk.foldable.foldable
 import arrow.core.extensions.mapk.functor.functor
 import arrow.core.extensions.mapk.functorFilter.functorFilter
@@ -26,7 +27,6 @@ import arrow.test.laws.MonoidLaws
 import arrow.test.laws.ShowLaws
 import arrow.test.laws.TraverseLaws
 import arrow.typeclasses.Eq
-import arrow.typeclasses.EqK
 import io.kotlintest.properties.Gen
 import io.kotlintest.properties.forAll
 
@@ -35,11 +35,6 @@ class MapKTest : UnitSpec() {
   val EQ: Eq<Kind2<ForMapK, String, Int>> = object : Eq<Kind2<ForMapK, String, Int>> {
     override fun Kind2<ForMapK, String, Int>.eqv(b: Kind2<ForMapK, String, Int>): Boolean =
       fix()["key"] == b.fix()["key"]
-  }
-
-  val EQK = object : EqK<MapKPartialOf<String>> {
-    override fun <A> Kind<MapKPartialOf<String>, A>.eqK(other: Kind<MapKPartialOf<String>, A>, EQ: Eq<A>): Boolean =
-      MapK.eq(String.eq(), EQ).run { this@eqK.fix().eqv(other.fix()) }
   }
 
   init {
@@ -55,7 +50,7 @@ class MapKTest : UnitSpec() {
       HashLaws.laws(MapK.hash(String.hash(), Int.hash()), EQ_TC) { mapOf("key" to it).k() },
       AlignLaws.laws(MapK.align(),
         Gen.mapK(Gen.string(), Gen.int()) as Gen<Kind<MapKPartialOf<String>, Int>>,
-        EQK,
+        MapK.eqK(String.eq()),
         MapK.foldable()
       )
     )
