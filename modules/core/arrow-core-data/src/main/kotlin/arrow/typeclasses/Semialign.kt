8cb40eed0da386f5d3d911bb75f11c82b20e5ee8
Alphonse Bendt <alphonse.bendt@gmail.com>
2019-11-27 23:37:03 +0100

    wip add specialized aligns

--- modules/core/arrow-core-data/src/main/kotlin/arrow/typeclasses/Semialign.kt
+++ modules/core/arrow-core-data/src/main/kotlin/arrow/typeclasses/Semialign.kt
@@ -2,7 +2,10 @@ package arrow.typeclasses
 
 import arrow.Kind
 import arrow.core.Ior
+import arrow.core.Option
+import arrow.core.Tuple2
 import arrow.core.identity
+import arrow.core.toT
 
 /**
  * A type class used for aligning of functors with non-uniform shapes.
@@ -55,4 +58,40 @@ interface Semialign<F> : Functor<F> {
    * ```
    */
   fun <A, B, C> alignWith(a: Kind<F, A>, b: Kind<F, B>, fa: (Ior<A, B>) -> C): Kind<F, C> = align(a, b).map(fa)
+
+  /**
+   * aligns two structures and combine with the given Semigroup
+   */
+  fun <A> Kind<F, A>.salign(
+    SG: Semigroup<A>,
+    other: Kind<F, A>
+  ): Kind<F, A> =
+    alignWith(this, other) {
+      it.fold(::identity, ::identity) { a, b ->
+        SG.run { a.combine(b) }
+      }
+    }
+
+  /**
+   * Align two structures as in zip, but filling in blanks with None.
+   */
+  fun <A, B> Kind<F, A>.padZip(
+    other: Kind<F, B>
+  ): Kind<F, Tuple2<Option<A>, Option<B>>> =
+    alignWith(this, other) { ior ->
+      when (val e = ior.bimap({ Option.just(it) }, { Option.just(it) })) {
+        is Ior.Right -> Option.empty<A>() toT e.value
+        is Ior.Left -> e.value toT Option.empty()
+        is Ior.Both -> e.leftValue toT e.rightValue
+      }
+    }
+
+  /**
+   * Align two structures as in zipWith, but filling in blanks with None.
+   */
+  fun <A, B, C> Kind<F, A>.padZipWith(
+    other: Kind<F, B>,
+    fa: (Option<A>, Option<B>) -> C
+  ): Kind<F, C> =
+    padZip(other).map { fa(it.a, it.b) }
 }
