7c20ea105277723aa290097dc8eddad8e1bc2e4b
Alpha Ho <i.alphaho@gmail.com>
2019-12-24 17:55:55 +0800

    added NonEmptyListModule to arrow-integrations-jackson-module for serialize/deserialize NonEmptyList through Jackson #1192

---
+++ modules/integrations/arrow-integrations-jackson-module/src/test/kotlin/arrow/integrations/jackson/module/NonEmptyListModuleTest.kt
@@ -0,0 +1,41 @@
+package arrow.integrations.jackson.module
+
+import arrow.core.Nel
+import arrow.syntax.function.pipe
+import arrow.test.UnitSpec
+import arrow.test.generators.nonEmptyList
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.kotlintest.assertions.json.shouldMatchJson
+import io.kotlintest.properties.Gen
+import io.kotlintest.properties.assertAll
+import io.kotlintest.shouldBe
+
+class NonEmptyListModuleTest : UnitSpec() {
+  private val mapper = ObjectMapper().registerModule(NonEmptyListModule).registerKotlinModule()
+
+  init {
+    "serializing NonEmptyList should be the same as serializing the underlying list" {
+      assertAll(Gen.nonEmptyList(Gen.someObject())) { list ->
+        val actual = mapper.writeValueAsString(list)
+        val expected = mapper.writeValueAsString(list.all)
+
+        actual.shouldMatchJson(expected)
+      }
+    }
+
+    "serializing NonEmptyList and then deserialize it should be the same as before the deserialization" {
+      assertAll(Gen.nonEmptyList(Gen.someObject())) { list ->
+        val roundTripped = mapper.writeValueAsString(list).pipe { mapper.readValue<Nel<SomeObject>>(it) }
+
+        roundTripped shouldBe list
+      }
+    }
+  }
+}
+
+private data class SomeObject(val someString: String, val someInt: Int)
+
+private fun Gen.Companion.someObject(): Gen<SomeObject> =
+  bind(string(), int()) { str, int -> SomeObject(str, int) }
