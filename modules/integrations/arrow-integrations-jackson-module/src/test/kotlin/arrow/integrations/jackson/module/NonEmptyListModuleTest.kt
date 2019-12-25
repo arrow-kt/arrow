695faf8b3fb971dd3310bb45a8ae361b3aebc64b
Alpha Ho <i.alphaho@gmail.com>
2019-12-25 12:26:50 +0800

    updated NonEmptyListModuleTest and OptionModuleTest to cover primitive content types as well

--- modules/integrations/arrow-integrations-jackson-module/src/test/kotlin/arrow/integrations/jackson/module/NonEmptyListModuleTest.kt
+++ modules/integrations/arrow-integrations-jackson-module/src/test/kotlin/arrow/integrations/jackson/module/NonEmptyListModuleTest.kt
@@ -5,7 +5,7 @@ import arrow.syntax.function.pipe
 import arrow.test.UnitSpec
 import arrow.test.generators.nonEmptyList
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.kotlintest.assertions.json.shouldMatchJson
 import io.kotlintest.properties.Gen
@@ -17,7 +17,7 @@ class NonEmptyListModuleTest : UnitSpec() {
 
   init {
     "serializing NonEmptyList should be the same as serializing the underlying list" {
-      assertAll(Gen.nonEmptyList(Gen.someObject())) { list ->
+      assertAll(Gen.nonEmptyList(Gen.oneOf(Gen.someObject(), Gen.int(), Gen.string(), Gen.bool()))) { list ->
         val actual = mapper.writeValueAsString(list)
         val expected = mapper.writeValueAsString(list.all)
 
@@ -26,8 +26,15 @@ class NonEmptyListModuleTest : UnitSpec() {
     }
 
     "serializing NonEmptyList and then deserialize it should be the same as before the deserialization" {
-      assertAll(Gen.nonEmptyList(Gen.someObject())) { list ->
-        val roundTripped = mapper.writeValueAsString(list).pipe { mapper.readValue<Nel<SomeObject>>(it) }
+      assertAll(
+        Gen.oneOf(
+          Gen.nonEmptyList(Gen.someObject()).map { it to jacksonTypeRef<Nel<SomeObject>>() },
+          Gen.nonEmptyList(Gen.int()).map { it to jacksonTypeRef<Nel<Int>>() },
+          Gen.nonEmptyList(Gen.string()).map { it to jacksonTypeRef<Nel<String>>() },
+          Gen.nonEmptyList(Gen.bool()).map { it to jacksonTypeRef<Nel<Boolean>>() }
+        )
+      ) { (list, typeReference) ->
+        val roundTripped = mapper.writeValueAsString(list).pipe { mapper.readValue<Nel<*>>(it, typeReference) }
 
         roundTripped shouldBe list
       }
