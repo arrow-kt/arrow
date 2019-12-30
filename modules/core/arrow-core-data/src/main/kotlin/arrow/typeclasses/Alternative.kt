8831983fcc81dac319f656634385363731a560c1
Jannis <overesch.jannis@gmail.com>
2019-12-30 15:05:13 +0100

    Add some convenience methods to alternative

--- modules/core/arrow-core-data/src/main/kotlin/arrow/typeclasses/Alternative.kt
+++ modules/core/arrow-core-data/src/main/kotlin/arrow/typeclasses/Alternative.kt
@@ -1,7 +1,13 @@
 package arrow.typeclasses
 
 import arrow.Kind
+import arrow.core.Eval
+import arrow.core.ForListK
+import arrow.core.None
+import arrow.core.Option
 import arrow.core.SequenceK
+import arrow.core.Some
+import arrow.core.fix
 import arrow.core.k
 
 /**
@@ -48,4 +54,32 @@ interface Alternative<F> : Applicative<F>, MonoidK<F> {
   fun <A> Kind<F, A>.orElse(b: Kind<F, A>): Kind<F, A>
 
   override fun <A> Kind<F, A>.combineK(y: Kind<F, A>): Kind<F, A> = orElse(y)
+
+  /**
+   * Wraps the result in an optional. This never fails.
+   * @receiver computation to execute
+   * @return Option with either the result or none in case it failed
+   */
+  fun <A> Kind<F, A>.optional(): Kind<F, Option<A>> = map(::Some).orElse(just(None))
+
+  /**
+   * Lazy or else, useful when traversing a structure with asum which short circuits on success. In general this should be implemented
+   *  for every Alternative that models success and failure.
+   */
+  fun <A> Kind<F, A>.lazyOrElse(b: () -> Kind<F, A>): Kind<F, A> = orElse(b())
+
+  fun <T, A> Kind<T, Kind<F, A>>.asum(FT: Foldable<T>): Kind<F, A> = FT.run {
+    foldRight(Eval.now(empty<A>())) { v, acc ->
+      Eval.later { v.lazyOrElse { acc.value() } }
+    }.value()
+  }
+
+  fun <T, A> Kind<T, A>.afold(FT: Foldable<T>): Kind<F, A> = FT.run { toList().afromList() }
+
+  fun <A> List<A>.afromList(): Kind<F, A> = map { just(it) }.k().asum(object: Foldable<ForListK> {
+    override fun <A, B> Kind<ForListK, A>.foldLeft(b: B, f: (B, A) -> B): B = fix().foldLeft(b, f)
+    override fun <A, B> Kind<ForListK, A>.foldRight(lb: Eval<B>, f: (A, Eval<B>) -> Eval<B>): Eval<B> = fix().foldRight(lb, f)
+  })
+
+  fun <A> Option<A>.afromOption(): Kind<F, A> = fold({ empty() }, { just(it) })
 }
