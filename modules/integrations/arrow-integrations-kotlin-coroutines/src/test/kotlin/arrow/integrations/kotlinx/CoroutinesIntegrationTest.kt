61c78cadbed14c8d10d008a134517dd26dbf536d
Alberto Ballano <aballano@users.noreply.github.com>
2020-01-17 16:45:29 +0100

    Added temporary coroutines test to verify behaviour against

--- modules/integrations/arrow-integrations-kotlin-coroutines/src/test/kotlin/arrow/integrations/kotlinx/CoroutinesIntegrationTest.kt
+++ modules/integrations/arrow-integrations-kotlin-coroutines/src/test/kotlin/arrow/integrations/kotlinx/CoroutinesIntegrationTest.kt
@@ -1,18 +1,24 @@
 package arrow.integrations.kotlinx
 
-import arrow.core.Left
 import arrow.fx.IO
 import arrow.fx.extensions.fx
 import arrow.fx.extensions.io.dispatchers.dispatchers
 import arrow.fx.typeclasses.seconds
 import arrow.test.UnitSpec
-import arrow.test.generators.throwable
+import io.kotlintest.fail
 import io.kotlintest.properties.Gen
 import io.kotlintest.properties.forAll
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.coroutines.EmptyCoroutineContext
 
 class CoroutinesIntegrationTest : UnitSpec() {
+
+  class MyException : Exception()
+
   init {
     "scope cancellation should cancel given IO" {
       // TODO currently failing with more iterations
@@ -32,16 +38,37 @@ class CoroutinesIntegrationTest : UnitSpec() {
       }
     }
 
-    "given IO when exception occurs should return Either.Left" {
-      forAll(Gen.throwable()) { error ->
-        IO.fx {
-          val scope = CoroutineScope(IO.dispatchers().default())
-          !effect {
-            scope.launchIO {
-              IO { throw error }.unsafeRunSync()
-            }
+    // TODO currently behaves the same way as coroutines
+    "should rethrow exceptions within launchIO" {
+      val exception = MyException()
+      try {
+        coroutineScope {
+          launchIO {
+            IO { throw exception }
+            fail("Should rethrow the exception")
           }
-        }.attempt().unsafeRunSync() == Left(error)
+        }
+      } catch (myException: MyException) {
+        // Success
+      } catch (throwable: Throwable) {
+        fail("Should only throw MyException but was $throwable")
+      }
+    }
+
+    // DELETE
+    "coroutines test" {
+      val exception = MyException()
+      try {
+        coroutineScope {
+          launch {
+            suspend { throw exception }
+            fail("Should rethrow the exception")
+          }
+        }
+      } catch (myException: MyException) {
+        // Success
+      } catch (throwable: Throwable) {
+        fail("Should only throw MyException but was $throwable")
       }
     }
   }
