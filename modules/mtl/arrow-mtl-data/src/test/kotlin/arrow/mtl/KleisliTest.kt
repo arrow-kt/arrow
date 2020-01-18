90443a668d485dbe62f77d3fb5d93d0be10de05a
Jannis <overesch.jannis@gmail.com>
2020-01-18 18:36:10 +0100

    Fix kleisli test

--- modules/mtl/arrow-mtl-data/src/test/kotlin/arrow/mtl/KleisliTest.kt
+++ modules/mtl/arrow-mtl-data/src/test/kotlin/arrow/mtl/KleisliTest.kt
@@ -104,9 +104,9 @@ class KleisliTest : UnitSpec() {
     "andThen should continue sequence" {
       val kleisli: Kleisli<ForId, Int, Int> = Kleisli { a: Int -> Id(a) }
 
-      kleisli.andThen(Id.monad(), Id(3)).run(0) shouldBe 3
+      kleisli.andThen(Id.monad(), Id(3)).run(0) shouldBe Id(3)
 
-      kleisli.andThen(Id.monad()) { b -> Id(b + 1) }.run(0) shouldBe 1
+      kleisli.andThen(Id.monad()) { b -> Id(b + 1) }.run(0) shouldBe Id(1)
     }
   }
 }
