78865d9c1c29d8006fe41d132a821efa101f702b
Alphonse Bendt <alphonse.bendt@gmail.com>
2020-01-19 13:10:46 +0100

    implement Accum on top of AccumT

---
+++ modules/mtl/arrow-mtl-data/src/main/kotlin/arrow/mtl/Accum.kt
@@ -0,0 +1,33 @@
+package arrow.mtl
+
+import arrow.core.ForId
+import arrow.core.Id
+import arrow.core.Tuple2
+import arrow.core.extensions.id.applicative.applicative
+import arrow.core.extensions.id.monad.monad
+import arrow.core.fix
+import arrow.typeclasses.Monad
+
+typealias Accum<S, A> = AccumT<S, ForId, A>
+
+typealias AccumFun<S, A> = AccumTFun<S, ForId, A>
+
+fun <S, F, A> accum(MF: Monad<F>, f: (S) -> Tuple2<S, A>): AccumT<S, F, A> =
+  AccumT(MF) {
+    MF.just(f(it))
+  }
+
+fun <S, A> Accum<S, A>.runAccum(s: S): Tuple2<S, A> =
+  runAccumT(Id.monad(), s).fix().extract()
+
+fun <S, A> Accum<S, A>.execAccum(s: S): S =
+  execAccumT(Id.monad(), s).fix().extract()
+
+
+fun <S, A> Accum<S, A>.evalAccum(s: S): A =
+  evalAccumT(Id.monad(), s).fix().extract()
+
+fun <S, A, B> Accum<S, A>.mapAccum(f: (Tuple2<S, A>) -> Tuple2<S, B>): Accum<S, B> =
+  mapAccumT(Id.monad(), Id.applicative()) { Id.just(f(it.fix().extract())) }
+
+
