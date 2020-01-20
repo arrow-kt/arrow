aea084e0a81ed755c0023d8ee935801817262887
Alberto Ballano <aballano@users.noreply.github.com>
2020-01-20 17:23:06 +0100

    Formalize suspendCancellable extension to IOOf instead of IO

--- modules/integrations/arrow-integrations-kotlin-coroutines/src/main/kotlin/arrow/integrations/kotlinx/extensions.kt
+++ modules/integrations/arrow-integrations-kotlin-coroutines/src/main/kotlin/arrow/integrations/kotlinx/extensions.kt
@@ -1,7 +1,6 @@
 package arrow.integrations.kotlinx
 
 import arrow.core.Either
-import arrow.fx.IO
 import arrow.fx.IOOf
 import arrow.fx.OnCancel
 import arrow.fx.fix
@@ -14,8 +13,8 @@ import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
-suspend fun <A> IO<A>.suspendCancellable() = suspendCancellableCoroutine<A> { cont ->
-  val disposable = this.unsafeRunAsyncCancellable(OnCancel.ThrowCancellationException) { result ->
+suspend fun <A> IOOf<A>.suspendCancellable() = suspendCancellableCoroutine<A> { cont ->
+  val disposable = fix().unsafeRunAsyncCancellable(OnCancel.ThrowCancellationException) { result ->
     result.fold(cont::resumeWithException) { cont.resume(it) }
   }
 
