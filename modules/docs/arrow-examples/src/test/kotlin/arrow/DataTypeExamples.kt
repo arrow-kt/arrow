30225952b6c0daafbe1d36308c67f80f35b9511b
Kyle Goetz <kylegoetz@gmail.com>
2020-01-22 13:58:03 -0600

    DOCS: Examples - remove Try or replace Try with Either/IO/Option where appropriate

--- modules/docs/arrow-examples/src/test/kotlin/arrow/DataTypeExamples.kt
+++ modules/docs/arrow-examples/src/test/kotlin/arrow/DataTypeExamples.kt
@@ -12,12 +12,11 @@ import arrow.core.Option
 import arrow.core.Right
 import arrow.core.Some
 import arrow.core.Success
-import arrow.core.Try
-import arrow.core.TryException
 import arrow.core.Tuple3
 import arrow.core.getOrElse
-import arrow.core.extensions.`try`.applicative.applicative
-import arrow.core.extensions.`try`.functor.functor
+import arrow.core.extensions.either.applicative.applicative
+import arrow.core.extensions.either.applicativeError.handleError
+import arrow.core.extensions.either.functor.functor
 import arrow.core.extensions.fx
 import arrow.core.extensions.option.applicative.applicative
 import arrow.core.flatMap
@@ -98,73 +97,9 @@ class DataTypeExamples : FreeSpec() { init {
     }
   }
 
-  // http://arrow-kt.io/docs/apidocs/arrow-core-data/arrow.core/-try/
-  "Try and recover" - {
-
-    "Old school" {
-
-      val dollars = try {
-        playLottery(9)
-      } catch (e: AuthorizationException) {
-        0
-      }
-      dollars shouldBe 0
-    }
-
-    "Try { .. } " {
-      val gain: Try<Int> = Try { playLottery(9) }
-
-      gain shouldBe aFailureOfType(AuthorizationException::class)
-
-      gain.getOrElse { 0 } shouldBe 0
-    }
-
-    "filter" {
-      // If you want to perform a check on a possible success,
-      // you can use filter to convert successful computations in failures if conditions aren’t met:
-      val tryJackpot = Try { playLottery(10) }.filter { it > 500 }
-      tryJackpot shouldBe aFailureOfType(TryException.PredicateException::class)
-    }
-
-    "Recover" {
-      val gain = Try { playLottery(99) }
-      gain.handleError { 0 } shouldBe Try.Success(0)
-
-      gain.handleErrorWith { Try { playLottery(42) } } shouldBe Try.Success(1000)
-    }
-
-    "Fold" {
-      // When you want to handle both cases of the computation you can use fold.
-      // With fold we provide two functions,
-      // one for transforming a failure into a new value,
-      // the second one to transform the success value into a new one:
-      val gain = Try { playLottery(99) }
-      gain.fold({ 4 }, { error("not expected") }) shouldBe 4
-
-      val jackPot = Try { playLottery(42) }
-      jackPot.fold({ error("not expected") }, { it * 100 }) shouldBe 100_000
-    }
-
-    "Functor" {
-      // Transforming the value, if the computation is a success:
-      val actual = Try.functor().run { Try { "3".toInt() }.map { it + 1 } }
-      actual shouldBe Try.Success(4)
-    }
-
-    "Applicative" {
-      // Computing over independent values:
-      val tryHarder = Try.applicative().tupled(
-        Try { "3".toInt() },
-        Try { "5".toInt() },
-        Try { "nope".toInt() }
-      )
-      tryHarder shouldBe aFailureOfType(NumberFormatException::class)
-    }
-  }
-
   // Either http://arrow.io/docs/apidocs/arrow-core-data/arrow.core/-either/
   "Either left or right" - {
-    fun parse(s: String): ProblemOrInt = Try { Right(s.toInt()) }.getOrElse { Left(invalidInt) }
+    fun parse(s: String): ProblemOrInt = try { Either.right(s.toInt()) } catch(e: Throwable) { Either.left(invalidInt) }
     fun reciprocal(i: Int): Either<Problem, Double> = when (i) {
       0 -> Left(noReciprocal)
       else -> Either.Right(1.0 / i)
@@ -203,17 +138,68 @@ class DataTypeExamples : FreeSpec() { init {
       magic("Not a number") shouldBe Left(invalidInt)
       magic("1") shouldBe Either.right("1.0")
     }
+
+    "Fold" {
+      // When you want to handle both cases of the computation you can use fold.
+      // With fold we provide two functions,
+      // one for transforming a failure into a new value,
+      // the second one to transform the success value into a new one:
+      val gain = Either.catch { playLottery(99) }
+      gain.fold({ 4 }, { error("not expected") }) shouldBe 4
+
+      val jackPot = Either.catch { playLottery(42) }
+      jackPot.fold({ error("not expected") }, { it * 100 }) shouldBe 100_000
+    }
+
+    "Functor" {
+      // Transforming the value, if the computation is a success:
+      val actual = Either.functor<Int>().run { (try { Either.right("3".toInt()) } catch(e: Throwable) { Either.left(e) }).map { it + 1 } }
+      actual shouldBe Either.Right(4)
+    }
+
+    "Applicative" {
+      // Computing over independent values:
+      val tryHarder = Either.applicative<Throwable>().tupled(Either.Right(5), Either.Right(3), Either.Left(IllegalArgumentException("")))
+      tryHarder shouldBe aFailureOfType(IllegalArgumentException::class)
+    }
+  }
+
+  "Try and recover" - {
+
+    "Old school" {
+
+      val dollars = try {
+        playLottery(9)
+      } catch (e: AuthorizationException) {
+        0
+      }
+      dollars shouldBe 0
+    }
+
+    "Either.catch { .. } " {
+      val gain: Either<Throwable, Int> = Either.catch { playLottery(9) }
+
+      gain shouldBe aFailureOfType(AuthorizationException::class)
+
+      gain.getOrElse { 0 } shouldBe 0
+    }
+
+    "Recover" {
+      val gain = Either.catch { playLottery(99) }
+      gain.handleError { 0 } shouldBe Either.Right(0)
+      gain.handleErrorWith { try { Either.Right(playLottery ( 42)) } catch(e: Throwable) { Either.Left(e)} } shouldBe Either.Right(1000)
+    }
   }
 }
 
-  fun aFailureOfType(expected: KClass<*>): Matcher<Try<Int>> = object : Matcher<Try<Int>> {
-    override fun test(value: Try<Int>): Result = when (value) {
-      is Success -> Result(false, "Expected a failure, got $value", "Received expected failure")
-      is Failure -> {
-        val javaClass = value.exception.javaClass
+  fun aFailureOfType(expected: KClass<*>): Matcher<Either<Throwable, Int>> = object : Matcher<Either<Throwable, Int>> {
+    override fun test(value: Either<Throwable, Int>): Result = when (value) {
+      is Either.Right -> Result(false, "Expected a failure, got $value", "Received expected failure")
+      is Either.Left -> {
+        val javaClass = value.javaClass
         Result(
           expected.java.isAssignableFrom(javaClass),
-          "Expected Try.Failure(${expected.java}), got $value",
+          "Expected Either.Left(${expected.java}), got $value",
           "Received expected failure"
         )
       }
