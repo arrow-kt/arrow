e036a21eb6c8ad20a570b5701494e11dcb1e5feb
Alberto Ballano <aballano@users.noreply.github.com>
2020-01-29 16:48:51 +0100

    Fix BracketLaws guarantee test
    Add additional test for IO's guarantee

--- modules/core/arrow-test/src/main/kotlin/arrow/test/laws/BracketLaws.kt
+++ modules/core/arrow-test/src/main/kotlin/arrow/test/laws/BracketLaws.kt
@@ -36,7 +36,8 @@ object BracketLaws {
       Law("Bracket: guaranteeCase is derived from bracketCase") { BF.guaranteeCaseIsDerivedFromBracketCase({ BF.just(Unit) }, EQ) },
       Law("Bracket: bracket propagates transformer effects") { BF.bracketPropagatesTransformerEffects(EQ) },
       Law("Bracket: bracket must run release task on use error") { BF.bracketMustRunReleaseTaskOnUseError(EQ) },
-      Law("Bracket: bracket must run release task on adquire error") { BF.bracketMustRunReleaseTaskOnAdquireError(EQ) }
+      Law("Bracket: bracket must not run release task on acquire error") { BF.bracketMustNotRunReleaseTaskOnAcquireError(EQ) },
+      Law("Bracket: guarantee must run finalizer task") { BF.guaranteeMustRunFinalizerOnError(EQ) }
     )
   }
 
@@ -133,7 +134,7 @@ object BracketLaws {
     forAll(Gen.int()) { i ->
       val msg = AtomicIntW(0)
       just(i).bracket<Int, Int>(
-        release = { ii -> msg.value = ii; unit() },
+        release = { ii -> unit().map { msg.value = ii } },
         use = { throw Throwable("Expected failure!") }
       )
         .attempt()
@@ -141,13 +142,25 @@ object BracketLaws {
         .equalUnderTheLaw(just(i), EQ)
     }
 
-  fun <F> Bracket<F, Throwable>.bracketMustRunReleaseTaskOnAdquireError(EQ: Eq<Kind<F, Int>>): Unit =
-    forAll(Gen.int()) { i ->
-      val msg = AtomicIntW(0)
+  fun <F> Bracket<F, Throwable>.bracketMustNotRunReleaseTaskOnAcquireError(EQ: Eq<Kind<F, Int>>): Unit =
+    forAll(Gen.int(), Gen.int()) { expected, other ->
+      val actual = AtomicIntW(expected)
       raiseError<Int>(Throwable("Expected failure!")).bracket(
-        release = { ii -> msg.value = ii; unit() },
+        release = { unit().map { actual.value = other } },
         use = { just(it) }
       )
+        .attempt()
+        .map { actual.value }
+        .equalUnderTheLaw(just(expected), EQ)
+    }
+
+  fun <F> Bracket<F, Throwable>.guaranteeMustRunFinalizerOnError(
+    EQ: Eq<Kind<F, Int>>
+  ): Unit =
+    forAll(Gen.int(), Gen.throwable()) { i, t ->
+      val msg = AtomicIntW(0)
+      raiseError<Int>(t)
+        .guarantee(unit().map { msg.value = i })
         .attempt()
         .map { msg.value }
         .equalUnderTheLaw(just(i), EQ)
