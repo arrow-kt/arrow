dbf1c5b33fb03dd6157c75b74e89bbe845f56fa6
Simon Vergauwen <vergauwen.simon@gmail.com>
2020-02-03 00:25:15 +0100

    Fix EQ

--- modules/fx/arrow-fx/src/test/kotlin/arrow/fx/predef.kt
+++ modules/fx/arrow-fx/src/test/kotlin/arrow/fx/predef.kt
@@ -2,9 +2,9 @@ package arrow.fx
 
 import arrow.Kind
 import arrow.core.extensions.either.eq.eq
-import arrow.fx.extensions.io.applicative.applicative
+import arrow.core.extensions.option.eq.eq
 import arrow.fx.extensions.io.applicativeError.attempt
-import arrow.fx.extensions.io.concurrent.waitFor
+import arrow.fx.internal.TimeoutException
 import arrow.fx.typeclasses.Duration
 import arrow.fx.typeclasses.seconds
 import arrow.typeclasses.Eq
@@ -15,11 +15,11 @@ import java.util.concurrent.ThreadFactory
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
 
-fun <A> EQ(EQA: Eq<A> = Eq.any(), timeout: Duration = 5.seconds): Eq<Kind<ForIO, A>> = Eq { a, b ->
+fun <A> EQ(EQA: Eq<A> = Eq.any(), timeout: Duration = 60.seconds): Eq<Kind<ForIO, A>> = Eq { a, b ->
   arrow.core.Either.eq(Eq.any(), EQA).run {
-    IO.applicative().mapN(a.attempt(), b.attempt()) { (a, b) ->
-      a.eqv(b)
-    }.waitFor(timeout).unsafeRunSync()
+    IO.parMapN(a.attempt(), b.attempt()) { a, b -> a.eqv(b) }
+      .unsafeRunTimed(timeout)
+      .fold({ throw TimeoutException(timeout.toString()) }, { true })
   }
 }
 
