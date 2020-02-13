9b83a3696bbc7234e560425d50197ba87e310071
Alphonse Bendt <alphonse.bendt@gmail.com>
2020-02-13 08:40:13 +0100

    doc

--- modules/core/arrow-core-data/src/main/kotlin/arrow/typeclasses/MonadPlus.kt
+++ modules/core/arrow-core-data/src/main/kotlin/arrow/typeclasses/MonadPlus.kt
@@ -2,8 +2,51 @@ package arrow.typeclasses
 
 import arrow.Kind
 
+/**
+ *  MonadPlus is a typeclass that extends a Monad by supporting choice and failure also.
+ */
 interface MonadPlus<F> : Monad<F>, Alternative<F> {
+  /**
+   * the identity of `mPlus`
+   *
+   *  {: data-executable='true'}
+   *
+   * ```kotlin:ank
+   * import arrow.core.extensions.*
+   * import arrow.core.extensions.listk.monadPlus.monadPlus
+   * import arrow.core.*
+   *
+   * fun main(args: Array<String>) {
+   *   //sampleStart
+   *   val result = ListK.monadPlus().run {
+   *    listOf("A", "B").k().plusM(zeroM())
+   *   }
+   *   //sampleEnd
+   *   println(result)
+   * }
+   * ```
+   */
   fun <A> zeroM(): Kind<F, A> = empty()
 
+  /**
+   * an associative operation to combine two structures.
+   *
+   *  {: data-executable='true'}
+   *
+   * ```kotlin:ank
+   * import arrow.core.extensions.*
+   * import arrow.core.extensions.listk.monadPlus.monadPlus
+   * import arrow.core.*
+   *
+   * fun main(args: Array<String>) {
+   *   //sampleStart
+   *   val result = ListK.monadPlus().run {
+   *    listOf("A", "B").k().plusM(listOf(1, 2, 3).k())
+   *   }
+   *   //sampleEnd
+   *   println(result)
+   * }
+   * ```
+   */
   fun <A> Kind<F, A>.plusM(other: Kind<F, A>): Kind<F, A> = alt(other)
 }
