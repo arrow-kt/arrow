c169fd6b250a0cffe15f373d81a412de191edab3
danieh <danimontoya_86@hotmail.com>
2020-02-13 11:55:46 +0100

    Update tests for ApplicativeLaws (#2038)
    * Extract mapN and tupledN with arity > 3 as extension functions
    
    * ktlint
    
    * Replaced EQ to avoid weird results
    
    * Revert extracting mapN and tupleN for 0.10.x (because of breaking changes)
    
    * Adding tests for replicate - wip
    
    * Added test laws for replicate (replaced Gen.intSmall() that was not stack safe)
    
    Co-authored-by: Simon Vergauwen <nomisRev@users.noreply.github.com>

--- modules/core/arrow-test/src/main/kotlin/arrow/test/laws/ApplicativeLaws.kt
+++ modules/core/arrow-test/src/main/kotlin/arrow/test/laws/ApplicativeLaws.kt
@@ -1,7 +1,12 @@
 package arrow.test.laws
 
 import arrow.Kind
+import arrow.core.Tuple2
+import arrow.core.Tuple3
 import arrow.core.extensions.eq
+import arrow.core.extensions.monoid
+import arrow.core.extensions.tuple2.eq.eq
+import arrow.core.extensions.tuple3.eq.eq
 import arrow.test.generators.GenK
 import arrow.test.generators.functionAToB
 import arrow.test.generators.intSmall
@@ -17,15 +22,24 @@ object ApplicativeLaws {
   fun <F> laws(A: Applicative<F>, GENK: GenK<F>, EQK: EqK<F>): List<Law> = laws(A, A, GENK, EQK)
 
   fun <F> laws(A: Applicative<F>, FF: Functor<F>, GENK: GenK<F>, EQK: EqK<F>): List<Law> {
-    val EQ = EQK.liftEq(Int.eq())
+
+    val EQ: Eq<Kind<F, Int>> = EQK.liftEq(Int.eq())
     val G = GENK.genK(Gen.int())
+    val EQTuple2: Eq<Kind<F, Tuple2<Int, Int>>> = EQK.liftEq(Tuple2.eq(Int.eq(), Int.eq()))
+    val EQTuple3: Eq<Kind<F, Tuple3<Int, Int, Int>>> = EQK.liftEq(Tuple3.eq(Int.eq(), Int.eq(), Int.eq()))
+    val EQBoolean: Eq<Kind<F, Boolean>> = EQK.liftEq(Boolean.eq())
+
     return FunctorLaws.laws(A, GENK, EQK) + listOf(
       Law("Applicative Laws: ap identity") { A.apIdentity(G, EQ) },
       Law("Applicative Laws: homomorphism") { A.homomorphism(EQ) },
       Law("Applicative Laws: interchange") { A.interchange(GENK, EQ) },
       Law("Applicative Laws: map derived") { A.mapDerived(G, FF, EQ) },
-      Law("Applicative Laws: cartesian builder map") { A.cartesianBuilderMap(EQ) },
-      Law("Applicative Laws: cartesian builder tupled") { A.cartesianBuilderTupled(EQ) }
+      Law("Applicative Laws: cartesian builder map") { A.cartesianBuilderMap(EQTuple3) },
+      Law("Applicative Laws: cartesian builder tupled2") { A.cartesianBuilderTupled2(EQTuple2) },
+      Law("Applicative Laws: cartesian builder tupled3") { A.cartesianBuilderTupled3(EQTuple3) },
+      Law("Applicative Laws: replicate check size") { A.replicateSize(EQ) },
+      Law("Applicative Laws: replicate check list == 1") { A.replicateListOf1(EQBoolean) },
+      Law("Applicative Laws: replicate monoid") { A.replicateMonoid(EQ) }
     )
   }
 
@@ -49,13 +63,33 @@ object ApplicativeLaws {
       FF.run { fa.map(f) }.equalUnderTheLaw(fa.ap(just(f)), EQ)
     }
 
-  fun <F> Applicative<F>.cartesianBuilderMap(EQ: Eq<Kind<F, Int>>): Unit =
-    forAll(Gen.intSmall(), Gen.intSmall(), Gen.intSmall(), Gen.intSmall(), Gen.intSmall(), Gen.intSmall()) { a: Int, b: Int, c: Int, d: Int, e: Int, f: Int ->
-      mapN(just(a), just(b), just(c), just(d), just(e), just(f)) { (x, y, z, u, v, w) -> x + y + z - u - v - w }.equalUnderTheLaw(just(a + b + c - d - e - f), EQ)
+  fun <F> Applicative<F>.cartesianBuilderMap(EQ: Eq<Kind<F, Tuple3<Int, Int, Int>>>): Unit =
+    forAll(Gen.intSmall(), Gen.intSmall(), Gen.intSmall()) { a: Int, b: Int, c: Int ->
+      mapN(just(a), just(b), just(c)) { it }.equalUnderTheLaw(just(Tuple3(a, b, c)), EQ)
     }
 
-  fun <F> Applicative<F>.cartesianBuilderTupled(EQ: Eq<Kind<F, Int>>): Unit =
-    forAll(Gen.intSmall(), Gen.intSmall(), Gen.intSmall(), Gen.intSmall(), Gen.intSmall(), Gen.intSmall()) { a: Int, b: Int, c: Int, d: Int, e: Int, f: Int ->
-      tupledN(just(a), just(b), just(c), just(d), just(e), just(f)).map { (x, y, z, u, v, w) -> x + y + z - u - v - w }.equalUnderTheLaw(just(a + b + c - d - e - f), EQ)
+  fun <F> Applicative<F>.cartesianBuilderTupled2(EQ: Eq<Kind<F, Tuple2<Int, Int>>>): Unit =
+    forAll(Gen.intSmall(), Gen.intSmall()) { a: Int, b: Int ->
+      tupledN(just(a), just(b)).equalUnderTheLaw(just(Tuple2(a, b)), EQ)
+    }
+
+  fun <F> Applicative<F>.cartesianBuilderTupled3(EQ: Eq<Kind<F, Tuple3<Int, Int, Int>>>): Unit =
+    forAll(Gen.intSmall(), Gen.intSmall(), Gen.intSmall()) { a: Int, b: Int, c: Int ->
+      tupledN(just(a), just(b), just(c)).equalUnderTheLaw(just(Tuple3(a, b, c)), EQ)
+    }
+
+  fun <F> Applicative<F>.replicateSize(EQ: Eq<Kind<F, Int>>): Unit =
+    forAll(Gen.choose(0, 100)) { n ->
+      just(1).replicate(n).map { it.size }.equalUnderTheLaw(just(n), EQ)
+    }
+
+  fun <F> Applicative<F>.replicateListOf1(EQ: Eq<Kind<F, Boolean>>): Unit =
+    forAll(Gen.choose(0, 100)) { n ->
+      just(1).replicate(n).map { list -> list.all { it == 1 } }.equalUnderTheLaw(just(true), EQ)
+    }
+
+  fun <F> Applicative<F>.replicateMonoid(EQ: Eq<Kind<F, Int>>): Unit =
+    forAll(Gen.choose(0, 100)) { n ->
+      just(1).replicate(n, Int.monoid()).equalUnderTheLaw(just(n), EQ)
     }
 }
