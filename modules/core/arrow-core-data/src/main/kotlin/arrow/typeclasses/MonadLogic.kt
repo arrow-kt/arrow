52ca900303dd198f73d1d53431d262c541752b75
Alphonse Bendt <alphonse.bendt@gmail.com>
2020-02-13 08:39:48 +0100

    review changes

--- modules/core/arrow-core-data/src/main/kotlin/arrow/typeclasses/MonadLogic.kt
+++ modules/core/arrow-core-data/src/main/kotlin/arrow/typeclasses/MonadLogic.kt
@@ -5,31 +5,29 @@ import arrow.core.ForOption
 import arrow.core.Option
 import arrow.core.Tuple2
 import arrow.core.fix
-import arrow.core.identity
 
+/**
+ *  MonadLog is a typeclass that extends a MonadPlus.
+ */
 interface MonadLogic<F> : MonadPlus<F> {
 
   fun <A> Kind<F, A>.splitM(): Kind<F, Option<Tuple2<Kind<F, A>, A>>>
 
-  fun <A> Kind<F, A>.interleave(fa: Kind<F, A>): Kind<F, A> =
+  fun <A> Kind<F, A>.interleave(other: Kind<F, A>): Kind<F, A> =
     this.splitM().flatMap { option ->
-      option.map { (fa, a) ->
-        just(a).plusM(fa.interleave(fa))
-      }.fold({ fa }, ::identity)
+      option.fold({ other }, { (fa, a) -> just(a).plusM(other.interleave(fa)) })
     }
 
   fun <A, B> Kind<F, A>.unweave(ffa: (A) -> Kind<F, B>): Kind<F, B> =
     splitM().flatMap { option ->
       option.fold({ zeroM<Tuple2<Kind<F, A>, A>>() }, { just(it) })
     }.flatMap { (fa, a) ->
-      ffa(a).interleave(fa.flatMap(ffa))
+      ffa(a).interleave(fa.unweave(ffa))
     }
 
   fun <A, B> Kind<F, A>.ifThen(fb: Kind<F, B>, ffa: (A) -> Kind<F, B>): Kind<F, B> =
     splitM().flatMap { option ->
-      option.map { (fa, a) ->
-        ffa(a).plusM(fa.flatMap { ffa(it) })
-      }.fold({ fb }, ::identity)
+      option.fold({ fb }, { (fa, a) -> ffa(a).plusM(fa.flatMap(ffa)) })
     }
 
   fun <A> Kind<F, A>.once(): Kind<F, A> =
@@ -37,7 +35,7 @@ interface MonadLogic<F> : MonadPlus<F> {
       option.fold({ zeroM<A>() }, { just(it.b) })
     }
 
-  fun <A> Kind<F, A>.unitIfValue(): Kind<F, Unit> =
+  fun <A> Kind<F, A>.voidIfValue(): Kind<F, Unit> =
     once().ifThen(unit()) { zeroM() }
 }
 
