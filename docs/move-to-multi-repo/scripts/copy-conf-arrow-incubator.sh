cff4c51a4dbb4026c7b407abbfb4f80c265f7aaf
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-02-17 19:23:25 +0100

    Consider sync with release branch for every library

--- docs/move-to-multi-repo/scripts/copy-conf-arrow-incubator.sh
+++ docs/move-to-multi-repo/scripts/copy-conf-arrow-incubator.sh
@@ -20,6 +20,9 @@ cp $BASEDIR/d-arrow-module/.github/workflows/check* .github/workflows/
 sed -i "s/d-arrow-module/arrow-core/g" .github/workflows/*
 sed -i "s/d-arrow/arrow/g" .github/workflows/*
 sed -i "s/sh arrow-incubator-repository/sh/g" .github/workflows/*
+sed -i "s/on: pull_request/on:/g" .github/workflows/sync-release-branch-arrow-*.yml
+sed -i "s/#  schedule/  schedule/g" .github/workflows/sync-release-branch-arrow-*.yml
+sed -i "s/#    - cron/    - cron/g" .github/workflows/sync-release-branch-arrow-*.yml
 
 git co -b new-conf
 git add .
--- docs/move-to-multi-repo/scripts/copy-conf-arrow-integrations.sh
+++ docs/move-to-multi-repo/scripts/copy-conf-arrow-integrations.sh
@@ -19,6 +19,9 @@ cp $BASEDIR/d-arrow-module/.github/workflows/check* .github/workflows/
 sed -i "s/d-arrow-module/arrow-core/g" .github/workflows/*
 sed -i "s/d-arrow/arrow/g" .github/workflows/*
 sed -i "s/sh arrow-integrations-repository/sh/g" .github/workflows/*
+sed -i "s/on: pull_request/on:/g" .github/workflows/sync-release-branch-arrow-*.yml
+sed -i "s/#  schedule/  schedule/g" .github/workflows/sync-release-branch-arrow-*.yml
+sed -i "s/#    - cron/    - cron/g" .github/workflows/sync-release-branch-arrow-*.yml
 
 git co -b new-conf
 git add .
