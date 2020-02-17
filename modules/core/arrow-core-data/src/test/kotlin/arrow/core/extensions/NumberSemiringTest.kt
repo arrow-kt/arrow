218099ca0ebb89c3f2ad53149f1cd1f979308983
Alberto Ballano <aballano@users.noreply.github.com>
2020-02-17 17:31:49 +0100

    Nits

--- modules/core/arrow-core-data/src/test/kotlin/arrow/core/extensions/NumberSemiringTest.kt
+++ modules/core/arrow-core-data/src/test/kotlin/arrow/core/extensions/NumberSemiringTest.kt
@@ -5,27 +5,15 @@ import arrow.test.generators.byte
 import arrow.test.generators.short
 import arrow.test.laws.SemiringLaws
 import io.kotlintest.properties.Gen
-import io.kotlintest.shouldBe
 
 class NumberSemiringTest : UnitSpec() {
 
-    init {
-        testLaws(SemiringLaws.laws(Byte.semiring(), Gen.byte(), Byte.eq()))
-        testLaws(SemiringLaws.laws(Double.semiring(), Gen.double(), Double.eq()))
-        testLaws(SemiringLaws.laws(Int.semiring(), Gen.int(), Int.eq()))
-        testLaws(SemiringLaws.laws(Short.semiring(), Gen.short(), Short.eq()))
-        testLaws(SemiringLaws.laws(Float.semiring(), Gen.float(), Float.eq()))
-
-        "maybeCombineMultiplicate() should calculate 6 when called on 2 and 3" {
-            Int.semiring().run {
-                2.maybeCombineMultiplicate(3).shouldBe(6)
-            }
-        }
-
-        "maybeCombineAddition() should calculate 3 when called on 1 and 2" {
-            Int.semiring().run {
-                1.maybeCombineAddition(2).shouldBe(3)
-            }
-        }
-    }
+  init {
+    testLaws(SemiringLaws.laws(Byte.semiring(), Gen.byte(), Byte.eq()))
+    testLaws(SemiringLaws.laws(Double.semiring(), Gen.double(), Double.eq()))
+    testLaws(SemiringLaws.laws(Int.semiring(), Gen.int(), Int.eq()))
+    testLaws(SemiringLaws.laws(Short.semiring(), Gen.short(), Short.eq()))
+    testLaws(SemiringLaws.laws(Float.semiring(), Gen.float(), Float.eq()))
+    testLaws(SemiringLaws.laws(Long.semiring(), Gen.long(), Long.eq()))
+  }
 }
