75319c7c6ca1961c1c7840e0c8759cee5796fd38
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-02-19 16:44:14 +0100

    Revert "Simplify doc generation"
    This reverts commit fbf0d68a2ccc3ec70c81655b168ba68b62a2e22f.

--- scripts/project-locate-doc.sh
+++ scripts/project-locate-doc.sh
@@ -3,4 +3,9 @@
 set -ex
 echo "For $1 ..."
 cd $BASEDIR
-cp -r $1/build/site/* site/build/site/
+cp -r $1/**/build/site/* site/build/site/
+for module in $1/arrow-*; do
+    for submodule in `find $module -mindepth 1 -maxdepth 1 -type d -name "arrow-*"`; do 
+        cp -r $submodule/build/site/* site/build/site/
+    done
+done
