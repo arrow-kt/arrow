45302e8df5bb71d24bc9b61915d443f1b6b1cc2e
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-02-20 02:23:04 +0100

    Fix

--- scripts/project-locate-doc.sh
+++ scripts/project-locate-doc.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-set -ex
-echo "For $1 ..."
+set -e
+
+INITIAL_PATH=""
+if [ $# -eq 1 ]; then
+    echo "For $1 ..."
+    INITIAL_PATH="$1/"
+fi
+
 cd $BASEDIR
-cp -r $1/**/build/site/* site/build/site/
-for module in $1/arrow-*; do
+cp -r ${INITIAL_PATH}**/build/site/* site/build/site/
+for module in ${INITIAL_PATH}arrow-*; do
     for submodule in `find $module -mindepth 1 -maxdepth 1 -type d -name "arrow-*"`; do 
         cp -r $submodule/build/site/* site/build/site/
     done
