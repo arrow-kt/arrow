3c010f3c04ab09ab19c498589ffbc1ae10b248b6
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-02-21 00:45:23 +0100

    Conf: remove sources lists

--- scripts/site-prepare-env.sh
+++ scripts/site-prepare-env.sh
@@ -2,6 +2,9 @@
 
 set -ex
 echo "Prepare environment ..."
+sudo rm -rf /etc/apt/sources.list.d/microsoft*
+sudo rm -rf /etc/apt/sources.list.d/azure*
+sudo rm -rf /etc/apt/sources.list.d/dotnet*
 sudo apt-get update
 sudo apt-get install ruby-dev
 sudo gem install bundler --force
