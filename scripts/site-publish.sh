9189375cf0149554aeeec9ac7d373ec9095dee68
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-02-25 01:54:45 +0100

    Fix site publish

--- scripts/site-publish.sh
+++ scripts/site-publish.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 set -e
-echo "Publish $1 in S3 ..."
+echo "Publish in S3 ..."
 
-if [[ "$1" == "arrow-docs" ]]; then
-    for file in $BASEDIR/site/build/_site/*; do
-        echo "Sync with docs/next/$(basename $file)"
-        if [ -f "$file" ]; then
-            echo "Copying $file ..."
-            aws s3 cp $file s3://$S3_BUCKET/docs/next/$(basename $file) >> aws_sync_jekyll.log
-        else
-            echo "Sync $file ..."
-            aws s3 sync $file s3://$S3_BUCKET/docs/next/$(basename $file) >> aws_sync_jekyll.log
-        fi
-    done
-else
+if [ -d $BASEDIR/site/build/_site/apidocs ]; then
     for module in $BASEDIR/site/build/_site/apidocs/*; do
         echo "Sync with docs/next/apidocs/$(basename $module)"
         aws s3 sync $module s3://$S3_BUCKET/docs/next/apidocs/$(basename $module) > aws_sync_jekyll.log
     done
 fi
+
+#    for file in $BASEDIR/site/build/_site/*; do
+#        echo "Sync with docs/next/$(basename $file)"
+#        if [ -f "$file" ]; then
+#            echo "Copying $file ..."
+#            aws s3 cp $file s3://$S3_BUCKET/docs/next/$(basename $file) >> aws_sync_jekyll.log
+#        else
+#            echo "Sync $file ..."
+#            aws s3 sync $file s3://$S3_BUCKET/docs/next/$(basename $file) >> aws_sync_jekyll.log
+#        fi
+#    done
