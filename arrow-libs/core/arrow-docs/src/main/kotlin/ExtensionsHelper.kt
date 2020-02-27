528bbe3bd1a4c833fd7551221e7aa99c376116a9
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-02-27 19:46:02 +0100

    Move static doc from Arrow Docs to arrow-docs module. (#47)

--- arrow-libs/core/arrow-docs/src/main/kotlin/ExtensionsHelper.kt
+++ arrow-libs/core/arrow-docs/src/main/kotlin/ExtensionsHelper.kt
@@ -3,6 +3,7 @@ package arrow.reflect
 import arrow.aql.Box
 import arrow.syntax.function.partially2
 import kotlin.reflect.KClass
+import kotlin.reflect.full.declaredFunctions
 
 private val lineSeparator: String = System.getProperty("line.separator")
 
@@ -107,11 +108,46 @@ fun String.toKebabCase(): String {
 fun <A : Any> KClass<A>.docsMarkdownLink(moduleName: String?, packageName: String): String =
   "[$simpleName]({{ '/apidocs/$moduleName/${packageName.toKebabCase()}/${simpleName?.toKebabCase()}' | relative_url }})"
 
+fun TypeClass.hierarchyGraph(): String =
+  """
+    |#font: monoidregular
+    |#arrowSize: 1
+    |#bendSize: 0.3
+    |#direction: down
+    |#gutter: 5
+    |#edgeMargin: 0
+    |#edges: rounded
+    |#fillArrows: false
+    |#fontSize: 10
+    |#leading: 1.25
+    |#lineWidth: 1
+    |#padding: 8
+    |#spacing: 40
+    |#stroke: #485C8A
+    |#title: ${kclass.simpleName}
+    |#zoom: 1
+    |#.typeclass: fill=#FFFFFF visual=class bold
+    |${nomnomlMethods()}
+    |${nomnomlHierarchy()}
+  """.trimMargin()
+
+fun TypeClass.nomnomlMethods(): String =
+  "[<typeclass>${kclass.simpleName}|${kclass.declaredFunctions.groupBy { it.name }.toList().joinToString("|") {
+    if (it.second.size > 1) "${it.first}(${it.second.size})"
+    else it.first
+  }}]"
+
 fun TypeClass.nomnomlBlock(): String =
   "[<typeclass>${kclass.simpleName}]"
 
 fun Extends.nomnomlExtends(): String =
   "${b.nomnomlBlock()}<-${a.nomnomlBlock()}"
+
+fun TypeClass.nomnomlHierarchy(): String =
+  hierarchy().joinToString(lineSeparator, transform = Extends::nomnomlExtends)
+
+fun TypeClass.nomnomlExtensions(): String =
+  "${nomnomlBlock()}<-[${kclass.simpleName} @extension|${extensions().joinToString("|") { it.instance.kclass.java.simpleName }}]"
 
 fun List<TypeClass>.nomnomlMixedHierarchyGraph(): String =
   flatMap { it.hierarchy() }
