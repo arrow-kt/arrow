1e895dac12d8a7790d46f33ed63348b872d7eab0
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-03-05 17:27:20 +0100

    Remove all tests that do not belong to optics (#27)
    * Remove all tests that do not belong to optics
    
    * Remove Law, GenK and add `arrow-core-test` dependency

--- arrow-libs/optics/arrow-optics/src/test/kotlin/arrow/optics/std/TupleTest.kt
+++ arrow-libs/optics/arrow-optics/src/test/kotlin/arrow/optics/std/TupleTest.kt
@@ -14,7 +14,6 @@ import arrow.core.Tuple9
 import arrow.core.extensions.listk.eq.eq
 import arrow.core.extensions.monoid
 import arrow.core.extensions.option.eq.eq
-import arrow.core.extensions.tuple2.monoid.monoid
 import arrow.optics.first
 import arrow.optics.second
 import arrow.optics.third
@@ -31,7 +30,6 @@ import arrow.test.generators.tuple7
 import arrow.test.generators.tuple8
 import arrow.test.generators.tuple9
 import arrow.test.laws.LensLaws
-import arrow.test.laws.MonoidLaws
 import arrow.test.laws.TraversalLaws
 import arrow.typeclasses.Eq
 import io.kotlintest.properties.Gen
@@ -39,14 +37,6 @@ import io.kotlintest.properties.Gen
 class TupleTest : UnitSpec() {
 
   init {
-
-    testLaws(
-      MonoidLaws.laws(
-        M = Tuple2.monoid(Int.monoid(), String.monoid()),
-        GEN = Gen.tuple2(Gen.int(), Gen.string()),
-        EQ = Eq.any()
-      )
-    )
 
     testLaws(
       LensLaws.laws(
