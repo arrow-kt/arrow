a6e5443c5d953ecdd248b60f212a4400e207d1c9
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-03-31 17:20:43 +0200

    Test

--- scripts/libs-publish.sh
+++ scripts/libs-publish.sh
@@ -12,5 +12,5 @@ done
 replaceOSSbyLocalRepository $BASEDIR/arrow/generic-conf.gradle
 for lib in $(cat $BASEDIR/arrow/lists/libs.txt); do
     replaceGlobalPropertiesbyLocalConf $BASEDIR/$lib/gradle.properties
-    $BASEDIR/arrow/scripts/project-publish.sh $lib
+    #$BASEDIR/arrow/scripts/project-publish.sh $lib
 done
