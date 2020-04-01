eb304b4acce54d76ab9a66b1b9bb86a2700bfb4d
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-04-01 14:45:46 +0200

    Fix libraries location

--- scripts/libs-publish.sh
+++ scripts/libs-publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 . $BASEDIR/arrow/scripts/commons4gradle.sh
 
 echo "Check and prepare the environment ..."
@@ -12,5 +13,6 @@ done
 replaceOSSbyLocalRepository $BASEDIR/arrow/generic-conf.gradle
 for lib in $(cat $BASEDIR/arrow/lists/libs.txt); do
     replaceGlobalPropertiesbyLocalConf $BASEDIR/$lib/gradle.properties
+    $BASEDIR/arrow/scripts/project-install.sh $lib
     $BASEDIR/arrow/scripts/project-publish.sh $lib
 done
