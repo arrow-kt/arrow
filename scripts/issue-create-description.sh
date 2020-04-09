8d7ab6eef2306c1c9a9d91a967f7c6b568e47e50
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-04-09 22:29:19 +0200

    Conf: small fixes (#2127)
    - Extract env variable for ARROW_LIB
    - Fix links in created issues
    - Fix environment because of https://github.com/arrow-kt/arrow-core/issues/91
    - Add cache invalidation
    - Update templates

--- scripts/issue-create-description.sh
+++ scripts/issue-create-description.sh
@@ -3,4 +3,4 @@
 echo -e "## Details\n"
 echo -e "* **Commit hash**: \`$(git log -1 --pretty=format:%h)\`\n"
 echo -e "* **Author**: $(git log -1 --pretty=format:%an)\n"
-echo -e "* **More info**: [error log](https://github.com/arrow-kt/arrow-optics/commit/$GITHUB_SHA/checks)\n"
+echo -e "* **More info**: [error log](https://github.com/arrow-kt/$ARROW_LIB/commit/$GITHUB_SHA/checks)\n"
