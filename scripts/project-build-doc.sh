12aec816f3144d3282c0439767351b861da5da26
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-05-19 22:41:13 +0200

    Fix #2144 - Remove ERROR CLASS cases in documentation (#2145)

--- scripts/project-build-doc.sh
+++ scripts/project-build-doc.sh
@@ -4,5 +4,6 @@ set -e
 
 echo "For $1 ..."
 cd $BASEDIR/$1
-./gradlew clean dokka
+./gradlew clean assemble
+./gradlew dokka
 ./gradlew :arrow-docs:runAnk
--- scripts/project-run-dokka.sh
+++ scripts/project-run-dokka.sh
@@ -4,4 +4,4 @@ set -e
 
 echo "For $1 ..."
 cd $BASEDIR/$1
-./gradlew clean dokka
+./gradlew dokka
