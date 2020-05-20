95ea602eb088c3c0aaf0a19e6d90868943eca353
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-05-20 15:42:14 +0200

    Issue creation: add details and fix error log URL

--- scripts/issue-create-description.sh
+++ scripts/issue-create-description.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-echo -e "## Details\n"
+echo -e "## Data\n"
 echo -e "* **Commit hash**: \`$(git log -1 --pretty=format:%h)\`\n"
 echo -e "* **Author**: $(git log -1 --pretty=format:%an)\n"
-echo -e "* **More info**: [error log](https://github.com/arrow-kt/$ARROW_LIB/commit/$GITHUB_SHA/checks)\n"
+echo -e "* **More info**: [error log](https://github.com/${GITHUB_REPOSITORY}/runs/${GITHUB_RUN_ID})\n"
+
+if [ -f $BASEDIR/error.log ]; then
+    echo -e "\n## Error\n\n"
+    cat $BASEDIR/error.log
+fi
+
+if [ -f $BASEDIR/result.log ]; then
+    echo -e "\n## Result\n\n"
+    cat $BASEDIR/result.log
+fi
