15e7382935c8ada3432d86fa9625a2802034ea29
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-08-24 18:42:04 +0200

    Configuration: improve plugin management (#63)

--- arrow-libs/optics/scripts/checkout-orchestrator.sh
+++ arrow-libs/optics/scripts/checkout-orchestrator.sh
@@ -5,9 +5,5 @@ cd ${ROOTDIR}/..
 BASEDIR=$(pwd)
 if [ ! -d arrow ]; then
     echo "Clone arrow repository in $BASEDIR/arrow ..."
-    git clone https://github.com/arrow-kt/arrow.git
-else
-    echo "Updating $INSTALLDIR/arrow ..."
-    cd arrow
-    git pull
+    git clone git@github.com:arrow-kt/arrow.git
 fi
