a2b3c1d8240226b873240066bd3344dcfeb5017d
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-09-10 16:37:15 +0200

    Fix release notes format

--- scripts/create-release-notes-all-libraries.sh
+++ scripts/create-release-notes-all-libraries.sh
@@ -7,26 +7,30 @@ echo -e "$RELEASE_VERSION ($(date +%Y-%m-%d))\n---\n" >> $BASEDIR/arrow/release-
 for lib in $(cat $BASEDIR/arrow/lists/libs.txt); do
     cd $BASEDIR/$lib
     TAG_TIMESTAMP=$(git log $LATEST_PUBLISHED_VERSION --pretty="format:%ct" | head -1)
-    hub pr list --limit 500 --base master -s merged --format='%mt#%au#%t [%i](%U)%n' > PR-list-all.txt
-    while read line; do PR_TIMESTAMP=$(echo $line | cut -d# -f1); if [ $PR_TIMESTAMP -gt $TAG_TIMESTAMP ]; then echo $line; fi; done < PR-list-all.txt > PR-list.txt
-    cat PR-list.txt | cut -d# -f2 | sort | uniq > contributors.txt
-    while read contributor; do 
-        echo "### [@$contributor](https://github.com/$contributor)"
-        grep "#${contributor}#" PR-list.txt | cut -d# -f3- | perl -pe 's/^/ - /'
-    done < contributors.txt > local-release-notes.txt
+    hub pr list --limit 500 --base master -s merged --format='%mt#%au#[%i](%U) %t%n' > PR-list-all.txt
+    while read line; do 
+        pr_timestamp=$(echo $line | cut -d# -f1)
+        if [ $pr_timestamp -gt $TAG_TIMESTAMP ]; then
+            contribution=$(echo $line | cut -d# -f3- | perl -pe 's/^/ - /')
+            contributor=$(echo $line | cut -d# -f2)
+            echo "$contribution - [@$contributor](https://github.com/$contributor)"
+        fi 
+    done < PR-list-all.txt > local-release-notes.txt
     echo -e "\n## $lib\n" >> $BASEDIR/arrow/release-notes-all.txt
     cat local-release-notes.txt >> $BASEDIR/arrow/release-notes-all.txt
 done
 
 cd $BASEDIR/arrow
 TAG_TIMESTAMP=$(git log $LATEST_PUBLISHED_VERSION --pretty="format:%ct" | head -1)
-hub pr list --limit 500 --base master -s merged --format='%mt#%au#%t [%i](%U)%n' > PR-list-all.txt
-while read line; do PR_TIMESTAMP=$(echo $line | cut -d# -f1); if [ $PR_TIMESTAMP -gt $TAG_TIMESTAMP ]; then echo $line; fi; done < PR-list-all.txt > PR-list.txt
-cat PR-list.txt | cut -d# -f2 | sort | uniq > contributors.txt
-while read contributor; do
-    echo "### [@$contributor](https://github.com/$contributor)"
-    grep "#${contributor}#" PR-list.txt | cut -d# -f3- | perl -pe 's/^/ - /'
-done < contributors.txt > local-release-notes.txt
+hub pr list --limit 500 --base master -s merged --format='%mt#%au#[%i](%U) %t%n' > PR-list-all.txt
+while read line; do 
+    pr_timestamp=$(echo $line | cut -d# -f1)
+    if [ $pr_timestamp -gt $TAG_TIMESTAMP ]; then
+        contribution=$(echo $line | cut -d# -f3- | perl -pe 's/^/ - /')
+        contributor=$(echo $line | cut -d# -f2)
+        echo "$contribution - [@$contributor](https://github.com/$contributor)"
+    fi 
+done < PR-list-all.txt > local-release-notes.txt
 
 echo -e "\n## arrow\n" >> $BASEDIR/arrow/release-notes-all.txt
 cat local-release-notes.txt >> $BASEDIR/arrow/release-notes-all.txt
