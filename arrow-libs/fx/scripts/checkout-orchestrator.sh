f5c23d3dcc3dea3169f668d18c050308550eeea8
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-10-02 15:55:03 +0200

    Configuration: support both SSH and HTTPS

--- arrow-libs/fx/scripts/checkout-orchestrator.sh
+++ arrow-libs/fx/scripts/checkout-orchestrator.sh
@@ -5,5 +5,5 @@ cd ${ROOTDIR}/..
 BASEDIR=$(pwd)
 if [ ! -d arrow ]; then
     echo "Clone arrow repository in $BASEDIR/arrow ..."
-    git clone git@github.com:arrow-kt/arrow.git
+    git clone git@github.com:arrow-kt/arrow.git 2> /dev/null || git clone https://github.com/arrow-kt/arrow.git
 fi
