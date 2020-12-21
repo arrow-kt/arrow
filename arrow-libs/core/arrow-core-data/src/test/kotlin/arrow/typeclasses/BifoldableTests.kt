c050d67cf21659051a50257d96c563fc69756207
Simon Vergauwen <nomisRev@users.noreply.github.com>
2020-12-21 23:53:47 +0100

    Remove mtl dependency from Arrow Core Data (#277)
    * Remove mtl dependency from Arrow Core Data
    
    * Remove dependency from build.gradle
    
    Co-authored-by: Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
    Co-authored-by: Raúl Raja Martínez <raulraja@gmail.com>

--- arrow-libs/core/arrow-core-data/src/test/kotlin/arrow/typeclasses/BifoldableTests.kt
+++ arrow-libs/core/arrow-core-data/src/test/kotlin/arrow/typeclasses/BifoldableTests.kt
@@ -1,32 +1,22 @@
 package arrow.typeclasses
 
-import arrow.Kind2
 import arrow.core.Either
+import arrow.core.EitherOf
 import arrow.core.ForEither
 import arrow.core.extensions.either.bifoldable.bifoldable
-import arrow.mtl.typeclasses.Nested
-import arrow.mtl.typeclasses.binest
-import arrow.mtl.typeclasses.compose
 import arrow.core.test.UnitSpec
 import arrow.core.test.generators.GenK2
 import arrow.core.test.generators.either
-import arrow.core.test.generators.intSmall
 import arrow.core.test.laws.BifoldableLaws
 import io.kotlintest.properties.Gen
 
 class BifoldableTests : UnitSpec() {
   init {
-    val eitherComposeEither = Either.bifoldable().compose(Either.bifoldable())
-
-    val eitherGen = Gen.either(Gen.intSmall(), Gen.intSmall())
-
-    val genK2 = object : GenK2<Nested<ForEither, ForEither>> {
-      override fun <A, B> genK(genA: Gen<A>, genB: Gen<B>): Gen<Kind2<Nested<ForEither, ForEither>, A, B>> =
-        Gen.either(eitherGen, Gen.either(genA, genB)).map {
-          it.binest()
-        } as Gen<Kind2<Nested<ForEither, ForEither>, A, B>>
+    val genK2 = object : GenK2<ForEither> {
+      override fun <A, B> genK(genA: Gen<A>, genB: Gen<B>): Gen<EitherOf<A, B>> =
+        Gen.either(genA, genB) as Gen<EitherOf<A, B>>
     }
 
-    testLaws(BifoldableLaws.laws(eitherComposeEither, genK2))
+    testLaws(BifoldableLaws.laws(Either.bifoldable(), genK2))
   }
 }
