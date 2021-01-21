487b48af7b735470cefa6d20854192e1683e2b56
Stojan Anastasov <stolea@gmail.com>
2021-01-21 08:56:42 +0100

    Flow.retry using Schedule (#378)

---
+++ arrow-libs/fx/arrow-fx-coroutines/src/main/kotlin/arrow/fx/coroutines/flow.kt
@@ -0,0 +1,57 @@
+@file:JvmMultifileClass
+@file:JvmName("FlowExtensions")
+
+package arrow.fx.coroutines
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.retryWhen
+
+/**
+ * Retries collection of the given flow when an exception occurs in the upstream flow based on a decision by the [schedule].
+ * This operator is *transparent* to exceptions that occur in downstream flow and does not retry on exceptions that are thrown
+ * to cancel the flow.
+ *
+ * @see [Schedule] for how to build a schedule.
+ *
+ * ```kotlin:ank:playground
+ * import kotlinx.coroutines.flow.*
+ * import arrow.fx.coroutines.*
+ * suspend fun main(): Unit {
+ *   var counter = 0
+ *   val flow = flow {
+ *    emit(a)
+ *    if (++counter <= 5) throw RuntimeException("Bang!")
+ *   }
+ *   //sampleStart
+ *  val sum = flow.retry(Schedule.recurs(5))
+ *    .reduce { acc, int -> acc + int }
+ *   //sampleEnd
+ *   println(result)
+ * }
+ * ```
+ *
+ * @param schedule - the [Schedule] used for retrying the collection of the flow
+ */
+fun <A, B> Flow<A>.retry(schedule: Schedule<Throwable, B>): Flow<A> = flow {
+  (schedule as Schedule.ScheduleImpl<Any?, Throwable, B>)
+  var dec: Schedule.Decision<Any?, B>
+  var state: Any? = schedule.initialState()
+
+  val retryWhen = retryWhen { cause, _ ->
+    dec = schedule.update(cause, state)
+    state = dec.state
+
+    if (dec.cont) {
+      delay(dec.delay.millis)
+      true
+    } else {
+      false
+    }
+  }
+  retryWhen.collect {
+    emit(it)
+  }
+}
---
+++ arrow-libs/fx/arrow-fx-coroutines/src/test/kotlin/arrow/fx/coroutines/FlowTest.kt
@@ -0,0 +1,69 @@
+package arrow.fx.coroutines
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.longs.shouldBeGreaterThanOrEqual
+import io.kotest.matchers.longs.shouldBeLessThan
+import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.positiveInts
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.reduce
+import kotlinx.coroutines.test.runBlockingTest
+
+class FlowTest : ArrowFxSpec(spec = {
+
+  "Retry - flow fails" {
+    checkAll(Arb.int(), Arb.positiveInts(10)) { a, n ->
+      var counter = 0
+      val e = shouldThrow<RuntimeException> {
+        flow {
+          emit(a)
+          if (++counter <= 11) throw RuntimeException("Bang!")
+        }.retry(Schedule.recurs(n))
+          .collect()
+      }
+      e.message shouldBe "Bang!"
+    }
+  }
+
+  "Retry - flow succeeds" {
+    checkAll(Arb.int(), Arb.int(5, 10)) { a, n ->
+      var counter = 0
+      val sum = flow {
+        emit(a)
+        if (++counter <= 5) throw RuntimeException("Bang!")
+      }.retry(Schedule.recurs(n))
+        .reduce { acc, int -> acc + int }
+
+      sum shouldBe a * 6
+    }
+  }
+
+  "Retry - schedule with delay" {
+    runBlockingTest {
+      checkAll(Arb.int(), Arb.int(100, 1000)) { a, delayMs ->
+        val start = currentTime
+        val timestamps = mutableListOf<Long>()
+        shouldThrow<RuntimeException> {
+          flow {
+            emit(a)
+            timestamps.add(currentTime)
+            throw RuntimeException("Bang!")
+          }
+            .retry(Schedule.recurs<Throwable>(2) and Schedule.spaced(delayMs.milliseconds))
+            .collect()
+        }
+        timestamps.size shouldBe 3
+
+        // total run should be between start time + delay * 3 AND start + tolerance %
+        val min = start + (delayMs * 2).milliseconds.millis
+        val max = min + delayMs / 10
+
+        timestamps.last() shouldBeGreaterThanOrEqual min
+        timestamps.last() shouldBeLessThan max
+      }
+    }
+  }
+})
