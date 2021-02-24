6b3eea3ce89208108bc7d75f757531200c7c87ef
Simon Vergauwen <vergauwen.simon@gmail.com>
2021-02-24 12:40:21 +0100

    Monoid

--- arrow-libs/core/arrow-core/src/main/kotlin/arrow/core/extensions/MonoidInvariant.kt
+++ arrow-libs/core/arrow-core/src/main/kotlin/arrow/core/extensions/MonoidInvariant.kt
@@ -1,13 +1,15 @@
 package arrow.core.extensions
 
-import arrow.extension
 import arrow.typeclasses.ForMonoid
 import arrow.typeclasses.Invariant
 import arrow.typeclasses.Monoid
 import arrow.typeclasses.MonoidOf
 import arrow.typeclasses.fix
 
-@extension
+@Deprecated(
+  message = "Invariant typeclass is deprecated and will be removed in 0.13.0. Use concrete methods on Map.",
+  level = DeprecationLevel.WARNING
+)
 interface MonoidInvariant<A> : Invariant<ForMonoid> {
   override fun <A, B> MonoidOf<A>.imap(f: (A) -> B, g: (B) -> A): Monoid<B> =
     object : Monoid<B> {
