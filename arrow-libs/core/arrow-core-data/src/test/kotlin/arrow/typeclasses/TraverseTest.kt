0770c21632c30c57c42820436484d21755385fc1
Simon Vergauwen <vergauwen.simon@gmail.com>
2021-02-26 18:37:44 +0100

    Add missing import after merge

--- arrow-libs/core/arrow-core-data/src/test/kotlin/arrow/typeclasses/TraverseTest.kt
+++ arrow-libs/core/arrow-core-data/src/test/kotlin/arrow/typeclasses/TraverseTest.kt
@@ -3,12 +3,13 @@ package arrow.typeclasses
 import arrow.core.Left
 import arrow.core.Right
 import arrow.core.sequenceEither
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class TraverseTest : StringSpec({
   "traverse is stacksafe over very long collections and short circuits properly" {
     // This has to traverse 50k elements till it reaches None and terminates
     generateSequence(0) { it + 1 }.map { if (it < 50_000) Right(it) else Left(Unit) }
-      .sequenceEither() shouldbe Left(Unit)
+      .sequenceEither() shouldBe Left(Unit)
   }
 })
