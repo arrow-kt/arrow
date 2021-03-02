c412b6beba00bf7ce92245c4a7566fc7a4f582a5
Simon Vergauwen <vergauwen.simon@gmail.com>
2021-03-02 09:09:34 +0100

    Re-add HashDeprecation to Hashed

--- arrow-libs/core/arrow-core-data/src/main/kotlin/arrow/core/Hashed.kt
+++ arrow-libs/core/arrow-core-data/src/main/kotlin/arrow/core/Hashed.kt
@@ -28,9 +28,12 @@ typealias HashedKindedJ<A> = io.kindedj.Hk<ForHashed, A>
 inline fun <A> HashedOf<A>.fix(): Hashed<A> =
   this as Hashed<A>
 
+const val HashDeprecation = "Hash is deprecated in favor of hashCode(), since Kotlin's Std doesn't take Hash into account"
+
 /**
  * Wrapper type that caches a values precomputed hash with the value
  *
  * Provides a fast inequality check with its [Eq] instance and its [Hash] instance will use the cached hash.
  */
+@Deprecated(HashDeprecation)
 data class Hashed<A>(val hash: Int, val value: A) : HashedOf<A>
