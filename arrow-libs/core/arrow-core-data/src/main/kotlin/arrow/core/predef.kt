d88871a54fe44feaa48b5a14dd0e674b50b8c7ad
Simon Vergauwen <vergauwen.simon@gmail.com>
2021-03-04 14:05:53 +0100

    Deprecate Arrow Core predef.kt functions

--- arrow-libs/core/arrow-core-data/src/main/kotlin/arrow/core/predef.kt
+++ arrow-libs/core/arrow-core-data/src/main/kotlin/arrow/core/predef.kt
@@ -2,11 +2,32 @@ package arrow.core
 
 inline fun <A> identity(a: A): A = a
 
+@Deprecated(
+  "Use curried from the arrow.syntax.* package",
+  ReplaceWith(
+    "this.curried()",
+    "arrow.syntax.curried"
+  )
+)
 inline fun <A, B, Z> ((A, B) -> Z).curry(): (A) -> (B) -> Z = { p1: A -> { p2: B -> this(p1, p2) } }
 
+@Deprecated(
+  "Use compose from the arrow.syntax.* package",
+  ReplaceWith(
+    "this.compose(f)",
+    "arrow.syntax.compose"
+  )
+)
 infix fun <A, B, C> ((B) -> C).compose(f: (A) -> B): (A) -> C =
   AndThen(this).compose(f)
 
+@Deprecated(
+  "Use andThen from the arrow.syntax.* package",
+  ReplaceWith(
+    "this.andThen(g)",
+    "arrow.syntax.andThen"
+  )
+)
 infix fun <A, B, C> ((A) -> B).andThen(g: (B) -> C): (A) -> C =
   AndThen(this).andThen(g)
 
