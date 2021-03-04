6249e766b61ddfb9a668e3b73bab131d0217d093
Simon Vergauwen <vergauwen.simon@gmail.com>
2021-03-04 15:01:29 +0100

    Remove the duplicated AndThe data type

--- arrow-libs/core/arrow-core-data/src/main/kotlin/arrow/core/predef.kt
+++ arrow-libs/core/arrow-core-data/src/main/kotlin/arrow/core/predef.kt
@@ -1,35 +1,40 @@
 package arrow.core
 
+import arrow.syntax.function.curried
+import arrow.syntax.function.andThen as AndThen
+import arrow.syntax.function.compose as Compose
+
 inline fun <A> identity(a: A): A = a
 
 @Deprecated(
   "Use curried from the arrow.syntax.* package",
   ReplaceWith(
     "this.curried()",
-    "arrow.syntax.curried"
+    "arrow.syntax.function.curried"
   )
 )
-inline fun <A, B, Z> ((A, B) -> Z).curry(): (A) -> (B) -> Z = { p1: A -> { p2: B -> this(p1, p2) } }
+inline fun <A, B, Z> ((A, B) -> Z).curry(): (A) -> (B) -> Z =
+  this.curried()
 
 @Deprecated(
   "Use compose from the arrow.syntax.* package",
   ReplaceWith(
     "this.compose(f)",
-    "arrow.syntax.compose"
+    "arrow.syntax.function.compose"
   )
 )
 infix fun <A, B, C> ((B) -> C).compose(f: (A) -> B): (A) -> C =
-  AndThen(this).compose(f)
+  this Compose f
 
 @Deprecated(
   "Use andThen from the arrow.syntax.* package",
   ReplaceWith(
     "this.andThen(g)",
-    "arrow.syntax.andThen"
+    "arrow.syntax.function.andThen"
   )
 )
 infix fun <A, B, C> ((A) -> B).andThen(g: (B) -> C): (A) -> C =
-  AndThen(this).andThen(g)
+  this AndThen g
 
 internal object ArrowCoreInternalException : RuntimeException(
   "Arrow-Core internal error. Please let us know and create a ticket at https://github.com/arrow-kt/arrow-core/issues/new/choose",
