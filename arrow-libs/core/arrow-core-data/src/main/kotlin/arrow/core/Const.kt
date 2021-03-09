b40bc275a37d7cd43e307128d6caac3bf6cd9c6d
Simon Vergauwen <vergauwen.simon@gmail.com>
2021-03-09 10:31:37 +0100

    Const

--- arrow-libs/core/arrow-core-data/src/main/kotlin/arrow/core/Const.kt
+++ arrow-libs/core/arrow-core-data/src/main/kotlin/arrow/core/Const.kt
@@ -57,159 +57,6 @@ data class Const<A, out T>(private val value: A) : ConstOf<A, T> {
   companion object {
     fun <A, T> just(a: A): Const<A, T> =
       Const(a)
-
-    inline fun <A, B, C, T> mapN(
-      SG: Semigroup<A>,
-      b: Const<A, B>,
-      c: Const<A, C>,
-      map: (A, B) -> T
-    ): Const<A, T> =
-      b.retag<T>()
-        .combine(SG, c.retag())
-
-    inline fun <A, B, C, D, T> mapN(
-      SG: Semigroup<A>,
-      b: Const<A, B>,
-      c: Const<A, C>,
-      d: Const<A, D>,
-      map: (A, B, C) -> T
-    ): Const<A, T> =
-      b.retag<T>()
-        .combine(SG, c.retag())
-        .combine(SG, d.retag())
-
-    inline fun <A, B, C, D, E, T> mapN(
-      SG: Semigroup<A>,
-      b: Const<A, B>,
-      c: Const<A, C>,
-      d: Const<A, D>,
-      e: Const<A, E>,
-      map: (A, B, C, D) -> T
-    ): Const<A, T> =
-      b.retag<T>()
-        .combine(SG, c.retag())
-        .combine(SG, d.retag())
-        .combine(SG, e.retag())
-
-    inline fun <A, B, C, D, E, F, T> mapN(
-      SG: Semigroup<A>,
-      b: Const<A, B>,
-      c: Const<A, C>,
-      d: Const<A, D>,
-      e: Const<A, E>,
-      f: Const<A, F>,
-      map: (A, B, C, D, E) -> T
-    ): Const<A, T> =
-      b.retag<T>()
-        .combine(SG, c.retag())
-        .combine(SG, d.retag())
-        .combine(SG, e.retag())
-        .combine(SG, f.retag())
-
-    inline fun <A, B, C, D, E, F, G, T> mapN(
-      SG: Semigroup<A>,
-      b: Const<A, B>,
-      c: Const<A, C>,
-      d: Const<A, D>,
-      e: Const<A, E>,
-      f: Const<A, F>,
-      g: Const<A, G>,
-      map: (A, B, C, D, E, F) -> T
-    ): Const<A, T> =
-      b.retag<T>()
-        .combine(SG, c.retag())
-        .combine(SG, d.retag())
-        .combine(SG, e.retag())
-        .combine(SG, f.retag())
-        .combine(SG, g.retag())
-
-    inline fun <A, B, C, D, E, F, G, H, T> mapN(
-      SG: Semigroup<A>,
-      b: Const<A, B>,
-      c: Const<A, C>,
-      d: Const<A, D>,
-      e: Const<A, E>,
-      f: Const<A, F>,
-      g: Const<A, G>,
-      h: Const<A, H>,
-      map: (A, B, C, D, E, F, G) -> T
-    ): Const<A, T> =
-      b.retag<T>()
-        .combine(SG, c.retag())
-        .combine(SG, d.retag())
-        .combine(SG, e.retag())
-        .combine(SG, f.retag())
-        .combine(SG, g.retag())
-        .combine(SG, h.retag())
-
-    inline fun <A, B, C, D, E, F, G, H, I, T> mapN(
-      SG: Semigroup<A>,
-      b: Const<A, B>,
-      c: Const<A, C>,
-      d: Const<A, D>,
-      e: Const<A, E>,
-      f: Const<A, F>,
-      g: Const<A, G>,
-      h: Const<A, H>,
-      i: Const<A, I>,
-      map: (A, B, C, D, E, F, G, H) -> T
-    ): Const<A, T> =
-      b.retag<T>()
-        .combine(SG, c.retag())
-        .combine(SG, d.retag())
-        .combine(SG, e.retag())
-        .combine(SG, f.retag())
-        .combine(SG, g.retag())
-        .combine(SG, h.retag())
-        .combine(SG, i.retag())
-
-    inline fun <A, B, C, D, E, F, G, H, I, J, T> mapN(
-      SG: Semigroup<A>,
-      b: Const<A, B>,
-      c: Const<A, C>,
-      d: Const<A, D>,
-      e: Const<A, E>,
-      f: Const<A, F>,
-      g: Const<A, G>,
-      h: Const<A, H>,
-      i: Const<A, I>,
-      j: Const<A, J>,
-      map: (A, B, C, D, E, F, G, H, I) -> T
-    ): Const<A, T> =
-      b.retag<T>()
-        .combine(SG, c.retag())
-        .combine(SG, d.retag())
-        .combine(SG, e.retag())
-        .combine(SG, f.retag())
-        .combine(SG, g.retag())
-        .combine(SG, h.retag())
-        .combine(SG, i.retag())
-        .combine(SG, j.retag())
-
-    inline fun <A, B, C, D, E, F, G, H, I, J, K, T> mapN(
-      SG: Semigroup<A>,
-      b: Const<A, B>,
-      c: Const<A, C>,
-      d: Const<A, D>,
-      e: Const<A, E>,
-      f: Const<A, F>,
-      g: Const<A, G>,
-      h: Const<A, H>,
-      i: Const<A, I>,
-      j: Const<A, J>,
-      k: Const<A, K>,
-      map: (A, B, C, D, E, F, G, H, I, J) -> T
-    ): Const<A, T> =
-      b.retag<T>()
-        .combine(SG, c.retag())
-        .combine(SG, d.retag())
-        .combine(SG, e.retag())
-        .combine(SG, f.retag())
-        .combine(SG, g.retag())
-        .combine(SG, h.retag())
-        .combine(SG, i.retag())
-        .combine(SG, j.retag())
-        .combine(SG, k.retag())
   }
 
   fun value(): A =
@@ -217,6 +64,149 @@ data class Const<A, out T>(private val value: A) : ConstOf<A, T> {
 
   fun <U> map(f: (T) -> U): Const<A, U> =
     retag()
+
+  inline fun <B, C> zip(
+    SG: Semigroup<A>,
+    b: Const<A, B>,
+    map: (T, B) -> C
+  ): Const<A, C> =
+    b.retag<C>().combine(SG, b.retag())
+
+  inline fun <B, C, D> zip(
+    SG: Semigroup<A>,
+    b: Const<A, B>,
+    c: Const<A, C>,
+    map: (T, B, C) -> D
+  ): Const<A, D> =
+    retag<D>()
+      .combine(SG, b.retag())
+      .combine(SG, c.retag())
+
+  inline fun <B, C, D, E> zip(
+    SG: Semigroup<A>,
+    b: Const<A, B>,
+    c: Const<A, C>,
+    d: Const<A, D>,
+    map: (T, B, C, D) -> E
+  ): Const<A, E> =
+    retag<E>()
+      .combine(SG, b.retag())
+      .combine(SG, c.retag())
+      .combine(SG, d.retag())
+
+  inline fun <B, C, D, E, F> zip(
+    SG: Semigroup<A>,
+    b: Const<A, B>,
+    c: Const<A, C>,
+    d: Const<A, D>,
+    e: Const<A, E>,
+    map: (T, B, C, D, E) -> F
+  ): Const<A, F> =
+    retag<F>()
+      .combine(SG, b.retag())
+      .combine(SG, c.retag())
+      .combine(SG, d.retag())
+      .combine(SG, e.retag())
+
+  inline fun <B, C, D, E, F, G> zip(
+    SG: Semigroup<A>,
+    b: Const<A, B>,
+    c: Const<A, C>,
+    d: Const<A, D>,
+    e: Const<A, E>,
+    f: Const<A, F>,
+    map: (A, B, C, D, E, F) -> G
+  ): Const<A, G> =
+    retag<G>()
+      .combine(SG, b.retag())
+      .combine(SG, c.retag())
+      .combine(SG, d.retag())
+      .combine(SG, e.retag())
+      .combine(SG, f.retag())
+
+  inline fun <B, C, D, E, F, G, H> zip(
+    SG: Semigroup<A>,
+    b: Const<A, B>,
+    c: Const<A, C>,
+    d: Const<A, D>,
+    e: Const<A, E>,
+    f: Const<A, F>,
+    g: Const<A, G>,
+    map: (A, B, C, D, E, F, G) -> H
+  ): Const<A, T> =
+    retag<T>()
+      .combine(SG, b.retag())
+      .combine(SG, c.retag())
+      .combine(SG, d.retag())
+      .combine(SG, e.retag())
+      .combine(SG, f.retag())
+      .combine(SG, g.retag())
+
+  inline fun <B, C, D, E, F, G, H, I> zip(
+    SG: Semigroup<A>,
+    b: Const<A, B>,
+    c: Const<A, C>,
+    d: Const<A, D>,
+    e: Const<A, E>,
+    f: Const<A, F>,
+    g: Const<A, G>,
+    h: Const<A, H>,
+    map: (A, B, C, D, E, F, G, H) -> I
+  ): Const<A, T> =
+    retag<T>()
+      .combine(SG, b.retag())
+      .combine(SG, c.retag())
+      .combine(SG, d.retag())
+      .combine(SG, e.retag())
+      .combine(SG, f.retag())
+      .combine(SG, g.retag())
+      .combine(SG, h.retag())
+
+  inline fun <B, C, D, E, F, G, H, I, J> zip(
+    SG: Semigroup<A>,
+    b: Const<A, B>,
+    c: Const<A, C>,
+    d: Const<A, D>,
+    e: Const<A, E>,
+    f: Const<A, F>,
+    g: Const<A, G>,
+    h: Const<A, H>,
+    i: Const<A, I>,
+    map: (A, B, C, D, E, F, G, H, I) -> J
+  ): Const<A, J> =
+    retag<J>()
+      .combine(SG, b.retag())
+      .combine(SG, c.retag())
+      .combine(SG, d.retag())
+      .combine(SG, e.retag())
+      .combine(SG, f.retag())
+      .combine(SG, g.retag())
+      .combine(SG, h.retag())
+      .combine(SG, i.retag())
+
+  inline fun <B, C, D, E, F, G, H, I, J, K> zip(
+    SG: Semigroup<A>,
+    b: Const<A, B>,
+    c: Const<A, C>,
+    d: Const<A, D>,
+    e: Const<A, E>,
+    f: Const<A, F>,
+    g: Const<A, G>,
+    h: Const<A, H>,
+    i: Const<A, I>,
+    j: Const<A, J>,
+    map: (A, B, C, D, E, F, G, H, I, J) -> K
+  ): Const<A, K> =
+    retag<K>()
+      .combine(SG, b.retag())
+      .combine(SG, c.retag())
+      .combine(SG, d.retag())
+      .combine(SG, e.retag())
+      .combine(SG, f.retag())
+      .combine(SG, g.retag())
+      .combine(SG, h.retag())
+      .combine(SG, i.retag())
+      .combine(SG, j.retag())
 
   @Deprecated(ShowDeprecation)
   fun show(SA: Show<A>): String =
