1348b3fef855908b212b4c7a3745827bbfda320e
Simon Vergauwen <vergauwen.simon@gmail.com>
2021-03-10 13:15:10 +0100

    ktlintFormat

--- arrow-libs/core/arrow-core-data/src/main/kotlin/arrow/core/set.kt
+++ arrow-libs/core/arrow-core-data/src/main/kotlin/arrow/core/set.kt
@@ -1,9 +1,5 @@
 package arrow.core
 
-import arrow.typeclasses.Monoid
-import arrow.typeclasses.Semigroup
-import kotlin.collections.plus as _plus
-
 @Deprecated("Receiver SetExtensions object is deprecated, prefer to turn Set functions into top-level functions")
 object SetExtensions
 
