3180a9869a0510b20d16676d9a1df835df52848f
Simon Vergauwen <nomisRev@users.noreply.github.com>
2021-03-23 17:00:20 +0100

    Arrow Fx: Clean-up new code & breaking changes since 0.12.0 (#2333)

--- arrow-libs/fx/arrow-fx-stm/src/main/kotlin/arrow/fx/stm/STM.kt
+++ arrow-libs/fx/arrow-fx-stm/src/main/kotlin/arrow/fx/stm/STM.kt
@@ -1,6 +1,5 @@
 package arrow.fx.stm
 
-import arrow.core.Tuple2
 import arrow.fx.stm.internal.STMTransaction
 import arrow.fx.stm.internal.alterHamtWithHash
 import arrow.fx.stm.internal.lookupHamtWithHash
@@ -1314,30 +1313,6 @@ interface STM {
    * ```
    */
   operator fun <K, V> TMap<K, V>.set(k: K, v: V): Unit = insert(k, v)
-
-  /**
-   * Add a key value pair to the map
-   *
-   * ```kotlin:ank:playground
-   * import arrow.core.toT
-   * import arrow.fx.stm.TMap
-   * import arrow.fx.stm.atomically
-   *
-   * suspend fun main() {
-   *   //sampleStart
-   *   val tmap = TMap.new<Int, String>()
-   *   atomically {
-   *     tmap += (1 toT "Hello")
-   *   }
-   *   //sampleEnd
-   * }
-   * ```
-   */
-  @Deprecated(
-    "Deprecated in favor of Kotlin's Pair",
-    ReplaceWith("this.plusAssign(Pair(kv.a, kv.b))")
-  )
-  operator fun <K, V> TMap<K, V>.plusAssign(kv: Tuple2<K, V>): Unit = insert(kv.a, kv.b)
 
   /**
    * Add a key value pair to the map
