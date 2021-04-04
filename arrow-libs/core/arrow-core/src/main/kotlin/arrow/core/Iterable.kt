641df8c752a5537c8f0e4f0cfffce0175567ae1e
Jannis <overesch.jannis@gmail.com>
2021-04-05 01:12:48 +0200

    FoldRight order

--- arrow-libs/core/arrow-core/src/main/kotlin/arrow/core/Iterable.kt
+++ arrow-libs/core/arrow-core/src/main/kotlin/arrow/core/Iterable.kt
@@ -5,7 +5,6 @@ import arrow.core.Either.Left
 import arrow.core.Either.Right
 import arrow.typeclasses.Monoid
 import arrow.typeclasses.Semigroup
-import kotlin.collections.foldRight as _foldRight
 
 inline fun <B, C, D, E> Iterable<B>.zip(
   c: Iterable<C>,
@@ -282,10 +281,7 @@ internal fun <T> Iterable<T>.collectionSizeOrDefault(default: Int): Int =
   if (this is Collection<*>) this.size else default
 
 inline fun <A, B> Iterable<A>.foldRight(initial: B, operation: (A, acc: B) -> B): B =
-  when (this) {
-    is List -> _foldRight(initial, operation)
-    else -> reversed().fold(initial) { acc, a -> operation(a, acc) }
-  }
+  fold(initial) { acc, a -> operation(a, acc) }
 
 inline fun <E, A, B> Iterable<A>.traverseEither(f: (A) -> Either<E, B>): Either<E, List<B>> =
   foldRight<A, Either<E, List<B>>>(emptyList<B>().right()) { a, acc ->
