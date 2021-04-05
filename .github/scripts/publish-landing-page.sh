3d7c294395da5ca5d3f9f8c03174615c46fd6443
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2021-04-05 11:38:50 +0200

    Remove test

--- .github/scripts/publish-landing-page.sh
+++ .github/scripts/publish-landing-page.sh
@@ -8,11 +8,9 @@ cd _site/
 for file in ${MAIN_CONTENT[*]}; do
     if [ -f "$file" ]; then
         echo "Copying $file ..."
-        # TODO: --dryrun
-        aws s3 cp $file s3://$S3_BUCKET/$file --dryrun >> $BASEDIR/logs/aws_sync.log
+        aws s3 cp $file s3://$S3_BUCKET/$file >> $BASEDIR/logs/aws_sync.log
         continue
     fi
     echo "Sync $file ..."
-    # TODO: --dryrun
-    aws s3 sync $file s3://$S3_BUCKET/$file --dryrun --delete >> $BASEDIR/logs/aws_sync.log
+    aws s3 sync $file s3://$S3_BUCKET/$file --delete >> $BASEDIR/logs/aws_sync.log
 done
