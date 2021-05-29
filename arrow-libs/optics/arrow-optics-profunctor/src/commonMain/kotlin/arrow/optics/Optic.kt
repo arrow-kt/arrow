494fe6cbeee3e467e6af6506898058fd1718496f
Jannis <overesch.jannis@gmail.com>
2021-05-29 16:21:36 +0200

    Document internals

--- arrow-libs/optics/arrow-optics-profunctor/src/commonMain/kotlin/arrow/optics/Optic.kt
+++ arrow-libs/optics/arrow-optics-profunctor/src/commonMain/kotlin/arrow/optics/Optic.kt
@@ -5,6 +5,150 @@ import arrow.optics.internal.Profunctor
 
 typealias Optic_<K, I, S, A> = Optic<K, I, S, S, A, A>
 
+/**
+ * ** Base interface for optics.
+ *
+ * > Note: This documents the internals of optics and is rarely ever relevant for users
+ *
+ * An optic is a profunctor transformation from a profunctor that acts upon a focus to
+ *  a profunctor that acts upon a source. `Pro<A, B> -> Pro<S, T>`.
+ *
+ * > If you are not familiar with profunctors, read the documentation for the profunctor
+ *  typeclass (and the rest of its hierarchy) first.
+ *
+ * Optics:
+ *
+ *          Source/Target
+ *         S --------------> T
+ *          \              /
+ *   Getter  \            /  Setter
+ *  (Zoom in) \          / (Zoom-out)
+ *             \        /
+ *              A ---> B
+ *               Focus
+ *
+ * As you can see, the focus profunctor `A -> B` is the zoomed in part of the optic,
+ *  it operates on the elements we have focused. When running our transformation
+ *  we get back a profunctor from source to target which "embeds" our focus transformation
+ *  and thus represents our complete runnable optic.
+ *
+ * Another important observation is that we effectively have two sides:
+ * - The contravariant "getter" side of an optic `S -> A`
+ * - The covariant "setter" side of an optic `B -> T`
+ *
+ * This is very important to understand how the actual profunctor instances are used
+ *  to achieve their very different uses on the same optic.
+ *
+ * Creating the final profunctor usually involves passing in a terminal focus operation
+ *  such as `f` in `optic.modify(s, f)`. This terminal operation together with
+ *  the chosen profunctor instance determines the behavior of the optic.
+ *
+ * ** Profunctor instances:
+ *
+ * ```kotlin
+ * class Forget<R, A, B>(val f: (A) -> R) {
+ *   fun <C, D> dimap(f: (C -> A), g: (B) -> D): Forget<R, C, D> =
+ *     Forget { c -> this.f(f(c)) } // B/D are phantom types and thus free to reassign
+ * ```
+ *
+ * Forget is a profunctor which has no covariant side.
+ * As such, when used in the profunctor transform, it throws away the setter side
+ *  and, when used with `id` as the terminal operation, produces `Forget<A, S, T>`
+ *  which we can trivially pass `S` and get our focus `A` out. This perfectly fits the
+ *  getter use case. `Forget` can also do folds as long as `R` is a monoid. In this case
+ *  `Forget`'s `Traversing` instance is used with `Const` as an applicative, which simply
+ *  combines its elements.
+ *
+ * ```kotlin
+ * class Function1<A, B>(val f: (A) -> B)
+ * ```
+ *
+ * Function1 is a trivial profunctor which has no special effects other than composing
+ *  functions. It is the profunctor used when the setter side of an optic is needed without
+ *  any effects, such as when calling `modify/set`.
+ * Its terminal operation is often a transformation function passed from the user or
+ *  for `set` a constant function to the supplied element.
+ *
+ * ```kotlin
+ * class Star<F, A, B>(val f: (A) -> Kind<F, B>)
+ *   fun <C, D> dimap(AF: Functor<F>, f: (C -> A), g: (B) -> D): Star<F, C, D> =
+ *     Star { c -> AF.map(this.f(f(c)), g) }
+ *
+ * typealias Function1<A, B> == Star<ForIdentity, A, B> // Not used for perf reasons
+ * ```
+ *
+ * Star embeds and threads applicative effects through the optic.
+ * This makes it perfect for use when using `traverse-*` methods on the optic, other than
+ *  the fact that it exposes effects when running it has no difference to `Function1`
+ *
+ * ```kotlin
+ * class Tagged<A, B>(val b: B) {
+ *   fun <C, D> dimap(f: (C) -> A, g: (B) -> D): Tagged<C, D> =
+ *    Tagged(g(b))
+ * }
+ * ```
+ *
+ * Tagged is very similar to `Id` except that it includes a phantom argument as its
+ *  contravariant side. This it is opposite to `Forget` which has a phantom covariant side.
+ *
+ * Thus Tagged is excellent when only the setter side of an optic is needed, such as with
+ *  Prisms and `review`.
+ *
+ * ** The (phantom) parameters of the optic:
+ *
+ * `Optic` has a lot of type parameters, for those familiar with lenses `S`, `T`, `A`, `B`
+ *  should pose no problem to understand, however `K` and `I` take a bit to get used to,
+ *  especially when trying to decipher the compose method.
+ *
+ * *** Optics kind
+ *
+ * `K` is used to indicate the capabilities of an optic, all optics are profunctor transformation
+ *  but many require more advanced versions of the profunctor typeclass and some also
+ *  restrict the use of the final profunctor.
+ *
+ * For example, lenses require at least `Strong` from the profunctor and `Getter` requires
+ *  the use of `Forget` as an instance.
+ *
+ * To keep track of these restrictions/requirements a phantom parameter `K` is introduced.
+ * `K` will always be a phantom type and usually an empty interface.
+ * There are three general hierarchies used:
+ * - `FoldK`: One of the most general kinds, represents all optics that can be folded
+ * - `SetterK`: Another very general kind, represents everything that can be modified.
+ * - `ReviewK`: Represents reviews.
+ * All other optics kinds extend those three in some way, but also bring with them
+ *  unique requirements or abilities.
+ *
+ * `K` is also marked with `out` for variance to allow composing two different optics
+ *  so long as they have a common kind in their hierarchy.
+ *  E.g. composing a LensK with FoldK yield FoldK. PrismK and GetterK yield AffineFoldK
+ *
+ * This encoding leads to almost perfect inference when using compose with monomorphic optics,
+ *  it does however struggle when provided with polymorphic combinators for optics.
+ *  This can be seen by the boilerplate needed to make those work over any optic kind.
+ *  Those are technically not necessary, however they greatly improve the dsl for optics.
+ *
+ * *** Indices
+ *
+ * The last type parameter for optics to cover is `I`. `I` represents an index that
+ *  an optic can keep track of. This can be integers for list iteration, keys when
+ *  traversing all elements of a map or anything else really.
+ *
+ * For non-indexed terminal operations indices become phantom parameters and thus do
+ *  not affect the optic at all.
+ * When using indexed terminal operations, indexed variants of the profunctor instances
+ *  are used, which keep track of the indices. See `IxForget`, `IxStar`, `IxFunction`.
+ *
+ * The complicated part, however, isn't really what indices are or how they are used, but how they
+ *  compose.
+ *
+ * As you can see in the transform function, the result of a transform yields `I -> J` for
+ *  some existential `J`. This allows the index of the focus profunctor to differ from
+ *  the index of the optic, so long as one can map between the two.
+ *
+ * This leads to some type-tetris and nested functions in the compose functions, but is
+ *  really the only good way to embed indices decently well into profunctor optics.
+ */
+// TODO: Come back to indices docs when I understand them better...
 interface Optic<out K, I, in S, out T, A, B> {
   fun <P, J> Profunctor<P>.transform(focus: Pro<P, J, A, B>): Pro<P, (I) -> J, S, T>
 
--- arrow-libs/optics/arrow-optics-profunctor/src/commonMain/kotlin/arrow/optics/internal/Profunctor.kt
+++ arrow-libs/optics/arrow-optics-profunctor/src/commonMain/kotlin/arrow/optics/internal/Profunctor.kt
@@ -11,6 +11,22 @@ interface Pro<P, in I, in A, out B>
 //  ever fuse and produce equally performant implementations, also its not that much
 //  of an inconvenience since we don't have too many types implementing this
 
+/**
+ * A profunctor is a bifunctor with the first argument being contravariant and the
+ *  second argument covariant.
+ *
+ * The easiest profunctor to visualize is a function with one argument `(A) -> B`.
+ * This function is contravariant in `A` and covariant in `B`.
+ *
+ * A profunctor allows us to either contramap `lMap` or map `rMap` either of its arguments,
+ * or both at once using `dimap`
+ *
+ * For one argument functions `lMap == compose` `rMap == andThen/map`.
+ *
+ * This implementation of profunctors is tailored for optics and more specifically indexed optics.
+ * The `ixMap` method exists solely to contramap over the index and servers no other purpose
+ *  related to profunctos.
+ */
 interface Profunctor<P> {
   fun <I, A, B, C, D> Pro<P, I, B, C>.dimap(f: (A) -> B, g: (C) -> D): Pro<P, I, A, D>
   fun <I, A, B, C> Pro<P, I, B, C>.lMap(f: (A) -> B): Pro<P, I, A, C>
@@ -19,6 +35,12 @@ interface Profunctor<P> {
   fun <I, J, A, B> Pro<P, J, A, B>.ixMap(f: (I) -> J): Pro<P, I, A, B>
 }
 
+/**
+ * The strong typeclass extends profunctors with the ability to pass-through an argument.
+ *
+ * It essentially allows us to ignore an argument, in this case a part of a pair when
+ *  running a profunctor. With just `dimap` we always have to consume the full argument.
+ */
 interface Strong<P> : Profunctor<P> {
   fun <I, A, B, C> Pro<P, I, A, B>.first(): Pro<P, I, Pair<A, C>, Pair<B, C>>
   fun <I, A, B, C> Pro<P, I, A, B>.second(): Pro<P, I, Pair<C, A>, Pair<C, B>>
@@ -30,21 +52,41 @@ interface IxLinearF<I, S, T, A, B> {
   operator fun <F> invoke(FF: Functor<F>, s: S, f: (I, A) -> Kind<F, B>): Kind<F, T>
 }
 
+/**
+ * Co-strong is the dual of strong and is more relevant in lazy languages since it can be
+ *  used to implement looping with just profunctors.
+ *
+ * For this package it is only important in terms of the `Tagged` profunctor which has
+ *  a phantom type for the contravariant side and just holds a value we can easily
+ *  map over. It is also used in the `Re` profunctor which reverses the direction of the
+ *  underlying profunctor.
+ */
 interface CoStrong<P> : Profunctor<P> {
   fun <I, A, B, C> Pro<P, I, Pair<A, C>, Pair<B, C>>.unFirst(): Pro<P, I, A, B>
   fun <I, A, B, C> Pro<P, I, Pair<C, A>, Pair<C, B>>.unSecond(): Pro<P, I, A, B>
 }
 
+/**
+ * Choice augments our profunctor with the ability to branch based on its input.
+ */
 interface Choice<P> : Profunctor<P> {
   fun <I, A, B, C> Pro<P, I, A, B>.left(): Pro<P, I, Either<A, C>, Either<B, C>>
   fun <I, A, B, C> Pro<P, I, A, B>.right(): Pro<P, I, Either<C, A>, Either<C, B>>
 }
 
+/**
+ * Co-choice is the dual of choice and similarly only useful with the `Tagged` and `Re`
+ *  profunctors.
+ */
 interface CoChoice<P> : Profunctor<P> {
   fun <I, A, B, C> Pro<P, I, Either<A, C>, Either<B, C>>.unLeft(): Pro<P, I, A, B>
   fun <I, A, B, C> Pro<P, I, Either<C, A>, Either<C, B>>.unRight(): Pro<P, I, A, B>
 }
 
+/**
+ * Traversing extends profunctors with the ability to embed traversals (in VL style)
+ *  into the profunctor structure. This is what allows `Traversal` to exist.
+ */
 interface Traversing<P> : Strong<P>, Choice<P> {
   fun <I, S, T, A, B> Pro<P, I, A, B>.wander(f: WanderF<S, T, A, B>): Pro<P, I, S, T>
   fun <I, J, S, T, A, B> Pro<P, J, A, B>.iwander(f: IxWanderF<I, S, T, A, B>): Pro<P, (I) -> J, S, T>
@@ -53,6 +95,14 @@ interface Traversing<P> : Strong<P>, Choice<P> {
 // Traverse in disguise
 interface WanderF<S, T, A, B> {
   operator fun <F> invoke(AF: Applicative<F>, source: S, f: (A) -> Kind<F, B>): Kind<F, T>
+
+  /**
+   * Variant of invoke that generally uses `Applicative.apEval` or similar to lazily traverse
+   *  a structure. Optics usually choose either invoke or invokeLazy on their terminal
+   *  operation. E.g. firstOrNull uses invokeLazy to only traverse to the first element
+   *  even if the traversal is infinite. Others such as sum will use invoke since it is
+   *  strict in all elements anyway.
+   */
   fun <F> invokeLazy(AF: Applicative<F>, source: S, f: (A) -> Kind<F, B>): Kind<F, T> = invoke(AF, source, f)
 }
 
@@ -61,6 +111,10 @@ interface IxWanderF<I, S, T, A, B> {
   fun <F> invokeLazy(AF: Applicative<F>, source: S, f: (I, A) -> Kind<F, B>): Kind<F, T> = invoke(AF, source, f)
 }
 
+/**
+ * Mapping is a stronger form of Traversing in which the applicative used is Id.
+ * Rules out any effects, but also offers some flexibility. Used by setters.
+ */
 interface Mapping<P> : Traversing<P> {
   fun <I, S, T, A, B> Pro<P, I, A, B>.roam(f: (inner: (A) -> B, s: S) -> T): Pro<P, I, S, T>
   fun <I, J, S, T, A, B> Pro<P, J, A, B>.iroam(f: (inner: (I, A) -> B, s: S) -> T): Pro<P, (I) -> J, S, T>
@@ -125,14 +179,3 @@ internal interface ConstApplicative<R> : Applicative<Kind<ForConst, R>>, ConstFu
   override fun <A, B> map(fa: Kind<Kind<ForConst, R>, A>, f: (A) -> B): Kind<Kind<ForConst, R>, B> =
     fa as Const<R, B>
 }
-
-internal fun <F> Applicative<F>.backwards(): Applicative<F> =
-  object : Applicative<F> {
-    override fun <A, B> ap(ff: Kind<F, (A) -> B>, fa: Kind<F, A>): Kind<F, B> =
-      this@backwards.ap(
-        this@backwards.map(fa) { a: A -> { f: (A) -> B -> f(a) } },
-        ff
-      )
-
-    override fun <A> pure(a: A): Kind<F, A> = this@backwards.pure(a)
-  }
