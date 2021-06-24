08cb6f281d7bdd7cdecd34d27e877ba5cf8f661c
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2021-06-24 13:45:57 +0200

    Configuration: fixes  version

--- .github/scripts/set-env.sh
+++ .github/scripts/set-env.sh
@@ -9,18 +9,18 @@ if [ "$GITHUB_REF" == "refs/heads/main" ]; then
     LATEST_PUBLISHED_VERSION=$(curl -L https://repo1.maven.org/maven2/io/arrow-kt/arrow-core/maven-metadata.xml | ggrep -oP '<latest>\K[^<]*')
     if [ "$LATEST_PUBLISHED_VERSION" == "" ]; then exit 1; fi
     RELEASE_VERSION=$(grep LATEST_VERSION $BASEDIR/gradle.properties | cut -d= -f2)
-    NEW_RELEASE_VERSION_EXISTS=$([ "$LATEST_PUBLISHED_VERSION" == "$RELEASE_VERSION" ] && echo '0' || echo '1')
+    NEW_RELEASE_VERSION_EXISTS=$(expr "$LATEST_PUBLISHED_VERSION" != "$RELEASE_VERSION")
 else
     echo "Into release branch ..."
     BRANCH_VERSION=$(echo $GITHUB_REF | cut -d/ -f4)
     RELEASE_VERSION=$(grep LATEST_VERSION $BASEDIR/gradle.properties | cut -d= -f2)
-    NEW_RELEASE_VERSION_EXISTS=$([ "$BRANCH_VERSION" == "$RELEASE_VERSION" ] && echo '1' || echo '0')
-    if [ $NEW_RELEASE_VERSION_EXISTS == '0' ]; then
+    NEW_RELEASE_VERSION_EXISTS=$(expr "$BRANCH_VERSION" == "$RELEASE_VERSION")
+    if [ $NEW_RELEASE_VERSION_EXISTS == 0 ]; then
         perl -pe "s/^VERSION_NAME=.*/VERSION_NAME=$BRANCH_VERSION-SNAPSHOT/g" -i $BASEDIR/gradle.properties
     fi
 fi
 
-if [ $NEW_RELEASE_VERSION_EXISTS == '1' ]; then
+if [ $NEW_RELEASE_VERSION_EXISTS == 1 ]; then
     perl -pe "s/^VERSION_NAME=.*/VERSION_NAME=$RELEASE_VERSION/g" -i $BASEDIR/gradle.properties
     perl -pe "s/^org.gradle.parallel=.*/org.gradle.parallel=false/g" -i $BASEDIR/gradle.properties
 fi
