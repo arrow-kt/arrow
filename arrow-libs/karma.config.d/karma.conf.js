dd8cbcf682f59ac6417dea8a3303aac0c2758d86
Simon Vergauwen <nomisRev@users.noreply.github.com>
2021-07-08 20:35:07 +0200

    Use configured custom chrome headless

--- arrow-libs/karma.config.d/karma.conf.js
+++ arrow-libs/karma.config.d/karma.conf.js
@@ -1,9 +1,26 @@
 config.set({
+  "browsers": ["MyChromeHeadless"],
+  "customLaunchers": {
+      "MyChromeHeadless": {
+          base: "ChromeHeadless",
+          flags: [
+              "--allow-failed-policy-fetch-for-test",
+              "--allow-external-pages",
+              "--no-sandbox",
+              "--disable-web-security",
+              "--disable-setuid-sandbox",
+              "--enable-logging",
+              "--v=1"
+          ]
+      }
+  },
   "client": {
     "mocha": {
+      captureConsole: true,
       "timeout": 600000
     },
   },
   "pingTimeout": 600000,
+  "browserNoActivityTimeout": 600000,
   "browserDisconnectTimeout": 600000
 });
