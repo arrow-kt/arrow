801f2e8711f99664d498c3238f44232acf506125
Simon Vergauwen <nomisRev@users.noreply.github.com>
2021-07-27 20:55:24 +0200

    Add metered and fixedRate

--- arrow-libs/fx/arrow-fx-coroutines/src/commonMain/kotlin/arrow/fx/coroutines/flow.kt
+++ arrow-libs/fx/arrow-fx-coroutines/src/commonMain/kotlin/arrow/fx/coroutines/flow.kt
@@ -6,7 +6,6 @@ package arrow.fx.coroutines
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.delayEach
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.retryWhen
@@ -15,8 +14,6 @@ import kotlin.jvm.JvmMultifileClass
 import kotlin.jvm.JvmName
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
-import kotlin.time.TimeMark
-import kotlin.time.TimeSource
 
 /**
  * Retries collection of the given flow when an exception occurs in the upstream flow based on a decision by the [schedule].
@@ -65,6 +62,7 @@ public fun <A, B> Flow<A>.retry(schedule: Schedule<Throwable, B>): Flow<A> = flo
   }
 }
 
+/** Repeats the Flow forever */
 public fun <A> Flow<A>.repeat(): Flow<A> =
   flow {
     while (true) {
@@ -74,49 +72,73 @@ public fun <A> Flow<A>.repeat(): Flow<A> =
     }
   }
 
+/**
+ * Flow that emits [A] every [period] while taking into account how much time it takes downstream to consume the emission.
+ * If downstream takes longer to process than [period] than it immediately emits another [A].
+ *
+ * Use `onEach { delay(timeMillis) }` for an alternative that sleeps [period] between every element.
+ * This is different in that the time between every element is equal to the specified period,
+ * regardless of how much time it takes to process that tick downstream.
+ *
+ * i.e, for a period of 1 second and a delay(100), the timestamps of the emission would be 1s, 2s, 3s, ... when using [fixedRate].
+ * Whereas with `onEach { delay(timeMillis) }` it would run at timestamps 1s, 2.1s, 3.2s, ...
+ *
+ * @param period period between [Unit] emits of the resulting [Flow].
+ */
 @ExperimentalTime
 public fun <A> Flow<A>.metered(period: Duration): Flow<A> =
   fixedRate(period).zip(this) { _, a -> a }
 
-/**
- * Discrete flow that emits [Unit] every [period].
- *
- * Use `onEach { delay(timeMillis) }` for an alternative that sleeps [period] between every element.
- *
- * This operation differs in that the time between every element is roughly equal to the specified period,
- * regardless of how much time it takes to process that tick downstream.
- *
- * For example, with a 1 second period and a task that takes 100ms, the task would run at timestamps, 1s, 2s, 3s, etc when using `fixedRate.zip(task) { _, a -> a }`.
- * Whereas with delaying each element it would run at timestamps 1s, 2.1s, 3.2s, etc.
- *
- * In the case where task processing takes longer than a single period, 1 or more ticks are immediately emitted to "catch-up".
- * The `dampen` parameter controls whether a single tick is emitted or whether one per missed period is emitted.
- *
- * @param period period between emits of the resulting stream
- * @param dampen true if a single unit should be emitted when multiple periods have passed since last execution, false if a unit for each period should be emitted
- */
+public fun <A> Flow<A>.metered(period: Long): Flow<A> =
+  fixedRate(period).zip(this) { _, a -> a }
+
 @ExperimentalTime
 public fun fixedRate(
   period: Duration,
-  dampen: Boolean = true
+  dampen: Boolean = true,
+  timeStampInMillis: () -> Long = { timeInMillis() }
 ): Flow<Unit> =
-  if (period.inWholeNanoseconds == 0L) flowOf(Unit).repeat()
+  fixedRate(period.inWholeMilliseconds, dampen, timeStampInMillis)
+
+/**
+ * Flow that emits [Unit] every [period] while taking into account how much time it takes downstream to consume the emission.
+ * If downstream takes longer to process than [period] than it immediately emits another [Unit],
+ * if you set [dampen] to true it will send `n = downstream / period` [Unit] elements immediately.
+ *
+ * Use `onEach { delay(timeMillis) }` for an alternative that sleeps [period] between every element.
+ * This is different in that the time between every element is equal to the specified period,
+ * regardless of how much time it takes to process that tick downstream.
+ *
+ * i.e, for a period of 1 second and a delay(100), the timestamps of the emission would be 1s, 2s, 3s, ... when using [fixedRate].
+ * Whereas with `onEach { delay(timeMillis) }` it would run at timestamps 1s, 2.1s, 3.2s, ...
+ *
+ * @param period period between [Unit] emits of the resulting [Flow].
+ * @param dampen if you set [dampen] to true it will send `n` times [period] time it took downstream to process the emission.
+ * @param timeStampInMillis allows for supplying a different timestamp function, useful to override with `runBlockingTest`
+ */
+public fun fixedRate(
+  period: Long,
+  dampen: Boolean = true,
+  timeStampInMillis: () -> Long = { timeInMillis() }
+): Flow<Unit> =
+  if (period == 0L) flowOf(Unit).repeat()
   else flow {
-    val period = period.inWholeMilliseconds
-    var lastAwakeAt = timeInMillis()
+    val period = period
+    var lastAwakeAt = timeStampInMillis()
 
     while (true) {
-      val now = timeInMillis()
+      val now = timeStampInMillis()
       val next = lastAwakeAt + period
 
       if (next > now) {
-        delay(next - now)
+        val remaining = next - now
+        delay(remaining)
         emit(Unit)
         lastAwakeAt = next
       } else {
-        val ticks = (now - lastAwakeAt - 1) / period
+        val ticks: Long = (now - lastAwakeAt - 1) / period
         when {
-          ticks < 0 -> Unit
+          ticks < 0L -> Unit
           ticks == 0L || dampen -> emit(Unit)
           else -> repeat(ticks.toInt()) { emit(Unit) }
         }
@@ -124,3 +146,10 @@ public fun fixedRate(
       }
     }
   }
+
+public inline fun <A, B> Flow<A>.mapIndexed(crossinline f: suspend (Int, A) -> B): Flow<B> = flow {
+  var index = 0
+  collect { value ->
+    emit(f(index++, value))
+  }
+}
