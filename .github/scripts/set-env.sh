e933e490608b0fc7b0fb6e9a4b9e717aaaffe6be
franciscodr <francisco.d@47deg.com>
2021-09-14 22:12:33 +0200

    Testing windows job

--- .github/scripts/set-env.sh
+++ .github/scripts/set-env.sh
@@ -5,12 +5,12 @@ set -ex
 # echo "$JAVA_HOME_8_X64/bin" >> $GITHUB_PATH
 # echo "JAVA_HOME=$JAVA_HOME_8_X64" >> $GITHUB_ENV
 
-echo $OSTYPE
+echo "OS type: $OSTYPE"
 
 NEW_RELEASE_VERSION_EXISTS=0
-if [ "$GITHUB_REF" == "refs/heads/main" ]; then
+if [ "$GITHUB_REF" == "refs/heads/windows_build" ]; then
     if [[ $OSTYPE == 'darwin'* ]]; then
-      LATEST_PUBLISHED_VERSION=$(curl -L -N https://repo1.maven.org/maven2/io/arrow-kt/arrow-core/maven-metadata.xml | ggrep -oP '<latest>\K[^<]*')
+      LATEST_PUBLISHED_VERSION=$(curl -L https://repo1.maven.org/maven2/io/arrow-kt/arrow-core/maven-metadata.xml | ggrep -oP '<latest>\K[^<]*')
     else
       LATEST_PUBLISHED_VERSION=$(curl -L -N https://repo1.maven.org/maven2/io/arrow-kt/arrow-core/maven-metadata.xml | grep -oP '<latest>\K[^<]*')
     fi
