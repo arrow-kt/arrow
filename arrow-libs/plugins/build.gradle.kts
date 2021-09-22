63c034b9a4d3a3b7d3d7a22004eac08e9f903a99
Simon Vergauwen <nomisRev@users.noreply.github.com>
2021-09-22 23:29:49 +0200

    Remove new dokka gradle plugin

--- arrow-libs/plugins/build.gradle.kts
+++ arrow-libs/plugins/build.gradle.kts
@@ -14,10 +14,6 @@ dependencies {
 
 gradlePlugin {
   plugins {
-    register("documentation") {
-      id = "documentation"
-      implementationClass = "arrow.gradle.DocumentationPlugin"
-    }
     register("mpp-publish") {
       id = "mpp-publish"
       implementationClass = "arrow.gradle.MppPublishingPlugin"
