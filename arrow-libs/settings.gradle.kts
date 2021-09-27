d884c8d55a9e624d092875df4fe0cb27276fd9fb
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-09-27 23:19:25 +0200

    Change `settings.gradle` to `settings.gradle.kts`

--- arrow-libs/settings.gradle.kts
+++ arrow-libs/settings.gradle.kts
@@ -1,7 +1,7 @@
 /*
 
   This file includes the configuration of 'projectDir' beyond 'include' to
-  allow to work with:
+  allow working with:
 
     - All the libraries when opening 'arrow-libs'
     - CORE    libraries when opening 'arrow-libs/core'
@@ -14,50 +14,51 @@
 
 */
 
-rootProject.name = 'arrow-libs'
+rootProject.name = "arrow-libs"
 
 // Core
-include 'arrow-core'
-include 'arrow-core-test'
-include 'arrow-annotations'
-include 'arrow-continuations'
-include 'arrow-meta:arrow-meta-test-models'
-include 'arrow-meta'
-include 'arrow-core-retrofit'
+include("arrow-core")
+include("arrow-core-test")
+include("arrow-annotations")
+include("arrow-continuations")
+include("arrow-meta:arrow-meta-test-models")
+include("arrow-meta")
+include("arrow-core-retrofit")
 
 project(":arrow-core").projectDir = file("core/arrow-core")
 project(":arrow-core-test").projectDir = file("core/arrow-core-test")
 project(":arrow-annotations").projectDir = file("core/arrow-annotations")
 project(":arrow-continuations").projectDir = file("core/arrow-continuations")
-project(":arrow-meta:arrow-meta-test-models").projectDir = file("core/arrow-meta/arrow-meta-test-models")
+project(":arrow-meta:arrow-meta-test-models").projectDir =
+  file("core/arrow-meta/arrow-meta-test-models")
 project(":arrow-meta").projectDir = file("core/arrow-meta")
 project(":arrow-core-retrofit").projectDir = file("core/arrow-core-retrofit")
 
 // Fx
-include 'arrow-fx-coroutines'
-include 'arrow-fx-coroutines-test'
-include 'arrow-fx-stm'
+include("arrow-fx-coroutines")
+include("arrow-fx-coroutines-test")
+include("arrow-fx-stm")
 
 project(":arrow-fx-coroutines").projectDir = file("fx/arrow-fx-coroutines")
 project(":arrow-fx-coroutines-test").projectDir = file("fx/arrow-fx-coroutines-test")
 project(":arrow-fx-stm").projectDir = file("fx/arrow-fx-stm")
 
 // Optics
-include 'arrow-optics'
-include 'arrow-optics-test'
+include("arrow-optics")
+include("arrow-optics-test")
 
 project(":arrow-optics").projectDir = file("optics/arrow-optics")
 project(":arrow-optics-test").projectDir = file("optics/arrow-optics-test")
 
 // Ank
-include 'arrow-ank'
-include 'arrow-ank-gradle'
+include("arrow-ank")
+include("arrow-ank-gradle")
 
 project(":arrow-ank").projectDir = file("ank/arrow-ank")
 project(":arrow-ank-gradle").projectDir = file("ank/arrow-ank-gradle")
 
-include 'jekyll'
+include("jekyll")
 project(":jekyll").projectDir = file("dokka/jekyll")
 
 // Examples
-include 'examples'
+include("examples")
