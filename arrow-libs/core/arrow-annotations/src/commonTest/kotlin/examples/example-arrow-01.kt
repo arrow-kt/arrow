0cc13b661f5c6bad06d8c33a28787b85a7d0a8db
raulraja <raulraja@gmail.com>
2021-11-16 10:27:04 +0100

    knit generated examples compiling

---
+++ arrow-libs/core/arrow-annotations/src/commonTest/kotlin/examples/example-arrow-01.kt
@@ -0,0 +1,4 @@
+// This file was automatically generated from optics.kt by Knit tool. Do not edit.
+package arrow.annotations.examples.exampleArrow01
+
+val x = 1
---
+++ arrow-libs/core/arrow-core/src/commonTest/kotlin/examples/example-arrow-30.kt
@@ -0,0 +1,10 @@
+// This file was automatically generated from Either.kt by Knit tool. Do not edit.
+package arrow.core.examples.exampleArrow30
+
+import arrow.core.rightIfNotNull
+
+val value =
+ "value".rightIfNotNull { "left" }
+fun main() {
+ println(value)
+}
