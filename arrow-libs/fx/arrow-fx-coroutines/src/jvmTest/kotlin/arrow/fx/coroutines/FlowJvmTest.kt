3ad43e9350ba4747d17baa046e2ac462c235e051
Alejandro Serrano <trupill@gmail.com>
2021-12-21 09:19:36 +0100

    Fix compilation problem in test

--- arrow-libs/fx/arrow-fx-coroutines/src/jvmTest/kotlin/arrow/fx/coroutines/FlowJvmTest.kt
+++ arrow-libs/fx/arrow-fx-coroutines/src/jvmTest/kotlin/arrow/fx/coroutines/FlowJvmTest.kt
@@ -21,7 +21,6 @@ import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.take
-import kotlinx.coroutines.test.currentTime
 import kotlinx.coroutines.withTimeoutOrNull
 
 @ExperimentalTime
