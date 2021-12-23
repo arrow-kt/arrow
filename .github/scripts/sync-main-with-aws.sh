88aece9ba180346a64a62866125697fef56ecf35
Francisco Diaz <francisco.d@47deg.com>
2021-12-23 11:07:39 +0100

    Fixing documentation workflow (#2594)
    * Polish documentation workflow
    
    * Use GitHub Actions output
    
    * Remove unnecessary download-artifact step
    
    * Increase memory size for Java

--- .github/scripts/sync-main-with-aws.sh
+++ .github/scripts/sync-main-with-aws.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Reason: The root directory also has docs/, docs/next/ and docs/<major.minor>. 
+# Reason: it's not possible to sync all the directories because of 'docs/0.10', 'docs/next', etc.
+#         and latest release appears in the root directory
 
 set -e
 
-MAIN_CONTENT=("CNAME"  "code"  "css"  "error.html"  "fonts"  "img"  "index.html"  "js"  "redirects.json")
-
 cd _site/
-for file in ${MAIN_CONTENT[*]}; do
+
+for file in *; do
     if [ -f "$file" ]; then
         echo "Copying $file ..."
         aws s3 cp $file s3://$S3_BUCKET/$file >> $BASEDIR/logs/aws_sync.log
         continue
     fi
+
     echo "Sync $file ..."
     aws s3 sync $file s3://$S3_BUCKET/$file --delete >> $BASEDIR/logs/aws_sync.log
 done
