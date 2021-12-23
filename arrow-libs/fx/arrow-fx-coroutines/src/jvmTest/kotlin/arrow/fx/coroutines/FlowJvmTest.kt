a08a8dc9aed5101353076eeb74979a1dc6481fbc
Alejandro Serrano <trupill@gmail.com>
2021-12-23 10:15:46 +0100

    Upgrade to kotlinx-coroutines 1.6.0 (#2606)
    * Upgrade to kotlinx-coroutines 1.6.0
    * Rewrite Flow cancellation test
    * Use awaitCancellation
    * Generate map instead of list of distinct pairs
    
    Co-authored-by: Simon Vergauwen <nomisRev@users.noreply.github.com>

--- arrow-libs/fx/arrow-fx-coroutines/src/jvmTest/kotlin/arrow/fx/coroutines/FlowJvmTest.kt
+++ arrow-libs/fx/arrow-fx-coroutines/src/jvmTest/kotlin/arrow/fx/coroutines/FlowJvmTest.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.flow.toSet
+import kotlinx.coroutines.test.currentTime
 import kotlinx.coroutines.test.runBlockingTest
 import kotlin.time.ExperimentalTime
 import kotlin.time.milliseconds
