77bbf0ea6c999c41884b86961ba76906223aeeb4
i-walker <46971368+i-walker@users.noreply.github.com>
2022-02-03 12:40:53 +0100

    clean up workflow files and add knit files

---
+++ arrow-libs/core/arrow-core/src/jvmTest/kotlin/examples/example-effect-context-04.kt
@@ -0,0 +1,14 @@
+// This file was automatically generated from EffectScope.kt by Knit tool. Do not edit.
+package arrow.core.examples.exampleEffectContext04
+
+import arrow.core.Validated
+import arrow.core.continuations.effect
+import io.kotest.matchers.shouldBe
+
+suspend fun main() {
+  val validated = Validated.Valid(40)
+  effect<String, Int> {
+    val x: Int = validated.bind()
+    x
+  }.toValidated() shouldBe validated
+}
