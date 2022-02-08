76b26f6a2ef572f08fe88d9a0ef5fb7040092c57
i-walker <46971368+i-walker@users.noreply.github.com>
2022-02-08 01:26:24 +0100

    add similar tests from arrow.comptutations.nullable

--- arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/continuations/OptionSpec.kt
+++ arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/continuations/OptionSpec.kt
@@ -1,6 +1,7 @@
 package arrow.core.continuations
 
 import arrow.core.None
+import arrow.core.Some
 import arrow.core.toOption
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
@@ -48,5 +49,69 @@ class OptionSpec : StringSpec({
       x
       throw IllegalStateException("This should not be executed")
     } shouldBe None
+  }
+  "simple case" {
+    option.eager {
+      "s".length.bind()
+    } shouldBe 1
+  }
+
+  "multiple types" {
+    option.eager {
+      val number = "s".length
+      val string = number.toString().bind()
+      string
+    } shouldBe "1"
+  }
+
+  "short circuit" {
+    option.eager {
+      val number: Int = "s".length
+      (number.takeIf { it > 1 }?.toString()).bind()
+      throw IllegalStateException("This should not be executed")
+    } shouldBe null
+  }
+
+  "short circuit option" {
+    option.eager {
+      val number = Some("s".length)
+      number.filter { it > 1 }.map(Int::toString).bind()
+      throw IllegalStateException("This should not be executed")
+    } shouldBe null
+  }
+
+  "when expression" {
+    option.eager {
+      val number = "s".length.bind()
+      val string = when (number) {
+        1 -> number.toString()
+        else -> null
+      }.bind()
+      string
+    } shouldBe "1"
+  }
+
+  "if expression" {
+    option.eager {
+      val number = "s".length.bind()
+      val string = if (number == 1) {
+        number.toString()
+      } else {
+        null
+      }.bind()
+      string
+    } shouldBe "1"
+  }
+
+  "if expression short circuit" {
+    option.eager {
+      val number = "s".length.bind()
+      val string = if (number != 1) {
+        number.toString()
+      } else {
+        null
+      }.bind()
+      string
+    } shouldBe null
   }
 })
