6d956b2a59bb79367321e2c80bc52455cbed9ace
franciscodr <francisco.d@47deg.com>
2022-05-30 18:07:06 +0200

    Convert Arrow core eagerEffect into a non-inline function

--- arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/continuations/either.kt
+++ arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/continuations/either.kt
@@ -4,7 +4,7 @@ import arrow.core.Either
 
 @Suppress("ClassName")
 public object either {
-  public inline fun <E, A> eager(crossinline f: suspend EagerEffectScope<E>.() -> A): Either<E, A> =
+  public inline fun <E, A> eager(noinline f: suspend EagerEffectScope<E>.() -> A): Either<E, A> =
     eagerEffect(f).toEither()
 
   public suspend inline operator fun <E, A> invoke(noinline f: suspend EffectScope<E>.() -> A): Either<E, A> =
