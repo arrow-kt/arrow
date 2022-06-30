df687bb71bf0dbf209cf9c24639de9b0ef61a45a
i-walker <46971368+i-walker@users.noreply.github.com>
2022-06-30 15:45:47 +0200

    deprecate arrow-optics-test

--- arrow-libs/fx/arrow-fx-coroutines-test/src/jvmMain/kotlin/arrow/fx/coroutines/predef-test-jvm.kt
+++ arrow-libs/fx/arrow-fx-coroutines-test/src/jvmMain/kotlin/arrow/fx/coroutines/predef-test-jvm.kt
@@ -1,15 +1,21 @@
 package arrow.fx.coroutines
 
 import arrow.core.continuations.AtomicRef
+import arrow.core.test.concurrency.deprecateArrowTestModules
 import java.util.concurrent.ThreadFactory
 import kotlin.coroutines.CoroutineContext
 
+@Deprecated(deprecateArrowTestModules)
 public val singleThreadName: String = "single"
+
+@Deprecated(deprecateArrowTestModules)
 public val single: Resource<CoroutineContext> = Resource.singleThreadContext(singleThreadName)
 
+@Deprecated(deprecateArrowTestModules)
 public val threadName: suspend () -> String =
   { Thread.currentThread().name }
 
+@Deprecated(deprecateArrowTestModules)
 public class NamedThreadFactory(private val mkName: (Int) -> String) : ThreadFactory {
   private val count = AtomicRef(0)
   override fun newThread(r: Runnable): Thread =
