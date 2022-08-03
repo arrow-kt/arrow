5ef430df8fd700b9ba14daf977d145ffe285531c
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-08-03 08:47:41 +0200

    Clean-up Resource for 2.0

--- arrow-libs/fx/arrow-fx-coroutines/src/commonMain/kotlin/arrow/fx/coroutines/Resource.kt
+++ arrow-libs/fx/arrow-fx-coroutines/src/commonMain/kotlin/arrow/fx/coroutines/Resource.kt
@@ -5,14 +5,12 @@ import arrow.core.continuations.update
 import arrow.core.identity
 import arrow.core.prependTo
 import arrow.fx.coroutines.continuations.ResourceScope
-import kotlin.coroutines.CoroutineContext
+import arrow.fx.coroutines.continuations.resource
 import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
-import kotlin.experimental.ExperimentalTypeInference
 
 /**
  * [Resource] models resource allocation and releasing. It is especially useful when multiple resources that depend on each other
@@ -145,7 +143,7 @@ import kotlin.experimental.ExperimentalTypeInference
  * and automatic [NonCancellable] `acquire` and `release` steps use [bracketCase] or [bracket].
  **/
 public sealed class Resource<out A> {
-
+  
   /**
    * Use the created resource
    * When done will run all finalizers
@@ -171,9 +169,9 @@ public sealed class Resource<out A> {
    * ```
    * <!--- KNIT example-resource-04.kt -->
    */
-  @Suppress("UNCHECKED_CAST")
-  public tailrec suspend infix fun <B> use(f: suspend (A) -> B): B =
+  public suspend infix fun <B> use(f: suspend (A) -> B): B =
     when (this) {
+      is Allocate -> bracketCase(acquire, f, release)
       is Dsl -> {
         val effect = ResourceScopeImpl()
         val b = try {
@@ -191,526 +189,72 @@ public sealed class Resource<out A> {
         }
         b
       }
-
-      is Allocate -> bracketCase(acquire, f, release)
-      is Bind<*, *> -> Dsl {
-        val any = source.bind()
-        val ff = this@Resource.f as (Any?) -> Resource<A>
-        ff(any).bind()
-      }.use(f)
-
-      is Defer -> resource().use(f)
     }
-
+  
   public fun <B> map(f: suspend (A) -> B): Resource<B> =
-    arrow.fx.coroutines.continuations.resource { f(bind()) }
-
+    resource { f(bind()) }
+  
   /** Useful for setting up/configuring an acquired resource */
   public fun tap(f: suspend (A) -> Unit): Resource<A> =
-    arrow.fx.coroutines.continuations.resource { bind().also { f(it) } }
-
-  public fun <B> ap(ff: Resource<(A) -> B>): Resource<B> =
-    arrow.fx.coroutines.continuations.resource {
-      val a = bind()
-      ff.bind()(a)
-    }
-
+    resource { bind().also { f(it) } }
+  
   /**
-   * Create a resource value of [B] from a resource [A] by mapping [f].
-   *
-   * Useful when there is a need to create resources that depend on other resources,
-   * for combining independent values [zip] provides nicer syntax without the need for callback nesting.
-   *
-   * ```kotlin
-   * import arrow.fx.coroutines.*
-   *
-   * object Connection
-   * class DataSource {
-   *   fun connect(): Unit = println("Connecting dataSource")
-   *   fun connection(): Connection = Connection
-   *   fun close(): Unit = println("Closed dataSource")
-   * }
-   *
-   * class Database(private val database: DataSource) {
-   *   fun init(): Unit = println("Database initialising . . .")
-   *   fun shutdown(): Unit = println("Database shutting down . . .")
-   * }
-   *
-   * suspend fun main(): Unit {
-   *   val dataSource = resource {
-   *     DataSource().also { it.connect() }
-   *   } release DataSource::close
-   *
-   *   fun database(ds: DataSource): Resource<Database> =
-   *     resource {
-   *       Database(ds).also(Database::init)
-   *     } release Database::shutdown
-   *
-   *   dataSource.flatMap(::database)
-   *     .use { println("Using database which uses dataSource") }
-   * }
-   * ```
-   * <!--- KNIT example-resource-05.kt -->
-   *
-   * @see zip to combine independent resources together
-   * @see parZip for combining independent resources in parallel
+   * Chain two [Resource] in sequence.
+   * Shorthand for `resource { f(bind()).bind() }`
    */
-  public fun <B> flatMap(f: (A) -> Resource<B>): Resource<B> = arrow.fx.coroutines.continuations.resource {
-    f(bind()).bind()
-  }
-
-  public inline fun <B, C> zip(other: Resource<B>, crossinline combine: (A, B) -> C): Resource<C> =
-    arrow.fx.coroutines.continuations.resource {
-      combine(bind(), other.bind())
-    }
-
-  public fun <B> zip(other: Resource<B>): Resource<Pair<A, B>> =
-    zip(other, ::Pair)
-
-  /**
-   * Combines two independent resource values with the provided [map] function,
-   * returning the resulting immutable [Resource] value.
-   * The finalizers run in order of left to right by using [flatMap] under the hood,
-   * but [zip] provides a nicer syntax for combining values that don't depend on each-other.
-   *
-   * Useful to compose up to 9 independent resources,
-   * see example for more details on how to use in code.
-   *
-   * ```kotlin
-   * import arrow.fx.coroutines.*
-   *
-   * class UserProcessor {
-   *   fun start(): Unit = println("Creating UserProcessor")
-   *   fun shutdown(): Unit = println("Shutting down UserProcessor")
-   *   fun process(ds: DataSource): List<String> =
-   *    ds.users().map { "Processed $it" }
-   * }
-   *
-   * class DataSource {
-   *   fun connect(): Unit = println("Connecting dataSource")
-   *   fun users(): List<String> = listOf("User-1", "User-2", "User-3")
-   *   fun close(): Unit = println("Closed dataSource")
-   * }
-   *
-   * class Service(val db: DataSource, val userProcessor: UserProcessor) {
-   *   suspend fun processData(): List<String> = userProcessor.process(db)
-   * }
-   *
-   * val userProcessor = resource {
-   *   UserProcessor().also(UserProcessor::start)
-   * } release UserProcessor::shutdown
-   *
-   * val dataSource = resource {
-   *   DataSource().also { it.connect() }
-   * } release DataSource::close
-   *
-   * suspend fun main(): Unit {
-   *   userProcessor.zip(dataSource) { userProcessor, ds ->
-   *       Service(ds, userProcessor)
-   *     }.use { service -> service.processData() }
-   * }
-   * ```
-   * <!--- KNIT example-resource-06.kt -->
-   *
-   * @see parZip if you want to combine independent resources in parallel
-   * @see flatMap to combine resources that rely on each-other.
-   */
-  public inline fun <B, C, D> zip(
-    b: Resource<B>,
-    c: Resource<C>,
-    crossinline map: (A, B, C) -> D,
-  ): Resource<D> =
-    arrow.fx.coroutines.continuations.resource {
-      map(bind(), b.bind(), c.bind())
-    }
-
-  public inline fun <B, C, D, E> zip(
-    b: Resource<B>,
-    c: Resource<C>,
-    d: Resource<D>,
-    crossinline map: (A, B, C, D) -> E,
-  ): Resource<E> =
-    arrow.fx.coroutines.continuations.resource {
-      map(bind(), b.bind(), c.bind(), d.bind())
-    }
-
-  public inline fun <B, C, D, E, G> zip(
-    b: Resource<B>,
-    c: Resource<C>,
-    d: Resource<D>,
-    e: Resource<E>,
-    crossinline map: (A, B, C, D, E) -> G,
-  ): Resource<G> =
-    arrow.fx.coroutines.continuations.resource {
-      map(bind(), b.bind(), c.bind(), d.bind(), e.bind())
-    }
-
-  public inline fun <B, C, D, E, F, G, H> zip(
-    b: Resource<B>,
-    c: Resource<C>,
-    d: Resource<D>,
-    e: Resource<E>,
-    f: Resource<F>,
-    crossinline map: (A, B, C, D, E, F) -> G,
-  ): Resource<G> =
-    arrow.fx.coroutines.continuations.resource {
-      map(bind(), b.bind(), c.bind(), d.bind(), e.bind(), f.bind())
-    }
-
-  public inline fun <B, C, D, E, F, G, H> zip(
-    b: Resource<B>,
-    c: Resource<C>,
-    d: Resource<D>,
-    e: Resource<E>,
-    f: Resource<F>,
-    g: Resource<G>,
-    crossinline map: (A, B, C, D, E, F, G) -> H,
-  ): Resource<H> =
-    arrow.fx.coroutines.continuations.resource {
-      map(bind(), b.bind(), c.bind(), d.bind(), e.bind(), f.bind(), g.bind())
-    }
-
-  public inline fun <B, C, D, E, F, G, H, I> zip(
-    b: Resource<B>,
-    c: Resource<C>,
-    d: Resource<D>,
-    e: Resource<E>,
-    f: Resource<F>,
-    g: Resource<G>,
-    h: Resource<H>,
-    crossinline map: (A, B, C, D, E, F, G, H) -> I,
-  ): Resource<I> =
-    arrow.fx.coroutines.continuations.resource {
-      map(bind(), b.bind(), c.bind(), d.bind(), e.bind(), f.bind(), g.bind(), h.bind())
-    }
-
-  public inline fun <B, C, D, E, F, G, H, I, J> zip(
-    b: Resource<B>,
-    c: Resource<C>,
-    d: Resource<D>,
-    e: Resource<E>,
-    f: Resource<F>,
-    g: Resource<G>,
-    h: Resource<H>,
-    i: Resource<I>,
-    crossinline map: (A, B, C, D, E, F, G, H, I) -> J,
-  ): Resource<J> =
-    arrow.fx.coroutines.continuations.resource {
-      map(bind(), b.bind(), c.bind(), d.bind(), e.bind(), f.bind(), g.bind(), h.bind(), i.bind())
-    }
-
-  public inline fun <B, C, D, E, F, G, H, I, J, K> zip(
-    b: Resource<B>,
-    c: Resource<C>,
-    d: Resource<D>,
-    e: Resource<E>,
-    f: Resource<F>,
-    g: Resource<G>,
-    h: Resource<H>,
-    i: Resource<I>,
-    j: Resource<J>,
-    crossinline map: (A, B, C, D, E, F, G, H, I, J) -> K,
-  ): Resource<K> =
-    arrow.fx.coroutines.continuations.resource {
-      map(bind(), b.bind(), c.bind(), d.bind(), e.bind(), f.bind(), g.bind(), h.bind(), i.bind(), j.bind())
-    }
-
-  public fun <B, C> parZip(fb: Resource<B>, f: suspend (A, B) -> C): Resource<C> =
-    parZip(Dispatchers.Default, fb, f)
-
-  /**
-   * Composes two [Resource]s together by zipping them in parallel,
-   * by running both their `acquire` handlers in parallel, and both `release` handlers in parallel.
-   *
-   * Useful in the case that starting a resource takes considerable computing resources or time.
-   *
-   * ```kotlin
-   * import arrow.fx.coroutines.*
-   * import kotlinx.coroutines.delay
-   *
-   * class UserProcessor {
-   *   suspend fun start(): Unit { delay(750); println("Creating UserProcessor") }
-   *   fun shutdown(): Unit = println("Shutting down UserProcessor")
-   *   fun process(ds: DataSource): List<String> =
-   *    ds.users().map { "Processed $it" }
-   * }
-   *
-   * class DataSource {
-   *   suspend fun connect(): Unit { delay(1000); println("Connecting dataSource") }
-   *   fun users(): List<String> = listOf("User-1", "User-2", "User-3")
-   *   fun close(): Unit = println("Closed dataSource")
-   * }
-   *
-   * class Service(val db: DataSource, val userProcessor: UserProcessor) {
-   *   suspend fun processData(): List<String> = userProcessor.process(db)
-   * }
-   *
-   * val userProcessor = resource {
-   *   UserProcessor().also { it.start() }
-   * } release UserProcessor::shutdown
-   *
-   * val dataSource = resource {
-   *   DataSource().also { it.connect() }
-   * } release DataSource::close
-   *
-   * suspend fun main(): Unit {
-   *   userProcessor.parZip(dataSource) { userProcessor, ds ->
-   *       Service(ds, userProcessor)
-   *     }.use { service -> service.processData() }
-   * }
-   * ```
-   * <!--- KNIT example-resource-07.kt -->
-   */
-  public fun <B, C> parZip(
-    ctx: CoroutineContext = Dispatchers.Default,
-    fb: Resource<B>,
-    f: suspend (A, B) -> C,
-  ): Resource<C> =
-    arrow.fx.coroutines.continuations.resource {
-      parZip(ctx, { this@Resource.bind() }, { fb.bind() }) { a, b -> f(a, b) }
-    }
-
-  @Deprecated(
-    "Bind is being deprecated. Use resource DSL instead",
-    ReplaceWith(
-      "resource { f(source.bind()) }",
-      "arrow.fx.coroutines.continuations.resource"
-    )
-  )
-  public class Bind<A, B>(public val source: Resource<A>, public val f: (A) -> Resource<B>) : Resource<B>()
-
+  public fun <B> flatMap(f: (A) -> Resource<B>): Resource<B> =
+    resource { f(bind()).bind() }
+  
   public class Allocate<A>(
     public val acquire: suspend () -> A,
     public val release: suspend (A, ExitCase) -> Unit,
   ) : Resource<A>()
-
-  @Deprecated(
-    "Defer is being deprecated. Use resource DSL instead",
-    ReplaceWith(
-      "resource { resource.invoke().bind() }",
-      "arrow.fx.coroutines.continuations.resource"
-    )
-  )
-  public class Defer<A>(public val resource: suspend () -> Resource<A>) : Resource<A>()
-
+  
   public data class Dsl<A>(public val dsl: suspend ResourceScope.() -> A) : Resource<A>()
-
-  public companion object {
-
-    @PublishedApi
-    @Deprecated("This will be removed from the binary in Arrow 2.0", level = DeprecationLevel.ERROR)
-    internal val unit: Resource<Unit> = just(Unit)
-
-    /**
-     * Construct a [Resource] from an allocating function [acquire] and a release function [release].
-     *
-     * ```kotlin
-     * import arrow.fx.coroutines.*
-     *
-     * suspend fun acquireResource(): Int = 42.also { println("Getting expensive resource") }
-     * suspend fun releaseResource(r: Int, exitCase: ExitCase): Unit = println("Releasing expensive resource: $r, exit: $exitCase")
-     *
-     * suspend fun main(): Unit {
-     *   val resource = Resource(::acquireResource, ::releaseResource)
-     *   resource.use {
-     *     println("Expensive resource under use! $it")
-     *   }
-     * }
-     * ```
-     * <!--- KNIT example-resource-08.kt -->
-     */
-    public operator fun <A> invoke(
-      acquire: suspend () -> A,
-      release: suspend (A, ExitCase) -> Unit,
-    ): Resource<A> = Allocate(acquire, release)
-
-    /**
-     * Create a [Resource] from a pure value [A].
-     */
-    public fun <A> just(r: A): Resource<A> =
-      Resource({ r }, { _, _ -> Unit })
-
-    @Deprecated(
-      "defer is being deprecated. Use resource DSL instead",
-      ReplaceWith(
-        "resource { f().bind() }",
-        "arrow.fx.coroutines.continuations.resource"
-      )
-    )
-    public fun <A> defer(f: suspend () -> Resource<A>): Resource<A> =
-      Resource.Defer(f)
-  }
+  
+  public companion object
 }
 
 /**
- * Marker for `suspend () -> A` to be marked as the [Use] action of a [Resource].
- * Offers a convenient DSL to use [Resource] for simple resources.
+ * Construct a [Resource] from an allocating function [acquire] and a release function [release].
  *
  * ```kotlin
  * import arrow.fx.coroutines.*
  *
- * class File(url: String) {
- *   suspend fun open(): File = this
- *   suspend fun close(): Unit {}
- *   override fun toString(): String = "This file contains some interesting content!"
- * }
- *
- * suspend fun openFile(uri: String): File = File(uri).open()
- * suspend fun closeFile(file: File): Unit = file.close()
- * suspend fun fileToString(file: File): String = file.toString()
+ * suspend fun acquireResource(): Int = 42.also { println("Getting expensive resource") }
+ * suspend fun releaseResource(r: Int, exitCase: ExitCase): Unit = println("Releasing expensive resource: $r, exit: $exitCase")
  *
  * suspend fun main(): Unit {
- *   val res = resource {
- *     openFile("data.json")
- *   } release { file ->
- *     closeFile(file)
- *   } use { file ->
- *     fileToString(file)
+ *   val resource = Resource(::acquireResource, ::releaseResource)
+ *   resource.use {
+ *     println("Expensive resource under use! $it")
  *   }
- *
- *   println(res)
  * }
  * ```
- * <!--- KNIT example-resource-09.kt -->
+ * <!--- KNIT example-resource-08.kt -->
  */
-@Deprecated(
-  "Use the resource computation DSL instead",
-  ReplaceWith(
-    "resource { acquire() }",
-    "arrow.fx.coroutines.continuations.resource"
-  )
-)
-public inline class Use<A>(internal val acquire: suspend () -> A)
-
-/**
- * Marks an [acquire] operation as the [Resource.use] step of a [Resource].
- */
-@Deprecated(
-  "Use the resource computation DSL instead",
-  ReplaceWith(
-    "resource { acquire() }",
-    "arrow.fx.coroutines.continuations.resource"
-  )
-)
-public fun <A> resource(acquire: suspend () -> A): Use<A> = Use(acquire)
-
-@Deprecated("Use the resource computation DSL instead")
-public infix fun <A> Use<A>.release(release: suspend (A) -> Unit): Resource<A> =
-  Resource(acquire) { a, _ -> release(a) }
+public fun <A> Resource(
+  acquire: suspend () -> A,
+  release: suspend (A, ExitCase) -> Unit,
+): Resource<A> = Resource.Allocate(acquire, release)
 
 /**
  * Composes a [release] action to a [Resource.use] action creating a [Resource].
  */
 public infix fun <A> Resource<A>.release(release: suspend (A) -> Unit): Resource<A> =
-  arrow.fx.coroutines.continuations.resource {
+  resource {
     val a = bind()
     Resource({ a }, { _, _ -> release(a) }).bind()
   }
-
-@Deprecated("Use the resource computation DSL instead")
-public infix fun <A> Use<A>.releaseCase(release: suspend (A, ExitCase) -> Unit): Resource<A> =
-  Resource(acquire, release)
 
 /**
  * Composes a [releaseCase] action to a [Resource.use] action creating a [Resource].
  */
 public infix fun <A> Resource<A>.releaseCase(release: suspend (A, ExitCase) -> Unit): Resource<A> =
-  arrow.fx.coroutines.continuations.resource {
+  resource {
     val a = bind()
     Resource({ a }, { _, ex -> release(a, ex) }).bind()
   }
-
-@Deprecated("traverseResource is being renamed to traverse to simplify the Arrow API", ReplaceWith("traverse(f)"))
-public inline fun <A, B> Iterable<A>.traverseResource(crossinline f: (A) -> Resource<B>): Resource<List<B>> =
-  arrow.fx.coroutines.continuations.resource {
-    map { a ->
-      f(a).bind()
-    }
-  }
-
-/**
- * Traverse this [Iterable] and collects the resulting `Resource<B>` of [f] into a `Resource<List<B>>`.
- *
- * ```kotlin
- * import arrow.fx.coroutines.*
- *
- * class File(url: String) {
- *   suspend fun open(): File = this
- *   suspend fun close(): Unit {}
- *   override fun toString(): String = "This file contains some interesting content!"
- * }
- *
- * suspend fun openFile(uri: String): File = File(uri).open()
- * suspend fun closeFile(file: File): Unit = file.close()
- * suspend fun fileToString(file: File): String = file.toString()
- *
- * suspend fun main(): Unit {
- *   val res: List<String> = listOf(
- *     "data.json",
- *     "user.json",
- *     "resource.json"
- *   ).traverse { uri ->
- *     resource {
- *      openFile(uri)
- *     } release { file ->
- *       closeFile(file)
- *     }
- *   }.use { files ->
- *     files.map { fileToString(it) }
- *   }
- *   res.forEach(::println)
- * }
- * ```
- * <!--- KNIT example-resource-10.kt -->
- */
-@OptIn(ExperimentalTypeInference::class)
-@OverloadResolutionByLambdaReturnType
-public inline fun <A, B> Iterable<A>.traverse(crossinline f: (A) -> Resource<B>): Resource<List<B>> =
-  arrow.fx.coroutines.continuations.resource {
-    map { a ->
-      f(a).bind()
-    }
-  }
-
-/**
- * Sequences this [Iterable] of [Resource]s.
- * [Iterable.map] and [sequence] is equivalent to [traverse].
- *
- * ```kotlin
- * import arrow.fx.coroutines.*
- *
- * class File(url: String) {
- *   suspend fun open(): File = this
- *   suspend fun close(): Unit {}
- *   override fun toString(): String = "This file contains some interesting content!"
- * }
- *
- * suspend fun openFile(uri: String): File = File(uri).open()
- * suspend fun closeFile(file: File): Unit = file.close()
- * suspend fun fileToString(file: File): String = file.toString()
- *
- * suspend fun main(): Unit {
- *   val res: List<String> = listOf(
- *     "data.json",
- *     "user.json",
- *     "resource.json"
- *   ).map { uri ->
- *     resource {
- *      openFile(uri)
- *     } release { file ->
- *       closeFile(file)
- *     }
- *   }.sequence().use { files ->
- *     files.map { fileToString(it) }
- *   }
- *   res.forEach(::println)
- * }
- * ```
- * <!--- KNIT example-resource-11.kt -->
- */
-@Suppress("NOTHING_TO_INLINE")
-public inline fun <A> Iterable<Resource<A>>.sequence(): Resource<List<A>> =
-  traverse(::identity)
 
 /**
  * Runs [Resource.use] and emits [A] of the resource
@@ -759,17 +303,6 @@ private class ResourceScopeImpl : ResourceScope {
           Platform.composeErrors(e, e2)?.let { throw it }
         }
       })
-
-      is Resource.Bind<*, *> -> {
-        val dsl: suspend ResourceScope.() -> A = {
-          val any = source.bind()
-          val ff = f as (Any?) -> Resource<A>
-          ff(any).bind()
-        }
-        dsl(this@ResourceScopeImpl)
-      }
-
-      is Resource.Defer -> resource().bind()
     }
 }
 
