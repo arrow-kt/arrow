e0656a702c3f6485ade13f9de6b2782b8514107a
Marc Moreno Ferrer <ignis.fatue@gmail.com>
2022-08-11 12:53:09 +0200

    remove(arrow): Remove Validated reference formr comments

--- arrow-libs/core/arrow-continuations/src/jsMain/kotlin/arrow/continuations/generic/ControlThrowable.kt
+++ arrow-libs/core/arrow-continuations/src/jsMain/kotlin/arrow/continuations/generic/ControlThrowable.kt
@@ -4,7 +4,7 @@ package arrow.continuations.generic
  * A [Throwable] class intended for control flow.
  * Instance of [ControlThrowable.kt] should **not** be caught,
  * and `arrow.core.NonFatal` does not catch this [Throwable].
- * Thus by extension `Either.catch` and `Validated.catch` also don't catch [ControlThrowable.kt].
+ * Thus by extension `Either.catch` also don't catch [ControlThrowable.kt].
  */
 @Deprecated(deprecateArrowContinuation)
 public actual open class ControlThrowable : Throwable()
