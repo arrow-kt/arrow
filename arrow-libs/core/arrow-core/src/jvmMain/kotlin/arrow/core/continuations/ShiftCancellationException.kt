2bc6c7e37399ace503c7e9a04e5d73dcc102e985
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-09-02 20:34:21 +0200

    Re-add isTraced on JVM

--- arrow-libs/core/arrow-core/src/jvmMain/kotlin/arrow/core/continuations/ShiftCancellationException.kt
+++ arrow-libs/core/arrow-core/src/jvmMain/kotlin/arrow/core/continuations/ShiftCancellationException.kt
@@ -7,11 +7,11 @@ internal actual class ShiftCancellationException actual constructor(
   @JvmField internal actual val shift: Shift<Any?>,
   @JvmField internal actual val isTraced: Boolean
 ) : CancellationException("Shifted Continuation") {
-  // override fun fillInStackTrace(): Throwable =
-  //   if (isTraced) super.fillInStackTrace()
-  //   else {
-  //     Prevent Android <= 6.0 bug. https://github.com/Kotlin/kotlinx.coroutines/issues/1866
-      // stackTrace = emptyArray()
-      // this
-    // }
+  override fun fillInStackTrace(): Throwable =
+    if (isTraced) super.fillInStackTrace()
+    else {
+      // Prevent Android <= 6.0 bug. https://github.com/Kotlin/kotlinx.coroutines/issues/1866
+      stackTrace = emptyArray()
+      this
+    }
 }
