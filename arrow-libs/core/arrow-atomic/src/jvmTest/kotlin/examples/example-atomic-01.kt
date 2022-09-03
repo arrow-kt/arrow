a9a25c62c77731fb7222f7520007cd911d8a6a14
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-09-03 13:56:40 +0200

    Fix build

---
+++ arrow-libs/core/arrow-atomic/src/jvmTest/kotlin/examples/example-atomic-01.kt
@@ -0,0 +1,12 @@
+// This file was automatically generated from Atomic.kt by Knit tool. Do not edit.
+package arrow.atomic.examples.exampleAtomic01
+
+import arrow.atomic.Atomic
+
+suspend fun main() {
+  val count = Atomic(0)
+  (0 until 20_000).parTraverse {
+    count.update(Int::inc)
+  }
+  println(count.value)
+}
