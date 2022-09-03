d45adea79a3df1270fa73389d46ffb27f331d43c
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-09-03 15:37:41 +0200

    Fix knit attempt #2

--- arrow-libs/core/arrow-atomic/src/jvmTest/kotlin/examples/example-atomic-01.kt
+++ arrow-libs/core/arrow-atomic/src/jvmTest/kotlin/examples/example-atomic-01.kt
@@ -4,7 +4,6 @@ package arrow.atomic.examples.exampleAtomic01
 import arrow.atomic.Atomic
 import arrow.atomic.update
 import arrow.fx.coroutines.parTraverse
-
 suspend fun main() {
   val count = Atomic(0)
   (0 until 20_000).parTraverse {
