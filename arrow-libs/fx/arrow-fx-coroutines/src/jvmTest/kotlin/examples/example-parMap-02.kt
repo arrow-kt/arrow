e25348b1dc76cf2627c8f6961ac9b53b08df0b2f
Marc Moreno Ferrer <ignis.fatue@gmail.com>
2022-09-28 18:25:28 +0200

    doc(arrow2): Add knit samples
    Related issue id: #2788

---
+++ arrow-libs/fx/arrow-fx-coroutines/src/jvmTest/kotlin/examples/example-parMap-02.kt
@@ -0,0 +1,18 @@
+// This file was automatically generated from ParMap.kt by Knit tool. Do not edit.
+package arrow.fx.coroutines.examples.exampleParMap02
+
+import arrow.fx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+
+data class User(val id: Int, val createdOn: String)
+
+suspend fun main(): Unit {
+  //sampleStart
+  suspend fun getUserById(id: Int): User =
+    User(id, Thread.currentThread().name)
+
+  val res = listOf(1, 2, 3)
+    .parMap(Dispatchers.IO) { getUserById(it) }
+ //sampleEnd
+ println(res)
+}
