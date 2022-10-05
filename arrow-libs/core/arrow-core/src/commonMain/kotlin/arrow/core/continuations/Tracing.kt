ec46faf6e14716edb54562de71d25f022993e84d
franciscodr <francisco.d@47deg.com>
2022-10-05 12:52:19 +0200

    Remove more Shift occurences

--- arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/continuations/Tracing.kt
+++ arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/continuations/Tracing.kt
@@ -9,21 +9,21 @@ import kotlin.experimental.ExperimentalTypeInference
 import kotlin.jvm.JvmMultifileClass
 import kotlin.jvm.JvmName
 
-/** Tracing result of `R`. Allows to inspect `R`, and the traces from where it was shifted. */
-public data class Traced<R>(private val exception: CancellationException, private val shifted: R) {
+/** Tracing result of `R`. Allows to inspect `R`, and the traces from where it was raised. */
+public data class Traced<R>(private val exception: CancellationException, private val raised: R) {
   /**
    * Returns the stacktrace as a [String]
    *
-   * Note, the first line in the stacktrace will be the `ShiftCancellationException`.
-   * The users call to `shift` can found in the_second line of the stacktrace.
+   * Note, the first line in the stacktrace will be the `RaiseCancellationException`.
+   * The users call to `raise` can found in the_second line of the stacktrace.
    */
   public fun stackTraceToString(): String = exception.stackTraceToString()
   
   /**
    * Prints the stacktrace.
    *
-   * Note, the first line in the stacktrace will be the `ShiftCancellationException`.
-   * The users call to `shift` can found in the_second line of the stacktrace.
+   * Note, the first line in the stacktrace will be the `RaiseCancellationException`.
+   * The users call to `raise` can found in the_second line of the stacktrace.
    */
   public fun printStackTrace(): Unit =
     exception.printStackTrace()
@@ -32,7 +32,7 @@ public data class Traced<R>(private val exception: CancellationException, privat
    * Returns the suppressed exceptions that occurred during cancellation of the surrounding coroutines,
    *
    * For example when working with `Resource`, or `bracket`:
-   * When consuming a `Resource` fails due to [Shift.shift] it results in `ExitCase.Cancelled`,
+   * When consuming a `Resource` fails due to [Raise.raise] it results in `ExitCase.Cancelled`,
    * if the finalizer then results in a `Throwable` it will be added as a `suppressedException` to the [CancellationException].
    */
   public fun suppressedExceptions(): List<Throwable> =
@@ -49,25 +49,25 @@ public fun <R, A> EagerEffect<R, A>.traced(recover: Raise<R>.(traces: Traced<R>)
  * Inspect a [Traced] value of [R].
  *
  * Tracing [R] can be useful to know where certain errors, or failures are coming from.
- * Let's say you have a `ValidationError`, but it might be shifted from many places in the project.
+ * Let's say you have a `ValidationError`, but it might be raised from many places in the project.
  *
  * You would have to manually trace where this error is coming from,
- * instead [Traced] offers you ways to inspect the actual stacktrace of where the shifted value occurred.
+ * instead [Traced] offers you ways to inspect the actual stacktrace of where the raised value occurred.
  *
  * ```kotlin
  * public fun main() {
- *   val error = effect<String, Int> { shift("error") }
+ *   val error = effect<String, Int> { raise("error") }
  *   error.traced { traced -> traced.printStackTrace() }
  *     .fold({ require(it == "error") }, { error("impossible") })
  * }
  * ```
  * ```text
- * arrow.core.continuations.ShiftCancellationException: Shifted Continuation
- *   at arrow.core.continuations.DefaultShift.shift(Fold.kt:77)
+ * arrow.core.continuations.RaiseCancellationException: Raised Continuation
+ *   at arrow.core.continuations.DefaultRaise.raise(Fold.kt:77)
  *   at MainKtKt$main$error$1.invoke(MainKt.kt:6)
  *   at MainKtKt$main$error$1.invoke(MainKt.kt:6)
- *   at arrow.core.continuations.Shift$DefaultImpls.bind(Shift.kt:22)
- *   at arrow.core.continuations.DefaultShift.bind(Fold.kt:74)
+ *   at arrow.core.continuations.Raise$DefaultImpls.bind(Raise.kt:22)
+ *   at arrow.core.continuations.DefaultRaise.bind(Fold.kt:74)
  *   at arrow.core.continuations.Effect__TracingKt$traced$2.invoke(Tracing.kt:46)
  *   at arrow.core.continuations.Effect__TracingKt$traced$2.invoke(Tracing.kt:46)
  *   at arrow.core.continuations.Effect__FoldKt.fold(Fold.kt:92)
@@ -77,7 +77,7 @@ public fun <R, A> EagerEffect<R, A>.traced(recover: Raise<R>.(traces: Traced<R>)
  * ```
  *
  * NOTE:
- * This implies a performance penalty of creating a stacktrace when calling [Shift.shift],
+ * This implies a performance penalty of creating a stacktrace when calling [Raise.raise],
  * but **this only occurs** when composing `traced`.
  * The stacktrace creation is disabled if no `traced` calls are made within the function composition.
  */
@@ -88,8 +88,8 @@ public inline fun <R, A> Raise<R>.traced(
   val nested = DefaultRaise(true)
   return try {
     program.invoke(nested)
-  } catch (e: ShiftCancellationException) {
-    val r: R = e.shiftedOrRethrow(nested)
+  } catch (e: RaiseCancellationException) {
+    val r: R = e.raisedOrRethrow(nested)
     recover(Traced(e, r))
     raise(r)
   }
