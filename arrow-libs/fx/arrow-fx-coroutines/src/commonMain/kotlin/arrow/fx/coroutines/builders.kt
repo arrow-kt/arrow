ebd3406effb503ca1909e3b7110232ef85a9ba06
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-10-07 13:42:17 +0200

    Arrow Fx: deprecate Platform#composeError, never and unit() (#2837)
    * Deprecate some niche apis
    
    * Revert unrelated changes

--- arrow-libs/fx/arrow-fx-coroutines/src/commonMain/kotlin/arrow/fx/coroutines/builders.kt
+++ arrow-libs/fx/arrow-fx-coroutines/src/commonMain/kotlin/arrow/fx/coroutines/builders.kt
@@ -2,9 +2,15 @@ package arrow.fx.coroutines
 
 import kotlinx.coroutines.awaitCancellation
 
-// TODO deprecate?
+@Deprecated(
+  "Prefer using awaitCancellation from KotlinX",
+  ReplaceWith("awaitCancellation()", "kotlinx.coroutines.awaitCancellation")
+)
 public suspend fun <A> never(): A =
   awaitCancellation()
 
-// TODO deprecate?
+@Deprecated(
+  "Prefer simply using Unit, or empty lambda when needed",
+  ReplaceWith("Unit")
+)
 public suspend fun unit(): Unit = Unit
