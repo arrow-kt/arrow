79e990ea09bfc4fff39ea9adcbaa9b89cd45b1c0
Alejandro Serrano <trupill@gmail.com>
2022-11-18 20:54:21 +0100

    Update various versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,3 +86,13 @@ apiValidation {
 
   ignoredProjects.addAll(ignoreApiValidation)
 }
+
+rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
+  rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().apply {
+    versions.webpackDevServer.version = "4.11.1"
+    versions.webpack.version = "5.75.0"
+    versions.webpackCli.version = "4.10.0"
+    versions.karma.version = "6.4.1"
+    versions.mocha.version = "10.1.0"
+  }
+}
