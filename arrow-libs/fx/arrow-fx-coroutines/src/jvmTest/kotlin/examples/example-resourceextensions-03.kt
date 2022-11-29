1b335d3a932d560ce3d42d508b363da315744f28
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-11-29 15:47:27 +0100

    Additional deprecations, and backports for JVM (#2856)

--- arrow-libs/fx/arrow-fx-coroutines/src/jvmTest/kotlin/examples/example-resourceextensions-03.kt
+++ arrow-libs/fx/arrow-fx-coroutines/src/jvmTest/kotlin/examples/example-resourceextensions-03.kt
@@ -1,13 +1,13 @@
 // This file was automatically generated from ResourceExtensions.kt by Knit tool. Do not edit.
 package arrow.fx.coroutines.examples.exampleResourceextensions03
 
-import arrow.fx.coroutines.*
+import arrow.fx.coroutines.resourceScope
+import arrow.fx.coroutines.autoCloseable
 import java.io.FileInputStream
 
 suspend fun copyFile(src: String, dest: String): Unit =
-  Resource.fromAutoCloseable { FileInputStream(src) }
-    .zip(Resource.fromAutoCloseable { FileInputStream(dest) })
-    .use { (a: FileInputStream, b: FileInputStream) ->
-       /** read from [a] and write to [b]. **/
-       // Both resources will be closed accordingly to their #close methods
-    }
+  resourceScope {
+    val a: FileInputStream = autoCloseable { FileInputStream(src) }
+    val b: FileInputStream = autoCloseable { FileInputStream(dest) }
+    /** read from [a] and write to [b]. **/
+  } // Both resources will be closed accordingly to their #close methods
