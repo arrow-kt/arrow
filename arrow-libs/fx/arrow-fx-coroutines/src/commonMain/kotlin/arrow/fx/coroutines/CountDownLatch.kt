be63562a1025482544cb0537d31d0d6cb314aa3a
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-12-04 12:22:04 +0100

    Add CountDownLatch (#2854)

---
+++ arrow-libs/fx/arrow-fx-coroutines/src/commonMain/kotlin/arrow/fx/coroutines/CountDownLatch.kt
@@ -0,0 +1,44 @@
+package arrow.fx.coroutines
+
+import arrow.core.continuations.AtomicRef
+import arrow.core.continuations.loop
+import kotlinx.coroutines.CompletableDeferred
+
+/**
+ * [CountDownLatch] allows for awaiting a given number of countdown signals.
+ * Models the behavior of java.util.concurrent.CountDownLatch in Kotlin with `suspend`.
+ *
+ * Must be initialised with an [initial] value of 1 or higher,
+ * if constructed with 0 or negative value then it throws [IllegalArgumentException].
+ */
+public class CountDownLatch @Throws(IllegalArgumentException::class) constructor(private val initial: Long) {
+  private val signal = CompletableDeferred<Unit>()
+  private val count = AtomicRef(initial)
+  
+  init {
+    require(initial > 0) {
+      "CountDownLatch must be constructed with positive non-zero initial count, but was $initial"
+    }
+  }
+  
+  /** Remaining count */
+  public fun count(): Long = count.get()
+  
+  /** Await [count] to reach zero */
+  public suspend fun await(): Unit = signal.await()
+  
+  /** Decrement [count] by one */
+  @Suppress("ReturnCount")
+  public fun countDown() {
+    count.loop { current ->
+      when {
+        current == 0L -> return
+        current == 1L && count.compareAndSet(1L, 0L) -> {
+          signal.complete(Unit)
+          return
+        }
+        count.compareAndSet(current, current - 1) -> return
+      }
+    }
+  }
+}
