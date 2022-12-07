3f44a648a4dcc6f53479d9b4bbde95cd112930f4
Simon Vergauwen <nomisRev@users.noreply.github.com>
2022-12-07 09:46:59 +0100

    Replace old Duration.second() usage in docs

--- arrow-libs/fx/arrow-fx-coroutines/src/commonMain/kotlin/arrow/fx/coroutines/CircuitBreaker.kt
+++ arrow-libs/fx/arrow-fx-coroutines/src/commonMain/kotlin/arrow/fx/coroutines/CircuitBreaker.kt
@@ -52,9 +52,9 @@ import kotlin.time.DurationUnit
  * //sampleStart
  *   val circuitBreaker = CircuitBreaker.of(
  *     maxFailures = 2,
- *     resetTimeout = Duration.seconds(2),
+ *     resetTimeout = 2.seconds,
  *     exponentialBackoffFactor = 1.2,
- *     maxResetTimeout = Duration.seconds(60),
+ *     maxResetTimeout = 60.seconds,
  *   )
  *   circuitBreaker.protectOrThrow { "I am in Closed: ${circuitBreaker.state()}" }.also(::println)
  *
@@ -99,9 +99,9 @@ import kotlin.time.DurationUnit
  *   //sampleStart
  *   val circuitBreaker = CircuitBreaker.of(
  *     maxFailures = 2,
- *     resetTimeout = seconds(2),
+ *     resetTimeout = 2.seconds,
  *     exponentialBackoffFactor = 2.0, // enable exponentialBackoffFactor
- *     maxResetTimeout = seconds(60), // limit exponential back-off time
+ *     maxResetTimeout = 60.seconds, // limit exponential back-off time
  *   )
  *
  *   suspend fun <A> resilient(schedule: Schedule<Throwable, *>, f: suspend () -> A): A =
@@ -116,11 +116,11 @@ import kotlin.time.DurationUnit
  *
  *   // Retry once and when the CircuitBreaker opens after 2 failures then retry with exponential back-off with same time as CircuitBreaker's resetTimeout
  *   val fiveTimesWithBackOff = Schedule.recurs<Throwable>(1) andThen
- *     Schedule.exponential(seconds(2)) and Schedule.recurs(5)
+ *     Schedule.exponential(2.seconds) and Schedule.recurs(5)
  *
  *   Either.catch {
  *     resilient(fiveTimesWithBackOff, ::apiCall)
- *   }.let { println("exponential(seconds(2)) and recurs(5) always retries with actual apiCall: $it") }
+ *   }.let { println("exponential(2.seconds) and recurs(5) always retries with actual apiCall: $it") }
  *   //sampleEnd
  * }
  * ```
