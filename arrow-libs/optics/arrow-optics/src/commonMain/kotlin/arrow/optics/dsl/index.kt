e60c86ead1beb69a484c27fefdaedabcde85281f
Alejandro Serrano <trupill@gmail.com>
2022-12-21 14:12:35 +0100

    Remove unnecessary method

--- arrow-libs/optics/arrow-optics/src/commonMain/kotlin/arrow/optics/dsl/index.kt
+++ arrow-libs/optics/arrow-optics/src/commonMain/kotlin/arrow/optics/dsl/index.kt
@@ -7,19 +7,9 @@ import arrow.optics.Traversal
 import arrow.optics.typeclasses.Index
 
 /**
- * DSL to compose [Index] with a [Lens] for a structure [S] to focus in on [A] at given index [I]
- *
- * @receiver [Lens] with a focus in [S]
- * @param idx [Index] instance to provide a [Optional] to focus into [S] at [I]
- * @param i index [I] to focus into [S] and find focus [A]
- * @return [Optional] with a focus in [A] at given index [I]
- */
-public fun <T, S, I, A> Lens<T, S>.index(idx: Index<S, I, A>, i: I): Optional<T, A> = this.compose(idx.index(i))
-
-/**
  * DSL to compose [Index] with an [Optional] for a structure [S] to focus in on [A] at given index [I]
  *
- * @receiver [Optional] or [Prism] with a focus in [S]
+ * @receiver [Optional], [Lens], or [Prism] with a focus in [S]
  * @param idx [Index] instance to provide a [Optional] to focus into [S] at [I]
  * @param i index [I] to focus into [S] and find focus [A]
  * @return [Optional] with a focus in [A] at given index [I]
