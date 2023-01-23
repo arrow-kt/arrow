4031daae1225216986bd5be34c94d91918ec7eb7
ibado <ignaciobado.unc@gmail.com>
2023-01-23 21:48:23 +0100

    Implement the requested changes

--- arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/NonEmptySet.kt
+++ arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/NonEmptySet.kt
@@ -2,23 +2,19 @@ package arrow.core
 
 public class NonEmptySet<out T>(
   private val first: T,
-  rest: Set<T>,
+  private val rest: Set<T>,
 ) : AbstractSet<T>() {
 
-  public val elements: Set<T>
-
-  init {
-      elements = setOf(first) + rest
-  }
+  public val elements: Set<T> by lazy { setOf(first) + rest }
 
   public operator fun plus(other: NonEmptySet<@UnsafeVariance T>): NonEmptySet<T> =
-    NonEmptySet(this.first, elements + other.elements)
+    NonEmptySet(this.first, rest + other.elements)
 
   public operator fun plus(s: Set<@UnsafeVariance T>): NonEmptySet<T> =
-    NonEmptySet(first, this.elements + s)
+    NonEmptySet(first, rest + s)
 
   public operator fun plus(s: @UnsafeVariance T): NonEmptySet<T> =
-    NonEmptySet(first, this.elements.plus(s))
+    NonEmptySet(first, rest + s)
 
   override val size: Int
     get() = elements.size
@@ -26,22 +22,13 @@ public class NonEmptySet<out T>(
   override fun iterator(): Iterator<T> = elements.iterator()
 
   override fun toString(): String = "NonEmptySet(${elements.joinToString()})"
-
-  override fun equals(other: Any?): Boolean = when {
-    other === this -> true
-    other !is Set<*> -> false
-    other is NonEmptySet<*> -> elements == other.elements
-    else -> elements == other
-  }
-
-  override fun hashCode(): Int = elements.hashCode()
 }
 
 public fun <T> nonEmptySetOf(fist: T, vararg rest: T): NonEmptySet<Any?> =
   NonEmptySet(fist, rest.toSet())
 
 public fun <T> Collection<T>.toNonEmptySetOrNull(): NonEmptySet<T>? =
-  firstOrNull()?.let { NonEmptySet(it, this.toSet()) }
+  firstOrNull()?.let { NonEmptySet(it, this.drop(1).toSet()) }
 
 public fun <T> Collection<T>.toNonEmptySetOrNone(): Option<NonEmptySet<T>> =
   toNonEmptySetOrNull().toOption()
--- arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/NonEmptySetTest.kt
+++ arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/NonEmptySetTest.kt
@@ -6,6 +6,7 @@ import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.next
 import io.kotest.property.checkAll
 
 class NonEmptySetTest : StringSpec({
@@ -20,6 +21,20 @@ class NonEmptySetTest : StringSpec({
     checkAll(Arb.nonEmptySet(Arb.int())) { nonEmptySet ->
       nonEmptySet.elements.toNonEmptySetOrNone() shouldBe nonEmptySet.some()
     }
+  }
+
+  "emptyList.toNonEmptySetOrNull should be null" {
+    listOf<Int>().toNonEmptySetOrNull() shouldBe null
+  }
+
+  "emptyList.toNonEmptySetOrNone should be none" {
+    listOf<Int>().toNonEmptySetOrNone() shouldBe none()
+  }
+
+  "adding an element already present doesn't change the set" {
+    val element = Arb.int().next()
+    val initialSet = nonEmptySetOf(element)
+    initialSet.plus(element) shouldBe initialSet
   }
 })
 
