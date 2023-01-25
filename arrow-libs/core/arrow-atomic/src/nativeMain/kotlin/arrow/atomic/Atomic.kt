edb5caf286230b5d3b0754af8143a6ee5920e1b0
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-01-25 13:48:26 +0100

    Add Atomic, AtomicInt, AtomicLong & AtomicBoolean into new module

---
+++ arrow-libs/core/arrow-atomic/src/nativeMain/kotlin/arrow/atomic/Atomic.kt
@@ -0,0 +1,28 @@
+@file:OptIn(FreezingIsDeprecated::class)
+package arrow.atomic
+
+import kotlin.native.concurrent.AtomicReference
+import kotlin.native.concurrent.freeze
+import kotlin.native.concurrent.isFrozen
+
+public actual class Atomic<V> actual constructor(initialValue: V) {
+  private val inner = AtomicReference(initialValue.freeze())
+
+  public actual fun get(): V = inner.value
+
+  public actual fun set(value: V) {
+    inner.value = value.freeze()
+  }
+
+  public actual fun compareAndSet(expected: V, new: V): Boolean =
+    inner.compareAndSet(expected, new.freeze())
+
+  public actual fun getAndSet(value: V): V {
+    if (inner.isFrozen) value.freeze()
+    while (true) {
+      val cur = inner.value
+      if (cur === value) return cur
+      if (inner.compareAndSwap(cur, value) === cur) return cur
+    }
+  }
+}
