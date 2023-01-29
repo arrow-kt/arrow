37696e5d7ada30cc66793b30060911b56760b287
ibado <ignaciobado.unc@gmail.com>
2023-01-29 10:57:43 +0100

    Implement map & isEmpty functions

--- arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/NonEmptySet.kt
+++ arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/NonEmptySet.kt
@@ -4,16 +4,24 @@ public class NonEmptySet<out T> private constructor(
   private val elements: Set<T>
 ) : Set<T> by elements {
 
-  public constructor(first: T, rest: Set<T>): this(setOf(first) + rest)
+  public constructor(first: T, rest: Set<T>) : this(setOf(first) + rest)
 
-  public operator fun plus(set: Set<@UnsafeVariance T>): NonEmptySet<T> = NonEmptySet(elements + set)
+  public operator fun plus(set: Set<@UnsafeVariance T>): NonEmptySet<T> =
+    NonEmptySet(elements + set)
 
-  public operator fun plus(element: @UnsafeVariance T): NonEmptySet<T> = NonEmptySet(elements + element)
+  public operator fun plus(element: @UnsafeVariance T): NonEmptySet<T> =
+    NonEmptySet(elements + element)
+
+  public fun <R> map(transform: (@UnsafeVariance T) -> R): NonEmptySet<R> =
+    NonEmptySet(elements.map { transform(it) }.toSet())
+
+  override fun isEmpty(): Boolean = false
 
   override fun toString(): String = "NonEmptySet(${this.joinToString()})"
 
-  override fun equals(other: Any?): Boolean {
-    return elements == other
+  override fun equals(other: Any?): Boolean = when (other) {
+    is Set<*> -> elements == other
+    else -> false
   }
 
   override fun hashCode(): Int {
@@ -21,8 +29,8 @@ public class NonEmptySet<out T> private constructor(
   }
 }
 
-public fun <T> nonEmptySetOf(fist: T, vararg rest: T): NonEmptySet<T> =
-  NonEmptySet(fist, rest.toSet())
+public fun <T> nonEmptySetOf(first: T, vararg rest: T): NonEmptySet<T> =
+  NonEmptySet(first, rest.toSet())
 
 public fun <T> Collection<T>.toNonEmptySetOrNull(): NonEmptySet<T>? =
   firstOrNull()?.let { NonEmptySet(it, minus(it).toSet()) }
