32119bd9ffc78d1a4eadb91bfc5de9d6b98342db
Alejandro Serrano <trupill@gmail.com>
2023-02-03 16:49:16 +0100

    Add more contract annotations

--- arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/Nullable.kt
+++ arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/Nullable.kt
@@ -1,42 +1,61 @@
 @file:Suppress("NAME_SHADOWING")
-
 package arrow.core
 
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 import kotlin.jvm.JvmStatic
 
+@OptIn(ExperimentalContracts::class)
 public object Nullable {
 
   @JvmStatic
-  public inline fun <A, R> zip(a: A?, fn: (A) -> R): R? =
-    zip(a, Unit) { a, _ -> fn(a) }
+  public inline fun <A, R> zip(a: A?, fn: (A) -> R): R? {
+    contract { callsInPlace(fn, InvocationKind.AT_MOST_ONCE) }
+    return zip(a, Unit) { a, _ -> fn(a) }
+  }
 
   @JvmStatic
-  public inline fun <A, B, R> zip(a: A?, b: B?, fn: (A, B) -> R): R? =
-    zip(a, b, Unit) { a, b, _ -> fn(a, b) }
+  public inline fun <A, B, R> zip(a: A?, b: B?, fn: (A, B) -> R): R? {
+    contract { callsInPlace(fn, InvocationKind.AT_MOST_ONCE) }
+    return zip(a, b, Unit) { a, b, _ -> fn(a, b) }
+  }
 
   @JvmStatic
-  public inline fun <A, B, C, R> zip(a: A?, b: B?, c: C?, fn: (A, B, C) -> R): R? =
-    zip(a, b, c, Unit) { a, b, c, _ -> fn(a, b, c) }
+  public inline fun <A, B, C, R> zip(a: A?, b: B?, c: C?, fn: (A, B, C) -> R): R? {
+    contract { callsInPlace(fn, InvocationKind.AT_MOST_ONCE) }
+    return zip(a, b, c, Unit) { a, b, c, _ -> fn(a, b, c) }
+  }
 
   @JvmStatic
-  public inline fun <A, B, C, D, R> zip(a: A?, b: B?, c: C?, d: D?, fn: (A, B, C, D) -> R): R? =
-    zip(a, b, c, d, Unit) { a, b, c, d, _ -> fn(a, b, c, d) }
+  public inline fun <A, B, C, D, R> zip(a: A?, b: B?, c: C?, d: D?, fn: (A, B, C, D) -> R): R? {
+    contract { callsInPlace(fn, InvocationKind.AT_MOST_ONCE) }
+    return zip(a, b, c, d, Unit) { a, b, c, d, _ -> fn(a, b, c, d) }
+  }
 
   @JvmStatic
-  public inline fun <A, B, C, D, E, R> zip(a: A?, b: B?, c: C?, d: D?, e: E?, fn: (A, B, C, D, E) -> R): R? =
-    zip(a, b, c, d, e, Unit) { a, b, c, d, e, _ -> fn(a, b, c, d, e) }
+  public inline fun <A, B, C, D, E, R> zip(a: A?, b: B?, c: C?, d: D?, e: E?, fn: (A, B, C, D, E) -> R): R? {
+    contract { callsInPlace(fn, InvocationKind.AT_MOST_ONCE) }
+    return zip(a, b, c, d, e, Unit) { a, b, c, d, e, _ -> fn(a, b, c, d, e) }
+  }
 
   @JvmStatic
-  public inline fun <A, B, C, D, E, F, R> zip(a: A?, b: B?, c: C?, d: D?, e: E?, f: F?, fn: (A, B, C, D, E, F) -> R): R? =
-    zip(a, b, c, d, e, f, Unit) { a, b, c, d, e, f, _ -> fn(a, b, c, d, e, f) }
+  public inline fun <A, B, C, D, E, F, R> zip(a: A?, b: B?, c: C?, d: D?, e: E?, f: F?, fn: (A, B, C, D, E, F) -> R): R? {
+    contract { callsInPlace(fn, InvocationKind.AT_MOST_ONCE) }
+    return zip(a, b, c, d, e, f, Unit) { a, b, c, d, e, f, _ -> fn(a, b, c, d, e, f) }
+  }
 
   @JvmStatic
-  public inline fun <A, B, C, D, E, F, G, R> zip(a: A?, b: B?, c: C?, d: D?, e: E?, f: F?, g: G?, fn: (A, B, C, D, E, F, G) -> R): R? =
-    zip(a, b, c, d, e, f, g, Unit) { a, b, c, d, e, f, g, _ -> fn(a, b, c, d, e, f, g) }
+  public inline fun <A, B, C, D, E, F, G, R> zip(a: A?, b: B?, c: C?, d: D?, e: E?, f: F?, g: G?, fn: (A, B, C, D, E, F, G) -> R): R? {
+    contract { callsInPlace(fn, InvocationKind.AT_MOST_ONCE) }
+    return zip(a, b, c, d, e, f, g, Unit) { a, b, c, d, e, f, g, _ -> fn(a, b, c, d, e, f, g) }
+  }
 
   @JvmStatic
-  public inline fun <A, B, C, D, E, F, G, H, R> zip(a: A?, b: B?, c: C?, d: D?, e: E?, f: F?, g: G?, h: H?, fn: (A, B, C, D, E, F, G, H) -> R): R? =
-    zip(a, b, c, d, e, f, g, h, Unit) { a, b, c, d, e, f, g, h, _ -> fn(a, b, c, d, e, f, g, h) }
+  public inline fun <A, B, C, D, E, F, G, H, R> zip(a: A?, b: B?, c: C?, d: D?, e: E?, f: F?, g: G?, h: H?, fn: (A, B, C, D, E, F, G, H) -> R): R? {
+    contract { callsInPlace(fn, InvocationKind.AT_MOST_ONCE) }
+    return zip(a, b, c, d, e, f, g, h, Unit) { a, b, c, d, e, f, g, h, _ -> fn(a, b, c, d, e, f, g, h) }
+  }
 
   @JvmStatic
   public inline fun <A, B, C, D, E, F, G, H, I, R> zip(
@@ -50,8 +69,10 @@ public object Nullable {
     h: H?,
     i: I?,
     fn: (A, B, C, D, E, F, G, H, I) -> R
-  ): R? =
-    zip(a, b, c, d, e, f, g, h, i, Unit) { a, b, c, d, e, f, g, h, i, _ -> fn(a, b, c, d, e, f, g, h, i) }
+  ): R? {
+    contract { callsInPlace(fn, InvocationKind.AT_MOST_ONCE) }
+    return zip(a, b, c, d, e, f, g, h, i, Unit) { a, b, c, d, e, f, g, h, i, _ -> fn(a, b, c, d, e, f, g, h, i) }
+  }
 
   @JvmStatic
   public inline fun <A, B, C, D, E, F, G, H, I, J, R> zip(
@@ -66,8 +87,9 @@ public object Nullable {
     i: I?,
     j: J?,
     fn: (A, B, C, D, E, F, G, H, I, J) -> R
-  ): R? =
-    a?.let { a ->
+  ): R? {
+    contract { callsInPlace(fn, InvocationKind.AT_MOST_ONCE) }
+    return a?.let { a ->
       b?.let { b ->
         c?.let { c ->
           d?.let { d ->
@@ -88,4 +110,5 @@ public object Nullable {
         }
       }
     }
+  }
 }
