55d441c299e976756e7408a1a79663bd0597e8c1
Jose Gutiérrez de Ory <jose.gutierrez@47deg.com>
2023-03-10 13:18:58 +0100

    [PROPOSAL] Ior API deprecation, and preparation for 2.x.x (#2928)
    Co-authored-by: Simon Vergauwen <nomisRev@users.noreply.github.com>
    Co-authored-by: gutiory <gutiory@users.noreply.github.com>

--- arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/IorTest.kt
+++ arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/IorTest.kt
@@ -9,6 +9,7 @@ import io.kotest.data.forAll
 import io.kotest.data.row
 import io.kotest.property.Arb
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 import io.kotest.property.arbitrary.int
 import io.kotest.property.arbitrary.long
 import io.kotest.property.arbitrary.orNull
@@ -92,6 +93,14 @@ class IorTest : StringSpec({
       }
     }
 
+    "map() should just right side of an Ior" {
+      checkAll(Arb.int(), Arb.string()) { a: Int, b: String ->
+        Ior.Left(a).map{ l:String -> l.length } shouldBe Ior.Left(a)
+        Ior.Right(b).map{ it.length } shouldBe Ior.Right(b.length)
+        Ior.Both(a, b).map{ it.length } shouldBe Ior.Both(a, b.length)
+      }
+    }
+
     "mapLeft() should modify only left value" {
       checkAll(Arb.int(), Arb.string()) { a: Int, b: String ->
         Ior.Right(b).mapLeft { a * 2 } shouldBe Ior.Right(b)
@@ -145,7 +154,16 @@ class IorTest : StringSpec({
       }
     }
 
-    "leftOrNull() should convert left values into a nullable" {
+    "getOrNull() should convert right values into a nullable, or return null if left" {
+      checkAll(Arb.int(), Arb.string()) { a: Int, b: String ->
+        Ior.Left(a).getOrNull() shouldBe null
+        Ior.Right(b).getOrNull() shouldBe b
+        Ior.Both(a, b).getOrNull() shouldBe b
+      }
+    }
+
+
+  "leftOrNull() should convert left values into a nullable" {
       checkAll(Arb.int(), Arb.string()) { a: Int, b: String ->
         Ior.Left(a).leftOrNull() shouldBe a
         Ior.Right(b).leftOrNull() shouldBe null
@@ -170,7 +188,49 @@ class IorTest : StringSpec({
       }
     }
 
-    "getOrElse() should return value" {
+    "leftNel() should build a correct Ior" {
+      checkAll(Arb.int()) { a: Int ->
+          Ior.leftNel<Int, Nothing>(a) shouldBe Ior.Left(nonEmptyListOf(a))
+      }
+    }
+
+    "bothNel() should build a correct Ior" {
+      checkAll(Arb.int(), Arb.string()) { a: Int, b:String ->
+        Ior.bothNel(a, b) shouldBe Ior.Both(nonEmptyListOf(a), b)
+      }
+    }
+
+    "lift(f) should apply the input function to an Ior correctly" {
+      checkAll(Arb.string()) { a: String ->
+        val f = Ior.lift<Nothing, String, String> { s: String -> "Hello $s"  }
+        f(Ior.Right(a)) shouldBe Ior.Right("Hello $a")
+      }
+    }
+
+    "lift(fa, fb) should apply the input functions to an Ior correctly" {
+      checkAll(Arb.string(), Arb.string()) { a: String, b:String ->
+        val fa = { s1: String -> "Hello $s1" }
+        val fb = { s2: String -> s2.length }
+        val f = Ior.lift(fa, fb)
+        f(Ior.Right(b)) shouldBe Ior.Right(b.length)
+        f(Ior.Left(a)) shouldBe Ior.Left("Hello $a")
+        f(Ior.Both(a, b)) shouldBe Ior.Both("Hello $a", b.length)
+      }
+    }
+
+    "foldLeft should fold an Ior correctly" {
+      checkAll(Arb.int(), Arb.string()) { a: Int, b:String ->
+        val left = Ior.Left(a)
+        val right = Ior.Right(b)
+        val both = Ior.Both(a, b)
+        val f = {c: Int, b: String -> c + b.length}
+        left.foldLeft(0, f) shouldBe 0
+        right.foldLeft(0, f) shouldBe b.length
+        both.foldLeft(0, f) shouldBe b.length
+      }
+    }
+
+  "getOrElse() should return value" {
       checkAll(Arb.int(), Arb.int()) { a: Int, b: Int ->
         Ior.Right(a).getOrElse { b } shouldBe a
         Ior.Left(a).getOrElse { b } shouldBe b
@@ -184,7 +244,20 @@ class IorTest : StringSpec({
       iorResult shouldBe Ior.Left(10)
     }
 
-    "combine cases for Semigroup" {
+
+  "Ior.monad.flatMap should combine Both values" {
+    val ior1 = Ior.Both(3, "Hello, world!")
+    val iorResult1 = ior1.flatMap(Semigroup.int()) { Ior.Both(7, "Again!") }
+    iorResult1 shouldBe Ior.Both(10, "Again!")
+
+    val iorResult2 = ior1.flatMap(Semigroup.int()) { Ior.Right("Again!") }
+    iorResult2 shouldBe Ior.Both(3, "Again!")
+
+  }
+
+
+
+  "combine cases for Semigroup" {
       Semigroup.ior(Semigroup.string(), Semigroup.int()).run {
         forAll(
           row("Hello, ".leftIor(), Ior.Left("Arrow!"), Ior.Left("Hello, Arrow!")),
@@ -337,4 +410,94 @@ class IorTest : StringSpec({
       }
     }
 
+    "isLeft() should return true with Left and false otherwise"{
+      checkAll(Arb.int(), Arb.string()){ a, b ->
+        Ior.Left(a).isLeft() shouldBe true
+        Ior.Right(b).isLeft() shouldBe false
+        Ior.Both(a, b).isLeft() shouldBe false
+      }
+    }
+
+    "isRight() should return true with Right and false otherwise"{
+      checkAll(Arb.int(), Arb.string()){ a, b ->
+        Ior.Left(a).isRight() shouldBe false
+        Ior.Right(b).isRight() shouldBe true
+        Ior.Both(a, b).isRight() shouldBe false
+      }
+    }
+
+    "isBoth() should return true with Both and false otherwise"{
+      checkAll(Arb.int(), Arb.string()){ a, b ->
+        Ior.Left(a).isBoth() shouldBe false
+        Ior.Right(b).isBoth() shouldBe false
+        Ior.Both(a, b).isBoth() shouldBe true
+      }
+    }
+
+    "isLeft(predicate) should return true with Left, if satisfies the predicate, and false otherwise"{
+      checkAll(Arb.int(), Arb.string()){ a, b ->
+        val predicate = {i: Int -> i % 2 == 0}
+
+        if(predicate(a)) Ior.Left(a).isLeft(predicate) shouldBe true
+        else Ior.Left(a).isLeft(predicate) shouldBe false
+
+        Ior.Right(b).isLeft(predicate) shouldBe false
+        Ior.Both(a, b).isLeft(predicate) shouldBe false
+      }
+    }
+
+    "isRight(predicate) should return true with Right, if satisfies the predicate, and false otherwise"{
+      checkAll(Arb.int(), Arb.string()){ a, b ->
+        val predicate = {s: String -> s.length % 2 == 0}
+
+        if(predicate(b)) Ior.Right(b).isRight(predicate) shouldBe true
+        else Ior.Right(b).isRight(predicate) shouldBe false
+
+        Ior.Left(a).isRight(predicate) shouldBe false
+        Ior.Both(a, b).isRight(predicate) shouldBe false
+      }
+    }
+
+    "isBoth(predicate) should return true with Both, if satisfies the predicate, and false otherwise"{
+      checkAll(Arb.int(), Arb.string()){ a, b ->
+        val leftPredicate = {i: Int-> i % 2 == 0}
+        val rightPredicate = {s: String -> s.length % 2 == 0}
+        if(leftPredicate(a) && rightPredicate(b)) Ior.Both(a, b).isBoth(leftPredicate, rightPredicate) shouldBe true
+        else Ior.Both(a, b).isBoth(leftPredicate, rightPredicate) shouldBe false
+
+        Ior.Left(a).isBoth(leftPredicate, rightPredicate) shouldBe false
+        Ior.Right(b).isBoth(leftPredicate, rightPredicate) shouldBe false
+      }
+    }
+
+    "widen should retype Right" {
+      checkAll(Arb.int(), Arb.string()) { a, b ->
+        val ior = Ior.Both(a, b)
+        ior.widen<Int, CharSequence, String>().shouldBeInstanceOf<Ior.Both<Int, CharSequence>>()
+      }
+    }
+
+    "compareTo should compare 2 Ior" {
+      val left1 = Ior.Left(1)
+      val left2 = Ior.Left(2)
+      val right1 = Ior.Right(1)
+      val right2 = Ior.Right(2)
+      val both11 = Ior.Both(1, 1)
+      val both22 = Ior.Both(2, 2)
+      left1.compareTo(left2) shouldBe -1
+      left1.compareTo(left1) shouldBe 0
+      left2.compareTo(left1) shouldBe 1
+      left1.compareTo(right1) shouldBe -1
+      left1.compareTo(both11) shouldBe -1
+      right1.compareTo(right2) shouldBe -1
+      right1.compareTo(right1) shouldBe 0
+      right2.compareTo(right1) shouldBe 1
+      right1.compareTo(left1) shouldBe 1
+      right1.compareTo(both11) shouldBe -1
+      both11.compareTo(both22) shouldBe -1
+      both11.compareTo(both11) shouldBe 0
+      both22.compareTo(both11) shouldBe 1
+      both11.compareTo(left1) shouldBe 1
+      both11.compareTo(right1) shouldBe 1
+    }
 })
