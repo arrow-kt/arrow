0a86ec2325c16382b0c8516bb3c43d7af169c181
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-03-16 22:18:12 +0100

    Tracing POC (#2946)
    Co-authored-by: Alejandro Serrano <trupill@gmail.com>
    Co-authored-by: franciscodr <francisco.d@47deg.com>
    Co-authored-by: Youssef Shoaib <canonballt@gmail.com>

---
+++ arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/raise/TraceSpec.kt
@@ -0,0 +1,46 @@
+package arrow.core.raise
+
+import arrow.core.right
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.string
+import io.kotest.property.checkAll
+import kotlinx.coroutines.CompletableDeferred
+
+@OptIn(ExperimentalTraceApi::class)
+class TraceSpec : StringSpec({
+  "trace is empty when no errors" {
+    checkAll(Arb.int()) { i ->
+      either<Nothing, Int> {
+        traced({ i }) { _,_ -> unreachable() }
+      } shouldBe i.right()
+    }
+  }
+
+  "trace is empty with exception" {
+    checkAll(Arb.string()) { msg ->
+      val error = RuntimeException(msg)
+      shouldThrow<RuntimeException> {
+        either<Nothing, Int> {
+          traced({ throw error }) { _,_ -> unreachable() }
+        }
+      }.message shouldBe msg
+    }
+  }
+
+  "nested tracing - identity" {
+    val inner = CompletableDeferred<String>()
+    ior(String::plus) {
+      traced({
+        traced({ raise("") }) { traced, _ ->
+          inner.complete(traced.stackTraceToString())
+        }
+      }) { traced, _ ->
+        inner.await() shouldBe traced.stackTraceToString()
+      }
+    }
+  }
+})
