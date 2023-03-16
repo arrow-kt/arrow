f6bc835f429bbc8aa82a2d0ff4538262d1f990d2
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-03-16 19:52:02 +0100

    Update arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/raise/Trace.kt

--- arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/raise/Trace.kt
+++ arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/raise/Trace.kt
@@ -11,7 +11,8 @@ public annotation class ExperimentalTraceApi
 
 /** Tracing result of `R`. Allows to inspect `R`, and the traces from where it was raised. */
 @ExperimentalTraceApi
-@JvmInline value class Trace(private val exception: CancellationException) {
+@JvmInline
+public value class Trace(private val exception: CancellationException) {
   /**
    * Returns the stacktrace as a [String]
    *
