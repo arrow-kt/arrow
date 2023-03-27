9e68e28a177e62330ceb7de62aa58f7f4ed92d1b
Alphonse Bendt <alphonse.bendt@gmail.com>
2023-03-27 18:50:46 +0200

    flatten GeneratorTests as kotest-js does not support nesting

--- arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/test/GeneratorsTest.kt
+++ arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/test/GeneratorsTest.kt
@@ -18,102 +18,98 @@ import io.kotest.property.arbitrary.string
 import io.kotest.property.checkAll
 
 class GeneratorsTest : FreeSpec({
-  "functionABCToD" - {
-    "should return same result when invoked multiple times" {
-      checkAll(
-        Arb.string(),
-        Arb.string(),
-        Arb.string(),
-        Arb.functionABCToD<String, String, String, Int>(Arb.int())
-      ) { a, b, c, fn ->
-        fn(a, b, c) shouldBe fn(a, b, c)
-      }
-    }
-
-    "should return some different values" {
-      val random = RandomSource.seeded(0)
-
-      val a = Arb.string().next(random)
-      val a2 = Arb.string().next(random)
-      val b = Arb.string().next(random)
-      val c = Arb.string().next(random)
-
-      // there should be at least one function that has a different value when the args are different
-      givenSamples( Arb.functionABCToD<String, String, String, Int>(Arb.int())).find { sample ->
-        val fn = sample.value
-        fn(a, b, c) != fn(a2, b, c)
-      }.shouldNotBeNull()
+  "functionABCToD: should return same result when invoked multiple times" {
+    checkAll(
+      Arb.string(),
+      Arb.string(),
+      Arb.string(),
+      Arb.functionABCToD<String, String, String, Int>(Arb.int())
+    ) { a, b, c, fn ->
+      fn(a, b, c) shouldBe fn(a, b, c)
     }
   }
 
-  "Arb.map2" - {
+  "functionABCToD: should return some different values" {
+    val random = RandomSource.seeded(0)
 
-    "generates maps with shared keys" - {
-      val result = givenSamples(
-        Arb.map2(
-          Arb.string(),
-          Arb.boolean(),
-          Arb.boolean()
-        )
-      ).map { it.value.first.keys.intersect(it.value.second.keys).size }
+    val a = Arb.string().next(random)
+    val a2 = Arb.string().next(random)
+    val b = Arb.string().next(random)
+    val c = Arb.string().next(random)
 
-      "at least one sample should share no keys" {
-        result.forAtLeastOne { it.shouldBeZero() }
-      }
-
-      "at least one sample should share some keys" {
-        result.forAtLeastOne { it.shouldBeGreaterThan(0) }
-      }
-    }
-
-    "nullable values" - {
-      "no null values if the arb does not produce nullables" {
-        givenSamples(Arb.map2(Arb.string(), Arb.boolean(), Arb.boolean()))
-          .forAll { sample ->
-            sample.value.first.values.forAll { it.shouldNotBeNull() }
-            sample.value.second.values.forAll { it.shouldNotBeNull() }
-          }
-      }
-
-      "can have null values if the arb produces nullables" {
-        givenSamples(Arb.map2(Arb.string(), Arb.boolean().orNull(), Arb.boolean().orNull()))
-          .forAtLeastOne { sample -> sample.value.first.values.forAtLeastOne { it.shouldBeNull() } }
-          .forAtLeastOne { sample -> sample.value.second.values.forAtLeastOne { it.shouldBeNull() } }
-      }
-    }
+    // there should be at least one function that has a different value when the args are different
+    givenSamples(Arb.functionABCToD<String, String, String, Int>(Arb.int())).find { sample ->
+      val fn = sample.value
+      fn(a, b, c) != fn(a2, b, c)
+    }.shouldNotBeNull()
   }
 
-  "Arb.map3" - {
+  "Arb.map2: at least one sample should share no keys" {
+    val result = givenSamples(
+      Arb.map2(
+        Arb.string(),
+        Arb.boolean(),
+        Arb.boolean()
+      )
+    ).map { it.value.first.keys.intersect(it.value.second.keys).size }
 
-    "generates maps with shared keys" - {
-      val result = givenSamples(Arb.map3(Arb.string(), Arb.boolean(), Arb.boolean(), Arb.boolean()))
-        .map { it.value.first.keys.intersect(it.value.second.keys).size }.toList()
-      "at least one sample should share no keys" {
-        result.forAtLeastOne { it.shouldBeZero() }
-      }
+    result.forAtLeastOne { it.shouldBeZero() }
+  }
 
-      "at least one sample should share some keys" {
-        result.forAtLeastOne { it.shouldBeGreaterThan(0) }
-      }
-    }
+  "Arb.map2: at least one sample should share some keys" {
+    val result = givenSamples(
+      Arb.map2(
+        Arb.string(),
+        Arb.boolean(),
+        Arb.boolean()
+      )
+    ).map { it.value.first.keys.intersect(it.value.second.keys).size }
 
-    "nullable values" - {
-      "no null values if the arb does not produce nullables" {
-        givenSamples(Arb.map3(Arb.string(), Arb.boolean(), Arb.boolean(), Arb.boolean()))
-          .forAll { sample ->
-            sample.value.first.values.forAll { it.shouldNotBeNull() }
-            sample.value.second.values.forAll { it.shouldNotBeNull() }
-            sample.value.third.values.forAll { it.shouldNotBeNull() }
-          }
-      }
+    result.forAtLeastOne { it.shouldBeGreaterThan(0) }
+  }
 
-      "can have null values if the arb produces nullables" {
-        givenSamples(Arb.map3(Arb.string(), Arb.boolean().orNull(), Arb.boolean().orNull(), Arb.boolean().orNull()))
-          .forAtLeastOne { sample -> sample.value.first.values.forAtLeastOne { it.shouldBeNull() } }
-          .forAtLeastOne { sample -> sample.value.second.values.forAtLeastOne { it.shouldBeNull() } }
-          .forAtLeastOne { sample -> sample.value.third.values.forAtLeastOne { it.shouldBeNull() } }
+  "Arb.map2: no null values if the arb does not produce nullables" {
+    givenSamples(Arb.map2(Arb.string(), Arb.boolean(), Arb.boolean()))
+      .forAll { sample ->
+        sample.value.first.values.forAll { it.shouldNotBeNull() }
+        sample.value.second.values.forAll { it.shouldNotBeNull() }
       }
-    }
+  }
+
+  "Arb.map2: can contain null values if the arb produces nullables" {
+    givenSamples(Arb.map2(Arb.string(), Arb.boolean().orNull(), Arb.boolean().orNull()))
+      .forAtLeastOne { sample -> sample.value.first.values.forAtLeastOne { it.shouldBeNull() } }
+      .forAtLeastOne { sample -> sample.value.second.values.forAtLeastOne { it.shouldBeNull() } }
+  }
+
+  "Arb.map3: at least one sample should share no keys" {
+    val result = givenSamples(Arb.map3(Arb.string(), Arb.boolean(), Arb.boolean(), Arb.boolean()))
+      .map { it.value.first.keys.intersect(it.value.second.keys).size }.toList()
+
+    result.forAtLeastOne { it.shouldBeZero() }
+  }
+
+  "Arb.map3: at least one sample should share some keys" {
+    val result = givenSamples(Arb.map3(Arb.string(), Arb.boolean(), Arb.boolean(), Arb.boolean()))
+      .map { it.value.first.keys.intersect(it.value.second.keys).size }.toList()
+
+    result.forAtLeastOne { it.shouldBeGreaterThan(0) }
+  }
+
+  "Arb.map3: no null values if the arb does not produce nullables" {
+    givenSamples(Arb.map3(Arb.string(), Arb.boolean(), Arb.boolean(), Arb.boolean()))
+      .forAll { sample ->
+        sample.value.first.values.forAll { it.shouldNotBeNull() }
+        sample.value.second.values.forAll { it.shouldNotBeNull() }
+        sample.value.third.values.forAll { it.shouldNotBeNull() }
+      }
+  }
+
+  "Arb.map3: can contain null values if the arb produces nullables" {
+    givenSamples(Arb.map3(Arb.string(), Arb.boolean().orNull(), Arb.boolean().orNull(), Arb.boolean().orNull()))
+      .forAtLeastOne { sample -> sample.value.first.values.forAtLeastOne { it.shouldBeNull() } }
+      .forAtLeastOne { sample -> sample.value.second.values.forAtLeastOne { it.shouldBeNull() } }
+      .forAtLeastOne { sample -> sample.value.third.values.forAtLeastOne { it.shouldBeNull() } }
   }
 })
 
