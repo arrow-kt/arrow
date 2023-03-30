fd8095736c8f7e3848910473b7e07a4598c152ef
Alphonse Bendt <alphonse.bendt@gmail.com>
2023-03-30 15:42:03 +0200

    add missing import

--- arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/EitherTest.kt
+++ arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/EitherTest.kt
@@ -24,6 +24,7 @@ import io.kotest.property.arbitrary.char
 import io.kotest.property.arbitrary.double
 import io.kotest.property.arbitrary.float
 import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.list
 import io.kotest.property.arbitrary.long
 import io.kotest.property.arbitrary.string
 import io.kotest.property.arbitrary.nonPositiveInt
