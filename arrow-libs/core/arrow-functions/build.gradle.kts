3c247b73ae01f38518db226337d1fb06a54c1685
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-03-30 21:06:44 +0200

    Move partials, memoise, and currying to seperate module

---
+++ arrow-libs/core/arrow-functions/build.gradle.kts
@@ -0,0 +1,68 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  id(libs.plugins.kotlin.multiplatform.get().pluginId)
+  alias(libs.plugins.arrowGradleConfig.kotlin)
+  alias(libs.plugins.arrowGradleConfig.publish)
+  alias(libs.plugins.kotest.multiplatform)
+}
+
+apply(from = property("TEST_COVERAGE"))
+apply(from = property("ANIMALSNIFFER_MPP"))
+
+val enableCompatibilityMetadataVariant =
+  providers.gradleProperty("kotlin.mpp.enableCompatibilityMetadataVariant")
+    .orNull?.toBoolean() == true
+
+if (enableCompatibilityMetadataVariant) {
+  tasks.withType<Test>().configureEach {
+    exclude("**/*")
+  }
+}
+
+kotlin {
+  sourceSets {
+    commonMain {
+      dependencies {
+        api(projects.arrowAtomic)
+        api(projects.arrowAnnotations)
+        api(libs.kotlin.stdlibCommon)
+      }
+    }
+    if (!enableCompatibilityMetadataVariant) {
+      commonTest {
+        dependencies {
+          implementation(projects.arrowFxCoroutines)
+          implementation(libs.kotest.frameworkEngine)
+          implementation(libs.kotest.assertionsCore)
+          implementation(libs.kotest.property)
+        }
+      }
+
+      jvmTest {
+        dependencies {
+          runtimeOnly(libs.kotest.runnerJUnit5)
+        }
+      }
+    }
+
+    jvmMain {
+      dependencies {
+        implementation(libs.kotlin.stdlib)
+      }
+    }
+
+    jsMain {
+      dependencies {
+        implementation(libs.kotlin.stdlibJS)
+      }
+    }
+  }
+}
+
+// enables context receivers for Jvm Tests
+tasks.named<KotlinCompile>("compileTestKotlinJvm") {
+  kotlinOptions.freeCompilerArgs += "-Xcontext-receivers"
+}
