ae18fa4f432e6446957e7775531c03052cc012f6
franciscodr <francisco.d@47deg.com>
2023-03-30 11:58:04 +0200

    Remove unused timeInMillis function

--- arrow-libs/resilience/arrow-resilience/src/jsMain/kotlin/arrow/resilience/timeInMillis.kt
+++
@@ -1,7 +0,0 @@
-package arrow.resilience
-
-@Deprecated("This is being removed in Arrow Fx Resilience and kotlin.time.TimeSource is the recommended replacement.")
-public actual fun timeInMillis(): Long {
-  val x: Number = js("new Date().getTime()")
-  return x.toLong()
-}
--- arrow-libs/resilience/arrow-resilience/src/jvmMain/kotlin/arrow/resilience/timeInMillis.kt
+++
@@ -1,5 +0,0 @@
-package arrow.resilience
-
-@Deprecated("This is being removed in Arrow Fx Resilience and kotlin.time.TimeSource is the recommended replacement.")
-public actual fun timeInMillis(): Long =
-  System.currentTimeMillis()
