54687df1a3d67314fd2990613a8bc21a1890d7ac
franciscodr <francisco.d@47deg.com>
2023-03-31 15:46:30 +0200

    Update knit files

--- arrow-libs/core/arrow-core/src/jvmTest/kotlin/examples/example-sequence-15.kt
+++ arrow-libs/core/arrow-core/src/jvmTest/kotlin/examples/example-sequence-15.kt
@@ -1,11 +1,11 @@
 // This file was automatically generated from Sequence.kt by Knit tool. Do not edit.
 package arrow.core.examples.exampleSequence15
 
-import arrow.core.unzipToPair
+import arrow.core.unzip
 
 fun main(args: Array<String>) {
   //sampleStart
-  val result = sequenceOf("A" to 1, "B" to 2).unzipToPair()
+  val result = sequenceOf("A" to 1, "B" to 2).unzip()
   //sampleEnd
   println("(${result.first}, ${result.second})")
 }
