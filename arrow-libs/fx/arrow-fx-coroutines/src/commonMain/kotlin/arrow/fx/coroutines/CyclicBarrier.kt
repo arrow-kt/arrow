209ad170771540ec7ac00a06a756543c188eae68
Simon Vergauwen <nomisRev@users.noreply.github.com>
2023-07-05 12:15:28 +0200

    Implement reset on CyclicBarrier

--- arrow-libs/fx/arrow-fx-coroutines/src/commonMain/kotlin/arrow/fx/coroutines/CyclicBarrier.kt
+++ arrow-libs/fx/arrow-fx-coroutines/src/commonMain/kotlin/arrow/fx/coroutines/CyclicBarrier.kt
@@ -5,6 +5,8 @@ import arrow.core.continuations.loop
 import arrow.core.continuations.update
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancelAndJoin
 
 /**
  * A [CyclicBarrier] is a synchronization mechanism that allows a set of coroutines to wait for each other
@@ -23,30 +25,98 @@ public class CyclicBarrier(public val capacity: Int) {
       "Cyclic barrier must be constructed with positive non-zero capacity $capacity but was $capacity > 0"
     }
   }
-  
-  private data class State(val awaiting: Int, val epoch: Long, val unblock: CompletableDeferred<Unit>)
-  
-  private val state: AtomicRef<State> = AtomicRef(State(capacity, 0, CompletableDeferred()))
-  
+
+  private sealed interface State
+
+  private data class Awaiting(
+    val awaiting: Int, val epoch: Long, val unblock: CompletableDeferred<Unit>
+  ) : State
+
+  private data class Resetting(
+    val awaitingNow: Int, val unblock: CompletableDeferred<Unit>
+  ) : State
+
+  private val state: AtomicRef<State> = AtomicRef(Awaiting(capacity, 0, CompletableDeferred()))
+
+  public suspend fun reset() {
+    when (val original = state.get()) {
+      is Awaiting -> {
+        val resetBarrier = CompletableDeferred<Unit>()
+        if (state.compareAndSet(
+            original, Resetting(original.awaiting, resetBarrier)
+          )
+        ) {
+          original.unblock.cancel(CyclicBarrierCancellationException())
+          resetBarrier.await()
+        } else reset()
+      }
+
+      // We're already resetting, await all waiters to finish
+      is Resetting -> original.unblock.await()
+    }
+  }
+
   /**
    * When [await] is called the function will suspend until the required number of coroutines have reached the barrier.
    * Once the [capacity] of the barrier has been reached, the coroutine will be released and continue execution.
    */
   public suspend fun await() {
-    state.loop { original ->
-      val (awaiting, epoch, unblock) = original
-      val awaitingNow = awaiting - 1
-      if (awaitingNow == 0 && state.compareAndSet(original, State(capacity, epoch + 1, CompletableDeferred()))) {
-        unblock.complete(Unit)
-        return
-      } else if (state.compareAndSet(original, State(awaitingNow, epoch, unblock))) {
-        return try {
-          unblock.await()
-        } catch (cancelled: CancellationException) {
-          state.update { s -> if (s.epoch == epoch) s.copy(awaiting = s.awaiting + 1) else s }
-          throw cancelled
+    state.loop { state ->
+      when (state) {
+        is Awaiting -> {
+          val (awaiting, epoch, unblock) = state
+          val awaitingNow = awaiting - 1
+          if (awaitingNow == 0 && this.state.compareAndSet(state, Awaiting(capacity, epoch + 1, CompletableDeferred()))) {
+            unblock.complete(Unit)
+            return
+          } else if (this.state.compareAndSet(state, Awaiting(awaitingNow, epoch, unblock))) {
+            return try {
+              unblock.await()
+            } catch (c: CyclicBarrierCancellationException) {
+              countdown(state, c)
+              throw c
+            } catch (cancelled: CancellationException) {
+              this.state.update { s ->
+                when {
+                  s is Awaiting && s.epoch == epoch -> s.copy(awaiting = s.awaiting + 1)
+                  else -> s
+                }
+              }
+              throw cancelled
+
+            }
+          }
+        }
+
+        is Resetting -> {
+          state.unblock.await()
+          // State resets to `Awaiting` after `reset.unblock`.
+          // Unless there is another racing reset, it will be in `Awaiting` in next loop.
+          await()
         }
       }
     }
   }
+
+  private fun countdown(original: Awaiting, ex: CyclicBarrierCancellationException): Boolean {
+    state.loop { state ->
+      when (state) {
+          is Resetting -> {
+            val awaitingNow = state.awaitingNow + 1
+            if (awaitingNow < capacity && this.state.compareAndSet(state, state.copy(awaitingNow = awaitingNow))) {
+              return false
+            } else if (awaitingNow == capacity && this.state.compareAndSet(
+                state, Awaiting(capacity, original.epoch + 1, CompletableDeferred())
+              )
+            ) {
+              return state.unblock.complete(Unit)
+            } else countdown(original, ex)
+          }
+
+        is Awaiting -> throw IllegalStateException("Awaiting appeared during resetting.")
+      }
+    }
+  }
 }
+
+public class CyclicBarrierCancellationException : CancellationException("CyclicBarrier was cancelled")
--- arrow-libs/fx/arrow-fx-coroutines/src/commonTest/kotlin/arrow/fx/coroutines/CyclicBarrierSpec.kt
+++ arrow-libs/fx/arrow-fx-coroutines/src/commonTest/kotlin/arrow/fx/coroutines/CyclicBarrierSpec.kt
@@ -1,7 +1,9 @@
 package arrow.fx.coroutines
 
 import arrow.core.Either
+import io.kotest.assertions.assertSoftly
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.assertions.timing.eventually
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeTypeOf
@@ -9,10 +11,10 @@ import io.kotest.property.Arb
 import io.kotest.property.arbitrary.constant
 import io.kotest.property.arbitrary.int
 import io.kotest.property.checkAll
-import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
 
 class CyclicBarrierSpec : StringSpec({
   "should raise an exception when constructed with a negative or zero capacity" {
@@ -21,21 +23,21 @@ class CyclicBarrierSpec : StringSpec({
         "Cyclic barrier must be constructed with positive non-zero capacity $i but was $i > 0"
     }
   }
-  
+
   "barrier of capacity 1 is a no op" {
     checkAll(Arb.constant(Unit)) {
       val barrier = CyclicBarrier(1)
       barrier.await()
     }
   }
-  
+
   "awaiting all in parallel resumes all coroutines" {
     checkAll(Arb.int(1, 100)) { i ->
       val barrier = CyclicBarrier(i)
       (0 until i).parTraverse { barrier.await() }
     }
   }
-  
+
   "should reset once full" {
     checkAll(Arb.constant(Unit)) {
       val barrier = CyclicBarrier(2)
@@ -43,32 +45,32 @@ class CyclicBarrierSpec : StringSpec({
       barrier.capacity shouldBe 2
     }
   }
-  
+
   "await is cancelable" {
     checkAll(Arb.int(2, Int.MAX_VALUE)) { i ->
       val barrier = CyclicBarrier(i)
       val exitCase = CompletableDeferred<ExitCase>()
-      
+
       val job =
         launch(start = CoroutineStart.UNDISPATCHED) {
           guaranteeCase({ barrier.await() }, exitCase::complete)
         }
-      
+
       job.cancelAndJoin()
       exitCase.isCompleted shouldBe true
       exitCase.await().shouldBeTypeOf<ExitCase.Cancelled>()
     }
   }
-  
-  "should clean up upon cancelation of await" {
+
+  "should clean up upon cancellation of await" {
     checkAll(Arb.constant(Unit)) {
       val barrier = CyclicBarrier(2)
       launch(start = CoroutineStart.UNDISPATCHED) { barrier.await() }.cancelAndJoin()
-      
+
       barrier.capacity shouldBe 2
     }
   }
-  
+
   "race fiber cancel and barrier full" {
     checkAll(Arb.constant(Unit)) {
       val barrier = CyclicBarrier(2)
@@ -78,6 +80,27 @@ class CyclicBarrierSpec : StringSpec({
         // after the barrier has already reset, causing this code to never terminate (test times out)
         is Either.Left -> parZip({ barrier.await() }, { barrier.await() }) { _, _ -> }
         is Either.Right -> Unit
+      }
+    }
+  }
+
+  "reset" {
+    checkAll(Arb.int(2..10)) { n ->
+      val barrier = CyclicBarrier(n)
+
+      val exits = (0 until n - 1).map { CompletableDeferred<ExitCase>() }
+
+      val jobs = (0 until n - 1).map { i ->
+        launch(start = CoroutineStart.UNDISPATCHED) {
+          guaranteeCase(barrier::await, exits[i]::complete)
+        }
+      }
+
+      barrier.reset()
+
+      exits.zip(jobs) { exitCase, job ->
+        exitCase.await().shouldBeTypeOf<ExitCase.Cancelled>()
+        job.isCancelled shouldBe true
       }
     }
   }
