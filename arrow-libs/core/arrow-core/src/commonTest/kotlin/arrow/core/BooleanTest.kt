e7ea7f3ae50dcf34ffeb60c2f032762725d78338
Tejas Mate <tejasmate@pm.me>
2023-10-31 03:49:48 +0530

    Update BooleanTest.kt

--- arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/BooleanTest.kt
+++ arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/BooleanTest.kt
@@ -2,12 +2,11 @@ package arrow.core
 
 import arrow.core.test.laws.MonoidLaws
 import arrow.core.test.testLaws
-import io.kotest.core.spec.style.StringSpec
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.boolean
 
-class BooleanTest : StringSpec({
+class BooleanTest {
     testLaws(
       MonoidLaws("Boolean", true, { x, y -> x && y }, Arb.boolean())
     )
-})
+}
