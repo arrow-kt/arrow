7cdbb3c0755445d08ede6e50266cf2e36b32595c
Alejandro Serrano Mena <trupill@gmail.com>
2023-11-10 10:13:44 +0100

    Laws, Every, Iso

--- arrow-libs/optics/arrow-optics/src/commonTest/kotlin/arrow/optics/test/laws/Laws.kt
+++ arrow-libs/optics/arrow-optics/src/commonTest/kotlin/arrow/optics/test/laws/Laws.kt
@@ -1,39 +1,29 @@
 package arrow.optics.test.laws
 
 import io.kotest.assertions.fail
-import io.kotest.core.names.TestName
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.core.spec.style.scopes.StringSpecScope
-import io.kotest.core.spec.style.scopes.addTest
-import io.kotest.core.test.TestContext
+import io.kotest.assertions.withClue
+import kotlinx.coroutines.test.TestResult
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.runTest
 
 interface LawSet {
   val laws: List<Law>
 }
 
-data class Law(val name: String, val test: suspend TestContext.() -> Unit)
+data class Law(val name: String, val test: suspend TestScope.() -> Unit)
 
 fun <A> A.equalUnderTheLaw(b: A, f: (A, A) -> Boolean = { x, y -> x == y }): Boolean =
   if (f(this, b)) true else fail("Found $this but expected: $b")
 
-fun StringSpec.testLaws(vararg lawSets: LawSet): Unit = testLaws(lawSets.flatMap { it.laws })
+fun testLaws(vararg lawSets: LawSet): TestResult = testLaws(lawSets.flatMap { it.laws })
 
-fun StringSpec.testLaws(prefix: String, vararg lawSets: LawSet): Unit = testLaws(prefix, lawSets.flatMap { it.laws })
-
-fun StringSpec.testLaws(vararg laws: List<Law>): Unit = laws
-  .flatMap { list: List<Law> -> list.asIterable() }
-  .distinctBy { law: Law -> law.name }
-  .forEach { law: Law ->
-    addTest(TestName(null, law.name, false), false, null) {
-      law.test(StringSpecScope(this.coroutineContext, testCase))
+fun testLaws(vararg laws: List<Law>): TestResult = runTest {
+  laws
+    .flatMap(List<Law>::asIterable)
+    .distinctBy(Law::name)
+    .forEach { law: Law ->
+      withClue("Testing ${law.name}") {
+        law.test(this@runTest)
+      }
     }
-  }
-
-fun StringSpec.testLaws(prefix: String, vararg laws: List<Law>): Unit = laws
-  .flatMap { list: List<Law> -> list.asIterable() }
-  .distinctBy { law: Law -> law.name }
-  .forEach { law: Law ->
-    addTest(TestName(prefix, law.name, false), false, null) {
-      law.test(StringSpecScope(this.coroutineContext, testCase))
-    }
-  }
+}
