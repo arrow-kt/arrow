563a8484acdfe6944ee8cd54d2597e35006eb463
Alejandro Serrano <trupill@gmail.com>
2023-12-20 11:48:15 +0100

    Add more options to memoization cache (#3296)
    Co-authored-by: Simon Vergauwen <nomisRev@users.noreply.github.com>

---
+++ arrow-libs/core/arrow-cache4k/build.gradle.kts
@@ -0,0 +1,67 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
+
+plugins {
+  `java-library`
+  id(libs.plugins.kotlin.multiplatform.get().pluginId)
+  alias(libs.plugins.arrowGradleConfig.publish)
+  alias(libs.plugins.spotless)
+}
+
+apply(from = property("ANIMALSNIFFER_MPP"))
+
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(8))
+  }
+}
+
+kotlin {
+  explicitApi()
+
+  sourceSets {
+    commonMain {
+      dependencies {
+        api(projects.arrowCore)
+        api(libs.cache4k)
+      }
+    }
+  }
+
+  jvm {
+    withJava()
+  }
+
+  js(IR) {
+    browser()
+    nodejs()
+  }
+
+  // Native: https://kotlinlang.org/docs/native-target-support.html
+  // -- Tier 1 --
+  linuxX64()
+  macosX64()
+  macosArm64()
+  iosSimulatorArm64()
+  iosX64()
+  // -- Tier 2 --
+  linuxArm64()
+  watchosSimulatorArm64()
+  watchosX64()
+  watchosArm64()
+  tvosSimulatorArm64()
+  tvosX64()
+  tvosArm64()
+  iosArm64()
+  // -- Tier 3 --
+  mingwX64()
+  // Android and watchOS not included
+  // -- Deprecated as of 1.8.20 --
+  // iosArm32() // deprecated as of 1.8.20
+  // watchosX86()
+}
+
+tasks.named<Jar>("jvmJar").configure {
+  manifest {
+    attributes["Automatic-Module-Name"] = "arrow.cache4k"
+  }
+}
--- arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/MemoizedDeepRecursiveFunction.kt
+++ arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/MemoizedDeepRecursiveFunction.kt
@@ -2,6 +2,30 @@ package arrow.core
 
 import arrow.atomic.Atomic
 import arrow.atomic.loop
+import kotlin.jvm.JvmInline
+import kotlin.jvm.JvmOverloads
+
+public interface MemoizationCache<K, V> {
+  public fun get(key: K): V?
+  public fun set(key: K, value: V): V
+}
+
+@JvmInline
+public value class AtomicMemoizationCache<K, V>(
+  private val cache: Atomic<Map<K, V>> = Atomic(emptyMap())
+): MemoizationCache<K, V> {
+  override fun get(key: K): V? = cache.get()[key]
+  override fun set(key: K, value: V): V = cache.loop { old ->
+    when (key) {
+      in old ->
+        return@set old.getValue(key)
+      else -> {
+        if (cache.compareAndSet(old, old + Pair(key, value)))
+          return@set value
+      }
+    }
+  }
+}
 
 /**
  * Defines a recursive **pure** function that:
@@ -13,25 +37,14 @@ import arrow.atomic.loop
  * [callRecursive][DeepRecursiveScope.callRecursive] function can be used to make a recursive call
  * to the declared function.
  */
+@JvmOverloads
 public fun <T, R> MemoizedDeepRecursiveFunction(
+  cache: MemoizationCache<T, R> = AtomicMemoizationCache(),
   block: suspend DeepRecursiveScope<T, R>.(T) -> R
 ): DeepRecursiveFunction<T, R> {
-  val cache = Atomic(emptyMap<T, R>())
   return DeepRecursiveFunction { x ->
-    when (val v = cache.get()[x]) {
-      null -> {
-        val result = block(x)
-        cache.loop { old ->
-          when (x) {
-            in old ->
-              return@DeepRecursiveFunction old.getValue(x)
-            else -> {
-              if (cache.compareAndSet(old, old + Pair(x, result)))
-                return@DeepRecursiveFunction result
-            }
-          }
-        }
-      }
+    when (val v = cache.get(x)) {
+      null -> cache.set(x, block(x))
       else -> v
     }
   }
