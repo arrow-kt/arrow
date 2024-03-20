e8a53f154c347915e325167bfabfcfa3126ff07c
Alejandro Serrano Mena <trupill@gmail.com>
2024-03-20 13:17:11 +0100

    Update to beta version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,6 @@ pluginManagement {
     kotlin_repo_url?.also { maven(it) }
     google()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlinx/dev")
   }
 }
 
