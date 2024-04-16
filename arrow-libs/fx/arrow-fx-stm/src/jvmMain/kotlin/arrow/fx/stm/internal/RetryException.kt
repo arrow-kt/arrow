af99eb434ef6f405e4e44d13cfe142c143874711
Alejandro Serrano <trupill@gmail.com>
2024-04-16 08:52:23 +0200

    Improvements to STM + do not expose internal members (#3398)
    * Improvements to STM + do not expose internal members
    
    * Additional fixes

--- arrow-libs/fx/arrow-fx-stm/src/jvmMain/kotlin/arrow/fx/stm/internal/RetryException.kt
+++ arrow-libs/fx/arrow-fx-stm/src/jvmMain/kotlin/arrow/fx/stm/internal/RetryException.kt
@@ -1,5 +1,6 @@
 package arrow.fx.stm.internal
 
 public actual object RetryException : Throwable("Arrow STM Retry. This should always be caught by arrow internally. Please report this as a bug if that is not the case!") {
+  private fun readResolve(): Any = RetryException
   override fun fillInStackTrace(): Throwable { return this }
 }
