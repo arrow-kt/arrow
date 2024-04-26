7128e37a1c3d7ccd068b9dafef849c33d8ab3ee2
Simon Vergauwen <nomisRev@users.noreply.github.com>
2024-04-26 09:59:44 +0200

    Publish Local working

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,23 +84,30 @@ allprojects {
   group = property("projects.group").toString()
 }
 
+private val kotlinXUpstream =
+  setOf(
+    "arrow-fx-coroutines",
+    "arrow-resilience",
+    "arrow-fx-stm",
+    "arrow-collectors"
+  )
+
 subprojects {
-  this@subprojects.tasks.withType<DokkaTaskPartial>().configureEach {
-    this@subprojects.extensions.findByType<KotlinProjectExtension>()?.sourceSets?.forEach { kotlinSourceSet ->
+  tasks.withType<DokkaTaskPartial>().configureEach {
+    extensions.findByType<KotlinProjectExtension>()?.sourceSets?.forEach { kotlinSourceSet ->
       dokkaSourceSets.named(kotlinSourceSet.name) {
         perPackageOption {
           matchingRegex.set(".*\\.internal.*")
           suppress.set(true)
         }
-        if (project.name == "arrow-fx-coroutines") externalDocumentationLink("https://kotlinlang.org/api/kotlinx.coroutines/")
+        if (project.name in kotlinXUpstream) externalDocumentationLink("https://kotlinlang.org/api/kotlinx.coroutines/")
         skipDeprecated.set(true)
         reportUndocumented.set(false)
-        val baseUrl: String = checkNotNull(properties["pom.smc.url"]?.toString())
 
         kotlinSourceSet.kotlin.srcDirs.filter { it.exists() }.forEach { srcDir ->
           sourceLink {
             localDirectory.set(srcDir)
-            remoteUrl.set(uri("$baseUrl/blob/main/${srcDir.relativeTo(rootProject.rootDir)}").toURL())
+            remoteUrl.set(uri("https://github.com/arrow-kt/arrow//blob/main/${srcDir.relativeTo(rootProject.rootDir)}").toURL())
             remoteLineSuffix.set("#L")
           }
         }
@@ -113,8 +120,13 @@ tasks {
   val undocumentedProjects =
     listOf(project(":arrow-optics-ksp-plugin"))
 
+  val copyCNameFile = register<Copy>("copyCNameFile") {
+    from(layout.projectDirectory.dir("static").file("CNAME"))
+    into(layout.projectDirectory.dir("docs"))
+  }
+
   dokkaHtmlMultiModule {
-    dependsOn("copyCNameFile")
+    dependsOn(copyCNameFile)
     removeChildTasks(undocumentedProjects)
   }
 
@@ -123,11 +135,6 @@ tasks {
   withType<DokkaMultiModuleTask>().configureEach {
     outputDirectory.set(file("docs"))
     moduleName.set("Arrow")
-  }
-
-  register<Copy>("copyCNameFile") {
-    from(layout.projectDirectory.dir("static").file("CNAME"))
-    into(layout.projectDirectory.dir("docs"))
   }
 }
 
