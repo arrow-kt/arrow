dc7baf1541295f6d2de2e1ca43adedb652dcf7f8
Simon Vergauwen <nomisRev@users.noreply.github.com>
2024-05-16 20:35:12 +0200

    Arrow Optics, DSL syntax improvements  (#3427)

--- arrow-libs/optics/arrow-optics/src/commonMain/kotlin/arrow/optics/dsl/at.kt
+++ arrow-libs/optics/arrow-optics/src/commonMain/kotlin/arrow/optics/dsl/at.kt
@@ -1,10 +1,12 @@
 package arrow.optics.dsl
 
+import arrow.core.Option
 import arrow.optics.Lens
 import arrow.optics.Optional
 import arrow.optics.Prism
 import arrow.optics.Traversal
 import arrow.optics.typeclasses.At
+import kotlin.jvm.JvmName
 
 /**
  * DSL to compose [At] with a [Lens] for a structure [S] to focus in on [A] at given index [I].
@@ -35,3 +37,25 @@ public fun <T, S, I, A> Optional<T, S>.at(at: At<S, I, A>, i: I): Optional<T, A>
  * @return [Traversal] with a focus in [A] at given index [I].
  */
 public fun <T, S, I, A> Traversal<T, S>.at(at: At<S, I, A>, i: I): Traversal<T, A> = this.compose(at.at(i))
+
+public fun <T, K, V> Lens<T, Map<K, V>>.at(key: K): Lens<T, Option<V>> =
+  this.compose(At.map<K, V>().at(key))
+
+public fun <T, K, V> Optional<T, Map<K, V>>.at(key: K): Optional<T, Option<V>> =
+  this.compose(At.map<K, V>().at(key))
+
+public fun <T, K, V> Traversal<T, Map<K, V>>.at(key: K): Traversal<T, Option<V>> =
+  this.compose(At.map<K, V>().at(key))
+
+@JvmName("atSet")
+public fun <T, A> Lens<T, Set<A>>.at(value: A): Lens<T, Boolean> =
+  this.compose(At.set<A>().at(value))
+
+@JvmName("atSet")
+public fun <T, A> Optional<T, Set<A>>.at(value: A): Optional<T, Boolean> =
+  this.compose(At.set<A>().at(value))
+
+@JvmName("atSet")
+public fun <T, A> Traversal<T, Set<A>>.at(value: A): Traversal<T, Boolean> =
+  this.compose(At.set<A>().at(value))
+
--- arrow-libs/optics/arrow-optics/src/commonMain/kotlin/arrow/optics/dsl/index.kt
+++ arrow-libs/optics/arrow-optics/src/commonMain/kotlin/arrow/optics/dsl/index.kt
@@ -1,10 +1,12 @@
 package arrow.optics.dsl
 
+import arrow.core.NonEmptyList
 import arrow.optics.Lens
 import arrow.optics.Optional
 import arrow.optics.Prism
 import arrow.optics.Traversal
 import arrow.optics.typeclasses.Index
+import kotlin.jvm.JvmName
 
 /**
  * DSL to compose [Index] with an [Optional] for a structure [S] to focus in on [A] at given index [I]
@@ -25,3 +27,41 @@ public fun <T, S, I, A> Optional<T, S>.index(idx: Index<S, I, A>, i: I): Optiona
  * @return [Traversal] with a focus in [A] at given index [I].
  */
 public fun <T, S, I, A> Traversal<T, S>.index(idx: Index<S, I, A>, i: I): Traversal<T, A> = this.compose(idx.index(i))
+
+public fun <T, A> Optional<T, List<A>>.index(index: Int): Optional<T, A> =
+  this.compose(Index.list<A>().index(index))
+
+public fun <T, A> Traversal<T, List<A>>.index(index: Int): Traversal<T, A> =
+  this.compose(Index.list<A>().index(index))
+
+@JvmName("indexNonEmptyList")
+public fun <T, A> Optional<T, NonEmptyList<A>>.index(index: Int): Optional<T, A> =
+  this.compose(Index.nonEmptyList<A>().index(index))
+
+@JvmName("indexNonEmptyList")
+public fun <T, A> Traversal<T, NonEmptyList<A>>.index(index: Int): Traversal<T, A> =
+  this.compose(Index.nonEmptyList<A>().index(index))
+
+@JvmName("indexValues")
+public fun <T, K, A> Optional<T, Map<K, A>>.index(key: K): Optional<T, A> =
+  this.compose(Index.map<K, A>().index(key))
+
+@JvmName("indexValues")
+public fun <T, K, A> Traversal<T, Map<K, A>>.index(key: K): Traversal<T, A> =
+  this.compose(Index.map<K, A>().index(key))
+
+@JvmName("indexSequence")
+public fun <T, A> Optional<T, Sequence<A>>.index(index: Int): Optional<T, A> =
+  this.compose(Index.sequence<A>().index(index))
+
+@JvmName("indexSequence")
+public fun <T, A> Traversal<T, Sequence<A>>.index(index: Int): Traversal<T, A> =
+  this.compose(Index.sequence<A>().index(index))
+
+@JvmName("indexString")
+public fun <T> Optional<T, String>.index(index: Int): Optional<T, Char> =
+  this.compose(Index.string().index(index))
+
+@JvmName("indexString")
+public fun <T> Traversal<T, String>.index(index: Int): Traversal<T, Char> =
+  this.compose(Index.string().index(index))
