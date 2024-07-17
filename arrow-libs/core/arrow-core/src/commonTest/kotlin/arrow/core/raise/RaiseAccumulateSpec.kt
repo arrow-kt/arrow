af72b386a80cc976c827b6b0ab1419a86d910d2e
Sam Pengilly <sam.pengilly@gmail.com>
2024-07-17 19:32:13 +1000

    Support Ior mapOrAccumulate
    Add support for mapOrAccumulate functions which accumulate errors
    into a "Both" case when run inside an IorRaise scope.

--- arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/raise/RaiseAccumulateSpec.kt
+++ arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/raise/RaiseAccumulateSpec.kt
@@ -1,5 +1,7 @@
 package arrow.core.raise
 
+import arrow.core.Either
+import arrow.core.Ior
 import arrow.core.NonEmptyList
 import arrow.core.left
 import arrow.core.nonEmptyListOf
@@ -15,5 +17,40 @@ class RaiseAccumulateSpec {
         { mapOrAccumulate(1..2) { ensure(false) { "$it: IsFalse" } } }
       ) { _, _ -> 1 }
     } shouldBe nonEmptyListOf("false", "1: IsFalse", "2: IsFalse").left()
+  }
+
+  @Test fun mapOrAccumulateSemanticsDependOnReceiver() = runTest {
+    val numbers = listOf(1, 2, 3, 4, 5, 6)
+    val func: Raise<String>.(Int) -> Int = { if (it > 4) raise("$it") else it * 10 }
+
+    val outerResult = either {
+      ior(combineError = String::plus) { this.mapOrAccumulate(numbers, func) } shouldBe Ior.Both("56", listOf(10, 20, 30, 40))
+      iorNel { this.mapOrAccumulate(numbers, func) } shouldBe Ior.Both(nonEmptyListOf("5", "6"), listOf(10, 20, 30, 40))
+      either { this.mapOrAccumulate(numbers, func) } shouldBe Either.Left(nonEmptyListOf("5", "6"))
+      iorNel { this.mapOrAccumulate(numbers, func) }.toEither().bind()
+    }
+
+    outerResult shouldBe Either.Right(listOf(10, 20, 30, 40))
+  }
+
+  @Test fun iorMapOrAccumulateResultsInBothIfAllErrors() = runTest {
+    val numbers = listOf(1, 2, 3, 4, 5, 6)
+    val func: Raise<String>.(Int) -> Int = { raise("$it") }
+
+    ior(combineError = String::plus) { mapOrAccumulate(numbers, func) } shouldBe Ior.Both("123456", emptyList())
+  }
+
+  @Test fun iorMapOrAccumulateResultsInBothIfAnySuccesses() = runTest {
+    val numbers = listOf(1, 2, 3, 4, 5, 6)
+    val func: Raise<String>.(Int) -> Int = { if (it > 4) raise("$it") else it * 10 }
+
+    ior(combineError = String::plus) { mapOrAccumulate(numbers, func) } shouldBe Ior.Both("56", listOf(10, 20, 30, 40))
+  }
+
+  @Test fun iorMapOrAccumulateResultsInRightIfAllSuccesses() = runTest {
+    val numbers = listOf(1, 2, 3, 4, 5, 6)
+    val func: Raise<String>.(Int) -> Int = { it * 10 }
+
+    ior(combineError = String::plus) { mapOrAccumulate(numbers, func) } shouldBe Ior.Right(listOf(10, 20, 30, 40, 50, 60))
   }
 }
