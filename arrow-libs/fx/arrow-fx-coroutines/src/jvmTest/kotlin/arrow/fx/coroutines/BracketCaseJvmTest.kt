affbdb9597d2bd9048fa6d79435cec9aceab566a
Youssef Shoaib <canonballt@gmail.com>
2024-11-01 17:51:07 +0000

    Add tests for short-circuiting fatal bracket

---
+++ arrow-libs/fx/arrow-fx-coroutines/src/jvmTest/kotlin/arrow/fx/coroutines/BracketCaseJvmTest.kt
@@ -0,0 +1,28 @@
+package arrow.fx.coroutines
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.test.runTest
+import kotlin.test.DefaultAsserter.fail
+import kotlin.test.Test
+
+class BracketCaseJvmTest {
+  @Test
+  fun blowBracketOnFatal() = runTest {
+    val error = shouldThrow<LinkageError> {
+      bracket({ }, { throw LinkageError("BOOM!") }) { fail("Should never come here") }
+    }
+    error.message shouldBe "BOOM!"
+    error.suppressedExceptions.shouldBeEmpty()
+  }
+
+  @Test
+  fun blowBracketOnFatalInRelease() = runTest {
+    val error = shouldThrow<LinkageError> {
+      bracket({ }, { throw RuntimeException() }) { throw LinkageError("BOOM!") }
+    }
+    error.message shouldBe "BOOM!"
+    error.suppressedExceptions.shouldBeEmpty()
+  }
+}
