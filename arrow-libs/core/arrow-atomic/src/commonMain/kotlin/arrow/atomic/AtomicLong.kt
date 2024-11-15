7b11fb70b477239c6d62387ce388ac225818bfd3
Youssef Shoaib <canonballt@gmail.com>
2024-11-15 15:44:41 +0000

    Add contracts for atomics

--- arrow-libs/core/arrow-atomic/src/commonMain/kotlin/arrow/atomic/AtomicLong.kt
+++ arrow-libs/core/arrow-atomic/src/commonMain/kotlin/arrow/atomic/AtomicLong.kt
@@ -1,4 +1,10 @@
+@file:OptIn(ExperimentalContracts::class)
+
 package arrow.atomic
+
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 public expect class AtomicLong(initialValue: Long) {
 
@@ -23,31 +29,55 @@ public var AtomicLong.value: Long
 /**
  * Infinite loop that reads this atomic variable and performs the specified [action] on its value.
  */
-public inline fun AtomicLong.loop(action: (Long) -> Unit): Nothing { while(true) { action(value) } }
+public inline fun AtomicLong.loop(action: (Long) -> Unit): Nothing {
+  contract { callsInPlace(action, InvocationKind.AT_LEAST_ONCE) }
+  do { action(value) } while(true)
+}
 
-public inline fun AtomicLong.tryUpdate(function: (Long) -> Long): Boolean = tryUpdate(function) { _, _ -> }
+public inline fun AtomicLong.tryUpdate(function: (Long) -> Long): Boolean {
+  contract { callsInPlace(function, InvocationKind.EXACTLY_ONCE) }
+  return tryUpdate(function) { _, _ -> }
+}
 
-public inline fun AtomicLong.update(function: (Long) -> Long): Unit = update(function) { _, _ -> }
+public inline fun AtomicLong.update(function: (Long) -> Long) {
+  contract { callsInPlace(function, InvocationKind.AT_LEAST_ONCE) }
+  update(function) { _, _ -> }
+}
 
 /**
  * Updates variable atomically using the specified [function] of its value and returns its old value.
  */
-public inline fun AtomicLong.getAndUpdate(function: (Long) -> Long): Long = update(function) { old, _ -> old }
+public inline fun AtomicLong.getAndUpdate(function: (Long) -> Long): Long {
+  contract { callsInPlace(function, InvocationKind.AT_LEAST_ONCE) }
+  return update(function) { old, _ -> old }
+}
 
 /**
  * Updates variable atomically using the specified [function] of its value and returns its new value.
  */
-public inline fun AtomicLong.updateAndGet(function: (Long) -> Long): Long = update(function) { _, new -> new }
+public inline fun AtomicLong.updateAndGet(function: (Long) -> Long): Long {
+  contract { callsInPlace(function, InvocationKind.AT_LEAST_ONCE) }
+  return update(function) { _, new -> new }
+}
 
 @PublishedApi
 internal inline fun <R> AtomicLong.update(function: (Long) -> Long, transform: (old: Long, new: Long) -> R): R {
-  while (true) {
-    tryUpdate(function) { old, new -> return transform(old, new) }
+  contract {
+    callsInPlace(function, InvocationKind.AT_LEAST_ONCE)
+    callsInPlace(transform, InvocationKind.AT_MOST_ONCE)
+  }
+  loop { cur ->
+    val upd = function(value)
+    if(compareAndSet(cur, upd)) return transform(cur, upd)
   }
 }
 
 @PublishedApi
 internal inline fun AtomicLong.tryUpdate(function: (Long) -> Long, onUpdated: (old: Long, new: Long) -> Unit): Boolean {
+  contract {
+    callsInPlace(function, InvocationKind.EXACTLY_ONCE)
+    callsInPlace(onUpdated, InvocationKind.AT_MOST_ONCE)
+  }
   val cur = value
   val upd = function(cur)
   return compareAndSet(cur, upd).also { if (it) onUpdated(cur, upd) }
