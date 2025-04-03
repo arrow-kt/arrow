a3acb15b81ea18d84981f05630ddf363e2f7b2d4
Alejandro Serrano Mena <trupill@gmail.com>
2025-04-03 14:25:30 +0200

    Race until one of the tasks succeeds

---
+++ arrow-libs/fx/arrow-fx-coroutines/src/commonMain/kotlin/arrow/fx/coroutines/RaceIf.kt
@@ -0,0 +1,41 @@
+package arrow.fx.coroutines
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+// Based on https://medium.com/@sam-cooper/custom-kotlin-coroutine-races-77161141b2ac
+
+/**
+ * Races the participants [tasks] in parallel on the [Dispatchers.Default],
+ * until one of them returns a value which satisfies [condition].
+ * The winner of the race cancels the other participants.
+ * Cancelling the operation cancels all participants.
+ */
+public suspend inline fun <A, B> raceN(
+  crossinline condition: (A) -> Boolean,
+  vararg tasks: suspend CoroutineScope.() -> A
+): A = raceN(Dispatchers.Default, condition, *tasks)
+
+/**
+ * Races the participants [tasks] in parallel on the provided [ctx],
+ * until one of them returns a value which satisfies [condition].
+ * The winner of the race cancels the other participants.
+ * Cancelling the operation cancels all participants.
+ */
+public suspend inline fun <A> raceN(
+  ctx: CoroutineContext = EmptyCoroutineContext,
+  crossinline condition: (A) -> Boolean,
+  vararg tasks: suspend CoroutineScope.() -> A
+): A {
+  return coroutineScope {
+    channelFlow {
+      tasks.forEach { launch(ctx) { send(it()) } }
+    }.first { condition(it) }
+  }
+}
