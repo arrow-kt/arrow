584f48f2eadf18e1947ceba6e7f599949a5c55d5
Youssef Shoaib <canonballt@gmail.com>
2025-07-28 23:59:19 +0100

    Use Error.stackTraceLimit for future proofing

--- arrow-libs/core/arrow-core/src/jsMain/kotlin/arrow/core/raise/CancellationExceptionNoTrace.kt
+++ arrow-libs/core/arrow-core/src/jsMain/kotlin/arrow/core/raise/CancellationExceptionNoTrace.kt
@@ -7,26 +7,20 @@ package arrow.core.raise
 )
 private class NoTraceImpl(raised: Any?, raise: Raise<Any?>) : RaiseCancellationException(raised, raise)
 
-private object SpecialRaised
-private object SpecialRaise : Raise<Any?> {
-  override fun raise(r: Any?): Nothing = error("Should never be called")
+private val Error = js("Error")
+
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/stackTraceLimit
+private inline fun <R> withNoStackTrace(block: () -> R): R {
+  val prevLimit = Error.stackTraceLimit
+  Error.stackTraceLimit = 0 // Disable stack trace generation
+  try {
+    return block()
+  } finally {
+    Error.stackTraceLimit = prevLimit // Restore original limit
+  }
 }
 
-private val noTrace = NoTraceImpl(raised = SpecialRaised, raise = SpecialRaise)
-
-private fun entriesOf(jsObject: dynamic): List<Pair<String, dynamic>> =
-  (js("Object").entries(jsObject) as Array<Array<dynamic>>)
-    .map { entry -> entry[0] as String to entry[1] }
-
-private val noTraceKeys: Pair<String, String> = run {
-  val entries = entriesOf(js("Object").getOwnPropertyDescriptors(noTrace))
-  entries.first { (key, value) -> value.value == SpecialRaised }.first to entries.first { (key, value) -> value.value == SpecialRaise }.first
-} // Seems to always just be raised_1 and raise_1, but we compute them on startup to be safe.
-
 @DelicateRaiseApi
-internal actual fun NoTrace(raised: Any?, raise: Raise<Any?>): RaiseCancellationException {
-  val descs = js("Object").getOwnPropertyDescriptors(noTrace)
-  descs[noTraceKeys.first].value = raised
-  descs[noTraceKeys.second].value = raise
-  return js("Object").defineProperties(js("Object").create(js("Object").getPrototypeOf(noTrace)), descs)
+internal actual fun NoTrace(raised: Any?, raise: Raise<Any?>): RaiseCancellationException = withNoStackTrace {
+  NoTraceImpl(raised, raise)
 }
