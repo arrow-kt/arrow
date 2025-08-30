1089a8a0cb8ef0c5b87d5de3c622845948f692af
Ivan Yakushev <theycome@mail.ru>
2025-08-30 19:41:24 +0300

    Test coverage for `ResultRaise`

--- arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/raise/ResultSpec.kt
+++ arrow-libs/core/arrow-core/src/commonTest/kotlin/arrow/core/raise/ResultSpec.kt
@@ -1,11 +1,25 @@
 package arrow.core.raise
 
+import arrow.core.test.result
+import arrow.core.toNonEmptyListOrThrow
+import arrow.core.toNonEmptySetOrThrow
 import io.kotest.matchers.shouldBe
-import kotlin.test.Test
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.list
+import io.kotest.property.arbitrary.map
+import io.kotest.property.arbitrary.set
+import io.kotest.property.checkAll
 import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
 
 class ResultSpec {
+
   val boom = RuntimeException("Boom!")
+  val range = 0..3
+  val nelRange = 1..4
+  val iterationsSmall = 10
+  val iterations = 20
 
   @Test fun resultException() = runTest {
     result {
@@ -27,5 +41,99 @@ class ResultSpec {
       val two = Result.success(2).bind()
       one + two
     } shouldBe Result.success(3)
+  }
+
+  @Test
+  fun bind() = runTest {
+    checkAll(iterationsSmall, Arb.result(Arb.int())) { a ->
+      result {
+        a.bind()
+      } shouldBe a
+    }
+  }
+
+  @Test
+  fun bindAllMap() = runTest {
+    checkAll(iterations, Arb.map(Arb.int(), Arb.result(Arb.int()), range.first, range.last)) { a ->
+      val expected = a.values.firstOrNull { it.isFailure }
+        ?: Result.success(
+          a.mapValues {
+            it.value.getOrThrow()
+          },
+        )
+
+      result {
+        a.bindAll()
+      } shouldBe expected
+    }
+  }
+
+  @Test
+  fun bindAllIterable() = runTest {
+    checkAll(iterations, Arb.list(Arb.result(Arb.int()), range)) { a ->
+      val expected = a.firstOrNull { it.isFailure }
+        ?: Result.success(
+          a.map {
+            it.getOrThrow()
+          },
+        )
+
+      result {
+        a.bindAll()
+      } shouldBe expected
+    }
+  }
+
+  @Test
+  fun bindAllNel() = runTest {
+    checkAll(iterations, Arb.list(Arb.result(Arb.int()), nelRange)) { generated ->
+      val a = generated.toNonEmptyListOrThrow()
+      val expected = a.firstOrNull { it.isFailure }
+        ?: Result.success(
+          a.map {
+            it.getOrThrow()
+          },
+        )
+
+      result {
+        a.bindAll()
+      } shouldBe expected
+    }
+  }
+
+  @Test
+  fun bindAllNes() = runTest {
+    checkAll(iterations, Arb.set(Arb.result(Arb.int()), nelRange)) { generated ->
+      val a = generated.toNonEmptySetOrThrow()
+      val expected = a.firstOrNull { it.isFailure }
+        ?: Result.success(
+          a.map {
+            it.getOrThrow()
+          }.toNonEmptySetOrThrow(),
+        )
+
+      result {
+        a.bindAll()
+      } shouldBe expected
+    }
+  }
+
+  @Test
+  fun recover() = runTest {
+    checkAll(iterationsSmall, Arb.result(Arb.int())) { a ->
+      val expected = a.fold(
+        onSuccess = { Result.success(it.toString()) },
+        onFailure = { Result.success("recovered") },
+      )
+
+      result {
+        recover(
+          {
+            a.bind().toString()
+          },
+          { "recovered" },
+        )
+      } shouldBe expected
+    }
   }
 }
