fb5e5ef4148dc0b7666f95d1399e300b43b48590
Michael Friend <16885048+mrf7@users.noreply.github.com>
2025-12-29 17:12:57 -0500

    add unit tests for receiving functions

--- arrow-libs/integrations/arrow-raise-ktor-server-resources/src/jvmTest/kotlin/arrow/raise/ktor/server/resources/RoutingReceivingResourcesTest.kt
+++ arrow-libs/integrations/arrow-raise-ktor-server-resources/src/jvmTest/kotlin/arrow/raise/ktor/server/resources/RoutingReceivingResourcesTest.kt
@@ -1,18 +1,19 @@
 package arrow.raise.ktor.server.resources
 
 import arrow.core.raise.context.ensure
+import arrow.raise.ktor.server.response.raise
 import arrow.core.raise.context.raise
+import arrow.raise.ktor.server.resources.RoutingResourcesTest.Foo
 import arrow.raise.ktor.server.response.Response.Companion.invoke
 import io.kotest.matchers.shouldBe
-import io.ktor.client.request.get
 import io.ktor.client.request.patch
 import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
-import io.ktor.resources.Resource
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.resources.Resources
@@ -22,40 +23,7 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlin.test.Test
 
-class RespondOrRaiseTypedTest {
-
-  @Resource("/foo")
-  class Foo {
-    @Resource("/{name}")
-    data class Id(val parent: Foo = Foo(), val name: String)
-  }
-
-  @Test
-  fun `respond result of getOrError`() = testApplication {
-    install(Resources)
-    routing {
-      getOrRaise<Foo, _> { "bar" }
-    }
-
-    client.get("/foo").let {
-      it.status shouldBe HttpStatusCode.OK
-      it.bodyAsText() shouldBe "bar"
-    }
-  }
-
-  @Test
-  fun `respond result with params of getOrError`() = testApplication {
-    install(Resources)
-    routing {
-      getOrRaise<Foo.Id, _> { it.name.uppercase() }
-    }
-
-    client.get("/foo/bar").let {
-      it.status shouldBe HttpStatusCode.OK
-      it.bodyAsText() shouldBe "BAR"
-    }
-  }
-
+class RoutingReceivingResourcesTest {
   @Test
   fun `receive and respond typed success with string body and params`() = testApplication {
     install(Resources)
@@ -90,7 +58,7 @@ class RespondOrRaiseTypedTest {
   }
 
   @Test
-  fun `receive with typed body`() = testApplication {
+  fun `receive with typed body and content negotiation`() = testApplication {
     install(Resources)
     install(ContentNegotiation) {
       json(Json.Default)
@@ -134,5 +102,41 @@ class RespondOrRaiseTypedTest {
         it.bodyAsText() shouldBe ""
         it.status shouldBe HttpStatusCode.BadRequest
       }
+  }
+
+  @Test
+  fun `all http methods receive and  respond successfully`() = testApplication {
+    install(Resources)
+    routing {
+      patchOrRaise<Foo.Id, _, _> { route, body: String -> route.name.uppercase() + body.uppercase() }
+      postOrRaise<Foo.Id, _, _> { route, body: String -> route.name.uppercase() + body.uppercase() }
+      putOrRaise<Foo.Id, _, _> { route, body: String -> route.name.uppercase() + body.uppercase() }
+    }
+    listOf(
+      client.patch("/foo/bar") { setBody("bazz") },
+      client.post("/foo/bar") { setBody("bazz") },
+      client.put("/foo/bar") { setBody("bazz") },
+    ).forEach {
+      it.status shouldBe HttpStatusCode.OK
+      it.bodyAsText() shouldBe "BARBAZZ"
+    }
+  }
+
+  @Test
+  fun `all http methods respond and receive with error`() = testApplication {
+    install(Resources)
+    routing {
+      patchOrRaise<Foo.Id, String, String> { _, _ -> raise(HttpStatusCode.BadRequest, "BAR") }
+      postOrRaise<Foo.Id, String, String> { _, _ -> raise(HttpStatusCode.BadRequest, "BAR") }
+      putOrRaise<Foo.Id, String, String> { _, _ -> raise(HttpStatusCode.BadRequest, "BAR") }
+    }
+    listOf(
+      client.patch("/foo/bar"),
+      client.post("/foo/bar"),
+      client.put("/foo/bar"),
+    ).forEach {
+      it.status shouldBe HttpStatusCode.BadRequest
+      it.bodyAsText() shouldBe "BAR"
+    }
   }
 }
